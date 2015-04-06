3afdfa89c0d9f812b20fdafeaab741d9c56a51b8
Ben Lesh <blesh@netflix.com>
2015-04-05 21:24:34 -0700

    add observation scheduling

--- src/observer/observer.js
+++ src/observer/observer.js
@@ -1,31 +1,46 @@
+import currentFrameScheduler from '../scheduler/global/current-frame';
+
 export default class Observer {
-  constructor(generator, subscriptionDisposable) {
+  constructor(generator, subscriptionDisposable, scheduler) {
     this._generator = generator;
     this._subscriptionDisposable = subscriptionDisposable;
+    this._scheduler = scheduler || currentFrameScheduler;
   }
 
   next(value) {
-    if (this._subscriptionDisposable.isDisposed) return;
-    var iterationResult = this._generator.next(value);
+    this._scheduler.schedule([value, this], this._next)
+  }
+
+  _next(scheduler, [value, observer]) {
+    if (observer._subscriptionDisposable.isDisposed) return;
+    var iterationResult = observer._generator.next(value);
     if(typeof iterationResult !== 'undefined' && iterationResult.done) {
-      this._subscriptionDisposable.dispose();
+      observer._subscriptionDisposable.dispose();
     }
     return iterationResult;
   }
 
   throw(err) {
-    if (this._subscriptionDisposable.isDisposed) return;
-    this._subscriptionDisposable.dispose();
-    if(this._generator.throw) {
-      return this._generator.throw(err);
+    this._scheduler.schedule([err, this], this._throw);
+  }
+
+  _throw(scheduler, [err, observer]) {
+    if (observer._subscriptionDisposable.isDisposed) return;
+    observer._subscriptionDisposable.dispose();
+    if(observer._generator.throw) {
+      return observer._generator.throw(err);
     }
   }
 
   return(value) {
-    if (this._subscriptionDisposable.isDisposed) return;
-    this._subscriptionDisposable.dispose();
-    if(this._generator.return) {
-      return this._generator.return(value);
+    this._scheduler.schedule([value, this], this._return);
+  }
+
+  _return(scheduler, [value, observer]) {
+    if (observer._subscriptionDisposable.isDisposed) return;
+    observer._subscriptionDisposable.dispose();
+    if(observer._generator.return) {
+      return observer._generator.return(value);
     }
   }
 }
