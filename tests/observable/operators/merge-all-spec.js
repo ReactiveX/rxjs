ea738f840d7c177440a5b04eb082270d1f2012eb
Ben Lesh <blesh@netflix.com>
2015-04-14 17:13:52 -0700

    add primitive test scenario runner for deterministic tests

--- tests/observable/operators/merge-all-spec.js
+++ tests/observable/operators/merge-all-spec.js
@@ -1,4 +1,5 @@
 import { Observable } from 'src/observable/observable';
+import TestScenario from '../../test-observable';
 
 describe('Observable.mergeAll', () => {
   it('should merge many observables into one', done => {
@@ -22,5 +23,23 @@ describe('Observable.mergeAll', () => {
         done();
       }
     });
+  });
+
+  //HACK: this needs it's own spot.
+  it('should use the TestScenario', () => {
+    var scenario = new TestScenario();
+    var test = scenario.createColdObservable([10, 'b'], [23, 'c'], [0, 'a']);
+    var results = [];
+    test.observer({
+      next: x => {
+        results.push(x);
+      }
+    });
+
+    expect(results).toEqual([]);
+
+    scenario.run();
+
+    expect(results).toEqual(['a','b','c']);
   });
 });
