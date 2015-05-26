1a543b068ba39845b7e0339cf62666acf8848a33
Jeff Cross <middlefloor@gmail.com>
2015-05-26 13:46:21 -0700

    fix(rx): add hack to export global until better global build exists

--- src/rx.ts
+++ src/rx.ts
@@ -1,1 +1,3 @@
-export * from './observable/observable';
+import {Observable} from './observable/observable';
+//TODO: remove this hack once we have a better global bundling solution
+(<any>window).Observable = Observable;
