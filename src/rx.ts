57cf29804701c7171ddbb4332462e0973f89e957
Jeff Cross <middlefloor@gmail.com>
2015-05-26 15:46:01 -0700

    chore(global): add Subscription constructor to global

--- src/rx.ts
+++ src/rx.ts
@@ -1,3 +1,7 @@
 import {Observable} from './observable/observable';
+import Subscription from './subscription/subscription';
 //TODO: remove this hack once we have a better global bundling solution
-(<any>window).Observable = Observable;
+(<any>window).Rx3 = {
+  Observable,
+  Subscription
+};
