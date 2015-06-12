7d3f2b67d51f21c014a0e977743ec758091ea7f8
Ben Lesh <blesh@netflix.com>
2015-06-11 17:06:49 -0700

    add throw file (oops)

---
+++ src/observable/throw.ts
@@ -0,0 +1,42 @@
+import OperatorObservable from '../OperatorObservable';
+import Scheduler from '../Scheduler';
+
+class ThrowObservable extends OperatorObservable {
+  scheduler:Scheduler;
+  err:any;
+  
+  constructor(err:any, scheduler:Scheduler) {
+    super(null);
+    this.err = err;
+    this.scheduler = scheduler;
+  }
+  
+  _subscribe(observer) {
+    var scheduler = this.scheduler;
+    var err = this.err;
+    if(scheduler) {
+        return scheduler.schedule(0, { observer, err }, dispatch);
+    }
+    observer["throw"](this.err);
+  }
+}
+
+ThrowObservable.prototype._subscribe = function _subscribe(observer) {
+    var scheduler = this.scheduler;
+
+    if(scheduler) {
+        return scheduler.schedule(observer, dispatch);
+    }
+
+    observer["throw"]();
+};
+
+function dispatch({ observer, err }) {
+    observer["throw"](err);
+}
+
+var staticEmpty = new ThrowObservable(undefined, undefined);
+
+export default function _throw(err:any=undefined, scheduler:Scheduler=Scheduler.immediate):OperatorObservable {
+    return new ThrowObservable(err, scheduler);
+};
