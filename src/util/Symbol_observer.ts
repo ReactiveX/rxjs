2b0dbdfc52cf50cd7d36460858da5fd2174fe678
Ben Lesh <blesh@netflix.com>
2015-06-24 13:08:57 -0700

    fix Symbol.observer, add Symbol.dispose

--- src/util/Symbol_observer.ts
+++ src/util/Symbol_observer.ts
@@ -5,10 +5,11 @@ if(!root.Symbol) {
 }
 
 if(!root.Symbol.observer) {
-  if(typeof root.Symbol.for === 'function') {
+  if (typeof root.Symbol.for === 'function') {
     root.Symbol.observer = root.Symbol.for('observer');
+  } else {
+    root.Symbol.observer = '@@observer';
   }
-  root.Symbol.observer = '@@observer';
 }
 
 export default root.Symbol.observer;
