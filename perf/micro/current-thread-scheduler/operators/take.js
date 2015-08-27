d426a082573a16e205ac5f2271d4e9ad9edf18ef
Steve Ayers <smaye81@gmail.com>
2015-08-26 23:32:19 -0400

    Added new file for the take operator micro perf test

---
+++ perf/micro/current-thread-scheduler/operators/take.js
@@ -0,0 +1,19 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function (suite) {
+
+  var oldTakeWithCurrentThreadScheduler = RxOld.Observable.range(0, 25, RxOld.Scheduler.currentThread).take(5);
+  var newTakeWithCurrentThreadScheduler = RxNew.Observable.range(0, 25, RxNew.Scheduler.immediate).take(5);
+
+  return suite
+    .add('old take with current thread scheduler', function () {
+      oldTakeWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+    })
+    .add('new take with current thread scheduler', function () {
+      newTakeWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+    });
+  function _next(x) { }
+  function _error(e){ }
+  function _complete(){ }
+};
