92acef6198c059ccd186021345d29bcf8f1c0396
kwonoj <kwon.ohjoong@gmail.com>
2015-08-27 21:41:43 -0700

    chore(test): add micro-benchmark performance test for concatAll operator

---
+++ perf/micro/current-thread-scheduler/operators/concat-all.js
@@ -0,0 +1,20 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function (suite) {
+
+  var oldConcatAllWithCurrentThreadScheduler = RxOld.Observable.range(0, 25, RxOld.Scheduler.currentThread).map(function (x) { return RxOld.Observable.range(x, 25, RxOld.Scheduler.currentThread); }).concatAll();
+  var newConcatAllWithCurrentThreadScheduler = RxNew.Observable.range(0, 25).map(function (x) { return RxNew.Observable.range(x, 25); }).concatAll();
+
+  return suite
+    .add('old concatAll with current thread scheduler', function () {
+      oldConcatAllWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+    })
+    .add('new concatAll with current thread scheduler', function () {
+      newConcatAllWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+    });
+
+  function _next(x) { }
+  function _error(e){ }
+  function _complete(){ }
+};
