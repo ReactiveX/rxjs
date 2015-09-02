e5354dfd740a175f0ad5dea7fb1c8c4898be9919
kwonoj <kwon.ohjoong@gmail.com>
2015-09-02 15:54:26 -0700

    chore(test): add micro-benchmark performance test for Observable.throw

---
+++ perf/micro/current-thread-scheduler/observable/throw.js
@@ -0,0 +1,20 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function _throw(suite) {
+
+    var oldThrowWithCurrentThreadScheduler = RxOld.Observable.throw(new Error('error'), RxOld.Scheduler.currentThread);
+    var newThrowWithCurrentThreadScheduler = RxNew.Observable.throw(new Error('error'), RxNew.Scheduler.immediate);
+
+    // add tests
+    return suite
+        .add('old throw with current thread scheduler', function () {
+            oldThrowWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+        })
+        .add('new throw with current thread scheduler', function () {
+            newThrowWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+        });
+    function _next(x) { }
+    function _error(e){ }
+    function _complete(){ }
+};
