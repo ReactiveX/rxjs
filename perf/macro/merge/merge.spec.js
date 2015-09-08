26661c8411848167bd52d66174909ef85612abd8
kwonoj <kwon.ohjoong@gmail.com>
2015-09-08 16:31:18 -0700

    chore(test): add macro performance tests for merge

---
+++ perf/macro/merge/merge.spec.js
@@ -0,0 +1,47 @@
+var benchpress = require('benchpress');
+var runner = new benchpress.Runner([
+  benchpress.SeleniumWebDriverAdapter.PROTRACTOR_BINDINGS,
+  benchpress.Validator.bindTo(benchpress.RegressionSlopeValidator),
+  benchpress.bind(benchpress.RegressionSlopeValidator.SAMPLE_SIZE).toValue(20),
+  benchpress.bind(benchpress.RegressionSlopeValidator.METRIC).toValue('scriptTime'),
+  benchpress.bind(benchpress.Options.FORCE_GC).toValue(false)
+]);
+
+describe('merge comparison', function () {
+  [
+    1000,
+    10000
+  ].forEach(function (val) {
+    it('should be fast in Rx2', function (done) {
+      browser.ignoreSynchronization = true;
+      browser.get('http://localhost:8080/perf/macro/merge/index.html?iterations=' + val);
+      runner.sample({
+        id: 'merge Rx2',
+        execute: function () {
+          $('#rx-2-merge').click();
+        },
+        bindings: [
+          benchpress.bind(benchpress.Options.SAMPLE_DESCRIPTION).toValue({
+            iterations: val
+          })
+        ]
+      }).then(done, done.fail);
+    });
+
+    it('should be fast in RxNext', function (done) {
+      browser.ignoreSynchronization = true;
+      browser.get('http://localhost:8080/perf/macro/merge/index.html?iterations=' + val);
+      runner.sample({
+        id: 'merge RxNext',
+        execute: function () {
+          $('#rx-3-merge').click();
+        },
+        bindings: [
+          benchpress.bind(benchpress.Options.SAMPLE_DESCRIPTION).toValue({
+            iterations: val
+          })
+        ]
+      }).then(done, done.fail);
+    });
+  });
+});
