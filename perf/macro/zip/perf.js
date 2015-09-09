ca42a3289d4404caabee22d2037bcad05320d738
kwonoj <kwon.ohjoong@gmail.com>
2015-09-09 13:21:47 -0700

    refactor(test): let macro perf test use range to create observables

--- perf/macro/zip/perf.js
+++ perf/macro/zip/perf.js
@@ -10,11 +10,11 @@ var numIterations = iterationInput.valueAsNumber || 1000;
 var Rx2Zip = document.querySelector('#rx-2-zip');
 var RxNextZip = document.querySelector('#rx-3-zip');
 
-var Rx2TestObservable = Rx.Observable.create(generator);
-var RxNextTestObservable = new RxNext.Observable(generator);
+var Rx2TestObservable = Rx.Observable.range(0, numIterations);
+var RxNextTestObservable = RxNext.Observable.range(0, numIterations);
 
-var Rx2TestArgObservable = Rx.Observable.create(generator);
-var RxNextTestArgObservable = new RxNext.Observable(generator);
+var Rx2TestArgObservable = Rx.Observable.range(0, numIterations);
+var RxNextTestArgObservable = RxNext.Observable.range(0, numIterations);
 
 Rx2Zip.addEventListener('click', function() {
   Rx2TestObservable.zip(Rx2TestArgObservable).subscribe();
@@ -23,11 +23,3 @@ Rx2Zip.addEventListener('click', function() {
 RxNextZip.addEventListener('click', function() {
   RxNextTestObservable.zip(RxNextTestArgObservable).subscribe();
 });
-
-function generator(observer) {
-  var index = -1;
-  while(++index < numIterations) {
-    observer.onNext(index);
-  }
-  observer.onCompleted();
-}
