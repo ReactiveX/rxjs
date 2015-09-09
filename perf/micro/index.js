fb21385d0a51a85b67ebf2e572e882c3beac0241
Ben Lesh <blesh@netflix.com>
2015-09-09 13:30:23 -0700

    chore(perf): improve micro perf ergonomics
    - automatically pick up new files without maintaining a huge array
    - add ability to filter down to specific tests: `node perf/micro testfilter` where "testfilter" is the name of the test files to be run
      for example `map` or `take` or `zip`

--- perf/micro/index.js
+++ perf/micro/index.js
@@ -3,101 +3,59 @@ var colors = require("colors");
 var Observable = Rx.Observable;
 var Benchmark = require("benchmark");
 var suite = new Benchmark.Suite;
+var glob = require('glob');
+var path = require('path');
 
-Observable.from([
-        require("./immediate-scheduler/observable/empty"),
-        require("./immediate-scheduler/observable/from-array"),
-        require("./immediate-scheduler/observable/from-with-array"),
-        require("./immediate-scheduler/observable/from-with-string"),
-        require("./immediate-scheduler/observable/of"),
-        require("./immediate-scheduler/observable/range"),
-        require("./immediate-scheduler/observable/throw"),
+Observable.create(function(observer) {
+  ["perf/micro/immediate-scheduler/**/*.js", "perf/micro/current-thread-scheduler/**/*.js"]
+  .forEach(function(pattern) { 
+    try {
+      glob.sync(pattern).forEach(function(file) {
+        observer.onNext(file);
+      })
+    } catch(err) {
+      observer.onError(err);
+    }
+  });
+  observer.onCompleted();
+})
+.map(function(filename) {
+  return './' + path.relative(__dirname, filename);
+})
+.filter(function(filePath) {
+  var argv = process.argv;
+  if(argv && argv.length > 2) {    
+    return argv.slice(2).some(function(val) {
+      return path.parse(filePath).name === val;
+    });
+  }
+  return true;
+})
+.map(require)
+.concatMap(function (test) {
+    var tests = test(new Benchmark.Suite());
+    return Observable.defer(function () {
 
-        require("./immediate-scheduler/operators/buffer-count"),
-        require("./immediate-scheduler/operators/catch"),
-        require("./immediate-scheduler/operators/combine-latest"),
-        require("./immediate-scheduler/operators/concat"),
-        require("./immediate-scheduler/operators/concat-all"),
-        require("./immediate-scheduler/operators/concat-many"),
-        require("./immediate-scheduler/operators/default-if-empty"),
-        require("./immediate-scheduler/operators/filter"),
-        require("./immediate-scheduler/operators/merge"),
-        require("./immediate-scheduler/operators/reduce"),
-        require("./immediate-scheduler/operators/repeat"),
+        var cycles = new Rx.ReplaySubject();
+        var complete = new Rx.ReplaySubject();
 
-        require("./immediate-scheduler/operators/flat-map"),
-        require("./immediate-scheduler/operators/flat-map-scalar"),
-        require("./immediate-scheduler/operators/flat-map-observable"),
-        require("./immediate-scheduler/operators/flat-map-observable-scalar"),
+        tests.on("cycle", function (e) {
+            cycles.onNext(String(e.target));
+        }).on("complete", function () {
+            var fastest = this.filter("fastest");
+            var fastestName = String(fastest.pluck("name"));
+            var fastestTime = parseFloat(this.filter("fastest").pluck("hz"));
+            var slowestTime = parseFloat(this.filter("slowest").pluck("hz"));
 
-        require("./immediate-scheduler/operators/map"),
-        require("./immediate-scheduler/operators/scan"),
-        require("./immediate-scheduler/operators/skip"),
-        require("./immediate-scheduler/operators/start-with"),
-        require("./immediate-scheduler/operators/take"),
-        require("./immediate-scheduler/operators/to-array"),
-        require("./immediate-scheduler/operators/window-count"),
-        require("./immediate-scheduler/operators/zip"),
+            // percent change formula: ((V2 - V1) / |V1|) * 100
+            if(fastestName.substr(0, 3) === "new") {
+                complete.onNext("\t" + (Math.round((fastestTime - slowestTime) / slowestTime * 10000) / 100) + "% " + "faster".green +" than Rx2\n");
+            } else {
+                complete.onNext("\t" + (Math.round((slowestTime - fastestTime) / fastestTime * 10000) / 100) + "% " + "slower".red + " than Rx2\n");
+            }
+        }).run({ "async": true });
 
-        require("./current-thread-scheduler/observable/empty"),
-        require("./current-thread-scheduler/observable/from-array"),
-        require("./current-thread-scheduler/observable/from-with-array"),
-        require("./current-thread-scheduler/observable/from-with-string"),
-        require("./current-thread-scheduler/observable/of"),
-        require("./current-thread-scheduler/observable/range"),
-        require("./current-thread-scheduler/observable/throw"),
-
-        require("./current-thread-scheduler/operators/buffer-count"),
-        require("./current-thread-scheduler/operators/catch"),
-        require("./current-thread-scheduler/operators/combine-latest"),
-        require("./current-thread-scheduler/operators/concat"),
-        require("./current-thread-scheduler/operators/concat-all"),
-        require("./current-thread-scheduler/operators/concat-many"),
-        require("./current-thread-scheduler/operators/default-if-empty"),
-        require("./current-thread-scheduler/operators/filter"),
-        require("./current-thread-scheduler/operators/merge"),
-        require("./current-thread-scheduler/operators/reduce"),
-        require("./current-thread-scheduler/operators/repeat"),
-
-        require("./current-thread-scheduler/operators/flat-map"),
-        require("./current-thread-scheduler/operators/flat-map-scalar"),
-        require("./current-thread-scheduler/operators/flat-map-observable"),
-        require("./current-thread-scheduler/operators/flat-map-observable-scalar"),
-
-        require("./current-thread-scheduler/operators/map"),
-        require("./current-thread-scheduler/operators/scan"),
-        require("./current-thread-scheduler/operators/skip"),
-        require("./current-thread-scheduler/operators/start-with"),
-        require("./current-thread-scheduler/operators/take"),
-        require("./current-thread-scheduler/operators/to-array"),
-        require("./current-thread-scheduler/operators/window-count"),
-        require("./current-thread-scheduler/operators/zip"),
-
-    ])
-    .concatMap(function (test) {
-        var tests = test(new Benchmark.Suite());
-        return Observable.defer(function () {
-
-            var cycles = new Rx.ReplaySubject();
-            var complete = new Rx.ReplaySubject();
-
-            tests.on("cycle", function (e) {
-                cycles.onNext(String(e.target));
-            }).on("complete", function () {
-                var fastest = this.filter("fastest");
-                var fastestName = String(fastest.pluck("name"));
-                var fastestTime = parseFloat(this.filter("fastest").pluck("hz"));
-                var slowestTime = parseFloat(this.filter("slowest").pluck("hz"));
-
-                // percent change formula: ((V2 - V1) / |V1|) * 100
-                if(fastestName.substr(0, 3) === "new") {
-                    complete.onNext("\t" + (Math.round((fastestTime - slowestTime) / slowestTime * 10000) / 100) + "% " + "faster".green +" than Rx2\n");
-                } else {
-                    complete.onNext("\t" + (Math.round((slowestTime - fastestTime) / fastestTime * 10000) / 100) + "% " + "slower".red + " than Rx2\n");
-                }
-            }).run({ "async": true });
-
-            return cycles.merge(complete).take(tests.length + 1);
-        });
-    })
-    .subscribe(console.log.bind(console));
+        return cycles.merge(complete).take(tests.length + 1);
+    });
+})
+.subscribe(console.log.bind(console));
