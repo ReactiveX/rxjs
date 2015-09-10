f9a36da5c8ae7a554f65ad3eee213be9a1c51065
Ben Lesh <blesh@netflix.com>
2015-09-10 10:14:58 -0700

    docs(Observable): add basic documentation for Observable

--- src/Observable.ts
+++ src/Observable.ts
@@ -11,12 +11,25 @@ import GroupSubject from './subjects/GroupSubject';
 import $$observable from './util/Symbol_observable';
 
 
+/**
+ * A representation of any set of values over any amount of time. This the most basic building block
+ * of RxJS.
+ * 
+ * @class Observable<T>
+ */
 export default class Observable<T>  {
 
   source: Observable<any>;
   operator: Operator<any, T>;
 
-  constructor(subscribe?: <R>(subscriber: Subscriber<R>) => Subscription<T> | Function | void) {
+  /**
+   * @constructor
+   * @param {Function} subscribe the function that is
+   * called when the Observable is initially subscribed to. This function is given a Subscriber, to which new values
+   * can be `next`ed, or an `error` method can be called to raise an error, or `complete` can be called to notify
+   * of a succesful completion.
+   */
+  constructor(subscribe?: <R>(subscriber: Subscriber<R>) => Subscription<T>|Function|void) {
     if (subscribe) {
       this._subscribe = subscribe;
     }
@@ -24,10 +37,25 @@ export default class Observable<T>  {
   
   // HACK: Since TypeScript inherits static properties too, we have to 
   // fight against TypeScript here so Subject can have a different static create signature.
-  static create: Function = <T>(subscribe?: <R>(subscriber: Subscriber<R>) => Subscription<T> | Function | void) => {
+  
+  /**
+   * @static
+   * @method create
+   * @param {Function} subscribe? the subscriber function to be passed to the Observable constructor
+   * @returns {Observable} a new cold observable
+   * @description creates a new cold Observable by calling the Observable constructor 
+   */
+  static create: Function = <T>(subscribe?: <R>(subscriber: Subscriber<R>) => Subscription<T>|Function|void) => {
     return new Observable<T>(subscribe);
   };
   
+  /**
+   * @method lift
+   * @param {Operator} the operator defining the operation to take on the observable
+   * @returns {Observable} a new observable with the Operator applied
+   * @description creates a new Observable, with this Observable as the source, and the passed 
+   * operator defined as the new observable's operator.
+   */
   lift<T, R>(operator: Operator<T, R>): Observable<T> {
     const observable = new Observable();
     observable.source = this;
@@ -35,10 +63,26 @@ export default class Observable<T>  {
     return observable;
   }
 
+  /**
+   * @method Symbol.observable
+   * @returns {Observable} this instance of the observable
+   * @description an interop point defined by the es7-observable spec https://github.com/zenparsing/es-observable
+   */
   [$$observable]() {
     return this;
   }
 
+  /**
+   * @method subscribe
+   * @param {Observer|Function} observerOrNext (optional) either an observer defining all functions to be called, 
+   *  or the first of three possible handlers, which is the handler for each value emitted from the observable.
+   * @param {Function} error (optional) a handler for a terminal event resulting from an error. If no error handler is provided,
+   *  the error will be thrown as unhandled
+   * @param {Function} complete (optional) a handler for a terminal event resulting from successful completion.
+   * @returns {Subscription} a subscription reference to the registered handlers
+   * @description registers handlers for handling emitted values, error and completions from the observable, and
+   *  executes the observable's subscriber function, which will take action to set up the underlying data stream
+   */
   subscribe(observerOrNext?: Observer<T> | ((value: T) => void),
             error?: (error: T) => void,
             complete?: () => void): Subscription<T> {
@@ -61,9 +105,15 @@ export default class Observable<T>  {
     return subscriber;
   }
 
-  forEach(nextHandler:(value:T) => void) {
+  /**
+   * @method forEach
+   * @param {Function} next a handler for each value emitted by the observable
+   * @returns {Promise} a promise that either resolves on observable completion or
+   *  rejects with the handled error
+   */
+  forEach(next:(value:T) => void) {
     return new Promise((resolve, reject) => {
-      this.subscribe(nextHandler, reject, resolve);
+      this.subscribe(next, reject, resolve);
     });
   }
 
