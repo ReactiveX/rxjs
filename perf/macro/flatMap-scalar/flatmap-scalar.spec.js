0949fbe7813b2560489c55d94a64834adb88e580
kwonoj <kwon.ohjoong@gmail.com>
2015-09-11 13:25:37 -0700

    refactor(test): let macro test share common test spec initializer

--- perf/macro/flatMap-scalar/flatmap-scalar.spec.js
+++ perf/macro/flatMap-scalar/flatmap-scalar.spec.js
@@ -1,47 +1,24 @@
-var benchpress = require('benchpress');
-var runner = new benchpress.Runner([
-  benchpress.SeleniumWebDriverAdapter.PROTRACTOR_BINDINGS,
-  benchpress.Validator.bindTo(benchpress.RegressionSlopeValidator),
-  benchpress.bind(benchpress.RegressionSlopeValidator.SAMPLE_SIZE).toValue(20),
-  benchpress.bind(benchpress.RegressionSlopeValidator.METRIC).toValue('scriptTime'),
-  benchpress.bind(benchpress.Options.FORCE_GC).toValue(false)
-]);
+var builder = require('../specbuilder');
+
+var preset = new builder.Preset();
+var runner = preset.runner;
 
 describe('flatMap comparison', function () {
-  [
-    1000,
-    10000
-  ].forEach(function (val) {
+  var testUrlIdentifier = 'flatMap-scalar';
+  
+  preset.iteration.forEach(function (val) {
+    
     it('should be fast in Rx2', function (done) {
-      browser.ignoreSynchronization = true;
-      browser.get('http://localhost:8080/perf/macro/flatMap-scalar/index.html?iterations=' + val);
-      runner.sample({
-        id: 'flatMap-range-to-scalar Rx2',
-        execute: function () {
-          $('#rx-2-flatmap-range-to-scalar').click();
-        },
-        bindings: [
-          benchpress.bind(benchpress.Options.SAMPLE_DESCRIPTION).toValue({
-            iterations: val
-          })
-        ]
-      }).then(done, done.fail);
+      preset.initBrowser(browser, testUrlIdentifier, val);
+      var param = preset.sampleParameter('flatMap-range-to-scalar Rx2', '#rx-2-flatmap-range-to-scalar', val);
+      runner.sample(param).then(done, done.fail);
     });
-
+    
     it('should be fast in RxNext', function (done) {
-      browser.ignoreSynchronization = true;
-      browser.get('http://localhost:8080/perf/macro/flatMap-scalar/index.html?iterations=' + val);
-      runner.sample({
-        id: 'flatMap-range-to-scalar RxNext',
-        execute: function () {
-          $('#rx-3-flatmap-range-to-scalar').click();
-        },
-        bindings: [
-          benchpress.bind(benchpress.Options.SAMPLE_DESCRIPTION).toValue({
-            iterations: val
-          })
-        ]
-      }).then(done, done.fail);
+      preset.initBrowser(browser, testUrlIdentifier , val);
+      var param = preset.sampleParameter('flatMap-range-to-scalar RxNext', '#rx-3-flatmap-range-to-scalar', val);
+      runner.sample(param).then(done, done.fail);
     });
+    
   });
 });
