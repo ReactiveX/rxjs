3764d7aea770468a93e3d196c4cde6d95ef97134
kwonoj <kwon.ohjoong@gmail.com>
2015-09-12 02:06:02 -0700

    chore(test): add micro-benchmark performance test for Observable.fromPromise

---
+++ perf/micro/immediate-scheduler/observable/fromPromise.js
@@ -0,0 +1,25 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function fromPromise(suite) {
+    
+  var promise = new Promise(function (resolve, reject) {
+      resolve(25);
+  });
+
+  var oldFromPromiseWithImmediateScheduler = RxOld.Observable.fromPromise(promise);
+  var newFromPromiseWithImmediateScheduler = RxNew.Observable.fromPromise(promise);
+
+  // add tests
+  return suite
+      .add('old fromPromise with immediate scheduler', function () {
+          oldFromPromiseWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new fromPromise with immediate scheduler', function () {
+          newFromPromiseWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+
+  function _next(x) { }
+  function _error(e){ }
+  function _complete(){ }
+};
