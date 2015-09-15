68194270c709ceeca064e77985d9e434fe6129e7
Ben Lesh <blesh@netflix.com>
2015-09-15 13:17:04 -0700

    refactor(flatMap): refactor flatMap to reduce inheritance
    - removes inheritance from merge
    - improves performance through fewer function calls
    - ensures scalar (same behavior as before, but readded)

--- src/operators/flatMap-support.ts
+++ src/operators/flatMap-support.ts
@@ -1,95 +1,123 @@
-import Operator from '../Operator';
-import Observer from '../Observer';
 import Observable from '../Observable';
+import Operator from '../Operator';
 import Subscriber from '../Subscriber';
-
-import { MergeSubscriber, MergeInnerSubscriber } from './merge-support';
-import ScalarObservable from '../observables/ScalarObservable';
-
+import Subscription from '../Subscription';
+import Observer from '../Observer';
 import tryCatch from '../util/tryCatch';
 import { errorObject } from '../util/errorObject';
 
-export class FlatMapOperator<T, R> implements Operator<T, R> {
+export default function flatMap<T, R, R2>(project: (value: T, index: number) => Observable<R>,
+                                      resultSelector?: (innerValue: R, outerValue: T, innerIndex: number, outerIndex: number) => R,
+                                      concurrent: number = Number.POSITIVE_INFINITY) {
+  return this.lift(new FlatMapOperator(project, resultSelector, concurrent));
+}
 
-  project: (x: T, ix: number) => Observable<any>;
-  projectResult: (x: T, y: any, ix: number, iy: number) => R;
-  concurrent: number;
-
-  constructor(project: (x: T, ix: number) => Observable<any>,
-              projectResult?: (x: T, y: any, ix: number, iy: number) => R,
-              concurrent: number = Number.POSITIVE_INFINITY) {
-    this.project = project;
-    this.projectResult = projectResult;
-    this.concurrent = concurrent;
+export class FlatMapOperator<T, R, R2> implements Operator<T, R> {
+  constructor(private project: (value: T, index: number) => Observable<R>,
+    private resultSelector?: (innerValue: R, outerValue: T, innerIndex: number, outerIndex: number) => R2,
+    private concurrent: number = Number.POSITIVE_INFINITY) {
   }
-
-  call(subscriber: Subscriber<R>): Subscriber<T> {
-    return new FlatMapSubscriber(subscriber, this.concurrent, this.project, this.projectResult);
+  
+  call(observer: Subscriber<R>): Subscriber<T> {
+    return new FlatMapSubscriber(observer, this.project, this.resultSelector, this.concurrent);
   }
 }
 
-export class FlatMapSubscriber<T, R> extends MergeSubscriber<T, R> {
-
-  project: (x: T, ix: number) => Observable<any>;
-  projectResult: (x: T, y: any, ix: number, iy: number) => R;
-
-  constructor(destination: Observer<R>,
-              concurrent: number,
-              project: (x: T, ix: number) => Observable<any>,
-              projectResult?: (x: T, y: any, ix: number, iy: number) => R) {
-    super(destination, concurrent);
-    this.project = project;
-    this.projectResult = projectResult;
+export class FlatMapSubscriber<T, R, R2> extends Subscriber<T> {
+  private hasCompleted: boolean = false;
+  private buffer: Observable<any>[] = [];
+  private active: number = 0;
+  protected index: number = 0;
+  
+  constructor(destination: Observer<T>, 
+    private project: (value: T, index: number) => Observable<R>,
+    private resultSelector?: (innerValue: R, outerValue: T, innerIndex: number, outerIndex: number) => R2,
+    private concurrent: number = Number.POSITIVE_INFINITY) {
+    super(destination);
   }
-
-  _project(value, index) {
-    const observable = tryCatch(this.project).call(this, value, index);
-    if (observable === errorObject) {
-      this.error(errorObject.e);
-      return null;
-    }
-    return observable;
-  }
-
-  _subscribeInner(observable:Observable<T>, value, index) {
-    const projectResult = this.projectResult;
-    if(projectResult) {
-      return observable._subscribe(new FlatMapInnerSubscriber(this.destination, this, value, index, projectResult));
-    } else if(observable._isScalar) {
-      this.destination.next((<any> observable).value);
-      this._innerComplete();
+  
+  _next(value: any) {
+    if(this.active < this.concurrent) {
+      const resultSelector = this.resultSelector;
+      const index = this.index++;
+      const observable = tryCatch(this.project)(value, index);
+      if(observable === errorObject) {
+        this.destination.error(observable.e);
+      } else {
+        this._innerSubscribe(observable, value, index);
+      }
     } else {
-      return observable._subscribe(new MergeInnerSubscriber(this.destination, this));
+      this.buffer.push(value);
+    }
+  }
+  
+  _innerSubscribe(observable, value, index) {
+    const resultSelector = this.resultSelector;
+    if(observable._isScalar) {
+      if(resultSelector) {
+        let result = tryCatch(resultSelector)(observable.value, value, 0, index);
+        if(result === errorObject) {
+          this.destination.error(result.e);
+        } else {
+          this.destination.next(result);
+        }
+      } else {
+        this.destination.next(observable.value);
+      }
+    } else {
+      const innerSub = new Subscription();
+      this.add(innerSub);
+      this.active++;
+      innerSub.add(observable.subscribe(new FlatMapInnerSubscriber(this.destination, this, innerSub, value, index, resultSelector)));
+    }
+  }
+  
+  _complete() {
+    this.hasCompleted = true;
+    if(this.active === 0 && this.buffer.length === 0) {
+      this.destination.complete();
+    }
+  }
+  
+  notifyComplete(innerSub: Subscription<T>) {
+    const buffer = this.buffer;
+    this.remove(innerSub);
+    this.active--;
+    if(buffer.length > 0) {
+      this._next(buffer.shift());
+    } else if (this.active === 0 && this.hasCompleted) {
+      this.destination.complete();
     }
   }
 }
 
-export class FlatMapInnerSubscriber<T, R> extends MergeInnerSubscriber<T, R> {
-
-  value: any;
-  index: number;
-  project: (x: T, y: any, ix: number, iy: number) => R;
-  count: number = 0;
-
-  constructor(destination: Observer<T>,
-              parent: FlatMapSubscriber<T, R>,
-              value: any,
-              index: number,
-              project?: (x: T, y: any, ix: number, iy: number) => R) {
-    super(destination, parent);
-    this.value = value;
-    this.index = index;
-    this.project = project;
+export class FlatMapInnerSubscriber<T, R, R2> extends Subscriber<R> {
+  index: number = 0;
+  
+  constructor(destination: Observer<R>, private parent: FlatMapSubscriber<any, R, R2>, 
+    private innerSub: Subscription<R>,
+    private outerValue: T,
+    private outerIndex: number,
+    private resultSelector?: (innerValue: T, outerValue: R, innerIndex: number, outerIndex: number) => R2) {
+    super(destination);
   }
-
-  _next(value) {
-    let result = value;
-    const index = this.count++;
-    result = tryCatch(this.project).call(this, this.value, value, this.index, index);
-    if (result === errorObject) {
-      this.destination.error(errorObject.e);
+  
+  _next(value: R) {
+    const resultSelector = this.resultSelector;
+    const index = this.index++;
+    if(resultSelector) {
+      let result = tryCatch(resultSelector)(value, this.outerValue, index, this.outerIndex);
+      if(result === errorObject) {
+        this.destination.error(result.e);
+      } else {
+        this.destination.next(result);
+      }
     } else {
-      this.destination.next(result);
+      this.destination.next(value);
     }
+  }
+  
+  _complete() {
+    this.parent.notifyComplete(this.innerSub);
   }
 }
--- src/operators/flatMap.ts
+++ src/operators/flatMap.ts
@@ -1,8 +1,8 @@
 import Observable from '../Observable';
 import { FlatMapOperator } from './flatMap-support';
 
-export default function flatMap<T, R>(project: (x: T, ix: number) => Observable<any>,
-                                      projectResult?: (x: T, y: any, ix: number, iy: number) => R,
-                                      concurrent?: number) {
-  return this.lift(new FlatMapOperator(project, projectResult, concurrent));
+export default function flatMap<T, R, R2>(project: (value: T, index: number) => Observable<R>,
+                                      resultSelector?: (innerValue: R, outerValue: T, innerIndex: number, outerIndex: number) => R,
+                                      concurrent: number = Number.POSITIVE_INFINITY) {
+  return this.lift(new FlatMapOperator(project, resultSelector, concurrent));
 }
