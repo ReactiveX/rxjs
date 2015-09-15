68ec15334dedd8652c381d3b3b5eabb50c41576f
Ben Lesh <blesh@netflix.com>
2015-09-15 13:44:45 -0700

    refactor(concatMapTo): update type signature

--- src/operators/concatMapTo.ts
+++ src/operators/concatMapTo.ts
@@ -1,7 +1,7 @@
 import Observable from '../Observable';
 import { FlatMapToOperator } from './flatMapTo-support';
 
-export default function concatMapTo<T, R>(observable: Observable<any>,
-                                          projectResult?: (x: T, y: any, ix: number, iy: number) => R) : Observable<R> {
+export default function concatMapTo<T, R, R2>(observable: Observable<R>,
+                                          projectResult?: (innerValue: R, outerValue: T, innerIndex: number, outerIndex: number) => R2) : Observable<R2> {
   return this.lift(new FlatMapToOperator(observable, projectResult, 1));
 }
