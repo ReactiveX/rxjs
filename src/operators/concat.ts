0794c21ff769883d043375235428ceceb0f7a623
Ben Lesh <blesh@netflix.com>
2015-09-15 13:43:09 -0700

    refactor(concat): update type signature
    - make ability to schedule more apparent
    - check for scheduler and insert concurrency of 1 at proper point for merge call

--- src/operators/concat.ts
+++ src/operators/concat.ts
@@ -1,8 +1,12 @@
 import merge from './merge-static';
 import Observable from '../Observable';
+import Scheduler from '../Scheduler';
 
-export default function concatProto<R>(...observables:any[]) : Observable<R> {
-  observables.unshift(this);
-  observables.push(1);
-  return merge.apply(this, observables);
+export default function concatProto<R>(...observables:(Observable<any>|Scheduler)[]) : Observable<R> {
+  var args = <any[]>observables;
+  args.unshift(this);
+  if(args.length > 1 && typeof args[args.length - 1].schedule === 'function') {
+    args.splice(args.length - 2, 0, 1);
+  }
+  return merge.apply(this, args);
 }
