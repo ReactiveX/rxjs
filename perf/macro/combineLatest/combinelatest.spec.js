a7ab267d0890cfa48f1e8fcf8b4222ad0bc14c62
kwonoj <kwon.ohjoong@gmail.com>
2015-09-15 00:13:04 -0700

    refactor(test): let macro test for combinelatest use spec initializer

--- perf/macro/combineLatest/combinelatest.spec.js
+++ perf/macro/combineLatest/combinelatest.spec.js
@@ -1,47 +1,24 @@
-var benchpress = require('benchpress');
-var runner = new benchpress.Runner([
-  benchpress.SeleniumWebDriverAdapter.PROTRACTOR_BINDINGS,
-  benchpress.Validator.bindTo(benchpress.RegressionSlopeValidator),
-  benchpress.bind(benchpress.RegressionSlopeValidator.SAMPLE_SIZE).toValue(20),
-  benchpress.bind(benchpress.RegressionSlopeValidator.METRIC).toValue('scriptTime'),
-  benchpress.bind(benchpress.Options.FORCE_GC).toValue(false)
-]);
+var builder = require('../specbuilder');
+
+var preset = new builder.Preset();
+var runner = preset.runner;
 
 describe('combineLatest comparison', function () {
-  [
-    1000,
-    10000
-  ].forEach(function (val) {
+  var testUrlIdentifier = 'combineLatest';
+  
+  preset.iteration.forEach(function (val) {
+    
     it('should be fast in Rx2', function (done) {
-      browser.ignoreSynchronization = true;
-      browser.get('http://localhost:8080/perf/macro/combineLatest/index.html?iterations=' + val);
-      runner.sample({
-        id: 'combineLatest Rx2',
-        execute: function () {
-          $('#rx-2-combineLatest').click();
-        },
-        bindings: [
-          benchpress.bind(benchpress.Options.SAMPLE_DESCRIPTION).toValue({
-            iterations: val
-          })
-        ]
-      }).then(done, done.fail);
+      preset.initBrowser(browser, testUrlIdentifier, val);
+      var param = preset.sampleParameter('combineLatest Rx2', '#rx-2-combineLatest', val);
+      runner.sample(param).then(done, done.fail);
     });
-
+      
     it('should be fast in RxNext', function (done) {
-      browser.ignoreSynchronization = true;
-      browser.get('http://localhost:8080/perf/macro/combineLatest/index.html?iterations=' + val);
-      runner.sample({
-        id: 'combineLatest RxNext',
-        execute: function () {
-          $('#rx-3-combineLatest').click();
-        },
-        bindings: [
-          benchpress.bind(benchpress.Options.SAMPLE_DESCRIPTION).toValue({
-            iterations: val
-          })
-        ]
-      }).then(done, done.fail);
+      preset.initBrowser(browser, testUrlIdentifier , val);
+      var param = preset.sampleParameter('combineLatest RxNext', '#rx-3-combineLatest', val);
+      runner.sample(param).then(done, done.fail);
     });
+    
   });
 });
