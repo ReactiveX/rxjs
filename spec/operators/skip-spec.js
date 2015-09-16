348c26a5b5fa0b44cf659b4f54bd664eae1e7655
kwonoj <kwon.ohjoong@gmail.com>
2015-09-16 15:13:30 -0700

    refactor(test): use test marbles for skip operator spec

--- spec/operators/skip-spec.js
+++ spec/operators/skip-spec.js
@@ -1,18 +1,19 @@
-/* globals describe, it, expect */
+/* globals describe, it, expect, expectObservable, hot */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.skip()', function () {
-  it('should skip values before a total', function (done) {
-    var expected = [4, 5];
-    Observable.of(1, 2, 3, 4, 5).skip(3).subscribe(function (x) {
-      expect(x).toBe(expected.shift());
-    }, null, done);
+  it('should skip values before a total', function () {
+    var source = hot('--a--b--c--d--e--|');
+    var expected =   '-----------d--e--|';
+    
+    expectObservable(source.skip(3)).toBe(expected);
   });
   
-  it('should skip all values without error if total is more than actual number of values', function (done) {
-    Observable.of(1, 2, 3, 4, 5).skip(6).subscribe(function (x) {
-      expect(true).toBe('this should never be called');
-    }, null, done);
+   it('should skip all values without error if total is more than actual number of values', function () {
+    var source = hot('--a--b--c--d--e--|');
+    var expected =   '-----------------|';
+    
+    expectObservable(source.skip(6)).toBe(expected);
   });
 });
