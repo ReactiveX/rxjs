c78f940638cae4f491229bedd2d55d642a992d98
Ben Lesh <blesh@netflix.com>
2015-09-15 17:10:12 -0700

    refactor(switchLatest): now switchLatest is a standalone implementation
    - adds a lot of virtual tests around switchLatest
    - switchLatest no longer depends on flatMap
    
    addresses #302

--- spec/operators/switchLatest-spec.js
+++ spec/operators/switchLatest-spec.js
@@ -3,15 +3,166 @@ var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 var immediateScheduler = Rx.Scheduler.immediate;
 
-describe('Observable.prototype.switchLatest()', function () {
+fdescribe('Observable.prototype.switchLatest()', function () {
   it("should switch with a selector function", function (done) {
     var a = Observable.of(1, 2, 3);
-    var r = [11, 12, 13, 12, 13, 14, 13, 14, 15];
-    var i = 0;
+    var expected = ['a1', 'b1', 'c1', 'a2', 'b2', 'c2', 'a3', 'b3', 'c3'];
     a.switchLatest(function(x) {
-      return Observable.range(x + 10, 3);
+      return Observable.of('a' + x, 'b' + x, 'c' + x);
     }).subscribe(function (x) {
-      expect(x).toBe(r[i++]);
+      expect(x).toBe(expected.shift());
     }, null, done);
   });
+
+  it('should switch inner cold observables', function (){
+    var x =   cold(         '--a--b--c--d--e--|')
+    var y =   cold(                   '---f---g---h---i--|');
+    var e1 =   hot('---------x---------y---------|');
+    var expected = '-----------a--b--c----f---g---h---i--|';
+    
+    var observableLookup = { x: x, y: y };
+    
+    expectObservable(e1.switchLatest(function(value) {
+      return observableLookup[value];
+    })).toBe(expected);    
+  });
+  
+  it('should switch inner hot observables', function (){
+    var x =    hot('-----a--b--c--d--e--|')
+    var y =    hot('--p-o-o-p-------------f---g---h---i--|');
+    var e1 =   hot('---------x---------y---------|');
+    var expected = '-----------c--d--e----f---g---h---i--|';
+    
+    var observableLookup = { x: x, y: y };
+    
+    expectObservable(e1.switchLatest(function(value) {
+      return observableLookup[value];
+    })).toBe(expected);    
+  });
+  
+  it('should switch inner empty and empty', function () {
+    var x = Observable.empty();
+    var y = Observable.empty();
+    var e1 =   hot('---------x---------y---------|');
+    var expected = '-----------------------------|';
+    
+    var observableLookup = { x: x, y: y };
+    
+    expectObservable(e1.switchLatest(function(value) {
+      return observableLookup[value];
+    })).toBe(expected);    
+  });
+  
+  it('should switch inner empty and never', function() {
+    var x =   Observable.empty()
+    var y =   Observable.never();
+    var e1 =   hot('---------x---------y---------|');
+    var expected = '----------------------------------';
+    
+    var observableLookup = { x: x, y: y };
+    
+    expectObservable(e1.switchLatest(function(value) {
+      return observableLookup[value];
+    })).toBe(expected);    
+  });
+  
+  it('should switch inner never and empty', function (){
+    var x = Observable.never();
+    var y = Observable.empty();
+    var e1 =   hot('---------x---------y---------|');
+    var expected = '-----------------------------|';
+    
+    var observableLookup = { x: x, y: y };
+    
+    expectObservable(e1.switchLatest(function(value) {
+      return observableLookup[value];
+    })).toBe(expected);    
+  });
+  
+  it('should switch inner never and throw', function (){
+    var x = Observable.never();
+    var y = Observable.throw(new Error('sad'));
+    var e1 =   hot('---------x---------y---------|');
+    var expected = '-------------------#';
+    
+    var observableLookup = { x: x, y: y };
+    
+    expectObservable(e1.switchLatest(function(value) {
+      return observableLookup[value];
+    })).toBe(expected, undefined, new Error('sad'));    
+  });
+  
+  it('should switch inner empty and throw', function (){
+    var x = Observable.empty();
+    var y = Observable.throw(new Error('sad'));
+    var e1 =   hot('---------x---------y---------|');
+    var expected = '-------------------#';
+    
+    var observableLookup = { x: x, y: y };
+    
+    expectObservable(e1.switchLatest(function(value) {
+      return observableLookup[value];
+    })).toBe(expected, undefined, new Error('sad'));    
+  });
+  
+  it('should handle outer empty', function (){
+    var e1 = Observable.empty();
+    var expected = '|';
+    expectObservable(e1.switchLatest(function(value) {
+      return Observable.of(value);
+    })).toBe(expected);
+  });
+  
+  it('should handle outer never', function (){
+    var e1 = Observable.never();
+    var expected = '----';
+    expectObservable(e1.switchLatest(function(value) {
+      return Observable.of(value);
+    })).toBe(expected);
+  });
+  
+  it('should handle outer throw', function (){
+    var e1 = Observable.throw(new Error('wah'));
+    var expected = '#';
+    expectObservable(e1.switchLatest(function(value) {
+      return Observable.of(value);
+    })).toBe(expected, undefined, new Error('wah'));
+  });
+  
+  it('should handle outer error', function (){
+    var x =   cold(         '--a--b--c--d--e--|')
+    var e1 =   hot('---------x---------#', undefined, new Error('boo-hoo'));
+    var expected = '-----------a--b--c-#';
+    
+    var observableLookup = { x: x };
+    
+    expectObservable(e1.switchLatest(function(value) {
+      return observableLookup[value];
+    })).toBe(expected, undefined, new Error('boo-hoo'));    
+  });
+  
+  it('should switch with resultSelector goodness', function (){
+    var x =   cold(         '--a--b--c--d--e--|')
+    var y =   cold(                   '---f---g---h---i--|');
+    var e1 =   hot('---------x---------y---------|');
+    var expected = '-----------a--b--c----f---g---h---i--|';
+    
+    var observableLookup = { x: x, y: y };
+    
+    var expectedValues = {
+      a: ['a', 'x', 0, 0],
+      b: ['b', 'x', 1, 0],
+      c: ['c', 'x', 2, 0],
+      f: ['f', 'y', 0, 1],
+      g: ['g', 'y', 1, 1],
+      h: ['h', 'y', 2, 1],
+      i: ['i', 'y', 3, 1]
+    };
+    
+    expectObservable(e1.switchLatest(function(value) {
+      return observableLookup[value];
+    }, function(innerValue, outerValue, innerIndex, outerIndex) {
+      return [innerValue, outerValue, innerIndex, outerIndex];
+    })).toBe(expected, expectedValues);    
+  })
 });
--- src/operators/switchLatest.ts
+++ src/operators/switchLatest.ts
@@ -3,7 +3,8 @@ import Observer from '../Observer';
 import Observable from '../Observable';
 import Subscriber from '../Subscriber';
 import Subscription from '../Subscription';
-
+import tryCatch from '../util/tryCatch';
+import { errorObject } from '../util/errorObject';
 import { FlatMapSubscriber } from './flatMap-support';
 
 export default function switchLatest<T, R, R2>(project: (value: T, index: number) => Observable<R>,
@@ -21,13 +22,77 @@ class SwitchLatestOperator<T, R, R2> implements Operator<T, R> {
   }
 }
 
-class SwitchLatestSubscriber<T, R, R2> extends FlatMapSubscriber<T, R, R2> {
+class SwitchLatestSubscriber<T, R, R2> extends Subscriber<T> {
 
-  innerSubscription: Subscription<T>;
-
+  private innerSubscription: Subscription<T>;
+  private hasCompleted = false;
+  index: number = 0;
+  
   constructor(destination: Observer<T>,
-              project: (value: T, index: number) => Observable<R>,
-              resultSelector?: (innerValue: R, outerValue: T, innerIndex: number, outerIndex: number) => R2) {
-    super(destination, project, resultSelector, 1);
+              private project: (value: T, index: number) => Observable<R>,
+              private resultSelector?: (innerValue: R, outerValue: T, innerIndex: number, outerIndex: number) => R2) {
+    super(destination);
+  }
+  
+  _next(value: any) {
+    const index = this.index++;
+    const destination = this.destination; 
+    let result = tryCatch(this.project)(value, index);
+    if(result === errorObject) {
+      destination.error(result.e);
+    } else {
+      const innerSubscription = this.innerSubscription;
+      if(innerSubscription) {
+        innerSubscription.unsubscribe();
+      }
+      this.add(this.innerSubscription = result.subscribe(new InnerSwitchLatestSubscriber(destination, this, this.resultSelector, index, value)))
+    }
+  }
+  
+  _complete() {
+    const innerSubscription = this.innerSubscription;
+    this.hasCompleted = true;
+    if(!innerSubscription || innerSubscription.isUnsubscribed) {
+      this.destination.complete();
+    }
+  }
+  
+  notifyComplete(innerSub: Subscription<R>) {
+    this.remove(innerSub);
+    this.innerSubscription = null;
+    if(this.hasCompleted) {
+      this.destination.complete();
+    }
+  }
+}
+
+class InnerSwitchLatestSubscriber<T, R, R2> extends Subscriber<T> {
+  private index: number = 0;
+  
+  constructor(destination: Observer<T>, private parent: SwitchLatestSubscriber<T, R, R2>, 
+    private resultSelector: (innerValue: R, outerValue: T, innerIndex: number, outerIndex: number) => R2,
+    private outerIndex: number,
+    private outerValue: any) {
+    super(destination);
+  }
+  
+  _next(value: T) {
+    const destination = this.destination;
+    const index = this.index++;
+    const resultSelector = this.resultSelector;
+    if(resultSelector) {
+      let result = tryCatch(resultSelector)(value, this.outerValue, index, this.outerIndex);
+      if(result === errorObject) {
+        destination.error(result.e);
+      } else {
+        destination.next(result);
+      }
+    } else {
+      destination.next(value);
+    }
+  }
+  
+  _complete() {
+    this.parent.notifyComplete(this);
   }
 }
