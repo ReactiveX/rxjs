556e2e6e63a75be98a5d2c258203adce7f780561
kwonoj <kwon.ohjoong@gmail.com>
2015-09-18 00:32:09 -0700

    chore(test): add micro-benchmark performance test for Observable.never

---
+++ perf/micro/immediate-scheduler/observable/never.js
@@ -0,0 +1,20 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function never(suite) {
+
+    var oldNeverWithImmediateScheduler = RxOld.Observable.never();
+    var newNeverWithImmediateScheduler = RxNew.Observable.never();
+
+    // add tests
+    return suite
+        .add('old never with immediate scheduler', function () {
+            oldNeverWithImmediateScheduler.subscribe(_next, _error, _complete);
+        })
+        .add('new never with immediate scheduler', function () {
+            newNeverWithImmediateScheduler.subscribe(_next, _error, _complete);
+        });
+    function _next(x) { }
+    function _error(e){ }
+    function _complete(){ }
+};
