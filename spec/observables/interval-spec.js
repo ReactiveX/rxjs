e2cf5b10bcf94e5111137aab70dc68260187173f
Ben Lesh <blesh@netflix.com>
2015-09-17 21:00:21 -0700

    chore(tests): move bufferToggle and interval tests over to marble tests

--- spec/observables/interval-spec.js
+++ spec/observables/interval-spec.js
@@ -5,22 +5,8 @@ var immediate = Rx.Scheduler.immediate;
 var Observer = Rx.Observer;
 
 describe('Observable.interval', function () {
-  it('should next 5 times then complete', function (done) {
-    var start = Date.now();
-    var expected = [0, 1, 2, 3, 4];
-    var i = 0;
-    var nextSpy = jasmine.createSpy('nextSpy');
-
-    Observable.interval(10).take(5)
-      .subscribe(nextSpy, null,
-        function () {
-          var now = Date.now();
-          expect(now - start >= 50).toBe(true, 'interval ended in ' + (now - start) + 'ms');
-          expect(nextSpy.calls.count()).toBe(5);
-          expected.forEach(function (v) {
-            expect(nextSpy.calls.argsFor(v)[0]).toBe(v);
-          });
-          done();
-        });
+  it('should set up an interval', function() {
+    var expected = '----------0---------1---------2---------3---------4---------5---------6-----';
+    expectObservable(Observable.interval(100, rxTestScheduler)).toBe(expected, [0,1,2,3,4,5,6]);
   });
 });
--- spec/operators/bufferToggle-spec.js
+++ spec/operators/bufferToggle-spec.js
@@ -3,14 +3,24 @@ var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.bufferToggle', function () {
-  it('should emit buffers that are opened by an observable from the first argument and closed by an observable returned by the function in the second argument', function (done) {
-    Observable.interval(100).take(10)
-      .bufferToggle(Observable.timer(320).mapTo('test'), function (n) {
-        expect(n).toBe('test');
-        return Observable.timer(320);
-      })
-      .subscribe(function (w) {
-        expect(w).toEqual([3, 4, 5])
-      }, null, done);
-  }, 2000);
+  it('should emit buffers that are opened by an observable from the first argument and closed by an observable returned by the function in the second argument', function () {
+    var e1 =   hot('-----a----b----c----d----e----f----g----h----i----|')
+    var e2 =  cold('-------------x-------------y--------------z-------|');
+    var e3 =               cold('---------------(j|)');
+    //                                         ---------------|
+    //                                                        --------|
+    var expected = '----------------------------q-------------r-------(s|)';
+    
+    var values = {
+      q: ['c','d','e'],
+      r: ['f','g','h'],
+      s: ['i']
+    };
+    var innerVals = ['x', 'y', 'z'];
+    
+    expectObservable(e1.bufferToggle(e2, function(x) {
+      expect(x).toBe(innerVals.shift());
+      return e3;
+    })).toBe(expected, values);
+  });
 });
