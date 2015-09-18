c627d3f64d719e0bbda6579f735efff2d34ea773
kwonoj <kwon.ohjoong@gmail.com>
2015-09-17 22:08:23 -0700

    chore(test): use test marbles for takeUntil operator spec

--- spec/operators/takeUntil-spec.js
+++ spec/operators/takeUntil-spec.js
@@ -1,21 +1,13 @@
-/* globals describe, it, expect */
+/* globals describe, it, expect, hot, cold, expectObservable */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.takeUntil()', function () {
-  it('should take values until another Observable emits', function (done) {
-    var expected = [0, 1, 2, 3, 4];
-    var i = 0;
-    var nextSpy = jasmine.createSpy('nextSpy');
-
-    Observable.timer(0, 100)
-      .takeUntil(Observable.timer(450))
-      .subscribe(nextSpy, null, function () {
-        expect(nextSpy.calls.count()).toBe(5);
-        expected.forEach(function (v) {
-          expect(nextSpy.calls.argsFor(v)[0]).toBe(v);
-        });
-        done();
-      });
+  it('should take values until another Observable emits', function () {
+    var source = hot('--a--b--c--d--e--f--g--|');
+    var other =  hot('-------------z--|');
+    var expected =   '--a--b--c--d-|'; 
+    
+    expectObservable(source.takeUntil(other)).toBe(expected);
   });
 });
