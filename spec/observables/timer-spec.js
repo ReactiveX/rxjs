2888dda9f2f33dfddae5c49dad5b58b7865b548a
kwonoj <kwon.ohjoong@gmail.com>
2015-09-20 14:22:34 -0700

    chore(test): use test marbles for Observable.timer spec

--- spec/observables/timer-spec.js
+++ spec/observables/timer-spec.js
@@ -1,20 +1,15 @@
-/* globals describe, it, expect, spyOn */
+/* globals describe, it, expect, expectObservable, hot, rxTestScheduler */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 var immediate = Rx.Scheduler.immediate;
 var Observer = Rx.Observer;
 
 describe('Observable.timer', function () {
-  it('schedule a value of 0 then complete', function (done) {
-    var start = Date.now();
-    Observable.timer(100)
-      .subscribe(function (x) {
-          expect(x).toBe(0);
-        }, null,
-        function () {
-          var now = Date.now();
-          expect(now - start >= 100).toBe(true, 'timer fired in ' + (now - start) + 'ms');
-          done();
-        });
+  it('should schedule a value of 0 then complete', function () {
+    
+    var source = Observable.timer(50, undefined, rxTestScheduler);
+    var expected = '-----(x|)';
+    
+    expectObservable(source).toBe(expected, {x: 0});
   });
 });
