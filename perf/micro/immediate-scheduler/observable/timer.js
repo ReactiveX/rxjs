b9ab56ef0e3e39ac9be53612af6a4232fb859aca
kwonoj <kwon.ohjoong@gmail.com>
2015-09-20 01:02:54 -0700

    chore(test): add micro-benchmark performance test for Observable.timer

---
+++ perf/micro/immediate-scheduler/observable/timer.js
@@ -0,0 +1,19 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function timer(suite) {
+
+    var oldTimerWithImmediateScheduler = RxOld.Observable.timer(25, RxOld.Scheduler.immediate).take(5);
+    var newTimerWithImmediateScheduler = RxNew.Observable.timer(25).take(5);
+
+    return suite
+        .add('old timer with immediate scheduler', function () {
+            oldTimerWithImmediateScheduler.subscribe(_next, _error, _complete);
+        })
+        .add('new timer with immediate scheduler', function () {
+            newTimerWithImmediateScheduler.subscribe(_next, _error, _complete);
+        });
+    function _next(x) { }
+    function _error(e){ }
+    function _complete(){ }
+};
