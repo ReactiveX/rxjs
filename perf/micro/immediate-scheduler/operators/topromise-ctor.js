5e8be3892531c14a7b650fda3c796fe6d75c7a3a
kwonoj <kwon.ohjoong@gmail.com>
2015-09-20 00:44:12 -0700

    chore(test): add micro-benchmark performance test for toPromise operator

---
+++ perf/micro/immediate-scheduler/operators/topromise-ctor.js
@@ -0,0 +1,18 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function (suite) {
+
+  var oldToPromiseWithImmediateScheduler = RxOld.Observable.of(25, RxOld.Scheduler.immediate).toPromise(Promise);
+  var newToPromiseWithImmediateScheduler = RxNew.Observable.of(25).toPromise(Promise);
+
+  return suite
+    .add('old toPromise(ctor) with immediate scheduler', function () {
+      oldToPromiseWithImmediateScheduler.then(_then);
+    })
+    .add('new toPromise(ctor) with immediate scheduler', function () {
+      newToPromiseWithImmediateScheduler.then(_then);
+    });
+  
+  function _then(x) { }
+};
---
+++ perf/micro/immediate-scheduler/operators/topromise.js
@@ -0,0 +1,18 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function (suite) {
+
+  var oldToPromiseWithImmediateScheduler = RxOld.Observable.of(25, RxOld.Scheduler.immediate).toPromise();
+  var newToPromiseWithImmediateScheduler = RxNew.Observable.of(25).toPromise();
+
+  return suite
+    .add('old toPromise() with immediate scheduler', function () {
+      oldToPromiseWithImmediateScheduler.then(_then);
+    })
+    .add('new toPromise() with immediate scheduler', function () {
+      newToPromiseWithImmediateScheduler.then(_then);
+    });
+  
+  function _then(x) { }
+};
