d7435f59e30b126cb05d98336a0efe34f9e91bde
kwonoj <kwon.ohjoong@gmail.com>
2015-09-22 00:27:38 -0700

    chore(test): add micro-benchmark performance test for Observable.interval

---
+++ perf/micro/immediate-scheduler/observable/interval.js
@@ -0,0 +1,19 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function interval(suite) {
+
+  var oldIntervalWithImmediateScheduler = RxOld.Observable.interval(25, RxOld.Scheduler.immediate).take(5);
+  var newIntervalWithImmediateScheduler = RxNew.Observable.interval(25).take(5);
+
+  return suite
+      .add('old interval with immediate scheduler', function () {
+          oldIntervalWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new interval with immediate scheduler', function () {
+          newIntervalWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+  function _next(x) { }
+  function _error(e){ }
+  function _complete(){ }
+};
