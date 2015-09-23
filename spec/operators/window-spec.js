cf67e31930d5112cfab540c57fb9608bffcaf114
kwonoj <kwon.ohjoong@gmail.com>
2015-09-23 01:17:03 -0700

    chore(test): use test marbles for window operator spec

--- spec/operators/window-spec.js
+++ spec/operators/window-spec.js
@@ -1,20 +1,13 @@
-/* globals describe, it, expect */
+/* globals describe, it, expect, expectObservable, hot, rxTestScheduler */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.window', function () {
-  it('should emit windows that close and reopen', function (done) {
-    var expected = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8]
-    ];
-    Observable.interval(100)
-      .window(Observable.interval(320))
-      .take(3)
-      .mergeMap(function (x) { return x.toArray(); })
-      .subscribe(function (w) {
-        expect(w).toEqual(expected.shift())
-      }, null, done);
-  }, 2000);
+  it('should emit windows that close and reopen', function () {
+    var source = hot('---a---b---c---d---e---f---g---h---i---|');
+    var expected =   '-------------x------------y------------(z|)';
+    
+    expectObservable(source.window(Observable.interval(130, rxTestScheduler)).mergeMap(function (x) { return x.toArray(); }))
+      .toBe(expected, {x: ['a','b','c'], y: ['d','e','f'], z: ['g','h','i'] });
+  });
 });
