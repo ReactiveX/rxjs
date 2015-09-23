fef727ccc76f94947e96c0719fb15cb30b7cd765
kwonoj <kwon.ohjoong@gmail.com>
2015-09-23 01:07:12 -0700

    chore(test): use test marbles for reduce operator spec

--- spec/operators/reduce-spec.js
+++ spec/operators/reduce-spec.js
@@ -1,15 +1,16 @@
-/* globals describe, it, expect */
+/* globals describe, it, expect, hot, cold, expectObservable */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.reduce()', function () {
-  it('should reduce', function (done) {
-    Observable.of('1', '2', '3').reduce(function (o, x) {
+  it('should reduce', function () {
+    var source = hot('--a--b--c--|');
+    var expected =   '-----------(x|)';
+    
+    var reduceFunction = function (o, x) {
       return o + x;
-    }, '')
-    .subscribe(function (x) {
-        expect(x).toBe('123');
-      },
-      null, done);
+    };
+    
+    expectObservable(source.reduce(reduceFunction, '')).toBe(expected, {x: 'abc'});
   });
 });
