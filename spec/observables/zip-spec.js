7474d9f02d6c717e955d53edfcb84e5e7bfcefe9
Ben Lesh <blesh@netflix.com>
2015-09-24 13:53:14 -0700

    fix(zip): ensure zip observable completes properly
    - adds additional tests
    - outerIndex is now public on InnerSubscriber
    - updates zip to complete when any inner observable has completed and its buffer is empty
    
    relates to #397

--- spec/observables/zip-spec.js
+++ spec/observables/zip-spec.js
@@ -2,7 +2,7 @@
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
-describe('Observable.zip', function(){
+fdescribe('Observable.zip', function(){
   it('should zip the provided observables', function(done) {
     var expected = ['a1', 'b2', 'c3'];
     var i = 0;
@@ -18,4 +18,32 @@ describe('Observable.zip', function(){
       expect(x).toBe(expected[i++])
     }, null, done);
   });
+  
+  it('should end once one observable completes and its buffer is empty', function (){
+    var e1 =   hot('---a--b--c--|');
+    var e2 =   hot('------d----e----f--------|');
+    var e3 =   hot('--------h----i----j-------------'); // doesn't complete
+    var expected = '--------x----y----(z|)'; // e1 complete and buffer empty
+    var values = {
+      x: ['a','d','h'],
+      y: ['b','e','i'],
+      z: ['c','f','j']
+    };
+    expectObservable(Observable.zip(e1,e2,e3)).toBe(expected, values);
+  });
+  
+  
+  it('should end once one observable nexts and zips value from completed other observable whose buffer is empty', function (){
+    var e1 =   hot('---a--b--c--|');
+    var e2 =   hot('------d----e----f|');
+    var e3 =   hot('--------h----i----j-------------'); // doesn't complete
+    var expected = '--------x----y----(z|)'; // e2 buffer empty and signaled complete
+    var values = {
+      x: ['a','d','h'],
+      y: ['b','e','i'],
+      z: ['c','f','j']
+    };
+    expectObservable(Observable.zip(e1,e2,e3)).toBe(expected, values);
+  });
+  
 });
