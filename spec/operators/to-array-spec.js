58b91fb7fc198ea4de38893f811e1ffb6317c40f
Adriano Melo <adriano@adrianomelo.com>
2015-09-23 22:14:49 -0300

    test(toArray): add marble tests for toArray
    Refactor test with multiple values and add missing tests for toArray operator.

--- spec/operators/to-array-spec.js
+++ spec/operators/to-array-spec.js
@@ -1,13 +1,61 @@
-/* globals describe, it, expect */
+/* globals describe, it, expect, expectObservable, hot */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('toArray', function () {
-  it('should reduce the values of an observable into an array', function (done) {
-    Observable.fromArray([1, 2, 3])
-      .toArray()
-      .subscribe(function (arr) {
-          expect(arr).toEqual([1, 2, 3]);
-        }, null, done);
+  it('should be never when source is never', function () {
+    var e1 = Observable.never();
+    var expected = '-';
+
+    expectObservable(e1.toArray()).toBe(expected);
+  });
+
+  it('should be never when source is empty', function () {
+    var e1 = Observable.empty();
+    var expected = '(w|)';
+
+    expectObservable(e1.toArray()).toBe(expected, { w: [] });
+  });
+
+  it('should be never when source doesn\'t complete', function () {
+    var e1 = hot('--x--^--y--');
+    var expected = '-';
+
+    expectObservable(e1.toArray()).toBe(expected);
+  });
+
+  it('should reduce observable without values into an array of length zero', function () {
+    var e1 = hot('-x-^---|');
+    var expected =  '----(w|)';
+
+    expectObservable(e1.toArray()).toBe(expected, { w: [] });
+  });
+
+  it('should reduce the a single value of an observable into an array', function () {
+    var e1 = hot('-x-^--y--|');
+    var expected =  '------(w|)';
+
+    expectObservable(e1.toArray()).toBe(expected, { w: ['y'] });
+  });
+
+  it('should reduce the values of an observable into an array', function () {
+    var e1 = hot('-x-^--y--z--|');
+    var expected =  '---------(w|)';
+
+    expectObservable(e1.toArray()).toBe(expected, { w: ['y', 'z'] });
+  });
+
+  it('should work with error', function () {
+    var e1 = hot('-x-^--y--z--#', { x: 1, y: 2, z: 3 }, 'too bad');
+    var expected =  '---------#';
+
+    expectObservable(e1.toArray()).toBe(expected, null, 'too bad');
+  });
+
+  it('should work with throw', function () {
+    var e1 = Observable.throw(new Error('too bad'));
+    var expected = '#';
+
+    expectObservable(e1.toArray()).toBe(expected, null, new Error('too bad'));
   });
 });
