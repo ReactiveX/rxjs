1d88a868877779d2e77c3b26206dc50df391f6cf
kwonoj <kwon.ohjoong@gmail.com>
2015-09-26 23:59:02 -0700

    chore(test): use test marbles for sample, sampleTime operator spec

--- spec/operators/sample-spec.js
+++ spec/operators/sample-spec.js
@@ -1,26 +1,69 @@
-/* globals describe, it, expect */
+/* globals describe, it, expect, expectObservable, hot */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.sample', function () {
-  it('should get samples when the notifier emits', function (done) {
-    var expected = [1, 3, 5];
-    Observable.interval(100)
-      .sample(Observable.interval(220))
-      .take(3)
-      .subscribe(function (x) {
-        expect(x).toBe(expected.shift());
-      }, null, done);
-  }, 2000);
-  
-  it('should not complete when the notifier completes, nor should it emit', function (done) {
-    Observable.interval(100)
-      .sample(Observable.timer(220))
-      .subscribe(function (x) {
-        expect(x).toBe(1);
-        setTimeout(done, 500);
-      }, null, function () {
-        throw 'should not be called';
-      });
+  it('should get samples when the notifier emits', function () {
+    var e1 =   hot('----a----b----c----d----e----f----|');
+    var e2 =   hot('-----------x----------x----------x----------|');
+    var expected = '-----------b----------d----------f|';
+    
+    expectObservable(e1.sample(e2)).toBe(expected);
   });
+  
+  it('should not complete when the notifier completes, nor should it emit', function () {
+    var e1 =   hot('----a----b----c----d----e----f----');
+    var e2 =   hot('------x-|');
+    var expected = '------a-'
+    
+    expectObservable(e1.sample(e2)).toBe(expected);
+  });
+  
+  it('should raise error if source raises error', function() {
+    var e1 =   hot('----a----b----c----d----#');
+    var e2 =   hot('-----------x----------x----------x----------|');
+    var expected = '-----------b----------d-#';
+    
+    expectObservable(e1.sample(e2)).toBe(expected);
+  });
+  
+  it('should completes if source does not emits', function() {
+    var e1 = Observable.empty();
+    var e2 =   hot('------x-------|');
+    var expected = '|';
+    
+    expectObservable(e1.sample(e2)).toBe(expected);
+  });
+  
+  it('should raise error if source throws immediately', function() {
+    var e1 =   Observable.throw('error');
+    var e2 =   hot('------x-------|');
+    var expected = '#';
+    
+    expectObservable(e1.sample(e2)).toBe(expected);
+  });
+ 
+ it('should not completes if source does not complete', function() {
+   var e1 =   Observable.never();
+   var e2 =   hot('------x-------|');
+   var expected = '-';
+   
+   expectObservable(e1.sample(e2)).toBe(expected);
+ });
+ 
+ it('should sample only until source completes', function() {
+    var e1 =   hot('----a----b----c----d-|');
+    var e2 =   hot('-----------x----------x------------|');
+    var expected = '-----------b---------|';
+    
+    expectObservable(e1.sample(e2)).toBe(expected);
+ });
+ 
+ it('should complete sampling if sample observable completes', function () {
+    var e1 =   hot('----a----b----c----d-|');
+    var e2 =   hot('|');
+    var expected = '---------------------|';
+    
+    expectObservable(e1.sample(e2)).toBe(expected);
+ });
 });
--- spec/operators/sampleTime-spec.js
+++ spec/operators/sampleTime-spec.js
@@ -1,15 +1,40 @@
-/* globals describe, it, expect */
+/* globals describe, it, expect, expectObservable, hot, rxTestScheduler */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.sampleTime', function () {
-  it('should get samples on a delay', function (done) {
-    var expected = [1, 3, 5];
-    Observable.interval(100)
-      .sampleTime(220)
-      .take(3)
-      .subscribe(function (x) {
-        expect(x).toBe(expected.shift());
-      }, null, done);
-  }, 2000);
+  it('should get samples on a delay', function() {
+    var e1 =   hot('----a----b----c----d----e----f----|');
+    var expected = '-----------b----------d----------f|';
+    
+    expectObservable(e1.sampleTime(110, rxTestScheduler)).toBe(expected);
+  });
+
+  it('should raise error if source raises error', function() {
+    var e1 =   hot('----a----b----c----d----#');
+    var expected = '-----------b----------d-#';
+    
+    expectObservable(e1.sampleTime(110, rxTestScheduler)).toBe(expected);
+  });
+  
+  it('should completes if source does not emits', function() {
+    var e1 = Observable.empty();
+    var expected = '|';
+    
+    expectObservable(e1.sampleTime(60, rxTestScheduler)).toBe(expected);
+  });
+  
+  it('should raise error if source throws immediately', function() {
+    var e1 = Observable.throw('error');
+    var expected = '#';
+    
+    expectObservable(e1.sampleTime(60, rxTestScheduler)).toBe(expected);
+  });
+  
+  it('should not completes if source does not complete', function() {
+    var e1 =   Observable.never();
+    var expected = '-';
+   
+    expectObservable(e1.sampleTime(60, rxTestScheduler)).toBe(expected);
+  });
 });
