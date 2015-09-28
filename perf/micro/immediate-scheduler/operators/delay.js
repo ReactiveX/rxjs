e4c287e146af9a129859f8d2a1047921c230a6dc
kwonoj <kwon.ohjoong@gmail.com>
2015-09-27 17:33:34 -0700

    chore(test): add micro-benchmark performance test for delay operator

---
+++ perf/micro/immediate-scheduler/operators/delay.js
@@ -0,0 +1,20 @@
+var RxOld = require("rx");
+var RxNew = require("../../../../index");
+
+module.exports = function (suite) {
+    
+    var oldDelayWithImmediateScheduler = RxOld.Observable.of(25, RxOld.Scheduler.immediate).delay(25);
+    var newDelayWithImmediateScheduler = RxNew.Observable.of(25).delay(25);
+
+    return suite
+        .add('old delay with immediate scheduler', function () {
+            oldDelayWithImmediateScheduler.subscribe(_next, _error, _complete);
+        })
+        .add('new delay with immediate scheduler', function () {
+            newDelayWithImmediateScheduler.subscribe(_next, _error, _complete);
+        });
+
+    function _next(x) { }
+    function _error(e){ }
+    function _complete(){ }
+};
