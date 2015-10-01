42d1add2d5fe7471e77a1473d305cac8364f761c
Andre Medeiros <andre.medeiros@futurice.com>
2015-10-01 17:21:03 +0300

    feat(count): add predicate support in count()
    Add a predicate function argument (and a thisArg argument) to count()
    operator. Also add respective tests that mirror RxJS 4 tests.
    
    Resolves issue #425.

--- src/operators/count.ts
+++ src/operators/count.ts
@@ -1,27 +1,60 @@
+import Observable from '../Observable';
 import Operator from '../Operator';
 import Observer from '../Observer';
 import Subscriber from '../Subscriber';
 
-export default function count() {
-  return this.lift(new CountOperator());
+import tryCatch from '../util/tryCatch';
+import {errorObject} from '../util/errorObject';
+import bindCallback from '../util/bindCallback';
+
+export default function count<T>(predicate?: (value: T,
+                                              index: number,
+                                              source: Observable<T>) => boolean,
+                                 thisArg?: any): Observable<T> {
+  return this.lift(new CountOperator(predicate, thisArg, this));
 }
 
 class CountOperator<T, R> implements Operator<T, R> {
-  call(subscriber: Subscriber<number>): Subscriber<T> {
-    return new CountSubscriber<T>(subscriber);
+  constructor(private predicate?: (value: T, index: number, source: Observable<T>) => boolean,
+              private thisArg?: any,
+              private source?: Observable<T>) {
+  }
+
+  call(subscriber: Subscriber<R>): Subscriber<T> {
+    return new CountSubscriber<T>(
+      subscriber, this.predicate, this.thisArg, this.source
+    );
   }
 }
 
 class CountSubscriber<T> extends Subscriber<T> {
+  private predicate: Function;
+  private count: number = 0;
+  private index: number = 0;
 
-  count: number = 0;
-
-  constructor(destination: Subscriber<number>) {
+  constructor(destination: Observer<T>,
+              predicate?: (value: T, index: number, source: Observable<T>) => boolean,
+              private thisArg?: any,
+              private source?: Observable<T>) {
     super(destination);
+    if (typeof predicate === 'function') {
+      this.predicate = bindCallback(predicate, thisArg, 3);
+    }
   }
 
-  _next(x) {
-    this.count += 1;
+  _next(value: T) {
+    const predicate = this.predicate;
+    let passed: any = true;
+    if (predicate) {
+      passed = tryCatch(predicate)(value, this.index++, this.source);
+      if (passed === errorObject) {
+        this.destination.error(passed.e);
+        return;
+      }
+    }
+    if (passed) {
+      this.count += 1;
+    }
   }
 
   _complete() {
