b66592d2d8ed62462b582b193107f84101d9a853
Adriano Melo <adriano@adrianomelo.com>
2015-10-02 23:32:38 -0300

    fix(buffer): change behavior of buffer to more closely match RxJS 4
    The behavior of buffer operator was different from RxJS 4. This patch changes the behavior of buffer and adds tests from RxJS 4 and tests with never, empty and error.

--- src/operators/buffer.ts
+++ src/operators/buffer.ts
@@ -8,13 +8,11 @@ import {errorObject} from '../util/errorObject';
 import bindCallback from '../util/bindCallback';
 
 /**
- * buffers the incoming observable values until the passed `closingNotifier`
- * emits a value, at which point it emits the buffer on the returned observable
- * and starts a new buffer internally, awaiting the next time `closingNotifier`
- * emits.
- *
- * @param {Observable<any>} closingNotifier an observable, that signals the buffer
- * to be emitted from the returned observable
+ * buffers the incoming observable values until the passed `closingNotifier` emits a value, at which point
+ * it emits the buffer on the returned observable and starts a new buffer internally, awaiting the
+ * next time `closingNotifier` emits
+ * 
+ * @param {Observable<any>} closingNotifier an observable, that signals the buffer to be emitted from the returned observable
  * @returns {Observable<T[]>} an observable of buffers, which are arrays of values
  */
 export default function buffer<T>(closingNotifier: Observable<any>): Observable<T[]> {
@@ -33,32 +31,28 @@ class BufferOperator<T, R> implements Operator<T, R> {
 
 class BufferSubscriber<T> extends Subscriber<T> {
   buffer: T[] = [];
-
+  
   constructor(destination: Subscriber<T>, closingNotifier: Observable<any>) {
     super(destination);
     this.add(closingNotifier._subscribe(new BufferClosingNotifierSubscriber(this)));
   }
-
+  
   _next(value: T) {
     this.buffer.push(value);
   }
-
+  
   _error(err: any) {
     this.destination.error(err);
   }
-
+  
   _complete() {
-    this.flushBuffer();
     this.destination.complete();
   }
-
+  
   flushBuffer() {
     const buffer = this.buffer;
     this.buffer = [];
-
-    if (buffer.length > 0) {
-      this.destination.next(buffer);
-    }
+    this.destination.next(buffer);
   }
 }
 
@@ -66,16 +60,16 @@ class BufferClosingNotifierSubscriber<T> extends Subscriber<T> {
   constructor(private parent: BufferSubscriber<any>) {
     super(null);
   }
-
+  
   _next(value: T) {
     this.parent.flushBuffer();
   }
-
+  
   _error(err: any) {
     this.parent.error(err);
   }
-
+  
   _complete() {
-    // noop
+    this.parent.complete();
   }
 }
