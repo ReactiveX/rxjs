c05fdc93ca69b76e6923444957ff185f705f3b2a
Luís Gabriel Lima <lampih@gmail.com>
2015-10-05 18:59:01 -0300

    test(map): rewrite tests to use marble style
    related to #389.

--- spec/operators/map-spec.js
+++ spec/operators/map-spec.js
@@ -2,37 +2,33 @@
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
+// function shortcuts
+var addDrama = function (x) { return x + '!'; };
+
 describe('Observable.prototype.map()', function () {
-  it('should map one value', function (done) {
-    Observable.of(42).map(function (x) {
-      return x + '!';
-    })
-    .subscribe(function (x) {
-      expect(x).toBe('42!');
-    }, null, done);
+  it('should map one value', function () {
+    var a =   cold('--x--|', {x: 42});
+    var expected = '--y--|';
+
+    var r = a.map(addDrama);
+    expectObservable(r).toBe(expected, {y: '42!'});
   });
 
-  it('should map multiple values', function (done) {
-    var expected = ['1!', '2!', '3!'];
-    var i = 0;
-    Observable.fromArray([1, 2, 3]).map(function (x) {
-      return x + '!';
-    })
-    .subscribe(function (x) {
-      expect(x).toBe(expected[i++]);
-    },
-    null, done);
+  it('should map multiple values', function () {
+    var a =   cold('--1--2--3--|');
+    var expected = '--x--y--z--|';
+
+    var r = a.map(addDrama);
+    expectObservable(r).toBe(expected, {x: '1!', y: '2!', z: '3!'});
   });
 
-  it('should send errors down the error path', function (done) {
-    Observable.of(42).map(function (x) { throw 'bad'; })
-      .subscribe(function (x) {
-        expect(true).toBe(false);
-      }, function (err) {
-        expect(err).toBe('bad');
-        done();
-      }, function () {
-        expect(true).toBe(false);
-      });
+  it('should send errors down the error path', function () {
+    var a =   cold('--x--|', {x: 42});
+    var expected = '--#';
+
+    var r = a.map(function (x) {
+      throw 'too bad';
+    });
+    expectObservable(r).toBe(expected, null, 'too bad');
   });
 });
