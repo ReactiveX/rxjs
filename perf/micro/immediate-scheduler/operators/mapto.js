0f6ede6a1ffbbf2a6ea0cc605a77b9cd0194d733
kwonoj <kwon.ohjoong@gmail.com>
2015-10-07 20:49:01 -0700

    test(mapTo): add micro-benchmark performance test for mapTo operator
    relates to #350

---
+++ perf/micro/immediate-scheduler/operators/mapto.js
@@ -0,0 +1,20 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  var object = { value: 25 };
+  var oldMapToWithImmediateScheduler = RxOld.Observable.range(0, 50, RxOld.Scheduler.immediate)
+    .map(object);
+  var newMapToWithImmediateScheduler = RxNew.Observable.range(0, 50).mapTo(object);
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old select(object) with immediate scheduler', function () {
+        oldMapToWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new mapTo() with immediate scheduler', function () {
+        newMapToWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+};
