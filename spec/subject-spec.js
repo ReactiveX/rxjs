9bbf04f158f2c04985e69b6d4808857ef2dc70de
Ben Lesh <blesh@netflix.com>
2015-10-08 12:15:00 -0700

    test(Subject): add test for basic Subject.create call
    related #483

--- spec/subject-spec.js
+++ spec/subject-spec.js
@@ -3,6 +3,7 @@ var Rx = require('../dist/cjs/Rx');
 
 var Subject = Rx.Subject;
 var nextTick = Rx.Scheduler.nextTick;
+var Observable = Rx.Observable;
 
 describe('Subject', function () {
   it('should pump values right on through itself', function (done) {
@@ -12,7 +13,7 @@ describe('Subject', function () {
     subject.subscribe(function (x) {
       expect(x).toBe(expected.shift());
     }, null, done);
-    
+
     subject.next('foo');
     subject.next('bar');
     subject.complete();
@@ -31,7 +32,7 @@ describe('Subject', function () {
     subject.subscribe(function (x) {
       expect(x).toBe(expected[j++]);
     }, null, done);
-    
+
     expect(subject.observers.length).toBe(2);
     subject.next('foo');
     subject.next('bar');
@@ -45,7 +46,7 @@ describe('Subject', function () {
     subject.subscribe(function (x) {
       expect(x).toBe(expected.shift());
     }, null, done);
-  
+
     subject.next('foo');
     subject.complete();
     subject.next('bar');
@@ -53,18 +54,63 @@ describe('Subject', function () {
 
   it('should clean out unsubscribed subscribers', function (done) {
     var subject = new Subject();
-    
+
     var sub1 = subject.subscribe(function (x) {
     });
-    
+
     var sub2 = subject.subscribe(function (x) {
     });
-    
+
     expect(subject.observers.length).toBe(2);
     sub1.unsubscribe();
     expect(subject.observers.length).toBe(1);
     sub2.unsubscribe();
     expect(subject.observers.length).toBe(0);
     done();
+  });
+
+  it('should have a static create function that works', function() {
+    expect(typeof Subject.create).toBe('function');
+    var source = Observable.of(1,2,3,4,5);
+    var nexts = [];
+    var error;
+    var complete = false;
+    var output = [];
+    var outputComplete = false;
+
+    var destination = {
+      isUnsubscribed: false,
+      next: function(x) {
+        nexts.push(x);
+      },
+      error: function(err) {
+        error = err;
+        this.isUnsubscribed = true;
+      },
+      complete: function() {
+        complete = true;
+        this.isUnsubscribed = true;
+      }
+    };
+
+    var sub = Subject.create(source, destination);
+
+    sub.subscribe(function(x) {
+      output.push(x);
+    }, null, function() {
+      outputComplete = true;
+    });
+
+    sub.next('a');
+    sub.next('b');
+    sub.next('c');
+    sub.complete();
+
+    expect(nexts).toEqual(['a','b','c']);
+    expect(complete).toBe(true);
+    expect(error).toBe(undefined);
+
+    expect(output).toEqual([1,2,3,4,5]);
+    expect(outputComplete).toBe(true);
   });
 });
