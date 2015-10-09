b3f683a903f74a4caf75eb8406fdb864c12cdb92
Ben Lesh <blesh@netflix.com>
2015-10-09 11:29:35 -0700

    test(windowTime): convert tests to primitive marble tests

--- spec/operators/windowTime-spec.js
+++ spec/operators/windowTime-spec.js
@@ -1,35 +1,64 @@
-/* globals describe, it, expect */
-var Rx = require('../../dist/cjs/Rx');
+/* globals describe, it, expect, hot, expectObservable, rxTestScheduler */
+var Rx = require('../../dist/cjs/Rx.KitchenSink');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.windowTime', function () {
-  it('should emit windows at intervals', function (done) {
-    var expected = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8]
-    ];
-    Observable.interval(100)
-      .windowTime(320)
-      .take(3)
-      .mergeMap(function (x) { return x.toArray(); })
-      .subscribe(function (w) {
-        expect(w).toEqual(expected.shift());
-      }, null, done);
-  }, 2000);
+  it('should emit windows at intervals', function () {
+    var e1 = hot('--1--2--^--a--b--c--d--e--f--g--h--|');
+    //  100 frames        0---------1---------2------|
+    var expected =       'x---------y---------z------|';
+    var x =              '---a--b--c|';
+    var y =              '------------d--e--f-|';
+    var z =              '---------------------g--h--|';
 
-  it('should emit windows that have been created at intervals and close after the specified delay', function (done) {
-    var expected = [
-      [0, 1, 2, 3, 4],
-      [2, 3, 4, 5, 6],
-      [4, 5, 6, 7, 8]
-    ];
-    Observable.interval(100)
-      .windowTime(520, 220)
-      .take(3)
-      .mergeMap(function (x) { return x.toArray(); })
-      .subscribe(function (w) {
-        expect(w).toEqual(expected.shift());
-      }, null, done);
-  }, 2000);
+    var values = {
+      x: Rx.TestScheduler.parseMarbles(x),
+      y: Rx.TestScheduler.parseMarbles(y),
+      z: Rx.TestScheduler.parseMarbles(z)
+    };
+
+    var source = e1.windowTime(100, null, rxTestScheduler)
+      .map(function (obs) {
+        var arr = [];
+
+        obs.materialize().map(function (notification) {
+          return { frame: rxTestScheduler.frame, notification: notification };
+        }).subscribe(function (value) { arr.push(value); });
+
+        return arr;
+      });
+
+    expectObservable(source).toBe(expected, values);
+  });
+
+  it('should emit windows that have been created at intervals and close after the specified delay', function () {
+    var e1 = hot('--1--2--^--a--b--c--d--e--f--g--h--|');
+    //  100 frames        0---------1---------2------|
+    //  50                 ----|
+  //  50                           ----|
+    //  50                                     ----|
+    var expected =       'x---------y---------z------|';
+    var x =              '---a-|';
+    var y =              '------------d--(e|)';
+    var z =              '---------------------g--h|';
+
+    var values = {
+      x: Rx.TestScheduler.parseMarbles(x),
+      y: Rx.TestScheduler.parseMarbles(y),
+      z: Rx.TestScheduler.parseMarbles(z)
+    };
+
+    var source = e1.windowTime(50, 100, rxTestScheduler)
+      .map(function (obs) {
+        var arr = [];
+
+        obs.materialize().map(function (notification) {
+          return { frame: rxTestScheduler.frame, notification: notification };
+        }).subscribe(function (value) { arr.push(value); });
+
+        return arr;
+      });
+
+    expectObservable(source).toBe(expected, values);
+  });
 });
