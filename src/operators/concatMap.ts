4ebb0179377c0620c1974426272168f93f49b030
Ben Lesh <blesh@netflix.com>
2015-10-13 14:27:55 -0700

    docs(concatMap): basic docs

--- src/operators/concatMap.ts
+++ src/operators/concatMap.ts
@@ -1,7 +1,26 @@
 import Observable from '../Observable';
 import { MergeMapOperator } from './mergeMap-support';
 
-export default function concatMap<T, R>(project: (x: T, ix: number) => Observable<any>,
-                                        projectResult?: (x: T, y: any, ix: number, iy: number) => R) {
+/**
+ * Maps values from the source observable into new Observables, then merges them in a serialized fashion,
+ * waiting for each one to complete before merging the next.
+ *
+ * __Warning:__ if incoming values arrive endlessly and faster than the observables they're being mapped
+ * to can complete, it will result in memory issues as created observables amass in an unbounded buffer
+ * waiting for their turn to be subscribed to.
+ *
+ * @param {function} project a function to map incoming values into Observables to be concatenated. accepts
+ * the `value` and the `index` as arguments.
+ * @param {function} [projectResult] an optional result selector that is applied to values before they're
+ * merged into the returned observable. The arguments passed to this function are:
+ * - `outerValue`: the value that came from the source
+ * - `innerValue`: the value that came from the projected Observable
+ * - `outerIndex`: the "index" of the value that came from the source
+ * - `innerIndex`: the "index" of the value from the projected Observable
+ * @returns {Observable} an observable of values merged from the projected Observables as they were subscribed to,
+ * one at a time. Optionally, these values may have been projected from a passed `projectResult` argument.
+ */
+export default function concatMap<T, R>(project: (value: T, index: number) => Observable<any>,
+                                        projectResult?: (outerValue: T, innerValue: any, outerIndex: number, innerIndex: number) => R) {
   return this.lift(new MergeMapOperator(project, projectResult, 1));
 }
