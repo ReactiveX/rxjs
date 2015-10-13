d2583c29d80b085751e0dcb8a6bd4be07814baed
Ben Lesh <blesh@netflix.com>
2015-10-13 12:58:46 -0700

    docs(bufferWhen): add basic docs

--- src/operators/bufferWhen.ts
+++ src/operators/bufferWhen.ts
@@ -9,6 +9,12 @@ import tryCatch from '../util/tryCatch';
 import {errorObject} from '../util/errorObject';
 import bindCallback from '../util/bindCallback';
 
+/**
+ * Opens a buffer immediately, then closes the buffer when the observable returned by calling `closingSelector` emits a value.
+ * It that immediately opens a new buffer and repeats the process
+ * @param {function} a function that takes no arguments and returns an Observable that signals buffer closure
+ * @returns {Observable<T[]>} an observable of arrays of buffered values.
+ */
 export default function bufferWhen<T>(closingSelector: () => Observable<any>): Observable<T[]> {
   return this.lift(new BufferWhenOperator(closingSelector));
 }
