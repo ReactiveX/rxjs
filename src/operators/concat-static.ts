59a3f3ff4d1bc68ef0498c97b534323c26cc7c14
Ben Lesh <blesh@netflix.com>
2015-10-13 13:30:49 -0700

    docs(Observable.concat): add basic docs for Observable.concat

--- src/operators/concat-static.ts
+++ src/operators/concat-static.ts
@@ -4,6 +4,13 @@ import Scheduler from '../Scheduler';
 import immediate from '../schedulers/immediate';
 import { CoreOperators } from '../CoreOperators';
 
+/**
+ * Joins multiple observables together by subscribing to them one at a time and merging their results
+ * into the returned observable. Will wait for each observable to complete before moving on to the next.
+ * @params {...Observable} the observables to concatenate
+ * @params {Scheduler} [scheduler] an optional scheduler to schedule each observable subscription on.
+ * @returns {Observable} All values of each passed observable merged into a single observable, in order, in serial fashion.
+ */
 export default function concat<R>(...observables: (Observable<any>|Scheduler)[]): Observable<R> {
   let scheduler: Scheduler = immediate;
   let args = <any[]>observables;
