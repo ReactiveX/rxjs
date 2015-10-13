da6eaf9a2b2851a3678789e7abaea25045b2ade7
Ben Lesh <blesh@netflix.com>
2015-10-13 14:42:19 -0700

    docs(count): basic docs

--- src/operators/count.ts
+++ src/operators/count.ts
@@ -7,6 +7,20 @@ import tryCatch from '../util/tryCatch';
 import {errorObject} from '../util/errorObject';
 import bindCallback from '../util/bindCallback';
 
+/**
+ * Returns an observable of a single number that represents the number of items that either:
+ * Match a provided predicate function, _or_ if a predicate is not provided, the number
+ * represents the total count of all items in the source observable. The count is emitted
+ * by the returned observable when the source observable completes.
+ * @param {function} [predicate] a boolean function to select what values are to be counted.
+ * it is provided with arguments of:
+ *   - `value`: the value from the source observable
+ *   - `index`: the "index" of the value from the source observable
+ *   - `source`: the source observable instance itself.
+ * @param {any} [thisArg] the optional `this` context to use in the `predicate` function
+ * @returns {Observable} an observable of one number that represents the count as described
+ * above
+ */
 export default function count<T>(predicate?: (value: T,
                                               index: number,
                                               source: Observable<T>) => boolean,
