acf56ec707e0721642db7fac79a4b63ca2c57a61
Ben Lesh <blesh@netflix.com>
2015-10-13 15:04:14 -0700

    test(map): add map scalar micro performance test

---
+++ perf/micro/immediate-scheduler/operators/map-scalar.js
@@ -0,0 +1,25 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  function square(x) {
+    return x * x;
+  }
+
+  function double(x) {
+    return x + x;
+  }
+  var oldSelectWithImmediateScheduler = RxOld.Observable.just(42).map(square).map(double);
+  var newSelectWithImmediateScheduler = RxNew.Observable.of(42).map(square).map(double);
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old map over scalar with immediate scheduler', function () {
+        oldSelectWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new map over scalar with immediate scheduler', function () {
+        newSelectWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+};
