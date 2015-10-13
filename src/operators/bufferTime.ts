01e01989111e9352e9b5427021198c0cc16ee565
Ben Lesh <blesh@netflix.com>
2015-10-13 12:58:01 -0700

    docs(bufferTime): add basic docs for bufferTime

--- src/operators/bufferTime.ts
+++ src/operators/bufferTime.ts
@@ -11,6 +11,14 @@ import tryCatch from '../util/tryCatch';
 import {errorObject} from '../util/errorObject';
 import bindCallback from '../util/bindCallback';
 
+/**
+ * buffers values from the source for a specific time period. Optionally allows new buffers to be set up at an interval.
+ * @param {number} the amount of time to fill each buffer for before emitting them and clearing them.
+ * @param {number} [bufferCreationInterval] the interval at which to start new buffers.
+ * @param {Scheduler} [scheduler] (optional, defaults to `nextTick` scheduler) The scheduler on which to schedule the
+ *  intervals that determine buffer boundaries.
+ * @returns {Observable<T[]>} an observable of arrays of buffered values.
+ */
 export default function bufferTime<T>(bufferTimeSpan: number,
                                       bufferCreationInterval: number = null,
                                       scheduler: Scheduler = nextTick): Observable<T[]> {
