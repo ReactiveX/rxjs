559fd8a5d4aaee907e506f29ec6da5bb422972ba
Ben Lesh <blesh@netflix.com>
2015-10-13 16:29:43 -0700

    test(count): add micro perf test for count

---
+++ perf/micro/immediate-scheduler/operators/count.js
@@ -0,0 +1,18 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  var oldConcatWithImmediateScheduler = RxOld.Observable.range(0, 25, RxOld.Scheduler.immediate).count();
+  var newConcatWithImmediateScheduler = RxNew.Observable.range(0, 25).count();
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old count with immediate scheduler', function () {
+        oldConcatWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new count with immediate scheduler', function () {
+        newConcatWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+};
