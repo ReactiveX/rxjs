febfe6cefd0598239deb8e0f0ea019d4e5d28fa2
Ben Lesh <blesh@netflix.com>
2015-10-13 13:07:35 -0700

    docs(catch): add basic docs

--- src/operators/catch.ts
+++ src/operators/catch.ts
@@ -6,6 +6,14 @@ import Observable from '../Observable';
 import tryCatch from '../util/tryCatch';
 import {errorObject} from '../util/errorObject';
 
+/**
+ * Catches errors on the observable to be handled by returning a new observable or throwing an error.
+ * @param {function} selector a function that takes as arguments `err`, which is the error, and `caught`, which
+ *  is the source observable, in case you'd like to "retry" that observable by returning it again. Whatever observable
+ *  is returned by the `selector` will be used to continue the observable chain.
+ * @return {Observable} an observable that originates from either the source or the observable returned by the
+ *  catch `selector` function.
+ */
 export default function _catch<T>(selector: (err: any, caught: Observable<any>) => Observable<any>) {
   let catchOperator = new CatchOperator(selector);
   let caught = this.lift(catchOperator);
