4d461cb30f6751e5f8f309dc38270a09173945ae
Ben Lesh <blesh@netflix.com>
2015-10-13 14:21:05 -0700

    docs(concatAll): basic docs

--- src/operators/concatAll.ts
+++ src/operators/concatAll.ts
@@ -1,5 +1,16 @@
 import { MergeAllOperator } from './mergeAll-support';
 
+/**
+ * Joins every Observable emitted by the source (an Observable of Observables), in a serial
+ * fashion. Subscribing to each one only when the previous one has completed, and merging
+ * all of their values into the returned observable.
+ *
+ * __Warning:__ If the source Observable emits Observables quickly and endlessly, and the
+ * Observables it emits generally complete slower than the source emits, you can run into
+ * memory issues as the incoming observables collect in an unbounded buffer.
+ * 
+ * @returns {Observable} an observable of values merged from the incoming observables.
+ */
 export default function concatAll() {
   return this.lift(new MergeAllOperator(1));
 }
