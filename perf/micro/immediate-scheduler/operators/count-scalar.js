86a9b452d0dcb279ffe348fdd28201c2d73f5145
Ben Lesh <blesh@netflix.com>
2015-10-13 16:34:02 -0700

    test(count): add test for counting over scalars

---
+++ perf/micro/immediate-scheduler/operators/count-scalar.js
@@ -0,0 +1,18 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  var oldConcatWithImmediateScheduler = RxOld.Observable.just(25, RxOld.Scheduler.immediate).count();
+  var newConcatWithImmediateScheduler = RxNew.Observable.of(25).count();
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old count over scalar with immediate scheduler', function () {
+        oldConcatWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new count over scalar with immediate scheduler', function () {
+        newConcatWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+};
