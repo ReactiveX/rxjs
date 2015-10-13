eb117367384d955c19405c8f0efb0d30016ce820
Ben Lesh <blesh@netflix.com>
2015-10-13 15:51:56 -0700

    test(reduce): add micro perf tests for reducing over scalar observables

---
+++ perf/micro/immediate-scheduler/operators/reduce-scalar-noseed.js
@@ -0,0 +1,21 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  function add(acc, x) {
+    return x + x;
+  }
+  var oldReduceWithImmediateScheduler = RxOld.Observable.just(25, RxOld.Scheduler.immediate).reduce(add);
+  var newReduceWithImmediateScheduler = RxNew.Observable.of(25).reduce(add);
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old reduce with immediate scheduler', function () {
+        oldReduceWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new reduce with immediate scheduler', function () {
+        newReduceWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+};
---
+++ perf/micro/immediate-scheduler/operators/reduce-scalar.js
@@ -0,0 +1,21 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  function add(acc, x) {
+    return x + x;
+  }
+  var oldReduceWithImmediateScheduler = RxOld.Observable.just(25, RxOld.Scheduler.immediate).reduce(add, 0);
+  var newReduceWithImmediateScheduler = RxNew.Observable.of(25).reduce(add, 0);
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old reduce with immediate scheduler', function () {
+        oldReduceWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new reduce with immediate scheduler', function () {
+        newReduceWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+};
