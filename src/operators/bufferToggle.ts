4791c1f98b66c046c018b06a485d109be3c980af
Ben Lesh <blesh@netflix.com>
2015-10-13 12:58:27 -0700

    docs(bufferToggle): add basic docs

--- src/operators/bufferToggle.ts
+++ src/operators/bufferToggle.ts
@@ -9,6 +9,14 @@ import tryCatch from '../util/tryCatch';
 import {errorObject} from '../util/errorObject';
 import bindCallback from '../util/bindCallback';
 
+/**
+ * buffers values from the source by opening the buffer via signals from an observable provided to `openings`, and closing
+ * and sending the buffers when an observable returned by the `closingSelector` emits.
+ * @param {Observable<O>} openings An observable of notifications to start new buffers
+ * @param {Function} an function, that takes the value emitted by the `openings` observable and returns an Observable, which,
+ *  when it emits, signals that the associated buffer should be emitted and cleared.
+ * @returns {Observable<T[]>} an observable of arrays of buffered values.
+ */
 export default function bufferToggle<T, O>(openings: Observable<O>,
                                            closingSelector: (openValue: O) => Observable<any>): Observable<T[]> {
   return this.lift(new BufferToggleOperator<T, T, O>(openings, closingSelector));
