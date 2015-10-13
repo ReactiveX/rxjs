d18e2bcba2b1b5cdb6cac6dd19329ee2a2ccfe18
Ben Lesh <blesh@netflix.com>
2015-10-13 16:42:02 -0700

    test(skip): add micro perf tests for skip over scalar

---
+++ perf/micro/immediate-scheduler/operators/skip-scalar-outofbounds.js
@@ -0,0 +1,18 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  var oldSkipWithImmediateScheduler = RxOld.Observable.just(50, RxOld.Scheduler.immediate).skip(25);
+  var newSkipWithImmediateScheduler = RxNew.Observable.of(50).skip(25);
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+    .add('old skip with immediate scheduler', function () {
+      oldSkipWithImmediateScheduler.subscribe(_next, _error, _complete);
+    })
+    .add('new skip with immediate scheduler', function () {
+      newSkipWithImmediateScheduler.subscribe(_next, _error, _complete);
+    });
+};
