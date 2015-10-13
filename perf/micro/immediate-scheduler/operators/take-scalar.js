be61d52e37f74dc9ac627bca2804f4c4c97e3ce6
Ben Lesh <blesh@netflix.com>
2015-10-13 16:46:27 -0700

    test(take): add micro perf test for take over scalars

---
+++ perf/micro/immediate-scheduler/operators/take-scalar.js
@@ -0,0 +1,18 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  var oldTakeWithImmediateScheduler = RxOld.Observable.just(50, RxOld.Scheduler.immediate).take(5);
+  var newTakeWithImmediateScheduler = RxNew.Observable.of(50).take(5);
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old take with immediate scheduler', function () {
+        oldTakeWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new take with immediate scheduler', function () {
+        newTakeWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+};
