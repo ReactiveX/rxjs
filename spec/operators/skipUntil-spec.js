6f0d98f671d98a3c05fad54755c104d1561e8033
kwonoj <kwon.ohjoong@gmail.com>
2015-10-14 00:31:35 -0700

    fix(skipUntil): update skipUntil behavior with error, completion
    - update skipUntil operator behavior
    - add test coverage
    
    closes #518

--- spec/operators/skipUntil-spec.js
+++ spec/operators/skipUntil-spec.js
@@ -4,10 +4,118 @@ var Observable = Rx.Observable;
 
 describe('Observable.prototype.skipUntil()', function () {
   it('should skip values until another observable notifies', function () {
-    var source = hot('--a--b--c--d--e--|');
+    var e1 =     hot('--a--b--c--d--e--|');
     var skip =   hot('-------------x--|');
     var expected =  ('--------------e--|');
 
-    expectObservable(source.skipUntil(skip)).toBe(expected);
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should skip value and raises error until another observable raises error', function () {
+    var e1 =   hot('--a--b--c--d--e--|');
+    var skip = hot('-------------#');
+    var expected = '-------------#';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should skip all element when another observable does not emit and completes early', function () {
+    var e1 =   hot('--a--b--c--d--e--|');
+    var skip = hot('------------|');
+    var expected = '-----------------|';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should skip all element when another observable is empty', function () {
+    var e1 =   hot('--a--b--c--d--e--|');
+    var skip = Observable.empty();
+    var expected = '-----------------|';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should not complete if source observable does not complete', function () {
+    var e1 =   hot('-');
+    var skip = hot('-------------x--|');
+    var expected = '-';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should not complete if source observable never completes', function () {
+    var e1 = Observable.never();
+    var skip = hot('-------------x--|');
+    var expected = '-';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should raise error if source does not completes when another observable raises error', function () {
+    var e1 =   hot('-');
+    var skip = hot('-------------#');
+    var expected = '-------------#';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should raise error if source never completes when another observable raises error', function () {
+    var e1 = Observable.never();
+    var skip = hot('-------------#');
+    var expected = '-------------#';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should skip all element and does not complete when another observable never completes', function () {
+    var e1 =   hot('--a--b--c--d--e--|');
+    var skip = Observable.never();
+    var expected = '-';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should skip all element and does not complete when another observable does not completes', function () {
+    var e1 =   hot('--a--b--c--d--e--|');
+    var skip = hot('-');
+    var expected = '-';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should skip all element and does not complete when another observable completes after source', function () {
+    var e1 =   hot('--a--b--c--d--e--|');
+    var skip = hot('------------------------|');
+    var expected = '-';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should not completes if source does not completes when another observable does not emit', function () {
+    var e1 =   hot('-');
+    var skip = hot('--------------|');
+    var expected = '-';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should not completes if source and another observable both does not complete', function () {
+    var e1 =   hot('-');
+    var skip = hot('-');
+    var expected = '-';
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
+  });
+
+  it('should skip all element when another observable unsubscribed early before emit', function () {
+    var e1 =   hot('--a--b--c--d--e--|');
+    var skip = hot('-------------x--|');
+    var expected = '-';
+
+    e1.subscribe(function () {
+      skip.unsubscribe();
+    });
+
+    expectObservable(e1.skipUntil(skip)).toBe(expected);
   });
 });
