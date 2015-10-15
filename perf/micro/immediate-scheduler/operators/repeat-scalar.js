c13651a32e431309df3abbbe516a55f2ee648640
kwonoj <kwon.ohjoong@gmail.com>
2015-10-15 14:11:50 -0700

    test(repeat): update micro-benchmark performance test for repeat
    - remove redundant scheduler parameter which operator doesn't accept
    - separate tests for scalar

--- perf/micro/immediate-scheduler/operators/repeat-scalar.js
+++ perf/micro/immediate-scheduler/operators/repeat-scalar.js
@@ -2,17 +2,17 @@ var RxOld = require('rx');
 var RxNew = require('../../../../index');
 
 module.exports = function (suite) {
-  var oldRepeatWithImmediateScheduler = RxOld.Observable.range(0, 25, RxOld.Scheduler.immediate).repeat(5);
-  var newRepeatWithImmediateScheduler = RxNew.Observable.range(0, 25).repeat(5);
+  var oldRepeatWithImmediateScheduler = RxOld.Observable.of(25, RxOld.Scheduler.immediate).repeat(5);
+  var newRepeatWithImmediateScheduler = RxNew.Observable.of(25).repeat(5);
 
   function _next(x) { }
   function _error(e) { }
   function _complete() { }
   return suite
-      .add('old repeat() with immediate scheduler', function () {
+      .add('old repeat(scalar) with immediate scheduler', function () {
         oldRepeatWithImmediateScheduler.subscribe(_next, _error, _complete);
       })
-      .add('new repeat() with immediate scheduler', function () {
+      .add('new repeat(scalar) with immediate scheduler', function () {
         newRepeatWithImmediateScheduler.subscribe(_next, _error, _complete);
       });
 };
