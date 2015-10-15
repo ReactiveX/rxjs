05ebb07b57a467155749462c71d950573c4e819a
Ben Lesh <blesh@netflix.com>
2015-10-14 19:29:58 -0700

    chore(combinLatest-spec): remove unnecessary async tests because Jasmine is freaking out on Travis

--- spec/operators/combineLatest-spec.js
+++ spec/operators/combineLatest-spec.js
@@ -4,35 +4,6 @@ var Observable = Rx.Observable;
 var immediateScheduler = Rx.Scheduler.immediate;
 
 describe('Observable.prototype.combineLatest', function () {
-  it('should combine a source with a second', function (done) {
-    var a = Observable.of(1, 2, 3);
-    var b = Observable.of(4, 5, 6, 7, 8);
-    var expected = [[3, 4], [3, 5], [3, 6], [3, 7], [3, 8]];
-    a.combineLatest(b).subscribe(function (vals) {
-      expect(vals).toEqual(expected.shift());
-    }, null, function () {
-      expect(expected.length).toEqual(0);
-      done();
-    });
-  });
-
-  it('should combineLatest the source with the provided observables', function (done) {
-    var expected = ['00', '01', '11', '12', '22', '23'];
-    var i = 0;
-    Observable.interval(50).take(3)
-      .combineLatest(Observable.interval(45).take(4), function (a, b) {
-        return '' + a + b;
-      })
-      .subscribe(function (x) {
-        expect(x).toBe(expected[i++]);
-      }, null, function () {
-        expect(i).toEqual(expected.length);
-        done();
-      });
-  }, 300);
-
-  // From RxJS 2 below
-
   it('should work with two nevers', function () {
     var e1 = Observable.never();
     var e2 = Observable.never();
