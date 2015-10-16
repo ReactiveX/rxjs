f414a0bfa6a000713c1f978a3bea62b1fee7b216
Luís Gabriel Lima <lampih@gmail.com>
2015-10-16 18:41:57 -0300

    test(VirtualTimeScheduler): add test using negative delay

--- spec/schedulers/VirtualTimeScheduler-spec.js
+++ spec/schedulers/VirtualTimeScheduler-spec.js
@@ -42,6 +42,24 @@ describe('VirtualTimeScheduler', function () {
     expect(invoked).toEqual([1, 3, 5, 2, 6, 4]);
   });
 
+  it('should schedule things in order when there are negative delays', function () {
+    var v = new VirtualTimeScheduler();
+    var invoked = [];
+    var invoke = function (state) {
+      invoked.push(state);
+    };
+    v.schedule(invoke, 0, 1);
+    v.schedule(invoke, 100, 2);
+    v.schedule(invoke, 0, 3);
+    v.schedule(invoke, -2, 4);
+    v.schedule(invoke, 0, 5);
+    v.schedule(invoke, -10, 6);
+
+    v.flush();
+
+    expect(invoked).toEqual([6, 4, 1, 3, 5, 2]);
+  });
+
   it('should support recursive scheduling', function () {
     var v = new VirtualTimeScheduler();
     var count = 0;
