bc717a482573058026a07e1d5d3f3f60c7baa4e8
Luís Gabriel Lima <lampih@gmail.com>
2015-10-16 18:36:16 -0300

    test(timer): add more test cases from RxJS 4

--- spec/observables/timer-spec.js
+++ spec/observables/timer-spec.js
@@ -11,4 +11,36 @@ describe('Observable.timer', function () {
 
     expectObservable(source).toBe(expected, {x: 0});
   });
+
+  it('should emit a single value immediately', function () {
+    var source = Observable.timer(0, rxTestScheduler);
+    var expected = '(x|)';
+
+    expectObservable(source).toBe(expected, {x: 0});
+  });
+
+  it('should start after delay and periodically emit values', function () {
+    var source = Observable.timer(40, 20, rxTestScheduler).take(5);
+    var expected = '----a-b-c-d-(e|)';
+    var values = { a: 0, b: 1, c: 2, d: 3, e: 4};
+
+    expectObservable(source).toBe(expected, values);
+  });
+
+  it('should start immediately and periodically emit values', function () {
+    var source = Observable.timer(0, 30, rxTestScheduler).take(5);
+    var expected = 'a--b--c--d--(e|)';
+    var values = { a: 0, b: 1, c: 2, d: 3, e: 4};
+
+    expectObservable(source).toBe(expected, values);
+  });
+
+  it('should stop emiting values when subscription is done', function () {
+    var source = Observable.timer(0, 30, rxTestScheduler);
+    var expected = 'a--b--c--d--e';
+    var unsub   =  '^            !';
+    var values = { a: 0, b: 1, c: 2, d: 3, e: 4};
+
+    expectObservable(source, unsub).toBe(expected, values);
+  });
 });
