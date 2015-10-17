aa683f946114a4742e3a33003101583a3fb2e886
Andre Medeiros <andre.medeiros@futurice.com>
2015-10-17 13:13:32 +0300

    test(retryWhen): add comprehensive marble tests for retryWhen

--- spec/operators/retryWhen-spec.js
+++ spec/operators/retryWhen-spec.js
@@ -51,4 +51,216 @@ describe('Observable.prototype.retryWhen()', function () {
         throw 'error should not be called';
       }, done);
   });
+
+  it('should apply an empty notifier on an empty source', function () {
+    var source = cold(  '|');
+    var subs =          '(^!)';
+    var notifier = cold('|');
+    var expected =      '|';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should apply a never notifier on an empty source', function () {
+    var source = cold(  '|');
+    var subs =          '(^!)';
+    var notifier = cold('-');
+    var expected =      '|';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should apply an empty notifier on a never source', function () {
+    var source = cold(  '-');
+    var unsub =         '                                         !';
+    var subs =          '^                                        !';
+    var notifier = cold('|');
+    var expected =      '-';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result, unsub).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should apply a never notifier on a never source', function () {
+    var source = cold(  '-');
+    var unsub =         '                                         !';
+    var subs =          '^                                        !';
+    var notifier = cold('-');
+    var expected =      '-';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result, unsub).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should return an empty observable given a just-throw source and empty notifier', function () {
+    var source = cold(  '#');
+    var notifier = cold('|');
+    var expected =      '|';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+  });
+
+  it('should return a never observable given a just-throw source and never notifier', function () {
+    var source = cold(  '#');
+    var notifier = cold('-');
+    var expected =      '-';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+  });
+
+  it('should hide errors using a never notifier on a source with eventual error', function () {
+    var source = cold(  '--a--b--c--#');
+    var subs =          '^          !';
+    var notifier = cold(           '-');
+    var expected =      '--a--b--c---------------------------------';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should replace error with complete using an empty notifier on a source ' +
+  'with eventual error', function () {
+    var source = cold(  '--a--b--c--#');
+    var subs =          '^          !';
+    var notifier = cold(           '|');
+    var expected =      '--a--b--c--|';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should mirror a basic cold source with complete, given a never notifier', function () {
+    var source = cold(  '--a--b--c--|');
+    var subs =          '^          !';
+    var notifier = cold(           '|');
+    var expected =      '--a--b--c--|';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should mirror a basic cold source with no termination, given a never notifier', function () {
+    var source = cold(  '--a--b--c---');
+    var subs =          '^           ';
+    var notifier = cold(           '|');
+    var expected =      '--a--b--c---';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should mirror a basic hot source with complete, given a never notifier', function () {
+    var source = hot('-a-^--b--c--|');
+    var subs =          '^        !';
+    var notifier = cold(         '|');
+    var expected =      '---b--c--|';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should handle a source with eventual error using a hot notifier', function () {
+    var source = cold( '-1--2--#');
+    var subs =        ['^      !                    ',
+                       '         ^      !           ',
+                       '                 ^      !   '];
+    var notifier = hot('---------r-------r---------#');
+    var expected =     '-1--2-----1--2----1--2-----#';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should tear down resources when result is unsubscribed early', function () {
+    var source = cold( '-1--2--#');
+    var unsub =        '                    !       ';
+    var subs =        ['^      !                    ',
+                       '         ^      !           ',
+                       '                 ^  !       '];
+    var notifier = hot('---------r-------r---------#');
+    var nsubs =        '       ^            !       ';
+    var expected =     '-1--2-----1--2----1--       ';
+
+    var result = source.retryWhen(function (errors) { return notifier; });
+
+    expectObservable(result, unsub).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+    expectSubscriptions(notifier.subscriptions).toBe(nsubs);
+  });
+
+  it('should handle a source with eventual error using a dynamic notifier ' +
+  'selector which eventually throws', function () {
+    var source = cold('-1--2--#');
+    var subs =       ['^      !              ',
+                      '       ^      !       ',
+                      '              ^      !'];
+    var expected =    '-1--2---1--2---1--2--#';
+
+    var invoked = 0;
+    var result = source.retryWhen(function (errors) {
+      return errors.map(function (err) {
+        if (++invoked === 3) {
+          throw 'error';
+        } else {
+          return 'x';
+        }
+      });
+    });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should handle a source with eventual error using a dynamic notifier ' +
+  'selector which eventually completes', function () {
+    var source = cold('-1--2--#');
+    var subs =       ['^      !              ',
+                      '       ^      !       ',
+                      '              ^      !'];
+    var expected =    '-1--2---1--2---1--2--|';
+
+    var invoked = 0;
+    var result = source.retryWhen(function (errors) {
+      return errors
+        .map(function () { return 'x'; })
+        .takeUntil(
+          errors.flatMap(function () {
+            if (++invoked < 3) {
+              return Observable.empty();
+            } else {
+              return Observable.of('stop!');
+            }
+          })
+        );
+    });
+
+    expectObservable(result).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
 });
