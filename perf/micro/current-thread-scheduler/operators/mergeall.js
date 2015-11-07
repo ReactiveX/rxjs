408d363b44edf364a641564567ae42e5cbb75b16
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2015-11-07 16:29:28 +0100

    chore(mergeall-perf): fix mergeall micro perf benchmarks
    Measure the mergeall operator without going through a map process.

--- perf/micro/current-thread-scheduler/operators/mergeall.js
+++ perf/micro/current-thread-scheduler/operators/mergeall.js
@@ -1,12 +1,18 @@
 var RxOld = require('rx');
 var RxNew = require('../../../../index');
 
+var source = Array.apply(null, { length: 25 });
+
 module.exports = function (suite) {
-  var oldMergeAllWithCurrentThreadScheduler = RxOld.Observable.range(0, 25, RxOld.Scheduler.currentThread)
-    .map(RxOld.Observable.range(0, 25), RxOld.Scheduler.currentThread)
+  var oldMergeAllWithCurrentThreadScheduler = RxOld.Observable.fromArray(
+    source.map(function () { return RxOld.Observable.range(0, 25, RxOld.Scheduler.currentThread); }),
+    RxOld.Scheduler.currentThread
+  )
     .mergeAll();
-  var newMergeAllWithCurrentThreadScheduler = RxNew.Observable.range(0, 25, RxNew.Scheduler.immediate)
-    .mapTo(RxNew.Observable.range(0, 25, RxNew.Scheduler.immediate))
+  var newMergeAllWithCurrentThreadScheduler = RxNew.Observable.fromArray(
+    source.map(function () { return RxNew.Observable.range(0, 25, RxNew.Scheduler.immediate); }),
+    RxNew.Scheduler.immediate
+  )
     .mergeAll();
 
   function _next(x) { }
