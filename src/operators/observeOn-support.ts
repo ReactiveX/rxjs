f492855ddd5d3f896e1ca68fcbb8499d84ace330
OJ Kwon <kwon.ohjoong@gmail.com>
2015-11-10 00:06:27 -0800

    test(observeOn): add test cases for observeOn operator

--- src/operators/observeOn-support.ts
+++ src/operators/observeOn-support.ts
@@ -5,13 +5,7 @@ import {Subscriber} from '../Subscriber';
 import {Notification} from '../Notification';
 
 export class ObserveOnOperator<T, R> implements Operator<T, R> {
-
-  delay: number;
-  scheduler: Scheduler;
-
-  constructor(scheduler: Scheduler, delay: number = 0) {
-    this.delay = delay;
-    this.scheduler = scheduler;
+  constructor(private scheduler: Scheduler, private delay: number = 0) {
   }
 
   call(subscriber: Subscriber<T>): Subscriber<T> {
@@ -20,42 +14,37 @@ export class ObserveOnOperator<T, R> implements Operator<T, R> {
 }
 
 export class ObserveOnSubscriber<T> extends Subscriber<T> {
-
   static dispatch({ notification, destination }) {
     notification.observe(destination);
   }
 
-  delay: number;
-  scheduler: Scheduler;
-
-  constructor(destination: Subscriber<T>, scheduler: Scheduler, delay: number = 0) {
+  constructor(destination: Subscriber<T>,
+              private scheduler: Scheduler,
+              private delay: number = 0) {
     super(destination);
-    this.delay = delay;
-    this.scheduler = scheduler;
   }
 
-  _next(x) {
-    this.add(this.scheduler.schedule(ObserveOnSubscriber.dispatch, this.delay,
-      new ObserveOnMessage(Notification.createNext(x), this.destination)));
+  private scheduleMessage(notification: Notification<any>): void {
+     this.add(this.scheduler.schedule(ObserveOnSubscriber.dispatch,
+                                      this.delay,
+                                      new ObserveOnMessage(notification, this.destination)));
+   }
+
+  _next(value: T): void {
+    this.scheduleMessage(Notification.createNext(value));
   }
 
-  _error(e) {
-    this.add(this.scheduler.schedule(ObserveOnSubscriber.dispatch, this.delay,
-      new ObserveOnMessage(Notification.createError(e), this.destination)));
+  _error(err: any): void {
+    this.scheduleMessage(Notification.createError(err));
   }
 
-  _complete() {
-    this.add(this.scheduler.schedule(ObserveOnSubscriber.dispatch, this.delay,
-      new ObserveOnMessage(Notification.createComplete(), this.destination)));
+  _complete(): void {
+    this.scheduleMessage(Notification.createComplete());
   }
 }
 
 class ObserveOnMessage {
-  notification: Notification<any>;
-  destination: Observer<any>;
-
-  constructor(notification: Notification<any>, destination: Observer<any>) {
-    this.notification = notification;
-    this.destination = destination;
+  constructor(public notification: Notification<any>,
+              public destination: Observer<any>) {
   }
 }
