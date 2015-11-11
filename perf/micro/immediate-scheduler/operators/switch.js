b9b58ccaa92442d3a5010fb02a042deaa167c2d7
Pierre Guilleminot <pierre.guilleminot@gmail.com>
2015-11-11 10:39:01 +0100

    chore(switch): add micro perf benchmarks for switch operator

---
+++ perf/micro/immediate-scheduler/operators/switch.js
@@ -0,0 +1,27 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+var source = Array.apply(null, { length: 25 });
+
+module.exports = function (suite) {
+  var oldSwitchWithImmediateScheduler = RxOld.Observable.fromArray(
+    source.map(function () { return RxOld.Observable.range(0, 25, RxOld.Scheduler.immediate); }),
+    RxOld.Scheduler.immediate
+  )
+    .switch();
+  var newSwitchWithImmediateScheduler = RxNew.Observable.fromArray(
+    source.map(function () { return RxNew.Observable.range(0, 25); })
+  )
+    .switch();
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+    .add('old switch with immediate scheduler', function () {
+      oldSwitchWithImmediateScheduler.subscribe(_next, _error, _complete);
+    })
+    .add('new switch with immediate scheduler', function () {
+      newSwitchWithImmediateScheduler.subscribe(_next, _error, _complete);
+    });
+};
