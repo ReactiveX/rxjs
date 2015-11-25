b8a8adf3464e6521ee0c625455390edf41f55032
Andre Staltz <andre@staltz.com>
2015-11-25 17:42:57 -0200

    test(ignoreElements): add subscription assertions on tests

--- spec/operators/ignoreElements-spec.js
+++ spec/operators/ignoreElements-spec.js
@@ -1,40 +1,50 @@
-/* globals describe, it, expect, hot, cold, expectObservable */
+/* globals describe, it, expect, hot, cold, expectObservable, expectSubscriptions */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.ignoreElements', function () {
   it('should ignore all the elements of the source', function () {
     var source = hot('--a--b--c--d--|');
+    var subs =       '^             !';
     var expected =   '--------------|';
 
     expectObservable(source.ignoreElements()).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should propagate errors from the source', function () {
     var source = hot('--a--#');
+    var subs =       '^    !';
     var expected =   '-----#';
 
     expectObservable(source.ignoreElements()).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should support Observable.empty', function () {
-    var source = Observable.empty();
-    var expected = '|';
+    var source = cold('|');
+    var subs =        '(^!)';
+    var expected =    '|';
 
     expectObservable(source.ignoreElements()).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should support Observable.never', function () {
-    var source = Observable.never();
-    var expected = '-';
+    var source = cold('-');
+    var subs =        '^';
+    var expected =    '-';
 
     expectObservable(source.ignoreElements()).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should support Observable.throw', function () {
-    var source = Observable.throw(new Error('oops'));
-    var expected = '#';
+    var source = cold('#');
+    var subs =        '(^!)';
+    var expected =    '#';
 
-    expectObservable(source.ignoreElements()).toBe(expected, undefined, new Error('oops'));
+    expectObservable(source.ignoreElements()).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 });
