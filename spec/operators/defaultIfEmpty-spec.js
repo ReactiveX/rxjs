73f78b90654a884d936ba5507f49c6e4e60fee23
Andre Staltz <andre@staltz.com>
2015-11-25 10:10:37 -0200

    test(defaultIfEmpty): add subscription assertions to defaultIfEmpty tests

--- spec/operators/defaultIfEmpty-spec.js
+++ spec/operators/defaultIfEmpty-spec.js
@@ -1,41 +1,50 @@
-/* globals describe, it, expect, expectObservable, hot */
+/* globals describe, it, expect, expectObservable, expectSubscriptions, cold, hot */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.defaultIfEmpty()', function () {
   it('should return the argument if Observable is empty', function () {
-    var e1 = Observable.empty();
+    var e1 =  cold('|');
+    var e1subs =   '(^!)';
     var expected = '(x|)';
 
     expectObservable(e1.defaultIfEmpty('x')).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should return null if the Observable is empty and no arguments', function () {
-    var e1 = Observable.empty();
+    var e1 =  cold('|');
+    var e1subs =   '(^!)';
     var expected = '(x|)';
 
     expectObservable(e1.defaultIfEmpty()).toBe(expected, { x: null });
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should return the Observable if not empty with a default value', function () {
     var e1 =   hot('--a--b--|');
+    var e1subs =   '^       !';
     var expected = '--a--b--|';
 
     expectObservable(e1.defaultIfEmpty('x')).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should return the Observable if not empty with no default value', function () {
     var e1 =   hot('--a--b--|');
+    var e1subs =   '^       !';
     var expected = '--a--b--|';
 
     expectObservable(e1.defaultIfEmpty()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should error if the Observable errors', function () {
-    var error = 'error';
-    var e1 = Observable.throw(error);
+    var e1 =  cold('#');
+    var e1subs =   '(^!)';
     var expected = '#';
 
-    expectObservable(e1.defaultIfEmpty('x')).toBe(expected, null, error);
+    expectObservable(e1.defaultIfEmpty('x')).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 });
