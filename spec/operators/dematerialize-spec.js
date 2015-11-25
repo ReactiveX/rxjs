8613dd64a8e51c39913ceb680ad7017c1b19049f
Andre Staltz <andre@staltz.com>
2015-11-25 10:17:12 -0200

    test(dematerialize): add subscription assertions for dematerialize tests

--- spec/operators/dematerialize-spec.js
+++ spec/operators/dematerialize-spec.js
@@ -1,4 +1,4 @@
-/* globals describe, it, expect, expectObservable, hot */
+/* globals describe, it, expect, expectObservable, expectSubscriptions, hot, cold */
 var Rx = require('../../dist/cjs/Rx');
 var Observable = Rx.Observable;
 var Notification = Rx.Notification;
@@ -13,9 +13,11 @@ describe('Observable.prototype.dematerialize()', function () {
     };
 
     var e1 =   hot('--a--b--c--d--|', values);
+    var e1subs =   '^          !';
     var expected = '--w--x--y--|';
 
     expectObservable(e1.dematerialize()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should dematerialize a sad stream', function () {
@@ -27,58 +29,74 @@ describe('Observable.prototype.dematerialize()', function () {
     };
 
     var e1 =   hot('--a--b--c--d--|', values);
+    var e1subs =   '^          !';
     var expected = '--w--x--y--#';
 
     expectObservable(e1.dematerialize()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should dematerialize stream does not completes', function () {
     var e1 =   hot('------');
+    var e1subs =   '^';
     var expected = '-';
 
     expectObservable(e1.dematerialize()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should dematerialize stream never completes', function () {
-    var e1 = Observable.never();
+    var e1 =  cold('-');
+    var e1subs =   '^';
     var expected = '-';
 
     expectObservable(e1.dematerialize()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should dematerialize stream does not emit', function () {
     var e1 =   hot('----|');
-    var expected = '----|)';
+    var e1subs =   '^   !';
+    var expected = '----|';
 
     expectObservable(e1.dematerialize()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should dematerialize empty stream', function () {
-    var e1 = Observable.empty();
+    var e1 =  cold('|');
+    var e1subs =   '(^!)';
     var expected = '|';
 
     expectObservable(e1.dematerialize()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should dematerialize stream throws', function () {
     var error = 'error';
     var e1 =   hot('(x|)', {x: Notification.createError(error)});
+    var e1subs =   '(^!)';
     var expected = '#';
 
     expectObservable(e1.dematerialize()).toBe(expected, null, error);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should dematerialize and completes when stream compltes with complete notification', function () {
     var e1 =   hot('----(a|)', { a: Notification.createComplete() });
+    var e1subs =   '^   !';
     var expected = '----|';
 
     expectObservable(e1.dematerialize()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should dematerialize and completes when stream emits complete notification', function () {
     var e1 =   hot('----a--|', { a: Notification.createComplete() });
+    var e1subs =   '^   !';
     var expected = '----|';
 
     expectObservable(e1.dematerialize()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 });
