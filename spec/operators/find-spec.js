47fa4bdbb91bdea76d52c9a5c30969c654bd5d22
Andre Staltz <andre@staltz.com>
2015-11-25 16:52:28 -0200

    test(find): add subscription assertions on find() tests

--- spec/operators/find-spec.js
+++ spec/operators/find-spec.js
@@ -1,4 +1,4 @@
-/* globals describe, it, expect, hot, expectObservable */
+/* globals describe, it, expect, cold, hot, expectObservable, expectSubscriptions */
 var Rx = require('../../dist/cjs/Rx.KitchenSink');
 var Observable = Rx.Observable;
 
@@ -9,19 +9,27 @@ describe('Observable.prototype.find()', function () {
 
   it('should not emit if source does not emit', function () {
     var source = hot('-');
+    var subs =       '^';
     var expected =   '-';
 
     expectObservable(source.find(truePredicate)).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should return undefined if source is empty to match predicate', function () {
-    var expected = '(x|)';
+    var source = cold('|');
+    var subs =        '(^!)';
+    var expected =    '(x|)';
 
-    expectObservable(Observable.empty().find(truePredicate)).toBe(expected, {x: undefined});
+    var result = source.find(truePredicate);
+
+    expectObservable(result).toBe(expected, {x: undefined});
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should return matching element from source emits single element', function () {
     var source = hot('--a--|');
+    var subs =       '^ !';
     var expected =   '--(a|)';
 
     var predicate = function (value) {
@@ -29,10 +37,12 @@ describe('Observable.prototype.find()', function () {
     };
 
     expectObservable(source.find(predicate)).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should return undefined if element does not match with predicate', function () {
     var source = hot('--a--b--c--|');
+    var subs =       '^          !';
     var expected =   '-----------(x|)';
 
     var predicate = function (value) {
@@ -40,10 +50,12 @@ describe('Observable.prototype.find()', function () {
     };
 
     expectObservable(source.find(predicate)).toBe(expected, { x: undefined });
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should raise if source raise error while element does not match with predicate', function () {
     var source = hot('--a--b--#');
+    var subs =       '^       !';
     var expected =   '--------#';
 
     var predicate = function (value) {
@@ -51,10 +63,12 @@ describe('Observable.prototype.find()', function () {
     };
 
     expectObservable(source.find(predicate)).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should raise error if predicate throws error', function () {
     var source = hot('--a--b--c--|');
+    var subs =       '^ !';
     var expected =   '--#';
 
     var predicate = function (value) {
@@ -62,5 +76,6 @@ describe('Observable.prototype.find()', function () {
     };
 
     expectObservable(source.find(predicate)).toBe(expected);
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 });
