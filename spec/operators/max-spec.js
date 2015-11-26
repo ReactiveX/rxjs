0a37e9fd12d79afdcfa0717a925f7beb0f0bfea0
Andre Staltz <andre@staltz.com>
2015-11-26 15:02:04 -0200

    test(max): add subscription assertions on max() tests

--- spec/operators/max-spec.js
+++ spec/operators/max-spec.js
@@ -1,48 +1,79 @@
-/* globals describe, it, expect, expectObservable, hot */
+/* globals describe, it, expect, expectObservable, expectSubscriptions, hot, cold */
 var Rx = require('../../dist/cjs/Rx.KitchenSink');
 var Observable = Rx.Observable;
 
 describe('max', function () {
   it('should be never when source is never', function () {
-    var e1 = Observable.never();
+    var e1 =  cold('-');
+    var e1subs =   '^';
     var expected = '-';
+
     expectObservable(e1.max()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should be zero when source is empty', function () {
-    var e1 = Observable.empty();
+    var e1 =  cold('|');
+    var e1subs =   '(^!)';
     var expected = '|';
+
     expectObservable(e1.max()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should be never when source doesn\'t complete', function () {
     var e1 = hot('--x--^--y--');
+    var e1subs =      '^     ';
     var expected =    '------';
+
     expectObservable(e1.max()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should be completes when source doesn\'t have values', function () {
     var e1 = hot('-x-^---|');
+    var e1subs =    '^   !';
     var expected =  '----|';
+
     expectObservable(e1.max()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should max the unique value of an observable', function () {
     var e1 = hot('-x-^--y--|', { y: 42 });
+    var e1subs =    '^     !';
     var expected =  '------(w|)';
+
     expectObservable(e1.max()).toBe(expected, { w: 42 });
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should max the values of an observable', function () {
     var source = hot('--a--b--c--|', { a: 42, b: -1, c: 0 });
+    var subs =       '^          !';
     var expected =   '-----------(x|)';
+
     expectObservable(source.max()).toBe(expected, { x: 42 });
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should max the values of an ongoing hot observable', function () {
     var source = hot('--a-^-b--c--d--|', { a: 42, b: -1, c: 0, d: 666 });
+    var subs =           '^          !';
     var expected =       '-----------(x|)';
+
     expectObservable(source.max()).toBe(expected, { x: 666 });
+    expectSubscriptions(source.subscriptions).toBe(subs);
+  });
+
+  it('should allow unsubscribing explicitly and early', function () {
+    var source = hot('--a--b--c--|', { a: 42, b: -1, c: 0 });
+    var unsub =      '      !     ';
+    var subs =       '^     !     ';
+    var expected =   '-------     ';
+
+    expectObservable(source.max(), unsub).toBe(expected, { x: 42 });
+    expectSubscriptions(source.subscriptions).toBe(subs);
   });
 
   it('should max a range() source observable', function (done) {
@@ -77,88 +108,126 @@ describe('max', function () {
 
   it('should work with error', function () {
     var e1 = hot('-x-^--y--z--#', { x: 1, y: 2, z: 3 }, 'too bad');
+    var e1subs =    '^        !';
     var expected =  '---------#';
+
     expectObservable(e1.max()).toBe(expected, null, 'too bad');
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should work with throw', function () {
-    var e1 = Observable.throw(new Error('too bad'));
+    var e1 =  cold('#');
+    var e1subs =   '(^!)';
     var expected = '#';
-    expectObservable(e1.max()).toBe(expected, null, new Error('too bad'));
+
+    expectObservable(e1.max()).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should handle a constant predicate on an empty hot observable', function () {
     var e1 = hot('-x-^---|');
+    var e1subs =    '^   !';
     var expected =  '----|';
+
     var predicate = function (x, y) {
       return 42;
     };
+
     expectObservable(e1.max(predicate)).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should handle a constant predicate on an never hot observable', function () {
     var e1 = hot('-x-^----');
-    var expected =  '-';
+    var e1subs =    '^    ';
+    var expected =  '-----';
+
     var predicate = function (x, y) {
       return 42;
     };
+
     expectObservable(e1.max(predicate)).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should handle a constant predicate on a simple hot observable', function () {
     var e1 = hot('-x-^-a-|', { a: 1 });
+    var e1subs =    '^   !';
     var expected =  '----(w|)';
+
     var predicate = function () {
       return 42;
     };
+
     expectObservable(e1.max(predicate)).toBe(expected, { w: 1 });
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should handle a constant predicate on observable with many values', function () {
     var e1 = hot('-x-^-a-b-c-d-e-f-g-|');
+    var e1subs =    '^               !';
     var expected =  '----------------(w|)';
+
     var predicate = function () {
       return 42;
     };
+
     expectObservable(e1.max(predicate)).toBe(expected, { w: 42 });
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should handle a predicate on observable with many values', function () {
     var e1 = hot('-a-^-b--c--d-|', { a: 42, b: -1, c: 0, d: 666 });
+    var e1subs =    '^         !';
     var expected =  '----------(w|)';
+
     var predicate = function (x, y) {
       return Math.min(x, y);
     };
+
     expectObservable(e1.max(predicate)).toBe(expected, { w: -1 });
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should handle a predicate for string on observable with many values', function () {
     var e1 = hot('-1-^-2--3--4-|');
+    var e1subs =    '^         !';
     var expected =  '----------(w|)';
+
     var predicate = function (x, y) {
       return x > y ? x : y;
     };
+
     expectObservable(e1.max(predicate)).toBe(expected, { w: '4' });
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should handle a constant predicate on observable that throws', function () {
     var e1 = hot('-1-^---#');
+    var e1subs =    '^   !';
     var expected =  '----#';
+
     var predicate = function () {
       return 42;
     };
+
     expectObservable(e1.max(predicate)).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should handle a predicate that throws, on observable with many values', function () {
     var e1 = hot('-1-^-2--3--|');
-    var expected =  '-----#';
+    var e1subs =    '^    !   ';
+    var expected =  '-----#   ';
+
     var predicate = function (x, y) {
       if (y === '3') {
         throw 'error';
       }
       return x > y ? x : y;
     };
+
     expectObservable(e1.max(predicate)).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 });
