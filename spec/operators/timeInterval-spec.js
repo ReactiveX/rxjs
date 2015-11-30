a643efac28c5df18866d1a8d65820cce65385870
Andre Staltz <andre@staltz.com>
2015-11-30 15:32:52 +0200

    test(timeInterval): add subscription assertions
    Add subscription assertions on timeInterval tests.

--- spec/operators/timeInterval-spec.js
+++ spec/operators/timeInterval-spec.js
@@ -1,10 +1,11 @@
-/* globals describe, it, expect, expectObservable, hot, rxTestScheduler */
+/* globals describe, it, expect, expectObservable, expectSubscriptions, cold, hot, rxTestScheduler */
 var Rx = require('../../dist/cjs/Rx.KitchenSink');
 var Observable = Rx.Observable;
 
 describe('Observable.prototype.timeInterval()', function () {
   it('should record interval if source emit elements', function () {
     var e1 = hot('--a--^b--c----d---e--|');
+    var e1subs =      '^               !';
     var expected =    '-w--x----y---z--|';
 
     var expectedValue = {
@@ -15,24 +16,30 @@ describe('Observable.prototype.timeInterval()', function () {
     };
 
     expectObservable(e1.timeInterval(rxTestScheduler)).toBe(expected, expectedValue);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should completes without record interval if source does not emits', function () {
     var e1 =   hot('---------|');
+    var e1subs =   '^        !';
     var expected = '---------|';
 
     expectObservable(e1.timeInterval(rxTestScheduler)).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should complete immediately if source is empty', function () {
-    var e1 = Observable.empty();
+    var e1 =  cold('|');
+    var e1subs =   '(^!)';
     var expected = '|';
 
     expectObservable(e1.timeInterval(rxTestScheduler)).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should record interval then does not completes if source emits but not completes', function () {
     var e1 =   hot('-a--b--');
+    var e1subs =   '^      ';
     var expected = '-y--z--';
 
     var expectedValue = {
@@ -41,24 +48,47 @@ describe('Observable.prototype.timeInterval()', function () {
     };
 
     expectObservable(e1.timeInterval(rxTestScheduler)).toBe(expected, expectedValue);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+  });
+
+  it('should allow unsubscribing explicitly and early', function () {
+    var e1 =   hot('-a--b-----c---d---|');
+    var unsub =    '       !           ';
+    var e1subs =   '^      !           ';
+    var expected = '-y--z---           ';
+
+    var expectedValue = {
+      y: new Rx.TimeInterval('a', 10),
+      z: new Rx.TimeInterval('b', 30)
+    };
+
+    var result = e1.timeInterval(rxTestScheduler);
+
+    expectObservable(result, unsub).toBe(expected, expectedValue);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should not completes if source never completes', function () {
-    var e1 = Observable.never();
+    var e1 =  cold('-');
+    var e1subs =   '^';
     var expected = '-';
 
     expectObservable(e1.timeInterval(rxTestScheduler)).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('raise error if source raises error', function () {
     var e1 =   hot('---#');
+    var e1subs =   '^  !';
     var expected = '---#';
 
     expectObservable(e1.timeInterval(rxTestScheduler)).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should record interval then raise error if source raises error after emit', function () {
     var e1 =   hot('-a--b--#');
+    var e1subs =   '^      !';
     var expected = '-y--z--#';
 
     var expectedValue = {
@@ -67,13 +97,15 @@ describe('Observable.prototype.timeInterval()', function () {
     };
 
     expectObservable(e1.timeInterval(rxTestScheduler)).toBe(expected, expectedValue);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should raise error if source immediately throws', function () {
-    var error = 'error';
-    var e1 = Observable.throw(error);
+    var e1 =  cold('#');
+    var e1subs =   '(^!)';
     var expected = '#';
 
-    expectObservable(e1.timeInterval(rxTestScheduler)).toBe(expected, null, error);
+    expectObservable(e1.timeInterval(rxTestScheduler)).toBe(expected);
+    expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 });
