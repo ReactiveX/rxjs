2c5bf410fe3af4f3abcee9a17eecabc48558757d
OJ Kwon <kwon.ohjoong@gmail.com>
2015-12-01 23:02:30 -0800

    refactor(SymbolShim): export SymbolShim using factory

--- src/util/SymbolShim.ts
+++ src/util/SymbolShim.ts
@@ -1,48 +1,57 @@
 import {root} from './root';
 
-if (!root.Symbol) {
-  root.Symbol = {};
-}
+export class SymbolDefinition {
+  observable: symbol = null;
+  iterator: symbol = null;
 
-if (!root.Symbol.observable) {
-  if (typeof root.Symbol.for === 'function') {
-    root.Symbol.observable = root.Symbol.for('observable');
-  } else {
-    root.Symbol.observable = '@@observable';
-  }
-}
+  private applyObservable(): symbol {
+    const root = this.root;
 
-if (!root.Symbol.iterator) {
-  if (typeof root.Symbol.for === 'function') {
-    root.Symbol.iterator = root.Symbol.for('iterator');
-  } else if (root.Set && typeof new root.Set()['@@iterator'] === 'function') {
-    // Bug for mozilla version
-    root.Symbol.iterator = '@@iterator';
-  } else if (root.Map) {
-    // es6-shim specific logic
-    let keys = Object.getOwnPropertyNames(root.Map.prototype);
-    for (let i = 0; i < keys.length; ++i) {
-      let key = keys[i];
-      if (key !== 'entries' && key !== 'size' && root.Map.prototype[key] === root.Map.prototype['entries']) {
-        root.Symbol.iterator = key;
-        break;
+    if (!root.Symbol.observable) {
+      if (typeof root.Symbol.for === 'function') {
+        root.Symbol.observable = root.Symbol.for('observable');
+      } else {
+        root.Symbol.observable = '@@observable';
       }
     }
-  } else {
-    root.Symbol.iterator = '@@iterator';
+
+    return root.Symbol.observable;
+  }
+
+  private applyIterator(): symbol {
+    const root = this.root;
+
+    if (!root.Symbol.iterator) {
+      if (typeof root.Symbol.for === 'function') {
+        root.Symbol.iterator = root.Symbol.for('iterator');
+      } else if (root.Set && typeof new root.Set()['@@iterator'] === 'function') {
+        // Bug for mozilla version
+        root.Symbol.iterator = '@@iterator';
+      } else if (root.Map) {
+        // es6-shim specific logic
+        let keys = Object.getOwnPropertyNames(root.Map.prototype);
+        for (let i = 0; i < keys.length; ++i) {
+          let key = keys[i];
+          if (key !== 'entries' && key !== 'size' && root.Map.prototype[key] === root.Map.prototype['entries']) {
+            root.Symbol.iterator = key;
+            break;
+          }
+        }
+      } else {
+        root.Symbol.iterator = '@@iterator';
+      }
+    }
+
+    return root.Symbol.iterator;
+  }
+
+  constructor(private root: any) {
+    if (!root.Symbol) {
+      root.Symbol = {};
+    }
+
+    this.observable = this.applyObservable();
+    this.iterator = this.applyIterator();
   }
 }
-
-if (!root.Symbol.dispose) {
-  if (typeof root.Symbol.for === 'function') {
-    root.Symbol.dispose = root.Symbol.for('dispose');
-  } else {
-    root.Symbol.dispose = '@@dispose';
-  }
-}
-
-export module SymbolShim {
-  export const observable: symbol = root.Symbol.observable;
-  export const iterator: symbol = root.Symbol.iterator;
-  export const dispose: symbol = root.Symbol.dispose;
-};
+export const SymbolShim = new SymbolDefinition(root);
