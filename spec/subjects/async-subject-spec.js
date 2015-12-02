ff53be678c030f61b32203874fb74d0b7509fd37
Luís Gabriel Lima <lampih@gmail.com>
2015-12-02 18:21:08 -0300

    refactor(AsyncSubject): minor refactor on spec file
    Also remove a duplicated test case.

--- spec/subjects/async-subject-spec.js
+++ spec/subjects/async-subject-spec.js
@@ -1,130 +1,84 @@
 var Rx = require('../../dist/cjs/Rx');
 
 var AsyncSubject = Rx.AsyncSubject;
-var nextTick = Rx.Scheduler.nextTick;
 var Observable = Rx.Observable;
+
+var TestObserver = function () {
+  this.results = [];
+};
+
+TestObserver.prototype = {
+  next: function (x) { this.results.push(x); },
+  error: function (err) { this.results.push(err); },
+  complete: function () { this.results.push('done'); }
+};
 
 describe('AsyncSubject', function () {
   it('should emit the last value when complete', function () {
     var subject = new AsyncSubject();
-    var results = [];
-
-    subject.subscribe(function (x) {
-      results.push(x);
-    }, null, function () {
-      results.push('done');
-    });
+    var observer = new TestObserver();
+    subject.subscribe(observer);
 
     subject.next(1);
-    expect(results).toEqual([]);
+    expect(observer.results).toEqual([]);
     subject.next(2);
-    expect(results).toEqual([]);
+    expect(observer.results).toEqual([]);
     subject.complete();
-    expect(results).toEqual([2, 'done']);
+    expect(observer.results).toEqual([2, 'done']);
   });
 
-  it('should emit the last value to subsequent subscriptions after complete', function () {
+  it('should keep emitting the last value to subsequent subscriptions', function () {
     var subject = new AsyncSubject();
-    var results = [];
-
-    var observer = {
-      next: function (x) {
-        results.push(x);
-      },
-      error: null,
-      complete: function () {
-        results.push('done');
-      }
-    };
+    var observer = new TestObserver();
     var subscription = subject.subscribe(observer);
 
     subject.next(1);
-    expect(results).toEqual([]);
+    expect(observer.results).toEqual([]);
     subject.next(2);
-    expect(results).toEqual([]);
+    expect(observer.results).toEqual([]);
     subject.complete();
-    expect(results).toEqual([2, 'done']);
+    expect(observer.results).toEqual([2, 'done']);
 
     subscription.unsubscribe();
 
-    results = [];
+    observer.results = [];
     subject.subscribe(observer);
-    expect(results).toEqual([2, 'done']);
+    expect(observer.results).toEqual([2, 'done']);
   });
 
   it('should just complete if no value has been nexted into it', function () {
     var subject = new AsyncSubject();
-    var results = [];
+    var observer = new TestObserver();
+    subject.subscribe(observer);
 
-    subject.subscribe(function (x) {
-      results.push(x);
-    }, null, function () {
-      results.push('done');
-    });
-
-    expect(results).toEqual([]);
+    expect(observer.results).toEqual([]);
     subject.complete();
-    expect(results).toEqual(['done']);
+    expect(observer.results).toEqual(['done']);
   });
 
-  it('should just complete to any subscription if no value has been nexted into it', function () {
+  it('should keep emitting complete to subsequent subscriptions', function () {
     var subject = new AsyncSubject();
-    var results = [];
-
-    var observer = {
-      next: function (x) {
-        results.push(x);
-      },
-      error: null,
-      complete: function () {
-        results.push('done');
-      }
-    };
+    var observer = new TestObserver();
     var subscription = subject.subscribe(observer);
 
-    expect(results).toEqual([]);
+    expect(observer.results).toEqual([]);
     subject.complete();
-    expect(results).toEqual(['done']);
+    expect(observer.results).toEqual(['done']);
 
     subscription.unsubscribe();
-    results = [];
+    observer.results = [];
     subject.subscribe(observer);
-    expect(results).toEqual(['done']);
+    expect(observer.results).toEqual(['done']);
   });
 
   it('should only error if an error is passed into it', function () {
     var subject = new AsyncSubject();
-    var results = [];
-
-    subject.subscribe(function (x) {
-      results.push(x);
-    }, function (err) {
-      results.push(err);
-    }, function () {
-      throw 'should not be called';
-    });
+    var observer = new TestObserver();
+    subject.subscribe(observer);
 
     subject.next(1);
-    expect(results).toEqual([]);
+    expect(observer.results).toEqual([]);
     subject.error(new Error('bad'));
-    expect(results).toEqual([new Error('bad')]);
-  });
-
-  it('should only error if an error is passed into it', function () {
-    var subject = new AsyncSubject();
-    var results = [];
-
-    subject.subscribe(function (x) {
-      results.push(x);
-    }, function (err) {
-      results.push(err);
-    }, function () {
-      throw 'should not be called';
-    });
-
-    subject.next(1);
-    expect(results).toEqual([]);
-    subject.error(new Error('bad'));
-    expect(results).toEqual([new Error('bad')]);
+    expect(observer.results).toEqual([new Error('bad')]);
   });
 });
