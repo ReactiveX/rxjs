fe0eb37aa06123e6fa59751aa5ff8950dab3e23a
OJ Kwon <kwon.ohjoong@gmail.com>
2015-12-03 13:04:07 -0800

    feat(mergeScan): support concurrency parameter for mergeScan
    - expose concurrency parameter to interface of mergeScan
    - expand test coverage to test concurrency works
    
    closes #868

--- src/operator/extended/mergeScan.ts
+++ src/operator/extended/mergeScan.ts
@@ -7,14 +7,16 @@ import {errorObject} from '../../util/errorObject';
 import {subscribeToResult} from '../../util/subscribeToResult';
 import {OuterSubscriber} from '../../OuterSubscriber';
 
-export function mergeScan<T, R>(project: (acc: R, x: T) => Observable<R>, seed: R) {
-  return this.lift(new MergeScanOperator(project, seed));
+export function mergeScan<T, R>(project: (acc: R, x: T) => Observable<R>,
+                                seed: R,
+                                concurrent: number = Number.POSITIVE_INFINITY) {
+  return this.lift(new MergeScanOperator(project, seed, concurrent));
 }
 
 export class MergeScanOperator<T, R> implements Operator<T, R> {
   constructor(private project: (acc: R, x: T) => Observable<R>,
               private seed: R,
-              private concurrent: number = Number.POSITIVE_INFINITY) {
+              private concurrent: number) {
   }
 
   call(subscriber: Subscriber<R>): Subscriber<T> {
@@ -34,7 +36,7 @@ export class MergeScanSubscriber<T, R> extends OuterSubscriber<T, R> {
   constructor(destination: Subscriber<R>,
               private project: (acc: R, x: T) => Observable<R>,
               private acc: R,
-              private concurrent: number = Number.POSITIVE_INFINITY) {
+              private concurrent: number) {
     super(destination);
   }
 
