7c3fbc8d7d626c62b5d4c5bfae1b4505a1a52955
OJ Kwon <kwon.ohjoong@gmail.com>
2015-12-04 16:06:23 -0800

    chore(package.json): update make package script to update corresponding build name
    closes #892

--- .make-packages.js
+++ .make-packages.js
@@ -11,14 +11,14 @@ cjsPkg.name = 'rxjs';
 cjsPkg.main = 'Rx.js';
 cjsPkg.typings = 'Rx.d.ts';
 
-//override stuff for ES6 package
-es6Pkg.name = 'rxjs-es6';
-es6Pkg.main = 'Rx.js';
-es6Pkg.typings = 'Rx.d.ts';
-
 fs.writeFileSync('dist/cjs/package.json', JSON.stringify(cjsPkg, null, 2));
 fs.writeFileSync('dist/cjs/LICENSE.txt', fs.readFileSync('./LICENSE.txt').toString());
 fs.writeFileSync('dist/cjs/README.md', fs.readFileSync('./README.md').toString());
+
+//override stuff for ES6 package
+es6Pkg.name = 'rxjs-es';
+es6Pkg.main = 'Rx.js';
+es6Pkg.typings = 'Rx.d.ts';
 
 fs.writeFileSync('dist/es6/package.json', JSON.stringify(es6Pkg, null, 2));
 fs.writeFileSync('dist/es6/LICENSE.txt', fs.readFileSync('./LICENSE.txt').toString());
