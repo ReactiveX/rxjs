e7e87b1b452b04caec0cac34946194e6268e04fc
OJ Kwon <kwon.ohjoong@gmail.com>
2015-12-07 17:54:52 -0800

    feat(Observable): export of, from by default
    closes #894

--- src/add/observable/fromArray.ts
+++
@@ -1,4 +0,0 @@
-import {Observable} from '../../Observable';
-import {ArrayObservable} from '../../observable/fromArray';
-Observable.fromArray = ArrayObservable.create;
-Observable.of = ArrayObservable.of;
