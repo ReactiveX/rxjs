ec69fe7b78585889f68d898eee43f9182ef43e76
Thiago Figueredo Cardoso <tfcardoso@gmail.com>
2015-12-13 00:00:36 -0300

    test(single): remove duplicated test

--- spec/operators/single-spec.js
+++ spec/operators/single-spec.js
@@ -144,19 +144,4 @@ describe('Observable.prototype.single()', function () {
     expectObservable(e1.single(predicate)).toBe(expected, {z: undefined});
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
-
-  it('should not break unsubscription chain when unsubscribed explicitly', function () {
-    var e1 =    hot('--a--b--c--|');
-    var unsub =     '   !        ';
-    var e1subs =    '^  !        ';
-    var expected =  '----        ';
-
-    var result = e1
-      .mergeMap(function (x) { return Observable.of(x); })
-      .single()
-      .mergeMap(function (x) { return Observable.of(x); });
-
-    expectObservable(result, unsub).toBe(expected);
-    expectSubscriptions(e1.subscriptions).toBe(e1subs);
-  });
 });
