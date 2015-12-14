f35dc2c42855990bd268e99d021d43d2a685d076
Jeff Cross <middlefloor@gmail.com>
2015-12-14 13:06:33 -0800

    chore(publish): make global bundle UMD and include in cjs dist

--- .make-packages.js
+++ .make-packages.js
@@ -1,5 +1,6 @@
 var pkg = require('./package.json');
 var fs = require('fs');
+var mkdirp = require('mkdirp');
 
 delete pkg.scripts;
 
@@ -15,7 +16,17 @@ var es6Pkg = Object.assign({}, cjsPkg, {
 fs.writeFileSync('dist/cjs/package.json', JSON.stringify(cjsPkg, null, 2));
 fs.writeFileSync('dist/cjs/LICENSE.txt', fs.readFileSync('./LICENSE.txt').toString());
 fs.writeFileSync('dist/cjs/README.md', fs.readFileSync('./README.md').toString());
+// Bundles for CJS only
+mkdirp('dist/cjs/bundles', function () {
+  fs.writeFileSync('dist/cjs/bundles/Rx.umd.js', fs.readFileSync('dist/global/Rx.umd.js').toString());
+  fs.writeFileSync('dist/cjs/bundles/Rx.umd.min.js', fs.readFileSync('dist/global/Rx.umd.min.js').toString());
+  fs.writeFileSync('dist/cjs/bundles/Rx.umd.min.js.map', fs.readFileSync('dist/global/Rx.umd.min.js.map').toString());
 
-fs.writeFileSync('dist/es6/package.json', JSON.stringify(es6Pkg, null, 2));
-fs.writeFileSync('dist/es6/LICENSE.txt', fs.readFileSync('./LICENSE.txt').toString());
-fs.writeFileSync('dist/es6/README.md', fs.readFileSync('./README.md').toString());
+  fs.writeFileSync('dist/es6/package.json', JSON.stringify(es6Pkg, null, 2));
+  fs.writeFileSync('dist/es6/LICENSE.txt', fs.readFileSync('./LICENSE.txt').toString());
+  fs.writeFileSync('dist/es6/README.md', fs.readFileSync('./README.md').toString());
+});
+
+
+
+
