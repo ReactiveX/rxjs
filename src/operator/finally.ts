0c47316dd789c10a2b940aeda9b2b8dc1289f861
Justin Woo <moomoowoo@gmail.com>
2015-12-15 12:54:32 -0600

    docs(finally): add inline docs for finally

--- src/operator/finally.ts
+++ src/operator/finally.ts
@@ -2,6 +2,12 @@ import {Operator} from '../Operator';
 import {Subscriber} from '../Subscriber';
 import {Subscription} from '../Subscription';
 
+/**
+ * Returns an Observable that mirrors the source Observable, but will call a specified function when
+ * the source terminates on complete or error.
+ * @param {function} finallySelector function to be called when source terminates.
+ * @returns {Observable} an Observable that mirrors the source, but will call the specified function on termination.
+ */
 export function _finally<T>(finallySelector: () => void) {
   return this.lift(new FinallyOperator(finallySelector));
 }
