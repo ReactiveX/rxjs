92d56298306c57cebed7c9e0ecd4765b4f203481
Justin Woo <moomoowoo@gmail.com>
2015-12-15 16:05:06 -0600

    docs(first): add inline docs for first

--- src/operator/first.ts
+++ src/operator/first.ts
@@ -5,6 +5,12 @@ import {tryCatch} from '../util/tryCatch';
 import {errorObject} from '../util/errorObject';
 import {EmptyError} from '../util/EmptyError';
 
+/**
+ * Returns an Observable that emits the first item of the source Observable that matches the specified condition.
+ * Throws an error if matching element is not found.
+ * @param {function} predicate function called with each item to test for condition matching.
+ * @returns {Observable} an Observable of the first item that matches the condition.
+ */
 export function first<T, R>(predicate?: (value: T, index: number, source: Observable<T>) => boolean,
                             resultSelector?: (value: T, index: number) => R,
                             defaultValue?: any): Observable<T> | Observable<R> {
