fa8f8682c18caf1fdd9be68dfeeea67147d9f035
Justin Woo <moomoowoo@gmail.com>
2015-12-15 10:32:22 -0600

    docs(delay): add inline docs to delay

--- src/operator/delay.ts
+++ src/operator/delay.ts
@@ -5,6 +5,13 @@ import {Scheduler} from '../Scheduler';
 import {Subscriber} from '../Subscriber';
 import {Notification} from '../Notification';
 
+/**
+ * Returns an Observable that delays the emission of items from the source Observable
+ * by a given timeout or until a given Date.
+ * @param {number|Date} delay the timeout value or date until which the emission of the source items is delayed.
+ * @param {Scheduler} [scheduler] the Scheduler to use for managing the timers that handle the timeout for each item.
+ * @returns {Observable} an Observable that delays the emissions of the source Observable by the specified timeout or Date.
+ */
 export function delay<T>(delay: number|Date,
                          scheduler: Scheduler = asap) {
   const absoluteDelay = isDate(delay);
