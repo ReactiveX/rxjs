b2cbeff1cb1b9b20b9b17161fa2aeb439dd87c25
Justin Woo <moomoowoo@gmail.com>
2015-12-15 16:02:20 -0600

    docs(findIndex): add inline docs for findIndex

--- src/operator/findIndex.ts
+++ src/operator/findIndex.ts
@@ -1,6 +1,12 @@
 import {Observable} from '../Observable';
 import {FindValueOperator} from './find-support';
 
+/**
+ * Returns an Observable that searches for the first item in the source Observable that
+ * matches the specified condition, and returns the the index of the item in the source.
+ * @param {function} predicate function called with each item to test for condition matching.
+ * @returns {Observable} an Observable of the index of the first item that matches the condition.
+ */
 export function findIndex<T>(predicate: (value: T, index: number, source: Observable<T>) => boolean, thisArg?: any): Observable<number> {
   return this.lift(new FindValueOperator(predicate, this, true, thisArg));
 }
