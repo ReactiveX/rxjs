b1922b64c1ea62385146c240c99cba7949afd956
Justin Woo <moomoowoo@gmail.com>
2015-12-15 10:41:30 -0600

    docs(distinctUntilKeyChanged): add inline docs for distinctUntilKeyChanged

--- src/operator/distinctUntilKeyChanged.ts
+++ src/operator/distinctUntilKeyChanged.ts
@@ -1,5 +1,14 @@
 import {distinctUntilChanged} from './distinctUntilChanged';
 
+/**
+ * Returns an Observable that emits all items emitted by the source Observable that are distinct by comparison from the previous item,
+ * using a property accessed by using the key provided to check if the two items are distinct.
+ * If a comparator function is provided, then it will be called for each item to test for whether or not that value should be emitted.
+ * If a comparator function is not provided, an equality check is used by default.
+ * @param {string} key string key for object property lookup on each item.
+ * @param {function} [compare] optional comparison function called to test if an item is distinct from the previous item in the source.
+ * @returns {Observable} an Observable that emits items from the source Observable with distinct values based on the key specified.
+ */
 export function distinctUntilKeyChanged<T>(key: string, compare?: (x: any, y: any) => boolean) {
   return distinctUntilChanged.call(this, function(x, y) {
     if (compare) {
