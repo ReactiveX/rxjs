9497baf667dfd6d52544ba6686e2ad7e31ca4c31
Justin Woo <moomoowoo@gmail.com>
2015-12-15 09:55:20 -0600

    docs(debounceTime): add inline docs for debounceTime

--- src/operator/debounceTime.ts
+++ src/operator/debounceTime.ts
@@ -5,6 +5,18 @@ import {Scheduler} from '../Scheduler';
 import {Subscription} from '../Subscription';
 import {asap} from '../scheduler/asap';
 
+/**
+ * Returns the source Observable delayed by the computed debounce duration,
+ * with the duration lengthened if a new source item arrives before the delay
+ * duration ends.
+ * In practice, for each item emitted on the source, this operator holds the
+ * latest item, waits for a silence for the `dueTime` length, and only then
+ * emits the latest source item on the result Observable.
+ * Optionally takes a scheduler for manging timers.
+ * @param {number} dueTime the timeout value for the window of time required to not drop the item.
+ * @param {Scheduler} [scheduler] the Scheduler to use for managing the timers that handle the timeout for each item.
+ * @returns {Observable} an Observable the same as source Observable, but drops items.
+ */
 export function debounceTime<T>(dueTime: number, scheduler: Scheduler = asap): Observable<T> {
   return this.lift(new DebounceTimeOperator(dueTime, scheduler));
 }
