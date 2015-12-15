db14a95d56a1ace9811d775007d04e1bb2117e08
Justin Woo <moomoowoo@gmail.com>
2015-12-15 10:41:11 -0600

    docs(distinctUntilChanged): add inline docs for distinctUntilChanged

--- src/operator/distinctUntilChanged.ts
+++ src/operator/distinctUntilChanged.ts
@@ -3,6 +3,13 @@ import {Subscriber} from '../Subscriber';
 import {tryCatch} from '../util/tryCatch';
 import {errorObject} from '../util/errorObject';
 
+/**
+ * Returns an Observable that emits all items emitted by the source Observable that are distinct by comparison from the previous item.
+ * If a comparator function is provided, then it will be called for each item to test for whether or not that value should be emitted.
+ * If a comparator function is not provided, an equality check is used by default.
+ * @param {function} [compare] optional comparison function called to test if an item is distinct from the previous item in the source.
+ * @returns {Observable} an Observable that emits items from the source Observable with distinct values.
+ */
 export function distinctUntilChanged<T>(compare?: (x: any, y: any) => boolean, keySelector?: (x: T) => any) {
   return this.lift(new DistinctUntilChangedOperator(compare, keySelector));
 }
