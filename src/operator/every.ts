d0549d4fc8c5fa15f7903a492e792afd57b752ad
Justin Woo <moomoowoo@gmail.com>
2015-12-15 12:36:49 -0600

    docs(every): add inline docs for every

--- src/operator/every.ts
+++ src/operator/every.ts
@@ -8,6 +8,12 @@ import {Subscriber} from '../Subscriber';
 import {tryCatch} from '../util/tryCatch';
 import {errorObject} from '../util/errorObject';
 
+/**
+ * Returns an Observable that emits whether or not every item of the source satisfies the condition specified.
+ * @param {function} predicate a function for determining if an item meets a specified condition.
+ * @param {any} [thisArg] optional object to use for `this` in the callback
+ * @returns {Observable} an Observable of booleans that determines if all items of the source Observable meet the condition specified.
+ */
 export function every<T>(predicate: (value: T, index: number, source: Observable<T>) => boolean,
                          thisArg?: any): Observable<boolean> {
   const source = this;
