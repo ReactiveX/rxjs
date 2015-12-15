a8c728b6d965f82bbb061cca765c800248746dfa
Justin Woo <moomoowoo@gmail.com>
2015-12-15 14:31:36 -0600

    docs(exhaust): add inline docs to exhaust

--- src/operator/exhaust.ts
+++ src/operator/exhaust.ts
@@ -5,6 +5,13 @@ import {Subscription} from '../Subscription';
 import {OuterSubscriber} from '../OuterSubscriber';
 import {subscribeToResult} from '../util/subscribeToResult';
 
+/**
+ * Returns an Observable that takes a source of observables and propagates the first observable exclusively
+ * until it completes before subscribing to the next.
+ * Items that come in before the first has exhausted will be dropped.
+ * Similar to `concatAll`, but will not hold on to items that come in before the first is exhausted.
+ * @returns {Observable} an Observable which contains all of the items of the first Observable and following Observables in the source.
+ */
 export function exhaust<T>(): Observable<T> {
   return this.lift(new SwitchFirstOperator());
 }
