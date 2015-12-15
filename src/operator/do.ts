09f6736516034bd2f5b578791f10dd869831f873
Justin Woo <moomoowoo@gmail.com>
2015-12-15 10:46:06 -0600

    docs(do): add inline docs for do

--- src/operator/do.ts
+++ src/operator/do.ts
@@ -6,6 +6,15 @@ import {noop} from '../util/noop';
 import {tryCatch} from '../util/tryCatch';
 import {errorObject} from '../util/errorObject';
 
+/**
+ * Returns a mirrored Observable of the source Observable, but modified so that the provided Observer is called
+ * for every item emitted by the source.
+ * This operator is useful for debugging your observables for the correct values or performing other side effects.
+ * @param {Observer|function} [nextOrObserver] a normal observer callback or callback for onNext.
+ * @param {function} [error] callback for errors in the source.
+ * @param {function} [complete] callback for the completion of the source.
+ * @reurns {Observable} a mirrored Observable with the specified Observer or callback attached for each item.
+ */
 export function _do<T>(nextOrObserver?: Observer<T>|((x: T) => void), error?: (e: any) => void, complete?: () => void) {
   let next;
   if (nextOrObserver && typeof nextOrObserver === 'object') {
