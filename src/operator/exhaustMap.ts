5eba86f31d660136b0fa14f73f911b7d471b9849
Justin Woo <moomoowoo@gmail.com>
2015-12-15 15:16:13 -0600

    docs(exhaustMap): add inline docs exhaustMap

--- src/operator/exhaustMap.ts
+++ src/operator/exhaustMap.ts
@@ -6,6 +6,13 @@ import {errorObject} from '../util/errorObject';
 import {OuterSubscriber} from '../OuterSubscriber';
 import {subscribeToResult} from '../util/subscribeToResult';
 
+/**
+ * Returns an Observable that applies the given function to each item of the source Observable
+ * to create a new Observable, which are then concatenated together to produce a new Observable.
+ * @param {function} project function called for each item of the source to produce a new Observable.
+ * @param {function} [resultSelector] optional function for then selecting on each inner Observable.
+ * @returns {Observable} an Observable containing all the projected Observables of each item of the source concatenated together.
+ */
 export function exhaustMap<T, R, R2>(project: (value: T, index: number) => Observable<R>,
                                      resultSelector?: (outerValue: T,
                                                        innerValue: R,
