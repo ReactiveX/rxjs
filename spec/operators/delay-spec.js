3eebf509b00ecdcb1436699b8fd5c92ea229da98
Andre Staltz <andre@staltz.com>
2015-12-17 17:24:18 +0200

    test(delay): use time() test helper in delay() tests

--- spec/operators/delay-spec.js
+++ spec/operators/delay-spec.js
@@ -4,98 +4,126 @@ var Observable = Rx.Observable;
 
 describe('Observable.prototype.delay()', function () {
   it.asDiagram('delay')('should delay by specified timeframe', function () {
-    var e1 =   hot('---a--b--|');
+    var e1 =   hot('---a--b--|  ');
+    var t =   time(   '--|      ');
     var expected = '-----a--b--|';
     var subs =     '^          !';
 
-    expectObservable(e1.delay(20, rxTestScheduler)).toBe(expected);
+    var result = e1.delay(t, rxTestScheduler);
+
+    expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(subs);
   });
 
   it('should delay by absolute time period', function () {
-    var e1 =   hot('--a--b--|');
+    var e1 =   hot('--a--b--|  ');
+    var t =   time(  '---|     ');
     var expected = '-----a--b--|';
     var subs =     '^          !';
-    var absoluteDelay = new Date(rxTestScheduler.now() + 30);
 
-    expectObservable(e1.delay(absoluteDelay, rxTestScheduler)).toBe(expected);
+    var absoluteDelay = new Date(rxTestScheduler.now() + t);
+    var result = e1.delay(absoluteDelay, rxTestScheduler);
+
+    expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(subs);
   });
 
   it('should delay by absolute time period after subscription', function () {
-    var e1 =   hot('---^--a--b--|');
+    var e1 =   hot('---^--a--b--|  ');
+    var t =   time(      '---|     ');
     var expected =    '------a--b--|';
     var subs =        '^           !';
-    var absoluteDelay = new Date(rxTestScheduler.now() + 30);
 
-    expectObservable(e1.delay(absoluteDelay, rxTestScheduler)).toBe(expected);
+    var absoluteDelay = new Date(rxTestScheduler.now() + t);
+    var result = e1.delay(absoluteDelay, rxTestScheduler);
+
+    expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(subs);
   });
 
   it('should raise error when source raises error', function () {
     var e1 =   hot('---a---b---#');
+    var t =   time(   '---|     ');
     var expected = '------a---b#';
     var subs =     '^          !';
 
-    expectObservable(e1.delay(30, rxTestScheduler)).toBe(expected);
+    var result = e1.delay(t, rxTestScheduler);
+
+    expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(subs);
   });
 
   it('should raise error when source raises error', function () {
     var e1 =   hot('--a--b--#');
+    var t =   time(  '---|   ');
     var expected = '-----a--#';
     var subs =     '^       !';
-    var absoluteDelay = new Date(rxTestScheduler.now() + 30);
 
-    expectObservable(e1.delay(absoluteDelay, rxTestScheduler)).toBe(expected);
+    var absoluteDelay = new Date(rxTestScheduler.now() + t);
+    var result = e1.delay(absoluteDelay, rxTestScheduler);
+
+    expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(subs);
   });
 
   it('should raise error when source raises error after subscription', function () {
     var e1 =   hot('---^---a---b---#');
+    var t =   time(       '---|     ');
     var expected =    '-------a---b#';
     var e1Sub =       '^           !';
-    var absoluteDelay = new Date(rxTestScheduler.now() + 30);
 
-    expectObservable(e1.delay(absoluteDelay, rxTestScheduler)).toBe(expected);
+    var absoluteDelay = new Date(rxTestScheduler.now() + t);
+    var result = e1.delay(absoluteDelay, rxTestScheduler);
+
+    expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1Sub);
   });
 
   it('should delay when source does not emits', function () {
-    var e1 =   hot('----|');
+    var e1 =   hot('----|   ');
+    var t =   time(    '---|');
     var expected = '-------|';
     var subs =     '^      !';
 
-    expectObservable(e1.delay(30, rxTestScheduler)).toBe(expected);
+    var result = e1.delay(t, rxTestScheduler);
+
+    expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(subs);
   });
 
   it('should delay when source is empty', function () {
-    var e1 = Observable.empty();
+    var e1 =  cold('|');
+    var t =   time('---|');
     var expected = '---|';
 
-    expectObservable(e1.delay(30, rxTestScheduler)).toBe(expected);
+    var result = e1.delay(t, rxTestScheduler);
+
+    expectObservable(result).toBe(expected);
   });
 
   it('should not complete when source does not completes', function () {
-    var e1 =   hot('---a---b-');
-    var expected = '------a---b-';
+    var e1 =   hot('---a---b---------');
+    var t =   time(   '---|          ');
+    var expected = '------a---b------';
     var unsub =    '----------------!';
     var subs =     '^               !';
 
-    expectObservable(e1.delay(30, rxTestScheduler), unsub).toBe(expected);
+    var result = e1.delay(t, rxTestScheduler);
+
+    expectObservable(result, unsub).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(subs);
   });
 
   it('should not break unsubscription chains when result is unsubscribed explicitly', function () {
     var e1 =   hot('---a---b----');
+    var t =   time(   '---|     ');
     var e1subs =   '^       !   ';
     var expected = '------a--   ';
     var unsub =    '        !   ';
 
     var result = e1
       .mergeMap(function (x) { return Observable.of(x); })
-      .delay(30, rxTestScheduler)
+      .delay(t, rxTestScheduler)
       .mergeMap(function (x) { return Observable.of(x); });
 
     expectObservable(result, unsub).toBe(expected);
@@ -103,9 +131,12 @@ describe('Observable.prototype.delay()', function () {
   });
 
   it('should not complete when source never completes', function () {
-    var e1 = Observable.never();
+    var e1 =  cold('-');
+    var t =   time('---|');
     var expected = '-';
 
-    expectObservable(e1.delay(30, rxTestScheduler)).toBe(expected);
+    var result = e1.delay(t, rxTestScheduler);
+
+    expectObservable(result).toBe(expected);
   });
 });
