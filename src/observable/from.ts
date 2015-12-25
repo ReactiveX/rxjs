c962f41fc5561772b77a68b828e0a36e01e375f3
Paul Taylor <paul.e.taylor@me.com>
2015-12-24 17:44:22 -0800

    refactor(promises): Update from/fromPromise/subscribeToResult.
    Use isPromise utility function to check if an instance is a Promise. Import and use `root.setTimeout` instead of using global `setTimeout` definition.

--- src/observable/from.ts
+++ src/observable/from.ts
@@ -1,3 +1,5 @@
+import {isArray} from '../util/isArray';
+import {isPromise} from '../util/isPromise';
 import {PromiseObservable} from './fromPromise';
 import {IteratorObservable} from'./IteratorObservable';
 import {ArrayObservable} from './fromArray';
@@ -9,30 +11,29 @@ import {Subscriber} from '../Subscriber';
 import {ObserveOnSubscriber} from '../operator/observeOn-support';
 import {queue} from '../scheduler/queue';
 
-const isArray = Array.isArray;
 
 export class FromObservable<T> extends Observable<T> {
   constructor(private ish: any, private scheduler: Scheduler) {
     super(null);
   }
 
-  static create<T>(ish: any, scheduler: Scheduler = queue): Observable<T> {
-    if (ish) {
-      if (isArray(ish)) {
-        return new ArrayObservable(ish, scheduler);
-      } else if (typeof ish.then === 'function') {
-        return new PromiseObservable(ish, scheduler);
-      } else if (typeof ish[SymbolShim.observable] === 'function') {
-        if (ish instanceof Observable) {
+  static create<T>(ish: any, scheduler: Scheduler = null): Observable<T> {
+    if (ish != null) {
+      if (typeof ish[SymbolShim.observable] === 'function') {
+        if (ish instanceof Observable && !scheduler) {
           return ish;
         }
         return new FromObservable(ish, scheduler);
+      } if (isArray(ish)) {
+        return new ArrayObservable(ish, scheduler);
+      } else if (isPromise(ish)) {
+        return new PromiseObservable(ish, scheduler);
       } else if (typeof ish[SymbolShim.iterator] === 'function') {
         return new IteratorObservable(ish, null, null, scheduler);
       }
     }
 
-    throw new TypeError((typeof ish) + ' is not observable');
+    throw new TypeError((ish !== null && typeof ish || ish) + ' is not observable');
   }
 
   _subscribe(subscriber: Subscriber<T>) {
