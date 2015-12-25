6b2aaf9f10ff022859eceee7581266283f75d815
Paul Taylor <paul.e.taylor@me.com>
2015-12-24 17:21:12 -0800

    refactor(operator): Add default operator implementation.

--- src/Operator.ts
+++ src/Operator.ts
@@ -1,10 +1,7 @@
-import {Observer} from './Observer';
 import {Subscriber} from './Subscriber';
 
-export interface Operator<T, R> {
-  call<T, R>(subscriber: Subscriber<R>): Subscriber<T>;
-}
-
-export function defaultCallFn<T, R>(observer: Observer<R>): Observer<T> {
-  return new Subscriber<T>(observer);
+export class Operator<T, R> {
+  call<T, R>(subscriber: Subscriber<R>): Subscriber<T> {
+    return new Subscriber<T>(subscriber);
+  }
 }
