235dac614cf04ee8f6a9688a77378c031d84ed77
Jake Murzy <jake@murzy.com>
2016-01-19 02:50:32 -0800

    refactor(Rx): import missing pairwise operator

--- src/Rx.ts
+++ src/Rx.ts
@@ -68,6 +68,7 @@ import './add/operator/mergeMap';
 import './add/operator/mergeMapTo';
 import './add/operator/multicast';
 import './add/operator/observeOn';
+import './add/operator/pairwise';
 import './add/operator/partition';
 import './add/operator/pluck';
 import './add/operator/publish';
