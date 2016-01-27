359ccaeeb0d0b4a07dee5345edd13556b10cd26b
Ben Lesh <blesh@netflix.com>
2016-01-26 20:41:47 -0800

    chore(withLatestFrom): add micro perf test

---
+++ perf/micro/immediate-scheduler/operators/withlatestfrom.js
@@ -0,0 +1,27 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  function add(x, y) {
+    return x + y;
+  }
+  var _old = RxOld.Observable.range(0, 25).withLatestFrom(
+    RxOld.Observable.range(0, 25),
+    add
+  );
+  var _new = RxNew.Observable.range(0, 25).withLatestFrom(
+    RxNew.Observable.range(0, 25),
+    add
+  );
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old withLatestFrom with immediate scheduler', function () {
+        _old.subscribe(_next, _error, _complete);
+      })
+      .add('new withLatestFrom with immediate scheduler', function () {
+        _new.subscribe(_next, _error, _complete);
+      });
+};
