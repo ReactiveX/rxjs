66678aa510fd7726a690bb17bf8dc0982b96fe5f
Ben Lesh <blesh@netflix.com>
2016-02-02 14:18:21 -0800

    chore(build): add gzip size checking tool
    LOL: KitchenSink is SMALLER gzipped than core

---
+++ tools/check-gzip-size.js
@@ -0,0 +1,24 @@
+var gzipSize = require('gzip-size');
+var fs = require('fs');
+var path = require('path');
+
+var files = ['../dist/global/Rx.min.js', '../dist/global/Rx.KitchenSink.min.js'];
+
+
+files.map(getGzipSize).forEach(function (size, i) {
+    console.log(path.basename(files[i]) + ': ' + formatSize(size) + ' gzipped');
+});
+
+function formatSize(size) {
+    return (size / 1000).toFixed(2) + 'kB';
+}
+
+function getGzipSize(file) {
+    var fileLoc = path.resolve(__dirname, file);
+
+    if (!fs.existsSync(fileLoc)) {
+        throw new Error('file not found: ' + fileLoc);
+    }
+
+    return gzipSize.sync(fs.readFileSync(fileLoc, 'utf8'));
+}
