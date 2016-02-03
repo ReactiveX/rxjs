8e5508d8bb1a87005d361bcbfd42d7aea6674ed6
Luís Gabriel Lima <lampih@gmail.com>
2016-02-03 17:42:57 -0300

    chore(micro-perf): add perf test for mergeMapTo with resultSelector
    Results on a Intel Core i7-3770 (Ivy Bridge) running Ubuntu:
    
                                          |     RxJS 4.0.7 | RxJS 5.0.0-beta.1 | factor | % improved
    -------------------------------------------------------------------------------------------------
    mergemapto-resultselector - immediate |   811 (±1.50%) |    5,376 (±1.10%) |  6.63x |     562.7%
                mergemapto-resultselector |   369 (±0.43%) |    1,957 (±0.45%) |  5.30x |     429.7%

---
+++ perf/micro/current-thread-scheduler/operators/mergemapto-scalar.js
@@ -0,0 +1,20 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  var oldMergeMapWithCurrentThreadScheduler = RxOld.Observable.range(0, 25, RxOld.Scheduler.currentThread)
+    .flatMap(RxOld.Observable.return(0, RxOld.Scheduler.currentThread));
+  var newMergeMapWithCurrentThreadScheduler = RxNew.Observable.range(0, 25, RxNew.Scheduler.queue)
+    .mergeMapTo(RxNew.Observable.of(0, RxNew.Scheduler.queue));
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+    .add('old mergeMap (scalar Observable) with current thread scheduler', function () {
+      oldMergeMapWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+    })
+    .add('new mergeMap (scalar Observable) with current thread scheduler', function () {
+      newMergeMapWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+    });
+};
