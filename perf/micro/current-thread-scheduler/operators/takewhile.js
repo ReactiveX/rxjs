d15e72d3468929fa8d7efa1c2a4fef9616429fe4
Luís Gabriel Lima <lampih@gmail.com>
2016-02-03 12:34:59 -0300

    chore(micro-perf): add perf tests for takeWhile
    Results on a Intel Core i7-3770 (Ivy Bridge) running Ubuntu:
    
                          |      RxJS 4.0.7 | RxJS 5.0.0-beta.1 | factor | % improved
    ----------------------------------------------------------------------------------
    takewhile - immediate | 22,885 (±1.53%) |   17,473 (±0.38%) |  0.76x |     -23.6%
                takewhile |  7,270 (±2.49%) |   14,739 (±0.58%) |  2.03x |     102.7%

---
+++ perf/micro/current-thread-scheduler/operators/takewhile.js
@@ -0,0 +1,21 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  var predicate = function (value) { return value < 48; };
+  var oldTakeWhileWithCurrentThreadScheduler =
+    RxOld.Observable.range(0, 50, RxOld.Scheduler.currentThread).takeWhile(predicate);
+  var newTakeWhileWithCurrentThreadScheduler =
+    RxNew.Observable.range(0, 50, RxNew.Scheduler.queue).takeWhile(predicate);
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old takeWhile with current thread scheduler', function () {
+        oldTakeWhileWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new takeWhile with current thread scheduler', function () {
+        newTakeWhileWithCurrentThreadScheduler.subscribe(_next, _error, _complete);
+      });
+};
---
+++ perf/micro/immediate-scheduler/operators/takewhile.js
@@ -0,0 +1,19 @@
+var RxOld = require('rx');
+var RxNew = require('../../../../index');
+
+module.exports = function (suite) {
+  var predicate = function (value) { return value < 48; };
+  var oldTakeWhileWithImmediateScheduler = RxOld.Observable.range(0, 50, RxOld.Scheduler.immediate).takeWhile(predicate);
+  var newTakeWhileWithImmediateScheduler = RxNew.Observable.range(0, 50).takeWhile(predicate);
+
+  function _next(x) { }
+  function _error(e) { }
+  function _complete() { }
+  return suite
+      .add('old takeWhile with immediate scheduler', function () {
+        oldTakeWhileWithImmediateScheduler.subscribe(_next, _error, _complete);
+      })
+      .add('new takeWhile with immediate scheduler', function () {
+        newTakeWhileWithImmediateScheduler.subscribe(_next, _error, _complete);
+      });
+};
