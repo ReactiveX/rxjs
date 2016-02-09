00db1ca9113178f793114267a7fefa2c36b34615
Ben Lesh <blesh@netflix.com>
2016-02-09 09:44:18 -0800

    test(cache): add test for both count and window time together

--- spec/operators/cache-spec.js
+++ spec/operators/cache-spec.js
@@ -1,4 +1,4 @@
-/* globals describe, it, expect, expectObservable, hot, rxTestScheduler */
+/* globals describe, it, expect, expectObservable, hot, rxTestScheduler, time */
 var Rx = require('../../dist/cjs/Rx');
 
 var asap = Rx.Scheduler.asap;
@@ -121,5 +121,62 @@ describe('Observable.prototype.cache', function () {
     rxTestScheduler.schedule(function () {
       expectObservable(s1).toBe(expected2);
     }, t);
+  });
+
+  it('should multicast a completion', function () {
+    var s1 = hot('--a--^--b------c-----d------e-|').cache();
+    var t1 = time(    '|                         ');
+    var e1 =          '---b------c-----d------e-|';
+    var t2 = time(    '----------|               ');
+    var e2 =          '          (bc)--d------e-|';
+    var t3 = time(    '----------------|         ');
+    var e3 =          '                (bcd)--e-|';
+
+    var expected = [e1, e2, e3];
+
+    [t1, t2, t3].forEach(function (t, i) {
+      rxTestScheduler.schedule(function () {
+        expectObservable(s1).toBe(expected[i]);
+      }, t);
+    });
+  });
+
+  it('should multicast an error', function () {
+    var s1 = hot('--a--^--b------c-----d------e-#').cache();
+    var t1 = time(    '|                         ');
+    var e1 =          '---b------c-----d------e-#';
+    var t2 = time(    '----------|               ');
+    var e2 =          '          (bc)--d------e-#';
+    var t3 = time(    '----------------|         ');
+    var e3 =          '                (bcd)--e-#';
+
+    var expected = [e1, e2, e3];
+
+    [t1, t2, t3].forEach(function (t, i) {
+      rxTestScheduler.schedule(function () {
+        expectObservable(s1).toBe(expected[i]);
+      }, t);
+    });
+  });
+
+  it('should limit replay by both count and a window time, test 2', function () {
+    var w = time(     '-----------|');
+    var s1 = hot('--a--^---b---c----d----e------f--g--h--i-------|').cache(4, w, rxTestScheduler);
+    var e1 =          '----b---c----d----e------f--g--h--i-------|';
+    var t1 = time(    '--------------------|');
+    //                          -----------|
+    var e2 =          '                    (de)-f--g--h--i-------|'; // time wins
+    var t2 = time(    '-----------------------------------|');
+    var e3 =          '                                   (fghi)-|'; // count wins
+    //                                         -----------|
+
+    expectObservable(s1).toBe(e1);
+    rxTestScheduler.schedule(function () {
+      expectObservable(s1).toBe(e2);
+    }, t1);
+
+    rxTestScheduler.schedule(function () {
+      expectObservable(s1).toBe(e3);
+    }, t2);
   });
 });
