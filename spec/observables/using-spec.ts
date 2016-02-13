093a8a2bef171dd8991764c184ec62cf8a5df9c3
Paul Taylor <paul.e.taylor@me.com>
2016-02-12 18:53:29 -0800

    feat(using): add static Observable.using creation operator.

---
+++ spec/observables/using-spec.ts
@@ -0,0 +1,24 @@
+import * as Rx from '../../dist/cjs/Rx.KitchenSink';
+import {it} from '../helpers/test-helper';
+
+const Observable = Rx.Observable;
+const Subscription = Rx.Subscription;
+
+describe('Observable.using', () => {
+  it('should dispose of the resource when the subscription is disposed', (done) => {
+    let disposed = false;
+    const source = Observable.using(
+      () => new Subscription(() => disposed = true),
+      (resource) => Observable.range(0, 3)
+    )
+    .take(2);
+
+    source.subscribe();
+
+    if (disposed) {
+      done();
+    } else {
+      done.fail('disposed should be true but was false');
+    }
+  });
+});
---
+++ src/add/observable/using.ts
@@ -0,0 +1,6 @@
+import {Observable} from '../../Observable';
+import {UsingObservable} from '../../observable/UsingObservable';
+
+Observable.using = UsingObservable.create;
+
+export var _void: void;
