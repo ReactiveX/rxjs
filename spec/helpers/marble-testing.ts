e4ff1fcaed19143c5607e48141ccd6a84c227310
OJ Kwon <kwon.ohjoong@gmail.com>
2016-02-15 19:56:52 -0800

    test(helper): migrate test into typescript compatible
    - migrate helper to typescript, primitive port focus on build pass without regression

---
+++ spec/helpers/marble-testing.ts
@@ -0,0 +1,48 @@
+///<reference path='../../typings/jasmine/jasmine.d.ts'/>
+import {Observable} from '../../dist/cjs/Observable';
+import {SubscriptionLog} from '../../dist/cjs/testing/SubscriptionLog';
+import {ColdObservable} from '../../dist/cjs/testing/ColdObservable';
+import {HotObservable} from '../../dist/cjs/testing/HotObservable';
+import {observableToBeFn, subscriptionLogsToBeFn} from '../../dist/cjs/testing/TestScheduler';
+
+declare const global: any;
+
+export function hot(marbles: string, values?: any, error?: any): HotObservable<any> {
+  if (!global.rxTestScheduler) {
+    throw 'tried to use hot() in async test';
+  }
+  return global.rxTestScheduler.createHotObservable.apply(global.rxTestScheduler, arguments);
+}
+
+export function cold(marbles: string, values?: any, error?: any): ColdObservable<any> {
+  if (!global.rxTestScheduler) {
+    throw 'tried to use cold() in async test';
+  }
+  return global.rxTestScheduler.createColdObservable.apply(global.rxTestScheduler, arguments);
+}
+
+export function expectObservable(observable: Observable<any>,
+                                 unsubscriptionMarbles: string = null): ({ toBe: observableToBeFn }) {
+  if (!global.rxTestScheduler) {
+    throw 'tried to use expectObservable() in async test';
+  }
+  return global.rxTestScheduler.expectObservable.apply(global.rxTestScheduler, arguments);
+}
+
+export function expectSubscriptions(actualSubscriptionLogs: SubscriptionLog[]): ({ toBe: subscriptionLogsToBeFn }) {
+  if (!global.rxTestScheduler) {
+    throw 'tried to use expectSubscriptions() in async test';
+  }
+  return global.rxTestScheduler.expectSubscriptions.apply(global.rxTestScheduler, arguments);
+}
+
+export function assertDeepEqual(actual: any, expected: any): void {
+  (<any>expect(actual)).toDeepEqual(expected);
+}
+
+export function time(marbles: string): number {
+  if (!global.rxTestScheduler) {
+    throw 'tried to use time() in async test';
+  }
+  return global.rxTestScheduler.createTime.apply(global.rxTestScheduler, arguments);
+}
---
+++ spec/helpers/test-helper.ts
@@ -0,0 +1,148 @@
+///<reference path='../../typings/es6-shim/es6-shim.d.ts'/>
+///<reference path='../../typings/jasmine/jasmine.d.ts'/>
+///<reference path='../../typings/lodash/lodash.d.ts'/>
+declare const global: any;
+declare const Symbol: any;
+
+//Fail timeouts faster
+//Individual suites/specs should specify longer timeouts if needed.
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 5000;
+
+import * as _ from 'lodash';
+import * as Rx from '../../dist/cjs/Rx.KitchenSink';
+import {root} from '../../dist/cjs/util/root';
+import {assertDeepEqual} from './marble-testing';
+
+global.rxTestScheduler = null;
+
+//amending type definition of jasmine which seems doesn't have this
+export interface DoneSignature {
+  (): void;
+  fail(description?: string): void;
+}
+
+const defaultAssertion: (expectation: string, assertion?: (done: DoneSignature) => void, timeout?: number) => void = global.it;
+const singleAssertion: (expectation: string, assertion?: (done: DoneSignature) => void, timeout?: number) => void = global.fit;
+
+function assertAction(done: DoneSignature, assertion: (done?: DoneSignature) => void): void {
+  global.rxTestScheduler = new Rx.TestScheduler(assertDeepEqual);
+  let error: any;
+  let errorHappened: boolean = false;
+
+  try {
+    assertion();
+    global.rxTestScheduler.flush();
+  } catch (e) {
+    errorHappened = true;
+    error = e;
+  } finally {
+    if (errorHappened) {
+      setTimeout(function () { done.fail(error); });
+    } else {
+      setTimeout(function () { done(); });
+    }
+  }
+}
+
+export function asDiagram(expectation: string): (expectation: string, assertion?: (done: DoneSignature) => void, timeout?: number) => void {
+  return it;
+}
+
+export function it(expectation: string, assertion?: (done?: DoneSignature) => void, timeout?: number): void {
+  if (assertion.length === 0) {
+    defaultAssertion(expectation, (done: DoneSignature) => {
+      assertAction(done, assertion);
+    });
+  } else {
+    defaultAssertion.apply(this, arguments);
+  }
+}
+
+export function fit(expectation: string, assertion?: (done?: DoneSignature) => void, timeout?: number): void {
+  if (assertion.length === 0) {
+    singleAssertion(expectation, (done: DoneSignature) => {
+      assertAction(done, assertion);
+    });
+  } else {
+    singleAssertion.apply(this, arguments);
+  }
+}
+
+export function lowerCaseO<T>(...args): Rx.Observable<T> {
+  const values = [].slice.apply(arguments);
+
+  const o = {
+    subscribe: function (observer) {
+      values.forEach(function (v) {
+        observer.next(v);
+      });
+      observer.complete();
+    }
+  };
+
+  o[Symbol.observable] = function () {
+    return this;
+  };
+
+  return <any>o;
+};
+
+function stringify(x) {
+  return JSON.stringify(x, function (key, value) {
+    if (Array.isArray(value)) {
+      return '[' + value
+        .map(function (i) {
+          return '\n\t' + stringify(i);
+        }) + '\n]';
+    }
+    return value;
+  })
+  .replace(/\\"/g, '"')
+  .replace(/\\t/g, '\t')
+  .replace(/\\n/g, '\n');
+}
+
+beforeEach(function () {
+  jasmine.addMatchers({
+    toDeepEqual: function (util, customEqualityTesters) {
+      return {
+        compare: function (actual, expected) {
+          let result: any = { pass: _.isEqual(actual, expected) };
+
+          if (!result.pass && Array.isArray(actual) && Array.isArray(expected)) {
+            result.message = 'Expected \n';
+            actual.forEach(function (x) {
+              result.message += stringify(x) + '\n';
+            });
+            result.message += '\nto deep equal \n';
+            expected.forEach(function (x) {
+              result.message += stringify(x) + '\n';
+            });
+          }
+
+          return result;
+        }
+      };
+    }
+  });
+});
+
+Object.defineProperty(Error.prototype, 'toJSON', {
+  value: function () {
+    const alt = {};
+
+    Object.getOwnPropertyNames(this).forEach(function (key) {
+      if (key !== 'stack') {
+        alt[key] = this[key];
+      }
+    }, this);
+    return alt;
+  },
+  configurable: true
+});
+
+global.__root__ = root;
+
+afterEach(function () {
+  global.rxTestScheduler = null;
+});
