6e421743afd4e71ddf3629932ace28b583ac6dd6
Andre Staltz <andre@staltz.com>
2016-02-19 11:25:26 +0200

    chore(package.json): rename docgen to publish_docs, add build_docs script
    Renamed npm script build_docs => publish_docs, because that's what it actually does, and renamed
    docgen.sh to publish_docs.sh. Then, made npm script build_docs that simply builds the docs webpage,
    without publishing.

--- publish_docs.sh
+++ publish_docs.sh
@@ -4,9 +4,7 @@
 # WARNING: Do NOT run this script unless you have remote `upstream` set properly
 #
 rm -rf tmp/docs
-npm run build_es6
-npm run tests2png
-./node_modules/.bin/esdoc -c esdoc.json
+npm run build_docs
 git checkout gh-pages
 git fetch upstream
 git rebase upstream/gh-pages
