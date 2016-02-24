9dbccf480a1e300fe2ca9db50db6e3ef736f9ead
David Driscoll <david.driscoll@gmail.com>
2016-02-24 17:37:15 -0500

    refactor(build): standardized how Rx.KitchenSink and Rx.DOM are built

--- src/Rx.DOM.ts
+++ src/Rx.DOM.ts
@@ -1,171 +1,25 @@
-/* tslint:disable:no-unused-variable */
-// Subject imported before Observable to bypass circular dependency issue since
-// Subject extends Observable and Observable references Subject in it's
-// definition
-import {Subject} from './Subject';
-/* tslint:enable:no-unused-variable */
-import {Observable} from './Observable';
-
-// statics
-/* tslint:disable:no-use-before-declare */
-import './add/observable/combineLatest';
-import './add/observable/concat';
-import './add/observable/merge';
-import './add/observable/bindCallback';
-import './add/observable/defer';
-import './add/observable/empty';
-import './add/observable/forkJoin';
-import './add/observable/from';
-import './add/observable/fromArray';
-import './add/observable/fromEvent';
-import './add/observable/fromEventPattern';
-import './add/observable/fromPromise';
-import './add/observable/interval';
-import './add/observable/never';
-import './add/observable/range';
-import './add/observable/throw';
-import './add/observable/timer';
-import './add/observable/zip';
+export * from './Rx';
 
 import './add/observable/dom/ajax';
 import './add/observable/dom/webSocket';
 
-//operators
-import './add/operator/buffer';
-import './add/operator/bufferCount';
-import './add/operator/bufferTime';
-import './add/operator/bufferToggle';
-import './add/operator/bufferWhen';
-import './add/operator/catch';
-import './add/operator/combineAll';
-import './add/operator/combineLatest';
-import './add/operator/concat';
-import './add/operator/concatAll';
-import './add/operator/concatMap';
-import './add/operator/concatMapTo';
-import './add/operator/count';
-import './add/operator/dematerialize';
-import './add/operator/debounce';
-import './add/operator/debounceTime';
-import './add/operator/defaultIfEmpty';
-import './add/operator/delay';
-import './add/operator/delayWhen';
-import './add/operator/distinctUntilChanged';
-import './add/operator/do';
-import './add/operator/expand';
-import './add/operator/filter';
-import './add/operator/finally';
-import './add/operator/first';
-import './add/operator/groupBy';
-import './add/operator/ignoreElements';
-import './add/operator/inspect';
-import './add/operator/inspectTime';
-import './add/operator/every';
-import './add/operator/last';
-import './add/operator/map';
-import './add/operator/mapTo';
-import './add/operator/materialize';
-import './add/operator/merge';
-import './add/operator/mergeAll';
-import './add/operator/mergeMap';
-import './add/operator/mergeMapTo';
-import './add/operator/multicast';
-import './add/operator/observeOn';
-import './add/operator/partition';
-import './add/operator/publish';
-import './add/operator/publishBehavior';
-import './add/operator/publishReplay';
-import './add/operator/publishLast';
-import './add/operator/reduce';
-import './add/operator/repeat';
-import './add/operator/retry';
-import './add/operator/retryWhen';
-import './add/operator/sample';
-import './add/operator/sampleTime';
-import './add/operator/scan';
-import './add/operator/share';
-import './add/operator/single';
-import './add/operator/skip';
-import './add/operator/skipUntil';
-import './add/operator/skipWhile';
-import './add/operator/startWith';
-import './add/operator/subscribeOn';
-import './add/operator/switch';
-import './add/operator/switchMap';
-import './add/operator/switchMapTo';
-import './add/operator/take';
-import './add/operator/takeLast';
-import './add/operator/takeUntil';
-import './add/operator/takeWhile';
-import './add/operator/throttle';
-import './add/operator/throttleTime';
-import './add/operator/timeout';
-import './add/operator/timeoutWith';
-import './add/operator/toArray';
-import './add/operator/toPromise';
-import './add/operator/window';
-import './add/operator/windowCount';
-import './add/operator/windowTime';
-import './add/operator/windowToggle';
-import './add/operator/windowWhen';
-import './add/operator/withLatestFrom';
-import './add/operator/zip';
-import './add/operator/zipAll';
+export {AjaxRequest, AjaxResponse, AjaxError, AjaxTimeoutError} from './observable/dom/AjaxObservable';
 
-/* tslint:disable:no-unused-variable */
-import {Subscription, UnsubscriptionError} from './Subscription';
-import {Subscriber} from './Subscriber';
-import {AsyncSubject} from './subject/AsyncSubject';
-import {ReplaySubject} from './subject/ReplaySubject';
-import {BehaviorSubject} from './subject/BehaviorSubject';
-import {ConnectableObservable} from './observable/ConnectableObservable';
-import {Notification} from './Notification';
-import {EmptyError} from './util/EmptyError';
-import {ArgumentOutOfRangeError} from './util/ArgumentOutOfRangeError';
-import {ObjectUnsubscribedError} from './util/ObjectUnsubscribedError';
+// Rebuild `Scheduler` for Rx.DOM
 import {asap} from './scheduler/asap';
 import {async} from './scheduler/async';
 import {queue} from './scheduler/queue';
 import {animationFrame} from './scheduler/animationFrame';
+/* tslint:disable:no-unused-variable */
 import {AsapScheduler} from './scheduler/AsapScheduler';
 import {AsyncScheduler} from './scheduler/AsyncScheduler';
 import {QueueScheduler} from './scheduler/QueueScheduler';
 import {AnimationFrameScheduler} from './scheduler/AnimationFrameScheduler';
-import {rxSubscriber} from './symbol/rxSubscriber';
-import {AjaxRequest, AjaxResponse, AjaxError, AjaxTimeoutError} from './observable/dom/AjaxObservable';
 /* tslint:enable:no-unused-variable */
 
-/* tslint:disable:no-var-keyword */
-var Scheduler = {
+export var Scheduler = {
   asap,
   async,
   queue,
   animationFrame
-};
-
-var Symbol = {
-  rxSubscriber
-};
-/* tslint:enable:no-var-keyword */
-
-export {
-  AjaxRequest,
-  AjaxResponse,
-  AjaxError,
-  AjaxTimeoutError,
-  Subject,
-  Scheduler,
-  Observable,
-  Subscriber,
-  Subscription,
-  Symbol,
-  AsyncSubject,
-  ReplaySubject,
-  BehaviorSubject,
-  ConnectableObservable,
-  Notification,
-  EmptyError,
-  ArgumentOutOfRangeError,
-  ObjectUnsubscribedError,
-  UnsubscriptionError
 };
--- src/Rx.KitchenSink.ts
+++ src/Rx.KitchenSink.ts
@@ -1,9 +1,4 @@
-/* tslint:disable:no-unused-variable */
-import {Subject} from './Subject';
-/* tslint:enable:no-unused-variable */
-import {Observable} from './Observable';
-
-import './Rx';
+export * from './Rx';
 
 // statics
 import './add/observable/using';
@@ -23,63 +18,3 @@ import './add/operator/mergeScan';
 import './add/operator/min';
 import './add/operator/pairwise';
 import './add/operator/timeInterval';
-
-/* tslint:disable:no-unused-variable */
-import {Operator} from './Operator';
-import {Observer} from './Observer';
-import {Subscription, UnsubscriptionError} from './Subscription';
-import {Subscriber} from './Subscriber';
-import {AsyncSubject} from './subject/AsyncSubject';
-import {ReplaySubject} from './subject/ReplaySubject';
-import {BehaviorSubject} from './subject/BehaviorSubject';
-import {ConnectableObservable} from './observable/ConnectableObservable';
-import {Notification} from './Notification';
-import {EmptyError} from './util/EmptyError';
-import {ObjectUnsubscribedError} from './util/ObjectUnsubscribedError';
-import {ArgumentOutOfRangeError} from './util/ArgumentOutOfRangeError';
-import {asap} from './scheduler/asap';
-import {async} from './scheduler/async';
-import {queue} from './scheduler/queue';
-import {AsapScheduler} from './scheduler/AsapScheduler';
-import {AsyncScheduler} from './scheduler/AsyncScheduler';
-import {QueueScheduler} from './scheduler/QueueScheduler';
-import {TimeInterval} from './operator/timeInterval';
-import {TestScheduler} from './testing/TestScheduler';
-import {VirtualTimeScheduler} from './scheduler/VirtualTimeScheduler';
-import {rxSubscriber} from './symbol/rxSubscriber';
-/* tslint:enable:no-unused-variable */
-
-/* tslint:disable:no-var-keyword */
-var Scheduler = {
-  asap,
-  async,
-  queue
-};
-
-var Symbol = {
-  rxSubscriber
-};
-/* tslint:enable:no-var-keyword */
-
-export {
-    Subject,
-    Scheduler,
-    Observable,
-    Observer,
-    Operator,
-    Subscriber,
-    Subscription,
-    AsyncSubject,
-    ReplaySubject,
-    BehaviorSubject,
-    ConnectableObservable,
-    Notification,
-    EmptyError,
-    ArgumentOutOfRangeError,
-    ObjectUnsubscribedError,
-    UnsubscriptionError,
-    TestScheduler,
-    VirtualTimeScheduler,
-    TimeInterval,
-    Symbol
-};
