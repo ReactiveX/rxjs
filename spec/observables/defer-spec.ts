7ace877178f6babf83b68c6102c5b54d7d4c0d9e
OJ Kwon <kwon.ohjoong@gmail.com>
2016-03-16 21:04:01 -0700

    test(deferObservable): add type definition test

--- spec/observables/defer-spec.ts
+++ spec/observables/defer-spec.ts
@@ -1,5 +1,5 @@
 import * as Rx from '../../dist/cjs/Rx';
-declare const {hot, expectObservable, expectSubscriptions};
+declare const {hot, expectObservable, expectSubscriptions, type};
 
 const Observable = Rx.Observable;
 
@@ -40,9 +40,9 @@ describe('Observable.defer', () => {
 
   it('should create an observable when factory throws', () => {
     //type definition need to be updated
-    const e1 = Observable.defer(<any>(() => {
+    const e1 = Observable.defer(() => {
       throw 'error';
-    }));
+    });
     const expected = '#';
 
     expectObservable(e1).toBe(expected);
@@ -71,5 +71,17 @@ describe('Observable.defer', () => {
 
     expectObservable(e1, unsub).toBe(expected);
     expectSubscriptions(source.subscriptions).toBe(sourceSubs);
+  });
+
+  type(() => {
+    const typeValue = {
+      val: 3,
+      str: 'str'
+    };
+
+    Observable.defer(() => Observable.of(typeValue)).subscribe(x => { x.str.toString(); });
+    Observable.defer(() => {
+      throw 'error';
+    }).subscribe();
   });
 });
