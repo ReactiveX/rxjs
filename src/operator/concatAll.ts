9872f39f1ec90d0aac87b161f6bfdcba272b0422
Andre Staltz <andre@staltz.com>
2016-03-25 18:50:40 +0100

    docs(operators): write comprehensive JSDoc for concatAll()

--- src/operator/concatAll.ts
+++ src/operator/concatAll.ts
@@ -1,15 +1,44 @@
 import {MergeAllOperator} from './mergeAll';
 
 /**
- * Joins every Observable emitted by the source (an Observable of Observables), in a serial
- * fashion. Subscribing to each one only when the previous one has completed, and merging
- * all of their values into the returned observable.
+ * Converts a higher-order Observable into a first-order Observable by
+ * concatenating the inner Observables in order.
  *
- * __Warning:__ If the source Observable emits Observables quickly and endlessly, and the
- * Observables it emits generally complete slower than the source emits, you can run into
- * memory issues as the incoming observables collect in an unbounded buffer.
+ * <span class="informal">Flattens an Observable-of-Observables by putting one
+ * inner Observable after the other.</span>
  *
- * @return {Observable} an observable of values merged from the incoming observables.
+ * <img src="./img/concatAll.png" width="100%">
+ *
+ * Joins every Observable emitted by the source (a higher-order Observable), in
+ * a serial fashion. It subscribes to each inner Observable only after the
+ * previous inner Observable has completed, and merges all of their values into
+ * the returned observable.
+ *
+ * __Warning:__ If the source Observable emits Observables quickly and
+ * endlessly, and the inner Observables it emits generally complete slower than
+ * the source emits, you can run into memory issues as the incoming Observables
+ * collect in an unbounded buffer.
+ *
+ * Note: `concatAll` is equivalent to `mergeAll` with concurrency parameter set
+ * to `1`.
+ *
+ * @example <caption>For each click event, tick every second from 0 to 3, with no concurrency</caption>
+ * var clicks = Rx.Observable.fromEvent(document, 'click');
+ * var higherOrder = clicks.map(ev => Rx.Observable.interval(1000).take(4));
+ * var firstOrder = higherOrder.concatAll();
+ * firstOrder.subscribe(x => console.log(x));
+ *
+ * @see {@link combineAll}
+ * @see {@link concatAll}
+ * @see {@link exhaust}
+ * @see {@link concat}
+ * @see {@link concatMap}
+ * @see {@link concatMapTo}
+ * @see {@link switch}
+ * @see {@link zipAll}
+ *
+ * @return {Observable} An Observable emitting values from all the inner
+ * Observables concatenated.
  * @method concatAll
  * @owner Observable
  */
