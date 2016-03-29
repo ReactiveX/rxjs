d70fba65a662a55f16896421c48e3ce15126b037
Andre Staltz <andre@staltz.com>
2016-03-29 15:04:37 +0300

    docs(operators): write comprehensive JSDoc for map() and mapTo()

--- src/operator/mapTo.ts
+++ src/operator/mapTo.ts
@@ -3,12 +3,28 @@ import {Subscriber} from '../Subscriber';
 import {Observable} from '../Observable';
 
 /**
- * Maps every value to the same value every time.
+ * Emits the given constant value on the output Observable every time the source
+ * Observable emits a value.
+ *
+ * <span class="informal">Like {@link map}, but it maps every source value to
+ * the same output value every time.</span>
  *
  * <img src="./img/mapTo.png" width="100%">
  *
- * @param {any} value the value to map each incoming value to
- * @return {Observable} an observable of the passed value that emits every time the source does
+ * Takes a constant `value` as argument, and emits that whenever the source
+ * Observable emits a value. In other words, ignores the actual source value,
+ * and simply uses the emission moment to know when to emit the given `value`.
+ *
+ * @example <caption>Map every every click to the string 'Hi'</caption>
+ * var clicks = Rx.Observable.fromEvent(document, 'click');
+ * var greetings = clicks.mapTo('Hi');
+ * greetings.subscribe(x => console.log(x));
+ *
+ * @see {@link map}
+ *
+ * @param {any} value The value to map each source value to.
+ * @return {Observable} An Observable that emits the given `value` every time
+ * the source Observable emits something.
  * @method mapTo
  * @owner Observable
  */
