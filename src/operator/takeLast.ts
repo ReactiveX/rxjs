4a6ead50d85ae636838d69ee91ca2f6ca76bccda
Andre Staltz <andre@staltz.com>
2016-03-30 16:00:56 +0300

    docs(operators): write comprehensive JSDoc for the take operators

--- src/operator/takeLast.ts
+++ src/operator/takeLast.ts
@@ -5,23 +5,51 @@ import {EmptyObservable} from '../observable/EmptyObservable';
 import {Observable} from '../Observable';
 
 /**
+ * Emits only the last `count` values emitted by the source Observable.
+ *
+ * <span class="informal">Remembers the latest `count` values, then emits those
+ * only when the source completes.</span>
+ *
+ * <img src="./img/takeLast.png" width="100%">
+ *
+ * `takeLast` returns an Observable that emits at most the last `count` values
+ * emitted by the source Observable. If the source emits fewer than `count`
+ * values then all of its values are emitted. This operator must wait until the
+ * `complete` notification emission from the source in order to emit the `next`
+ * values on the output Observable, because otherwise it is impossible to know
+ * whether or not more values will be emitted on the source. For this reason,
+ * all values are emitted synchronously, followed by the complete notification.
+ *
+ * @example <caption>Take the last 3 values of an Observable with many values</caption>
+ * var many = Rx.Observable.range(1, 100);
+ * var lastThree = many.takeLast(3);
+ * lastThree.subscribe(x => console.log(x));
+ *
+ * @see {@link take}
+ * @see {@link takeUntil}
+ * @see {@link takeWhile}
+ * @see {@link skip}
+ *
  * @throws {ArgumentOutOfRangeError} When using `takeLast(i)`, it delivers an
  * ArgumentOutOrRangeError to the Observer's `error` callback if `i < 0`.
- * @param total
- * @return {any}
+ *
+ * @param {number} count The maximum number of values to emit from the end of
+ * the sequence of values emitted by the source Observable.
+ * @return {Observable<T>} An Observable that emits at most the last count
+ * values emitted by the source Observable.
  * @method takeLast
  * @owner Observable
  */
-export function takeLast<T>(total: number): Observable<T> {
-  if (total === 0) {
+export function takeLast<T>(count: number): Observable<T> {
+  if (count === 0) {
     return new EmptyObservable<T>();
   } else {
-    return this.lift(new TakeLastOperator(total));
+    return this.lift(new TakeLastOperator(count));
   }
 }
 
 export interface TakeLastSignature<T> {
-  (total: number): Observable<T>;
+  (count: number): Observable<T>;
 }
 
 class TakeLastOperator<T> implements Operator<T, T> {
