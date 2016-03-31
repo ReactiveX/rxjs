5934caeb49aa791087939ee36facaf1f23b3134c
Andre Staltz <andre@staltz.com>
2016-03-31 12:54:41 +0300

    docs(operators): write comprehensive JSDoc for empty()

--- src/observable/EmptyObservable.ts
+++ src/observable/EmptyObservable.ts
@@ -11,8 +11,38 @@ import {TeardownLogic} from '../Subscription';
 export class EmptyObservable<T> extends Observable<T> {
 
   /**
-   * @param scheduler
-   * @return {Observable<T>}
+   * Returns an Observable that emits no items to the Observer and immediately
+   * emits a complete notification.
+   *
+   * <span class="informal">Just emits 'complete', and nothing else.
+   * </span>
+   *
+   * <img src="./img/empty.png" width="100%">
+   *
+   * This static operator is useful for creating a simple Observable that only
+   * emits the complete notification. It can be used for composing with other
+   * Observables, such as in a {@link mergeMap}.
+   *
+   * @example <caption>Emit the number 7, then complete.</caption>
+   * var result = Rx.Observable.empty().startWith(7);
+   * result.subscribe(x => console.log(x));
+   *
+   * @example <caption>Map and flatten only odd numbers to the sequence 'a', 'b', 'c'</caption>
+   * var interval = Rx.Observable.interval(1000);
+   * var result = interval.mergeMap(x =>
+   *   x % 2 === 1 ? Rx.Observable.of('a', 'b', 'c') : Rx.Observable.empty()
+   * );
+   * result.subscribe(x => console.log(x));
+   *
+   * @see {@link create}
+   * @see {@link never}
+   * @see {@link of}
+   * @see {@link throw}
+   *
+   * @param {Scheduler} [scheduler] A {@link Scheduler} to use for scheduling
+   * the emission of the complete notification.
+   * @return {Observable} An "empty" Observable: emits only the complete
+   * notification.
    * @static true
    * @name empty
    * @owner Observable
