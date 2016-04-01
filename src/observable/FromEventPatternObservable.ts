e32c4ea84411e3dd513c43cdbad7fda9e09429f9
Andre Staltz <andre@staltz.com>
2016-04-01 15:06:12 +0300

    docs(operators): write comprehensive JSDoc for fromEventPattern()

--- src/observable/FromEventPatternObservable.ts
+++ src/observable/FromEventPatternObservable.ts
@@ -12,10 +12,48 @@ import {Subscriber} from '../Subscriber';
 export class FromEventPatternObservable<T, R> extends Observable<T> {
 
   /**
-   * @param addHandler
-   * @param removeHandler
-   * @param selector
-   * @return {FromEventPatternObservable}
+   * Creates an Observable from an API based on addHandler/removeHandler
+   * functions.
+   *
+   * <span class="informal">Converts any addHandler/removeHandler API to an
+   * Observable.</span>
+   *
+   * <img src="./img/fromEventPattern.png" width="100%">
+   *
+   * Creates an Observable by using the `addHandler` and `removeHandler`
+   * functions to add and remove the handlers, with an optional selector
+   * function to project the event arguments to a result. The `addHandler` is
+   * called when the output Observable is subscribed, and `removeHandler` is
+   * called when the Subscription is unsubscribed.
+   *
+   * @example <caption>Emits clicks happening on the DOM document</caption>
+   * function addClickHandler(handler) {
+   *   document.addEventListener('click', handler);
+   * }
+   *  
+   * function removeClickHandler(handler) {
+   *   document.removeEventListener('click', handler);
+   * }
+   *  
+   * var clicks = Rx.Observable.fromEventPattern(
+   *   addClickHandler,
+   *   removeClickHandler
+   * );
+   * clicks.subscribe(x => console.log(x));
+   *
+   * @see {@link from}
+   * @see {@link fromEvent}
+   *
+   * @param {function(handler: Function): any} addHandler A function that takes
+   * a `handler` function as argument and attaches it somehow to the actual
+   * source of events.
+   * @param {function(handler: Function): void} removeHandler A function that
+   * takes a `handler` function as argument and removes it in case it was
+   * previously attached using `addHandler`.
+   * @param {function(...args: any): T} [selector] An optional function to
+   * post-process results. It takes the arguments from the event handler and
+   * should return a single value.
+   * @return {Observable<T>}
    * @static true
    * @name fromEventPattern
    * @owner Observable
