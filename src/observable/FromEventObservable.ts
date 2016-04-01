c5e1112e5fe959b04f08a74a40a44267643e6615
Andre Staltz <andre@staltz.com>
2016-04-01 12:39:02 +0300

    docs(operators): write comprehensive JSDoc for fromEvent()

--- src/observable/FromEventObservable.ts
+++ src/observable/FromEventObservable.ts
@@ -42,23 +42,56 @@ export type EventTargetLike = EventTarget | NodeStyleEventEmmitter | JQueryStyle
 export class FromEventObservable<T, R> extends Observable<T> {
 
   /**
-   * @param sourceObj
-   * @param eventName
-   * @param selector
-   * @return {FromEventObservable}
+   * Creates an Observable that emits events of a specific type coming from the
+   * given event target.
+   *
+   * <span class="informal">Creates an Observable from DOM events, or Node
+   * EventEmitter events or others.</span>
+   *
+   * <img src="./img/fromEvent.png" width="100%">
+   *
+   * Creates an Observable by attaching an event listener to an "event target",
+   * which may be an object with `addEventListener` and `removeEventListener`,
+   * a Node.js EventEmitter, a jQuery style EventEmitter, a NodeList from the
+   * DOM, or an HTMLCollection from the DOM. The event handler is attached when
+   * the output Observable is subscribed, and removed when the Subscription is
+   * unsubscribed.
+   *
+   * @example <caption>Emits clicks happening on the DOM document</caption>
+   * var clicks = Rx.Observable.fromEvent(document, 'click');
+   * clicks.subscribe(x => console.log(x));
+   *
+   * @see {@link from}
+   * @see {@link fromEventPattern}
+   *
+   * @param {EventTargetLike} target The DOMElement, event target, Node.js
+   * EventEmitter, NodeList or HTMLCollection to attach the event handler to.
+   * @param {string} eventName The event name of interest, being emitted by the
+   * `target`.
+   * @param {function(...args: any): T} [selector] An optional function to
+   * post-process results. It takes the arguments from the event handler and
+   * should return a single value.
+   * @return {Observable<T>}
    * @static true
    * @name fromEvent
    * @owner Observable
    */
-  static create<T>(sourceObj: EventTargetLike, eventName: string, selector?: (...args: Array<any>) => T): Observable<T> {
-    return new FromEventObservable(sourceObj, eventName, selector);
+  static create<T>(target: EventTargetLike,
+                   eventName: string,
+                   selector?: (...args: Array<any>) => T): Observable<T> {
+    return new FromEventObservable(target, eventName, selector);
   }
 
-  constructor(private sourceObj: EventTargetLike, private eventName: string, private selector?: (...args: Array<any>) => T) {
+  constructor(private sourceObj: EventTargetLike,
+              private eventName: string,
+              private selector?: (...args: Array<any>) => T) {
     super();
   }
 
-  private static setupSubscription<T>(sourceObj: EventTargetLike, eventName: string, handler: Function, subscriber: Subscriber<T>) {
+  private static setupSubscription<T>(sourceObj: EventTargetLike,
+                                      eventName: string,
+                                      handler: Function,
+                                      subscriber: Subscriber<T>) {
     let unsubscribe: () => void;
     if (isNodeList(sourceObj) || isHTMLCollection(sourceObj)) {
       for (let i = 0, len = sourceObj.length; i < len; i++) {
