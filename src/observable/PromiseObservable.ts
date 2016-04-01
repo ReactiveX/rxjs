dc88b9b623df52f4f120b1fc41d9978abe3bd8c2
Andre Staltz <andre@staltz.com>
2016-04-01 12:15:46 +0300

    docs(operators): write comprehensive JSDoc for fromPromise()

--- src/observable/PromiseObservable.ts
+++ src/observable/PromiseObservable.ts
@@ -14,9 +14,27 @@ export class PromiseObservable<T> extends Observable<T> {
   public value: T;
 
   /**
-   * @param promise
-   * @param scheduler
-   * @return {PromiseObservable}
+   * Converts a Promise to an Observable.
+   *
+   * <span class="informal">Returns an Observable that just emits the Promise's
+   * resolved value, then completes.</span>
+   *
+   * Converts an ES2015 Promise or a Promises/A+ spec compliant Promise to an
+   * Observable. If the Promise resolves with a value, the output Observable
+   * emits that resolved value as a `next`, and then completes. If the Promise
+   * is rejected, then the output Observable emits the corresponding Error.
+   *
+   * @example <caption>Convert the Promise returned by Fetch to an Observable</caption>
+   * var result = Rx.Observable.fromPromise(fetch('http://myserver.com/'));
+   * result.subscribe(x => console.log(x), e => console.error(e));
+   *
+   * @see {@link bindCallback}
+   * @see {@link from}
+   *
+   * @param {Promise<T>} promise The promise to be converted.
+   * @param {Scheduler} [scheduler] An optional Scheduler to use for scheduling
+   * the delivery of the resolved value (or the rejection).
+   * @return {Observable<T>} An Observable which wraps the Promise.
    * @static true
    * @name fromPromise
    * @owner Observable
