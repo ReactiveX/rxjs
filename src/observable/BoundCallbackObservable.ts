138cc63a9b4016c57ea045903bd887858b2bf11c
Andre Staltz <andre@staltz.com>
2016-04-01 11:36:31 +0300

    docs(operators): write comprehensive JSDoc for bindCallback()

--- src/observable/BoundCallbackObservable.ts
+++ src/observable/BoundCallbackObservable.ts
@@ -34,24 +34,46 @@ export class BoundCallbackObservable<T> extends Observable<T> {
   /* tslint:enable:max-line-length */
 
   /**
-   * Converts a callback function to an observable sequence.
-   * @param {function} callbackFunc Function with a callback as the last
-   * parameter.
-   * @param {function} selector A selector which takes the arguments from the
-   * callback to produce a single item to yield on next.
-   * @param {Scheduler} [scheduler] The scheduler on which to schedule
-   * the callbacks.
-   * @return {function(...params: *): Observable<T>} a function which returns the
-   * Observable that corresponds to the callback.
+   * Converts a callback API to a function that returns an Observable.
+   *
+   * <span class="informal">Give it a function `f` of type `f(x, callback)` and
+   * it will return a function `g` that when called as `g(x)` will output an
+   * Observable.</span>
+   *
+   * `bindCallback` is not an operator because its input and output are not
+   * Observables. The input is a function `func` with some parameters, but the
+   * last parameter must be a callback function that `func` calls when it is
+   * done. The output of `bindCallback` is a function that takes the same
+   * parameters as `func`, except the last one (the callback). When the output
+   * function is called with arguments, it will return an Observable where the
+   * results will be delivered to.
+   *
+   * @example <caption>Convert jQuery's getJSON to an Observable API</caption>
+   * // Suppose we have jQuery.getJSON('/my/url', callback)
+   * var getJSONAsObservable = Rx.Observable.bindCallback(jQuery.getJSON);
+   * var result = getJSONAsObservable('/my/url');
+   * result.subscribe(x => console.log(x), e => console.error(e));
+   *
+   * @see {@link bindNodeCallback}
+   * @see {@link from}
+   * @see {@link fromPromise}
+   *
+   * @param {function} func Function with a callback as the last parameter.
+   * @param {function} selector A function which takes the arguments from the
+   * callback and maps those a value to emit on the output Observable.
+   * @param {Scheduler} [scheduler] The scheduler on which to schedule the
+   * callbacks.
+   * @return {function(...params: *): Observable} A function which returns the
+   * Observable that delivers the same values the callback would deliver.
    * @static true
    * @name bindCallback
    * @owner Observable
    */
-  static create<T>(callbackFunc: Function,
+  static create<T>(func: Function,
                    selector: Function | void = undefined,
                    scheduler?: Scheduler): (...args: any[]) => Observable<T> {
     return (...args: any[]): Observable<T> => {
-      return new BoundCallbackObservable<T>(callbackFunc, <any>selector, args, scheduler);
+      return new BoundCallbackObservable<T>(func, <any>selector, args, scheduler);
     };
   }
 
