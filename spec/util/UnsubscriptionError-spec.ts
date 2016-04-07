4ef977879ca407dc2326ca32a057a804d2f4eeb6
Ben Lesh <blesh@netflix.com>
2016-04-07 02:19:21 -0700

    feat(UnsubscriptionError): add messages from inner errors to output message
    - Adds improved console logging behavior
    
    closes #1590

---
+++ spec/util/UnsubscriptionError-spec.ts
@@ -0,0 +1,28 @@
+import {expect} from 'chai';
+import * as Rx from '../../dist/cjs/Rx';
+
+const { Observable, UnsubscriptionError } = Rx;
+
+/** @test {UnsubscriptionError} */
+describe('UnsubscriptionError', () => {
+  it('should create a message that is a clear indication of its internal errors', () => {
+    const err1 = new Error('Swiss cheese tastes amazing but smells like socks');
+    const err2 = new Error('User too big to fit in tiny European elevator');
+    const source1 = Observable.create(() => () => { throw err1; });
+    const source2 = Observable.timer(1000);
+    const source3 = Observable.create(() => () => { throw err2; });
+    const source = source1.merge(source2, source3);
+
+    const subscription = source.subscribe();
+
+    try {
+      subscription.unsubscribe();
+    } catch (err) {
+      expect(err instanceof UnsubscriptionError).to.equal(true);
+      expect(err.message).to.equal(`2 errors occurred during unsubscription:
+1) ${err1}
+2) ${err2}`);
+      expect(err.name).to.equal('UnsubscriptionError');
+    }
+  });
+});
