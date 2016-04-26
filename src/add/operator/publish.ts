0e5991d9c66ad74ac694c2f8234ee0e7e33910ef
OJ Kwon <kwon.ohjoong@gmail.com>
2016-04-26 16:15:22 -0700

    feat(publish): support optional selectors
    closes #1629

--- src/add/operator/publish.ts
+++ src/add/operator/publish.ts
@@ -2,7 +2,7 @@
 import {Observable} from '../../Observable';
 import {publish, PublishSignature} from '../../operator/publish';
 
-Observable.prototype.publish = publish;
+Observable.prototype.publish = <any>publish;
 
 declare module '../../Observable' {
   interface Observable<T> {
