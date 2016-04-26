bc5427a8a547de70a0d5fd8bc98b9211cead89cb
Andre Staltz <andre@staltz.com>
2016-04-26 18:55:08 +0300

    docs(operators): write comprehensive JSDoc for materialize()

--- src/operator/materialize.ts
+++ src/operator/materialize.ts
@@ -4,17 +4,39 @@ import {Subscriber} from '../Subscriber';
 import {Notification} from '../Notification';
 
 /**
- * Returns an Observable that represents all of the emissions and notifications
- * from the source Observable into emissions marked with their original types
- * within a `Notification` objects.
+ * Represents all of the notifications from the source Observable as `next`
+ * emissions marked with their original types within {@link Notification}
+ * objects.
+ *
+ * <span class="informal">Wraps `next`, `error` and `complete` emissions in
+ * {@link Notification} objects, emitted as `next` on the output Observable.
+ * </span>
  *
  * <img src="./img/materialize.png" width="100%">
  *
- * @see {@link Notification}
+ * `materialize` returns an Observable that emits a `next` notification for each
+ * `next`, `error`, or `complete` emission of the source Observable. When the
+ * source Observable emits `complete`, the output Observable will emit `next` as
+ * a Notification of type "complete", and then it will emit `complete` as well.
+ * When the source Observable emits `error`, the output will emit `next` as a
+ * Notification of type "error", and then `complete`.
  *
- * @scheduler materialize does not operate by default on a particular Scheduler.
- * @return {Observable<Notification<T>>} an Observable that emits items that are the result of
- * materializing the items and notifications of the source Observable.
+ * This operator is useful for producing metadata of the source Observable, to
+ * be consumed as `next` emissions. Use it in conjunction with
+ * {@link dematerialize}.
+ *
+ * @example <caption>Convert a faulty Observable to an Observable of Notifications</caption>
+ * var letters = Rx.Observable.of('a', 'b', 13, 'd');
+ * var upperCase = letters.map(x => x.toUpperCase());
+ * var materialized = upperCase.materialize();
+ * materialized.subscribe(x => console.log(x));
+ *
+ * @see {@link Notification}
+ * @see {@link dematerialize}
+ *
+ * @return {Observable<Notification<T>>} An Observable that emits
+ * {@link Notification} objects that wrap the original emissions from the source
+ * Observable with metadata.
  * @method materialize
  * @owner Observable
  */
