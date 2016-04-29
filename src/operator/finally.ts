f17a1154afbe295c75ac8d0e22cde501a525d248
Ben Lesh <blesh@netflix.com>
2016-04-28 22:06:22 -0700

    refactor(finally): rename `finallySelector` to just `callback`
    the other name didn't make any sense, it didn't select anything

--- src/operator/finally.ts
+++ src/operator/finally.ts
@@ -6,25 +6,25 @@ import {Observable} from '../Observable';
 /**
  * Returns an Observable that mirrors the source Observable, but will call a specified function when
  * the source terminates on complete or error.
- * @param {function} finallySelector function to be called when source terminates.
+ * @param {function} callback function to be called when source terminates.
  * @return {Observable} an Observable that mirrors the source, but will call the specified function on termination.
  * @method finally
  * @owner Observable
  */
-export function _finally<T>(finallySelector: () => void): Observable<T> {
-  return this.lift(new FinallyOperator(finallySelector));
+export function _finally<T>(callback: () => void): Observable<T> {
+  return this.lift(new FinallyOperator(callback));
 }
 
 export interface FinallySignature<T> {
-  <T>(finallySelector: () => void): Observable<T>;
+  <T>(callback: () => void): Observable<T>;
 }
 
 class FinallyOperator<T> implements Operator<T, T> {
-  constructor(private finallySelector: () => void) {
+  constructor(private callback: () => void) {
   }
 
   call(subscriber: Subscriber<T>, source: any): any {
-    return source._subscribe(new FinallySubscriber(subscriber, this.finallySelector));
+    return source._subscribe(new FinallySubscriber(subscriber, this.callback));
   }
 }
 
@@ -34,8 +34,8 @@ class FinallyOperator<T> implements Operator<T, T> {
  * @extends {Ignored}
  */
 class FinallySubscriber<T> extends Subscriber<T> {
-  constructor(destination: Subscriber<T>, finallySelector: () => void) {
+  constructor(destination: Subscriber<T>, callback: () => void) {
     super(destination);
-    this.add(new Subscription(finallySelector));
+    this.add(new Subscription(callback));
   }
 }
