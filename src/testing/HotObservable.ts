1c3d6eae7c70136b33e69ee82a9132dcc7dde75a
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-05-23 00:54:19 +0900

    fix(typings): make HotObservavle._subscribe protected

--- src/testing/HotObservable.ts
+++ src/testing/HotObservable.ts
@@ -24,7 +24,7 @@ export class HotObservable<T> extends Subject<T> implements SubscriptionLoggable
     this.scheduler = scheduler;
   }
 
-  _subscribe(subscriber: Subscriber<any>): Subscription {
+  protected _subscribe(subscriber: Subscriber<any>): Subscription {
     const subject: HotObservable<T> = this;
     const index = subject.logSubscribedFrame();
     subscriber.add(new Subscription(() => {
