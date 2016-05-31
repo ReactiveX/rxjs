a7c136da4c6ea0318e08bbceffa8b30178f3c19b
Flavio <fs@sadeghi.ch>
2016-05-31 23:45:27 +0200

    test(WebSocketSubject): add test for multiplex in combination with retry
    It works since 5.0.0.beta.8, but there is no test which ensures this for the future.
    
    related #1466

--- spec/observables/dom/webSocket-spec.ts
+++ spec/observables/dom/webSocket-spec.ts
@@ -391,5 +391,38 @@ describe('Observable.webSocket', () => {
       expect(socket.close).have.been.called;
       (<any>socket.close).restore();
     });
+
+    it('should work in combination with retry (issue #1466)', () => {
+      const error = { wasClean: false};
+
+      var subCounter = 0;
+      var unsubCounter = 0;
+
+      const subject = Observable.webSocket(<any>{url: 'ws://mysocket'})
+      .multiplex(
+        () => subCounter++,
+        () => unsubCounter++,
+        () => true)
+      .retry(1);
+
+      var nextCalled = false;
+      var failed = null;
+      var completed = false;
+      subject.subscribe(
+        () => nextCalled = true,
+        (e) => failed = e,
+        () => completed = true);
+
+      let socket = MockWebSocket.lastSocket;
+
+      socket.triggerClose(error);
+
+      expect(subCounter).to.equal(2);
+      expect(unsubCounter).to.equal(2);
+
+      expect(nextCalled).to.be.false;
+      expect(failed).to.equal(error);
+      expect(completed).to.be.false;
+    });
   });
 });
