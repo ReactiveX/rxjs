d942ea3c34f55128c2ab0a20a5f89f5579f3a86c
OJ Kwon <kwon.ohjoong@gmail.com>
2016-06-15 15:16:58 -0700

    refactor(symbol-observable): bump up symbol-observable@1

--- tools/make-system-bundle.js
+++ tools/make-system-bundle.js
@@ -3,25 +3,28 @@ var Builder = require('systemjs-builder');
 var addLicenseToFile = require('./add-license-to-file');
 
 var config = {
-  baseURL: 'dist',
   paths: {
-    'rxjs/*': 'cjs/*.js',
-    'symbol-observable': '../node_modules/symbol-observable/index.js',
-    'ponyfill': '../node_modules/symbol-observable/ponyfill.js'
-  }
-};
+    'rxjs/': 'dist/cjs/',
+    'symbol-observable/*': 'node_modules/symbol-observable/*',
+  },
+  packages: {
+    'symbol-observable': {
+      main: 'index.js'
+    }
+  },
 
-build('rxjs/Rx', '../dist/global/Rx.js', '../dist/global/Rx.min.js');
+  defaultJSExtensions: true,
+};
 
 function build(name, inputFile, outputFile) {
   var devBuilder = new Builder();
 
   devBuilder.config(config);
 
-  devBuilder.build(name, path.resolve(__dirname, inputFile)).then(function () {
+  devBuilder.bundle(name, path.resolve(__dirname, inputFile)).then(function () {
     var prodBuilder = new Builder();
     prodBuilder.config(config);
-    prodBuilder.build(name, path.resolve(__dirname, outputFile), {sourceMaps: true, minify: true}).then(function () {
+    prodBuilder.bundle(name, path.resolve(__dirname, outputFile), {sourceMaps: true, minify: true}).then(function () {
       process.exit(0);
     }, function (err) {
       console.error('prod died', err);
@@ -32,5 +35,7 @@ function build(name, inputFile, outputFile) {
     process.exit(1);
   });
 }
+
+build('rxjs/Rx', '../dist/global/Rx.js', '../dist/global/Rx.min.js');
 
 process.stdin.resume();
