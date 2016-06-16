ca361e490c2eee37176a40198d8ddf76a77f1b9a
Paul Taylor <paul.e.taylor@me.com>
2016-06-15 22:08:29 -0700

    fix(multicast): Fixes multicast with selector to create a new source connection per subscriber.
    Multicast with a selector function should create a new ConnectableObservable for each subscriber to
    the MulticastObservable. This ensures each subscriber creates a new connection to the source
    Observable, and don't share subscription side-effects.

--- src/observable/MulticastObservable.ts
+++ src/observable/MulticastObservable.ts
@@ -1,3 +1,4 @@
+import {Subject} from '../Subject';
 import {Observable} from '../Observable';
 import {Subscriber} from '../Subscriber';
 import {Subscription} from '../Subscription';
@@ -5,14 +6,14 @@ import {ConnectableObservable} from '../observable/ConnectableObservable';
 
 export class MulticastObservable<T> extends Observable<T> {
   constructor(protected source: Observable<T>,
-              private connectable: ConnectableObservable<T>,
+              private subjectFactory: () => Subject<T>,
               private selector: (source: Observable<T>) => Observable<T>) {
     super();
   }
 
   protected _subscribe(subscriber: Subscriber<T>): Subscription {
-    const {selector, connectable} = this;
-
+    const { selector, source } = this;
+    const connectable = new ConnectableObservable(source, this.subjectFactory);
     const subscription = selector(connectable).subscribe(subscriber);
     subscription.add(connectable.connect());
     return subscription;
