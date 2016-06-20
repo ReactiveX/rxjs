304b3313cba038ec5259e029330e1ec6854f91cc
OJ Kwon <kwon.ohjoong@gmail.com>
2016-06-20 13:15:16 -0700

    chore(package.json): do not build SystemJS bundle

--- .make-packages.js
+++ .make-packages.js
@@ -2,7 +2,6 @@ var pkg = require('./package.json');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var path = require('path');
-var Builder = require('systemjs-builder');
 var licenseTool = require('./tools/add-license-to-file');
 var addLicenseToFile = licenseTool.addLicenseToFile;
 var addLicenseTextToFile = licenseTool.addLicenseTextToFile;
@@ -34,10 +33,6 @@ mkdirp.sync('dist/cjs/bundles');
 fs.writeFileSync('dist/cjs/bundles/Rx.umd.js', fs.readFileSync('dist/global/Rx.umd.js').toString());
 fs.writeFileSync('dist/cjs/bundles/Rx.umd.min.js', fs.readFileSync('dist/global/Rx.umd.min.js').toString());
 fs.writeFileSync('dist/cjs/bundles/Rx.umd.min.js.map', fs.readFileSync('dist/global/Rx.umd.min.js.map').toString());
-// System bundles
-fs.writeFileSync('dist/cjs/bundles/Rx.js', fs.readFileSync('dist/global/Rx.js').toString());
-fs.writeFileSync('dist/cjs/bundles/Rx.min.js', fs.readFileSync('dist/global/Rx.min.js').toString());
-fs.writeFileSync('dist/cjs/bundles/Rx.min.js.map', fs.readFileSync('dist/global/Rx.min.js.map').toString());
 
 // ES6 Package
 fs.writeFileSync('dist/es6/package.json', JSON.stringify(es6Pkg, null, 2));
@@ -47,9 +42,5 @@ fs.writeFileSync('dist/es6/README.md', fs.readFileSync('./README.md').toString()
 // Add licenses to tops of bundles
 addLicenseToFile('LICENSE.txt', 'dist/cjs/bundles/Rx.umd.js');
 addLicenseTextToFile(license, 'dist/cjs/bundles/Rx.umd.min.js');
-addLicenseToFile('LICENSE.txt', 'dist/cjs/bundles/Rx.js');
-addLicenseTextToFile('license', 'dist/cjs/bundles/Rx.min.js');
 addLicenseToFile('LICENSE.txt', 'dist/global/Rx.umd.js');
 addLicenseTextToFile(license, 'dist/global/Rx.umd.min.js');
-addLicenseToFile('LICENSE.txt', 'dist/global/Rx.js');
-addLicenseTextToFile(license, 'dist/global/Rx.min.js');
