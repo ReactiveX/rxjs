e3667345032ca6fc60f9e8e8da952f9c68ec9343
OJ Kwon <kwon.ohjoong@gmail.com>
2016-07-26 15:16:12 -0700

    refactor(Operator): replace operator<T, R> to interface (#1825)
    - remove base implemetation to call(), not being used

--- src/Operator.ts
+++ src/Operator.ts
@@ -1,8 +1,6 @@
 import {Subscriber} from './Subscriber';
 import {TeardownLogic} from './Subscription';
 
-export class Operator<T, R> {
-  call(subscriber: Subscriber<R>, source: any): TeardownLogic {
-    return source._subscribe(new Subscriber<T>(subscriber));
-  }
+export interface Operator<T, R> {
+  call(subscriber: Subscriber<R>, source: any): TeardownLogic;
 }
