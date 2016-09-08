34c39dddf3301dd578fc8e64bf408c068f6428b3
Rob Wormald <robwormald@gmail.com>
2016-09-08 10:54:30 -0700

    fix(operators): export reserved name operators on prototype
    closes #1924

--- src/add/operator/do.ts
+++ src/add/operator/do.ts
@@ -3,9 +3,11 @@ import { Observable } from '../../Observable';
 import { _do, DoSignature } from '../../operator/do';
 
 Observable.prototype.do = _do;
+Observable.prototype._do = _do;
 
 declare module '../../Observable' {
   interface Observable<T> {
     do: DoSignature<T>;
+    _do: DoSignature<T>;
   }
 }
--- src/add/operator/switch.ts
+++ src/add/operator/switch.ts
@@ -3,9 +3,11 @@ import { Observable } from '../../Observable';
 import { _switch, SwitchSignature } from '../../operator/switch';
 
 Observable.prototype.switch = _switch;
+Observable.prototype._switch = _switch;
 
 declare module '../../Observable' {
   interface Observable<T> {
     switch: SwitchSignature<T>;
+    _switch: SwitchSignature<T>;
   }
 }
