53de9f88336a93f623bf790af9669417af34aaaf
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-10-25 16:06:53 +0900

    refactor(util): remove unused type parameters in util/isScheduler

--- src/util/isScheduler.ts
+++ src/util/isScheduler.ts
@@ -1,4 +1,4 @@
 import { Scheduler } from '../Scheduler';
-export function isScheduler<T>(value: any): value is Scheduler {
+export function isScheduler(value: any): value is Scheduler {
   return value && typeof (<any>value).schedule === 'function';
 }
