9df86badcc3e4f09f66b8ab257c23b28365ae640
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-06 06:57:33 +0900

    fix(ErrorObservable): Add generic error type for ErrorObservable. (#2071)

--- src/observable/ErrorObservable.ts
+++ src/observable/ErrorObservable.ts
@@ -12,7 +12,7 @@ export interface DispatchArg {
  * @extends {Ignored}
  * @hide true
  */
-export class ErrorObservable extends Observable<any> {
+export class ErrorObservable<T> extends Observable<any> {
 
   /**
    * Creates an Observable that emits no items to the Observer and immediately
@@ -54,7 +54,7 @@ export class ErrorObservable extends Observable<any> {
    * @name throw
    * @owner Observable
    */
-  static create<T>(error: any, scheduler?: Scheduler) {
+  static create<T>(error: T, scheduler?: Scheduler): ErrorObservable<T> {
     return new ErrorObservable(error, scheduler);
   }
 
@@ -63,7 +63,7 @@ export class ErrorObservable extends Observable<any> {
     subscriber.error(error);
   }
 
-  constructor(public error: any, private scheduler?: Scheduler) {
+  constructor(public error: T, private scheduler?: Scheduler) {
     super();
   }
 
