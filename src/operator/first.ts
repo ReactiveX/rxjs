3aa1988d8c60be4851101a26eba53927ff85753a
Robert Raiford <github_rob3c@7b9.com>
2016-11-07 16:16:02 -0500

    feat(first): support type guards without casting

--- src/operator/first.ts
+++ src/operator/first.ts
@@ -53,10 +53,15 @@ import { EmptyError } from '../util/EmptyError';
  * @owner Observable
  */
 /* tslint:disable:max-line-length */
-export function first<T>(this: Observable<T>, predicate?: (value: T, index: number, source: Observable<T>) => boolean): Observable<T>;
-export function first<T, S extends T>(this: Observable<T>, predicate?: (value: T, index: number, source: Observable<T>) => value is S): Observable<S>;
+export function first<T, S extends T>(this: Observable<T>,
+                                      predicate?: ((value: T, index: number, source: Observable<T>) => boolean) |
+                                                  ((value: T, index: number, source: Observable<T>) => value is S)): Observable<S>;
 export function first<T>(this: Observable<T>, predicate: (value: T, index: number, source: Observable<T>) => boolean, resultSelector: void, defaultValue?: T): Observable<T>;
-export function first<T, S extends T>(this: Observable<T>, predicate: (value: T, index: number, source: Observable<T>) => value is S, resultSelector: void, defaultValue?: S): Observable<S>;
+export function first<T, S extends T, R>(this: Observable<T>,
+                                         predicate: ((value: T, index: number, source: Observable<T>) => boolean) |
+                                                    ((value: T, index: number, source: Observable<T>) => value is S),
+                                         resultSelector?: ((value: S, index: number) => R) | void,
+                                         defaultValue?: S): Observable<S>;
 export function first<T, R>(this: Observable<T>, predicate?: (value: T, index: number, source: Observable<T>) => boolean, resultSelector?: (value: T, index: number) => R, defaultValue?: R): Observable<R>;
 /* tslint:disable:max-line-length */
 export function first<T, R>(this: Observable<T>, predicate?: (value: T, index: number, source: Observable<T>) => boolean,
