22f7e4892134c3054dfb6d281a6699717d2c3f54
Tetsuharu OHZEKI <saneyuki.snyk@gmail.com>
2016-11-10 14:17:43 +0900

    style(linting): clarify the type of 2nd argument of Operator<T, R>.call()
    - This commit is spin-off of https://github.com/ReactiveX/rxjs/pull/2104/

--- src/Operator.ts
+++ src/Operator.ts
@@ -1,6 +1,7 @@
+import { Observable } from './Observable';
 import { Subscriber } from './Subscriber';
 import { TeardownLogic } from './Subscription';
 
 export interface Operator<T, R> {
-  call(subscriber: Subscriber<R>, source: any): TeardownLogic;
+  call(subscriber: Subscriber<R>, source: Observable<T>): TeardownLogic;
 }
