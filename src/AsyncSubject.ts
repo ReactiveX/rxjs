9dc4cc1e6f0099258d1ca49d33efaae7148347df
Victor Galan <victor.galan@liferay.com>
2017-01-14 15:57:22 +0100

    fix(AsynSubject): check if there is an error first

--- src/AsyncSubject.ts
+++ src/AsyncSubject.ts
@@ -11,15 +11,14 @@ export class AsyncSubject<T> extends Subject<T> {
   private hasCompleted: boolean = false;
 
   protected _subscribe(subscriber: Subscriber<any>): Subscription {
-    if (this.hasCompleted && this.hasNext) {
+    if (this.hasError) {
+      subscriber.error(this.thrownError);
+      return Subscription.EMPTY;
+    } else if (this.hasCompleted && this.hasNext) {
       subscriber.next(this.value);
       subscriber.complete();
       return Subscription.EMPTY;
-    } else if (this.hasError) {
-      subscriber.error(this.thrownError);
-      return Subscription.EMPTY;
     }
-
     return super._subscribe(subscriber);
   }
 
