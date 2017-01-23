2d65fa383e7c4c57fa44d2022f3c75c02b0b373f
Georgios Kalpakas <kalpakas.g@gmail.com>
2017-01-23 18:02:21 +0200

    docs(retryWhen): fix docs
    Fixes include:
    
    - Remove non-existent `scheduler` param.
    - Add missing param name and type for `notifier`.
    - Fix some typos.
    - Improve description.

--- src/operator/retryWhen.ts
+++ src/operator/retryWhen.ts
@@ -11,17 +11,15 @@ import { InnerSubscriber } from '../InnerSubscriber';
 import { subscribeToResult } from '../util/subscribeToResult';
 
 /**
- * Returns an Observable that emits the same values as the source observable with the exception of an `error`.
- * An `error` will cause the emission of the Throwable that cause the error to the Observable returned from
- * notificationHandler. If that Observable calls onComplete or `error` then retry will call `complete` or `error`
- * on the child subscription. Otherwise, this Observable will resubscribe to the source observable, on a particular
- * IScheduler.
+ * Returns an Observable that mirrors the source Observable with the exception of an `error`. If the source Observable
+ * calls `error`, this method will emit the Throwable that caused the error to the Observable returned from `notifier`.
+ * If that Observable calls `complete` or `error` then this method will call `complete` or `error` on the child
+ * subscription. Otherwise this method will resubscribe to the source Observable.
  *
  * <img src="./img/retryWhen.png" width="100%">
  *
- * @param {notificationHandler} receives an Observable of notifications with which a user can `complete` or `error`,
- * aborting the retry.
- * @param {scheduler} the IScheduler on which to subscribe to the source Observable.
+ * @param {function(errors: Observable): Observable} notifier - Receives an Observable of notifications with which a
+ * user can `complete` or `error`, aborting the retry.
  * @return {Observable} the source Observable modified with retry logic.
  * @method retryWhen
  * @owner Observable
