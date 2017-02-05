8b81fc61cea813ef6cd8e425743a9599d83d3ac4
Mateusz Podlasin <Podlas29@gmail.com>
2017-02-05 13:49:04 +0100

    fix(bindNodeCallback): emit undefined when callback has no success arguments
    Emit undefined insteady of empty array by resulting Observable,
    when callback function is called without success parameters.
    
    Closes #2254

--- spec/observables/bindNodeCallback-spec.ts
+++ spec/observables/bindNodeCallback-spec.ts
@@ -8,6 +8,24 @@ const Observable = Rx.Observable;
 /** @test {bindNodeCallback} */
 describe('Observable.bindNodeCallback', () => {
   describe('when not scheduled', () => {
+    it('should emit undefined when callback is called without success arguments', () => {
+      function callback(cb) {
+        cb(null);
+      }
+
+      const boundCallback = Observable.bindNodeCallback(callback);
+      const results = [];
+
+      boundCallback()
+        .subscribe((x: any) => {
+          results.push(typeof x);
+        }, null, () => {
+          results.push('done');
+        });
+
+      expect(results).to.deep.equal(['undefined', 'done']);
+    });
+
     it('should emit one value from a callback', () => {
       function callback(datum, cb) {
         cb(null, datum);
@@ -126,6 +144,26 @@ describe('Observable.bindNodeCallback', () => {
   });
 
   describe('when scheduled', () => {
+    it('should emit undefined when callback is called without success arguments', () => {
+      function callback(cb) {
+        cb(null);
+      }
+
+      const boundCallback = Observable.bindNodeCallback(callback, null, rxTestScheduler);
+      const results = [];
+
+      boundCallback()
+        .subscribe((x: any) => {
+          results.push(typeof x);
+        }, null, () => {
+          results.push('done');
+        });
+
+      rxTestScheduler.flush();
+
+      expect(results).to.deep.equal(['undefined', 'done']);
+    });
+
     it('should emit one value from a callback', () => {
       function callback(datum, cb) {
         cb(null, datum);
