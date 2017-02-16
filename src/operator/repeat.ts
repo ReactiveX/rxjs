375ce3e31ec1a3f483944687af41909398ad0068
John Detlefs <jdetle@gmail.com>
2017-02-15 17:34:12 -0800

    docs(repeat): Remove scheduler as param. (#2368)

--- src/operator/repeat.ts
+++ src/operator/repeat.ts
@@ -10,7 +10,6 @@ import { TeardownLogic } from '../Subscription';
  *
  * <img src="./img/repeat.png" width="100%">
  *
- * @param {Scheduler} [scheduler] the IScheduler to emit the items on.
  * @param {number} [count] the number of times the source Observable items are repeated, a count of 0 will yield
  * an empty Observable.
  * @return {Observable} an Observable that repeats the stream of items emitted by the source Observable at most
