2549322c4388756a70fc15360842cc9421ab73f1
Mateusz Podlasin <Podlas29@gmail.com>
2017-02-22 15:42:25 +0100

    docs(fromEventPattern): add more details about the operator
    Compare to fromEvent. Describe what addHandler and removeHandler
    functions are called with and when they are called. Describe
    how APIs that return cancellation token on handler registration
    can be handled and show example of that. Add note that only
    first parameter from event handler will appear in the output
    Observable. Describe how project function can be used to
    get access to other parameters and show an example of that.

--- src/observable/FromEventPatternObservable.ts
+++ src/observable/FromEventPatternObservable.ts
@@ -11,19 +11,49 @@ import { Subscriber } from '../Subscriber';
 export class FromEventPatternObservable<T> extends Observable<T> {
 
   /**
-   * Creates an Observable from an API based on addHandler/removeHandler
-   * functions.
+   * Creates an Observable from an arbitrary API for registering event handlers.
    *
-   * <span class="informal">Converts any addHandler/removeHandler API to an
-   * Observable.</span>
+   * <span class="informal">When that method for adding event handler was something {@link fromEvent}
+   * was not prepared for.</span>
    *
    * <img src="./img/fromEventPattern.png" width="100%">
    *
-   * Creates an Observable by using the `addHandler` and `removeHandler`
-   * functions to add and remove the handlers, with an optional selector
-   * function to project the event arguments to a result. The `addHandler` is
-   * called when the output Observable is subscribed, and `removeHandler` is
-   * called when the Subscription is unsubscribed.
+   * `fromEventPattern` allows you to convert into an Observable any API that supports registering handler functions
+   * for events. It is similar to {@link fromEvent}, but far
+   * more flexible. In fact, all use cases of {@link fromEvent} could be easily handled by
+   * `fromEventPattern` (although in slightly more verbose way).
+   *
+   * This operator accepts as a first argument an `addHandler` function, which will be injected with
+   * handler parameter. That handler is actually an event handler function that you now can pass
+   * to API expecting it. `addHandler` will be called whenever Observable
+   * returned by the operator is subscribed, so registering handler in API will not
+   * necessarily happen when `fromEventPattern` is called.
+   *
+   * After registration, every time an event that we listen to happens,
+   * Observable returned by `fromEventPattern` will emit value that event handler
+   * function was called with. Note that if event handler was called with more
+   * then one argument, second and following arguments will not appear in the Observable.
+   *
+   * If API you are using allows to unregister event handlers as well, you can pass to `fromEventPattern`
+   * another function - `removeHandler` - as a second parameter. It will be injected
+   * with the same handler function as before, which now you can use to unregister
+   * it from the API. `removeHandler` will be called when consumer of resulting Observable
+   * unsubscribes from it.
+   *
+   * In some APIs unregistering is actually handled differently. Method registering an event handler
+   * returns some kind of token, which is later used to identify which function should
+   * be unregistered or it itself has method that unregisters event handler.
+   * If that is the case with your API, make sure token returned
+   * by registering method is returned by `addHandler`. Then it will be passed
+   * as a second argument to `removeHandler`, where you will be able to use it.
+   *
+   * If you need access to all event handler parameters (not only the first one),
+   * or you need to transform them in any way, you can call `fromEventPattern` with optional
+   * third parameter - project function which will accept all arguments passed to
+   * event handler when it is called. Whatever is returned from project function will appear on
+   * resulting stream instead of usual event handlers first argument. This means
+   * that default project can be thought of as function that takes its first parameter
+   * and ignores the rest.
    *
    * @example <caption>Emits clicks happening on the DOM document</caption>
    * function addClickHandler(handler) {
@@ -40,20 +70,54 @@ export class FromEventPatternObservable<T> extends Observable<T> {
    * );
    * clicks.subscribe(x => console.log(x));
    *
-   * @see {@link from}
+   * // Whenever you click anywhere in the browser, DOM MouseEvent
+   * // object will be logged.
+   *
+   *
+   * @example <caption>Use with API that returns cancellation token</caption>
+   * const token = someAPI.registerEventHandler(function() {});
+   * someAPI.unregisterEventHandler(token); // this APIs cancellation method accepts
+   *                                        // not handler itself, but special token.
+   *
+   * const someAPIObservable = Rx.Observable.fromEventPattern(
+   *   function(handler) { return someAPI.registerEventHandler(handler); }, // Note that we return the token here...
+   *   function(handler, token) { someAPI.unregisterEventHandler(token); }  // ...to then use it here.
+   * );
+   *
+   *
+   * @example <caption>Use with project function</caption>
+   * someAPI.registerEventHandler((eventType, eventMessage) => {
+   *   console.log(eventType, eventMessage); // Logs "EVENT_TYPE" "EVENT_MESSAGE" to console.
+   * });
+   *
+   * const someAPIObservable = Rx.Observable.fromEventPattern(
+   *   handler => someAPI.registerEventHandler(handler),
+   *   handler => someAPI.unregisterEventHandler(handler)
+   *   (eventType, eventMessage) => eventType + " --- " + eventMessage // without that function only "EVENT_TYPE"
+   * );                                                                // would be emitted by the Observable
+   *
+   * someAPIObservable.subscribe(value => console.log(value));
+   *
+   * // Logs:
+   * // "EVENT_TYPE --- EVENT_MESSAGE"
+   *
+   *
    * @see {@link fromEvent}
+   * @see {@link bindCallback}
+   * @see {@link bindNodeCallback}
    *
    * @param {function(handler: Function): any} addHandler A function that takes
    * a `handler` function as argument and attaches it somehow to the actual
    * source of events.
-   * @param {function(handler: Function, signal?: any): void} [removeHandler] An optional function that
-   * takes a `handler` function as argument and removes it in case it was
-   * previously attached using `addHandler`. if addHandler returns signal to teardown when remove,
-   * removeHandler function will forward it.
-   * @param {function(...args: any): T} [selector] An optional function to
-   * post-process results. It takes the arguments from the event handler and
+   * @param {function(handler: Function, token?: any): void} [removeHandler] A function that
+   * takes a `handler` function as an argument and removes it from the event source. If `addHandler`
+   * returns some kind of token, `removeHandler` function will have it as a second parameter.
+   * @param {function(...args: any): T} [project] A function to
+   * transform results. It takes the arguments from the event handler and
    * should return a single value.
-   * @return {Observable<T>}
+   * @return {Observable<T>} Observable which, when an event happens, emits first parameter
+   * passed to registered event handler. Alternatively it emits whatever project function returns
+   * at that moment.
    * @static true
    * @name fromEventPattern
    * @owner Observable
