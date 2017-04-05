af12551eb9d80e2728ecf98739b454403cafce2f
oto <otololua@gmail.com>
2017-04-05 18:11:59 +0400

    doc(map) remove extra every

--- src/operator/map.ts
+++ src/operator/map.ts
@@ -16,7 +16,7 @@ import { Observable } from '../Observable';
  * applies a projection to each value and emits that projection in the output
  * Observable.
  *
- * @example <caption>Map every every click to the clientX position of that click</caption>
+ * @example <caption>Map every click to the clientX position of that click</caption>
  * var clicks = Rx.Observable.fromEvent(document, 'click');
  * var positions = clicks.map(ev => ev.clientX);
  * positions.subscribe(x => console.log(x));
