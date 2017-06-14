dd8f0f6e18ad421c46ebffa3f8b8a794317f5e8c
SangKa.Z <sangka23@163.com>
2017-06-15 00:42:56 +0800

    docs(concatMap): remove the unused @return tag (#2643)

--- src/operator/concatMap.ts
+++ src/operator/concatMap.ts
@@ -58,9 +58,6 @@ export function concatMap<T, I, R>(this: Observable<T>, project: (value: T, inde
  * - `innerValue`: the value that came from the projected Observable
  * - `outerIndex`: the "index" of the value that came from the source
  * - `innerIndex`: the "index" of the value from the projected Observable
- * @return {Observable} An observable of values merged from the projected
- * Observables as they were subscribed to, one at a time. Optionally, these
- * values may have been projected from a passed `projectResult` argument.
  * @return {Observable} An Observable that emits the result of applying the
  * projection function (and the optional `resultSelector`) to each item emitted
  * by the source Observable and taking values from each projected inner
