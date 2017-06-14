c250afc6f854220853dbe773da636ce1fd9a3070
OJ Kwon <kwon.ohjoong@gmail.com>
2017-06-14 10:54:05 -0700

    fix(Observable): errors thrown in observer/handlers without an operator applied will no longer be swallowed (#2626)
    - closes #2565

--- src/Observable.ts
+++ src/Observable.ts
@@ -198,7 +198,7 @@ export class Observable<T> implements Subscribable<T> {
     if (operator) {
       operator.call(sink, this.source);
     } else {
-      sink.add(this._trySubscribe(sink));
+      sink.add(this.source ? this._subscribe(sink) : this._trySubscribe(sink));
     }
 
     if (sink.syncErrorThrowable) {
