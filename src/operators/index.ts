b6e5b567e214127e3a5361f8672fdeca17e58d02
Ben Lesh <ben@benlesh.com>
2017-06-15 09:42:46 -0700

    feat(switchMap): add higher-order lettable version of switchMap

--- src/operators/index.ts
+++ src/operators/index.ts
@@ -2,3 +2,4 @@ export { concatMap } from './concatMap';
 export { filter } from './filter';
 export { map } from './map';
 export { mergeMap } from './mergeMap';
+export { switchMap } from './switchMap';
