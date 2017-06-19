f82b05870456f1ade1c4559f6457f9fa51d71b17
Ben Lesh <ben@benlesh.com>
2017-06-19 16:42:02 -0700

    refactor(Observable.if): reduce library size by shrinking `Observable.if`
    - implements `if` in terms of `defer`

--- src/observable/if.ts
+++ src/observable/if.ts
@@ -1,21 +1,25 @@
-import { SubscribableOrPromise } from '../Observable';
+
+/* tslint:disable:no-unused-variable */
+import { Observable, Subscribable, SubscribableOrPromise } from '../Observable';
+/* tslint:enable:no-unused-variable */
 import { defer } from './defer';
 import { _throw } from './throw';
 import { empty } from './empty';
 
 export const _if = <T, R>(condition: () => boolean | void,
   thenSource?: SubscribableOrPromise<T> | void,
-  elseSource?: SubscribableOrPromise<R> | void) => defer<T|R>(() => {
-    let result: boolean;
-    try {
-      result = Boolean(condition());
-    } catch (err) {
-      return _throw(err);
-    }
+  elseSource?: SubscribableOrPromise<R> | void): Observable<T|R> =>
+    defer<T|R>(() => {
+      let result: boolean;
+      try {
+        result = Boolean(condition());
+      } catch (err) {
+        return _throw(err);
+      }
 
-    if (result) {
-      return thenSource || empty();
-    } else {
-      return elseSource || empty();
-    }
-  });
+      if (result) {
+        return thenSource || empty();
+      } else {
+        return elseSource || empty();
+      }
+    });
