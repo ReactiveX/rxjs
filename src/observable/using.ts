54ac01efd95ae41e3416594ae49fd6c00ee0dd9e
Ben Lesh <ben@benlesh.com>
2017-06-19 18:01:17 -0700

    refactor(using): reduce size of `Observable.using`

--- src/observable/using.ts
+++ src/observable/using.ts
@@ -1,3 +1,36 @@
-import {  UsingObservable  } from './UsingObservable';
+/* tslint:disable:no-unused-variable */
+import { Observable, Subscribable, SubscribableOrPromise } from '../Observable';
+/* tslint:enable:no-unused-variable */
+import { AnonymousSubscription } from '../Subscription';
+import { Subscription } from '../Subscription';
+import { from } from './from';
+import { noop } from '../util/noop';
 
-export const using = UsingObservable.create;
+export const using = <T>(resourceFactory: () => AnonymousSubscription | void,
+  observableFactory: (resource: AnonymousSubscription) => SubscribableOrPromise<T> | void): Observable<T> =>
+    new Observable<T>(observer => {
+      let resource: AnonymousSubscription | void;
+      try {
+        resource = resourceFactory();
+      } catch (err) {
+        observer.error(err);
+        return noop;
+      }
+
+      const source: SubscribableOrPromise<T> | void = observableFactory(resource || Subscription.EMPTY); ;
+      let subscription: Subscription;
+      if (!source) {
+        observer.complete();
+      } else {
+        subscription = from(source).subscribe(observer);
+      }
+
+      return () => {
+        if (subscription) {
+          subscription.unsubscribe();
+        }
+        if (resource) {
+          resource.unsubscribe();
+        }
+      };
+    });
