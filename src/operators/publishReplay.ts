6daa0a67d10639654eb9061b15fd98594cefb42d
Martin Sikora <martin.sikora.ahoj@gmail.com>
2017-10-05 23:30:10 +0200

    fix(publishReplay): make the selector optional

--- src/operators/publishReplay.ts
+++ src/operators/publishReplay.ts
@@ -7,7 +7,7 @@ import { UnaryFunction } from '../interfaces';
 
 export function publishReplay<T>(bufferSize: number = Number.POSITIVE_INFINITY,
                                  windowTime: number = Number.POSITIVE_INFINITY,
-                                 selector: (source: Observable<T>) => Observable<T>,
+                                 selector?: (source: Observable<T>) => Observable<T>,
                                  scheduler?: IScheduler): UnaryFunction<Observable<T>, ConnectableObservable<T>> {
   const subject = new ReplaySubject<T>(bufferSize, windowTime, scheduler);
   return (source: Observable<T>) => multicast(() => subject, selector)(source) as ConnectableObservable<T>;
