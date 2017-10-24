6dc1b807fe0c60314cbf008655ce395bc2d6ed9b
Sean Healy <s@xib.ca>
2017-10-24 10:27:49 -0400

    docs(share): fix description in relation to multicast
    PR #2990 
    
    As suggested by https://github.com/ReactiveX/rxjs/issues/2937 this updates the docs on the share operator to better reflect it's actual behaviour.

--- src/operators/share.ts
+++ src/operators/share.ts
@@ -13,7 +13,7 @@ function shareSubjectFactory() {
  * Returns a new Observable that multicasts (shares) the original Observable. As long as there is at least one
  * Subscriber this Observable will be subscribed and emitting data. When all subscribers have unsubscribed it will
  * unsubscribe from the source Observable. Because the Observable is multicasting it makes the stream `hot`.
- * This is an alias for .publish().refCount().
+ * This is an alias for .multicast(() => new Subject()).refCount().
  *
  * <img src="./img/share.png" width="100%">
  *
