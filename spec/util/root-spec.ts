68ee4997c875ec0d2a4acdadf252d9ee685d8acf
Ben Lesh <ben@benlesh.com>
2017-11-02 20:51:27 -0700

    refactor(distinct): Remove Set polyfill
    BREAKING CHANGE: Using `distinct` requires a `Set` implementation and must be polyfilled in older runtimes

--- spec/util/root-spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect } from 'chai';
-import { root } from '../../src/util/root';
-
-/** @test {root} */
-describe('root', () => {
-  it('should exist', () => {
-    expect(typeof root).to.equal('object');
-  });
-});
