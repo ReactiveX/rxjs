7d722d4d36debcf5bb79fa4006f455bd72caeee1
Nicholas Jamieson <nicholas@cartant.com>
2017-11-28 03:27:58 +1000

    fix(scheduler): prevent unwanted clearInterval (#3044)
    * test(scheduler): add interval recycling tests
    
    * fix(scheduler): prevent unwanted clearInterval
    
    In AsyncAction, this.pending was assigned before the call to
    recycleAsyncId. That prevented the interval from being re-used resulting
    in the interval being cleared and re-created for each notification.
    
    Closes #3042

--- spec/schedulers/AsapScheduler-spec.ts
+++ spec/schedulers/AsapScheduler-spec.ts
@@ -40,6 +40,59 @@ describe('Scheduler.asap', () => {
     sandbox.restore();
   });
 
+  it('should reuse the interval for recursively scheduled actions with the same delay', () => {
+    const sandbox = sinon.sandbox.create();
+    const fakeTimer = sandbox.useFakeTimers();
+    // callThrough is missing from the declarations installed by the typings tool in stable
+    const stubSetInterval = (<any> sinon.stub(global, 'setInterval')).callThrough();
+    function dispatch(state: any): void {
+      state.index += 1;
+      if (state.index < 3) {
+        (<any> this).schedule(state, state.period);
+      }
+    }
+    const period = 50;
+    const state = { index: 0, period };
+    asap.schedule(dispatch, period, state);
+    expect(state).to.have.property('index', 0);
+    expect(stubSetInterval).to.have.property('callCount', 1);
+    fakeTimer.tick(period);
+    expect(state).to.have.property('index', 1);
+    expect(stubSetInterval).to.have.property('callCount', 1);
+    fakeTimer.tick(period);
+    expect(state).to.have.property('index', 2);
+    expect(stubSetInterval).to.have.property('callCount', 1);
+    stubSetInterval.restore();
+    sandbox.restore();
+  });
+
+  it('should not reuse the interval for recursively scheduled actions with a different delay', () => {
+    const sandbox = sinon.sandbox.create();
+    const fakeTimer = sandbox.useFakeTimers();
+    // callThrough is missing from the declarations installed by the typings tool in stable
+    const stubSetInterval = (<any> sinon.stub(global, 'setInterval')).callThrough();
+    function dispatch(state: any): void {
+      state.index += 1;
+      state.period -= 1;
+      if (state.index < 3) {
+        (<any> this).schedule(state, state.period);
+      }
+    }
+    const period = 50;
+    const state = { index: 0, period };
+    asap.schedule(dispatch, period, state);
+    expect(state).to.have.property('index', 0);
+    expect(stubSetInterval).to.have.property('callCount', 1);
+    fakeTimer.tick(period);
+    expect(state).to.have.property('index', 1);
+    expect(stubSetInterval).to.have.property('callCount', 2);
+    fakeTimer.tick(period);
+    expect(state).to.have.property('index', 2);
+    expect(stubSetInterval).to.have.property('callCount', 3);
+    stubSetInterval.restore();
+    sandbox.restore();
+  });
+
   it('should schedule an action to happen later', (done: MochaDone) => {
     let actionHappened = false;
     asap.schedule(() => {
