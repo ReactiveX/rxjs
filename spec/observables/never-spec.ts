d57fa52c6aca3161622f62d81f1985f3b559b8e1
Ben Lesh <ben@benlesh.com>
2018-01-22 15:51:14 -0800

    feat(never): always return the same instance (#3249)
    - never always returns the same Observable instance
    - Remove NeverObservable
    - never return value now typed as `Observable<never>` (lol)
    
    BREAKING CHANGE: `never()` always returns the same instance
    BREAKING CHANGE: TypeScript typing for `never()` is now `Observable<never>` and the function no longer requires a generic type.

--- spec/observables/never-spec.ts
+++ spec/observables/never-spec.ts
@@ -1,16 +1,19 @@
-import * as Rx from '../../src/Rx';
+import { never } from '../../src/create';
+import { expect } from 'chai';
 import marbleTestingSignature = require('../helpers/marble-testing'); // tslint:disable-line:no-require-imports
 
-declare const { asDiagram };
+declare const asDiagram: any;
 declare const expectObservable: typeof marbleTestingSignature.expectObservable;
 
-const Observable = Rx.Observable;
-
 /** @test {never} */
-describe('Observable.never', () => {
+describe('never', () => {
   asDiagram('never')('should create a cold observable that never emits', () => {
     const expected = '-';
-    const e1 = Observable.never();
+    const e1 = never();
     expectObservable(e1).toBe(expected);
+  });
+
+  it('should return the same instance every time', () => {
+    expect(never()).to.equal(never());
   });
 });
