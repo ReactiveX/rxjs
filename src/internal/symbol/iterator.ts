8825cd5d43e9d4e52d2367e2ba4b0cac538d9727
benlesh <ben@benlesh.com>
2018-03-07 20:38:56 -0800

    refactor(Symbol_iterator): remove warning

--- src/internal/symbol/iterator.ts
+++ src/internal/symbol/iterator.ts
@@ -1,10 +1,5 @@
 export function getSymbolIterator(): symbol {
-  /* NOTE: Warning users that they don't have a Symbol.iterator
-     polyfill. We don't want to throw on this, because it's not required
-     by the library. However it will provide clues to users on older
-     browsers why things like `from(iterable)` doesn't work. */
   if (typeof Symbol !== 'function' || !Symbol.iterator) {
-    console.warn('RxJS: Symbol.iterator does not exist, so things like from(iterable) won\'t work');
     return '@@iterator' as any;
   }
 
