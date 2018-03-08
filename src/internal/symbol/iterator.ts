6725be1b5275d82bbb2ef37af1c47a933d72d4f4
Jay Phelps <hello@jayphelps.com>
2018-03-07 23:22:41 -0500

    fix(Symbol.iterator): correctly handle case where Symbol constructor itself is not defined (#3394)

--- src/internal/symbol/iterator.ts
+++ src/internal/symbol/iterator.ts
@@ -1,14 +1,17 @@
+export function getSymbolIterator(): symbol {
+  /* NOTE: Warning users that they don't have a Symbol.iterator
+     polyfill. We don't want to throw on this, because it's not required
+     by the library. However it will provide clues to users on older
+     browsers why things like `from(iterable)` doesn't work. */
+  if (typeof Symbol !== 'function' || !Symbol.iterator) {
+    console.warn('RxJS: Symbol.iterator does not exist, so things like from(iterable) won\'t work');
+    return '@@iterator' as any;
+  }
 
-/* NOTE: Warning users that they don't have a Symbol.iterator
-   polyfill. We don't want to throw on this, because it's not required
-   by the library. However it will provide clues to users on older
-   browsers why things like `from(iterable)` doesn't work. */
-if (!Symbol || !Symbol.iterator) {
-  console.warn('RxJS: Symbol.observable does not exist');
+  return Symbol.iterator;
 }
 
-/** The native Symbol.iterator instance or a string */
-export const iterator = Symbol && Symbol.iterator || '@@iterator';
+export const iterator = getSymbolIterator();
 
 /**
  * @deprecated use {@link iterator} instead
