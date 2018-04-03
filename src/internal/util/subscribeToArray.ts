eb8ff5dfc6defd32cb5d85bae34fa145c2dc28d0
Ben Lesh <ben@benlesh.com>
2018-04-03 13:20:04 -0700

    fix(closure-compiler): removes bad \@params comments that caused issues
    There were some params comments added that should not have been there, given that they were part of the result of a higher order function, not the initial arguments.

--- src/internal/util/subscribeToArray.ts
+++ src/internal/util/subscribeToArray.ts
@@ -3,7 +3,6 @@ import { Subscriber } from '../Subscriber';
 /**
  * Subscribes to an ArrayLike with a subscriber
  * @param array The array or array-like to subscribe to
- * @param subscriber The subscriber to subscribe with.
  */
 export const subscribeToArray = <T>(array: ArrayLike<T>) => (subscriber: Subscriber<T>) => {
   for (let i = 0, len = array.length; i < len && !subscriber.closed; i++) {
