8dc93cc87988c05f6ce6d753822e81d6c0ac4de9
Ben Lesh <ben@benlesh.com>
2018-04-23 11:44:40 -0700

    test(from): add take tests

--- modules/core/internal/create/from-spec.ts
+++ modules/core/internal/create/from-spec.ts
@@ -1,5 +1,6 @@
 import { from } from './from';
 import { expect } from 'chai';
+import { take } from '../operators/take';
 
 describe('from', () => {
   it('should convert a Promise to an observable', (done) => {
@@ -16,10 +17,27 @@ describe('from', () => {
       });
   });
 
-  it('should convert an Array to an observable', () => {
-    const results: any[] = [];
-    const source = [1, 2, 3];
-    from(source)
+  describe('from(Array)', () => {
+    it('should convert an Array to an observable', () => {
+      const results: any[] = [];
+      const source = [1, 2, 3];
+      from(source)
+        .subscribe({
+          next(value) { results.push(value); },
+          complete() {
+            results.push('done');
+          },
+        });
+
+      expect(results).to.deep.equal([1, 2, 3, 'done']);
+    });
+
+    it('should work with take', () => {
+      const results: any[] = [];
+      const source = [1, 2, 3, 4, 5];
+      from(source).pipe(
+        take(3),
+      )
       .subscribe({
         next(value) { results.push(value); },
         complete() {
@@ -27,19 +45,45 @@ describe('from', () => {
         },
       });
 
-    expect(results).to.deep.equal([1, 2, 3, 'done']);
+      expect(results).to.deep.equal([1, 2, 3, 'done']);
+    });
   });
 
-  it('should convert an ArrayLike to an observable', () => {
-    const results: any[] = [];
-    const source = {
-      '0': 1,
-      '1': 2,
-      '2': 3,
-      length: 3,
-    };
+  describe('from(ArrayLike)', () => {
+    it('should convert an ArrayLike to an observable', () => {
+      const results: any[] = [];
+      const source = {
+        '0': 1,
+        '1': 2,
+        '2': 3,
+        length: 3,
+      };
 
-    from(source)
+      from(source)
+        .subscribe({
+          next(value) { results.push(value); },
+          complete() {
+            results.push('done');
+          },
+        });
+
+      expect(results).to.deep.equal([1, 2, 3, 'done']);
+    });
+
+    it('should work with take', () => {
+      const results: any[] = [];
+      const source = {
+        '0': 1,
+        '1': 2,
+        '2': 3,
+        '3': 4,
+        '4': 5,
+        length: 5,
+      };
+
+      from(source).pipe(
+        take(3),
+      )
       .subscribe({
         next(value) { results.push(value); },
         complete() {
@@ -47,14 +91,34 @@ describe('from', () => {
         },
       });
 
-    expect(results).to.deep.equal([1, 2, 3, 'done']);
+      expect(results).to.deep.equal([1, 2, 3, 'done']);
+    });
   });
 
-  it('should convert an Iterable to an observable', () => {
-    const results: any[] = [];
-    const source = 'Weee!';
+  describe('from(Iterable)', () => {
+    it('should convert an Iterable to an observable', () => {
+      const results: any[] = [];
+      const source = 'Weee!';
 
-    from(source)
+      from(source)
+        .subscribe({
+          next(value) { results.push(value); },
+          complete() {
+            results.push('done');
+          },
+        });
+
+      expect(results).to.deep.equal(['W', 'e', 'e', 'e', '!', 'done']);
+    });
+
+
+    it('should work with take', () => {
+      const results: any[] = [];
+      const source = 'Weeeeeeeeeeeeeee!';
+
+      from(source).pipe(
+        take(3),
+      )
       .subscribe({
         next(value) { results.push(value); },
         complete() {
@@ -62,6 +126,7 @@ describe('from', () => {
         },
       });
 
-    expect(results).to.deep.equal(['W', 'e', 'e', 'e', '!', 'done']);
+      expect(results).to.deep.equal(['W', 'e', 'e', 'done']);
+    });
   });
 });
