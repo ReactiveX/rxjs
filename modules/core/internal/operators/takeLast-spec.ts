9d8a3a17c48430e8c5dab01ee593d117b9b30889
Ben Lesh <ben@benlesh.com>
2018-04-25 14:54:55 -0700

    feat(takeLast): add basic takeLast

---
+++ modules/core/internal/operators/takeLast-spec.ts
@@ -0,0 +1,45 @@
+import { expect } from 'chai';
+import { of } from '../create/of';
+import { takeLast } from './takeLast';
+
+describe('takeLast', () => {
+  it('should take the last n values when the observable completes', () => {
+    const results: any[] = [];
+    of(1, 2, 3, 4, 5, 6, 7).pipe(
+      takeLast(4),
+    )
+    .subscribe({
+      next(value) { results.push(value); },
+      complete() { results.push('done'); },
+    });
+
+    expect(results).to.deep.equal([4, 5, 6, 7, 'done']);
+  });
+
+  it('should default to just taking the very last value', () => {
+    const results: any[] = [];
+    of(1, 2, 3, 4, 5, 6, 7).pipe(
+      takeLast(),
+    )
+    .subscribe({
+      next(value) { results.push(value); },
+      complete() { results.push('done'); },
+    });
+
+    expect(results).to.deep.equal([7, 'done']);
+  });
+
+  // TODO: This really needs a marbles test
+  it('should wait and complete for counts less than one', () => {
+    const results: any[] = [];
+    of(1, 2, 3, 4, 5, 6, 7).pipe(
+      takeLast(-1),
+    )
+    .subscribe({
+      next(value) { results.push(value); },
+      complete() { results.push('done'); },
+    });
+
+    expect(results).to.deep.equal(['done']);
+  });
+});
---
+++ modules/core/internal/operators/takeLast.ts
@@ -0,0 +1,29 @@
+import { Operation, FOType, Sink, SinkArg } from "../types";
+import { Observable, sourceAsObservable } from "../Observable";
+import { Subscription } from "../Subscription";
+
+export function takeLast<T>(count: number = 1): Operation<T, T> {
+  return (source: Observable<T>) =>
+    sourceAsObservable((type: FOType.SUBSCRIBE, dest: Sink<T>, subs: Subscription) => {
+      if (type === FOType.SUBSCRIBE) {
+        const buffer: T[] = [];
+        source(FOType.SUBSCRIBE, (t: FOType, v: SinkArg<T>, subs: Subscription) => {
+          switch (t) {
+            case FOType.NEXT:
+              buffer.push(v);
+              while (buffer.length > count) buffer.shift();
+              break;
+            case FOType.COMPLETE:
+              while (buffer.length > 0) {
+                dest(FOType.NEXT, buffer.shift(), subs);
+              }
+              dest(FOType.COMPLETE, undefined, subs);
+              break;
+            default:
+              dest(t, v, subs);
+              break;
+          }
+        }, subs);
+      }
+    });
+  }
