22b5ca3fa351791eb7b94f831949f286448ea084
Ben Lesh <ben@benlesh.com>
2018-04-26 11:55:45 -0700

    feat(schedulers): `scheduled` and `active` properties deprecated
    related #3572

--- src/internal/scheduler/AsyncScheduler.ts
+++ src/internal/scheduler/AsyncScheduler.ts
@@ -11,6 +11,7 @@ export class AsyncScheduler extends Scheduler {
    * A flag to indicate whether the Scheduler is currently executing a batch of
    * queued actions.
    * @type {boolean}
+   * @deprecated internal use only
    */
   public active: boolean = false;
   /**
@@ -18,6 +19,7 @@ export class AsyncScheduler extends Scheduler {
    * coming from `setTimeout`, `setInterval`, `requestAnimationFrame`, and
    * others.
    * @type {any}
+   * @deprecated internal use only
    */
   public scheduled: any = undefined;
 
