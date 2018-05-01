a030a194dea49a0cad407e4552f040308f0d54cd
Ben Lesh <ben@benlesh.com>
2018-05-01 11:59:11 -0700

    test(isInteropObservable): add tests

---
+++ modules/core/internal/util/isInteropObservable-spec.ts
@@ -0,0 +1,26 @@
+import { isInteropObservable } from "./isInteropObservable";
+import { expect } from "chai";
+
+describe('isInteropObservable', () => {
+  before(() => {
+    if (!Symbol.observable) {
+      (Symbol as any).observable = Symbol('test polyfill observable');
+    }
+  });
+
+  it('should pass for objects with Symbol.observable', () => {
+    const obj = {
+      [Symbol.observable]() { /* noop */ },
+    };
+
+    expect(isInteropObservable(obj)).to.be.true;
+  });
+
+  it('should fail for objects that are just subscribables', () => {
+    const obj = {
+      subscribe() { /* noop */ },
+    };
+
+    expect(isInteropObservable(obj)).to.be.false;
+  });
+});
