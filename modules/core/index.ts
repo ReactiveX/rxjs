432da248036c17101f41324c1ab0a4fbc0446ea2
Ben Lesh <ben@benlesh.com>
2018-05-01 16:23:46 -0700

    refactor(exports): update exports

--- modules/core/index.ts
+++ modules/core/index.ts
@@ -1,5 +1,9 @@
 export { Observable } from './internal/Observable';
 export { Subscription } from './internal/Subscription';
 
-export { of } from './internal/create/of';
+export { combineLatest } from './internal/create/combineLatest';
+export { defer } from './internal/create/defer';
 export { from } from './internal/create/from';
+export { of } from './internal/create/of';
+export { race } from './internal/create/race';
+export { zip } from './internal/create/zip';
--- modules/core/operators.ts
+++ modules/core/operators.ts
@@ -1,5 +1,11 @@
 export { catchError } from './internal/operators/catchError';
+export { filter } from './internal/operators/filter';
+export { groupBy } from './internal/operators/groupBy';
 export { map } from './internal/operators/map';
 export { mergeMap } from './internal/operators/mergeMap';
 export { repeat } from './internal/operators/repeat';
+export { scan } from './internal/operators/scan';
+export { share } from './internal/operators/share';
+export { switchMap } from './internal/operators/switchMap';
 export { take } from './internal/operators/take';
+export { takeLast } from './internal/operators/takeLast';
