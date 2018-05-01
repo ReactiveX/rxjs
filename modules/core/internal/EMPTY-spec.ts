353095142a2e6dd2214b107fbf311a64a0858f6e
Ben Lesh <ben@benlesh.com>
2018-05-01 13:59:38 -0700

    chore(WIP): moved schedulers, add EMPTY

---
+++ modules/core/internal/EMPTY-spec.ts
@@ -0,0 +1,17 @@
+import { EMPTY } from "./EMPTY";
+import { expect } from "chai";
+
+describe('EMPTY', () => {
+  it('should complete and emit nothing', () => {
+    let completed = false;
+    EMPTY.subscribe({
+      next() { throw new Error('this should not happen'); },
+      error(err) { throw err; },
+      complete() {
+        completed = true;
+      }
+    });
+
+    expect(completed).to.be.true;
+  });
+});
