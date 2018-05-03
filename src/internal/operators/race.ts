1595c4056f1a47a89d357b196e0eed5521c52a92
natmegs <nataliemegans@gmail.com>
2018-05-03 13:04:22 -0700

    refactor(race): keep original documentation and mark as deprecated

--- src/internal/operators/race.ts
+++ src/internal/operators/race.ts
@@ -15,6 +15,12 @@ export function race<T, R>(...observables: Array<Observable<any> | Array<Observa
 /* tslint:enable:max-line-length */
 
 /**
+ * Returns an Observable that mirrors the first source Observable to emit an item
+ * from the combination of this Observable and supplied Observables.
+ * @param {...Observables} ...observables Sources used to race for which Observable emits first.
+ * @return {Observable} An Observable that mirrors the output of the first Observable to emit an item.
+ * @method race
+ * @owner Observable
  * @deprecated Deprecated in favor of static race.
  */
 export function race<T>(...observables: Array<Observable<T> | Array<Observable<T>>>): MonoTypeOperatorFunction<T> {
