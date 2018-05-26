e3d21a71bbbd09b226650bdb2bf6fffbb4f5efac
natmegs <nataliemegans@gmail.com>
2018-05-25 21:44:22 -0700

    refactor(mergeAll-spec): update mergeAll operator tests to use pipeable operators

--- spec/operators/mergeAll-spec.ts
+++ spec/operators/mergeAll-spec.ts
@@ -1,44 +1,42 @@
 import { expect } from 'chai';
-import * as Rx from 'rxjs/Rx';
+import { mergeAll, mergeMap } from 'rxjs/operators';
 import { hot, cold, expectObservable, expectSubscriptions } from '../helpers/marble-testing';
-import { throwError } from 'rxjs';
+import { throwError, from, of, Observable } from 'rxjs';
 
 declare function asDiagram(arg: string): Function;
 declare const type: Function;
 
-const Observable = Rx.Observable;
-
 /** @test {mergeAll} */
-describe('Observable.prototype.mergeAll', () => {
+describe('mergeAll oeprator', () => {
   asDiagram('mergeAll')('should merge a hot observable of cold observables', () => {
     const x = cold(    '--a---b--c---d--|      ');
     const y = cold(           '----e---f--g---|');
     const e1 = hot(  '--x------y-------|       ', { x: x, y: y });
     const expected = '----a---b--c-e-d-f--g---|';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
   });
 
   it('should merge all observables in an observable', () => {
-    const e1 = Observable.from([
-      Observable.of('a'),
-      Observable.of('b'),
-      Observable.of('c')
+    const e1 = from([
+      of('a'),
+      of('b'),
+      of('c')
     ]);
     const expected = '(abc|)';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
   });
 
   it('should throw if any child observable throws', () => {
-    const e1 = Observable.from([
-      Observable.of('a'),
+    const e1 = from([
+      of('a'),
       throwError('error'),
-      Observable.of('c')
+      of('c')
     ]);
     const expected = '(a#)';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
   });
 
   it('should handle merging a hot observable of observables', () => {
@@ -50,7 +48,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^                !';
     const expected =  '--a--db--ec--f---|';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -65,7 +63,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^                         !';
     const expected =  '--a---b---c---d---e---f---|';
 
-    expectObservable(e1.mergeAll(1)).toBe(expected);
+    expectObservable(e1.pipe(mergeAll(1))).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -82,7 +80,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^                     !';
     const expected =  '--a--db--ec--f--g---h-|';
 
-    expectObservable(e1.mergeAll(2)).toBe(expected);
+    expectObservable(e1.pipe(mergeAll(2))).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(z.subscriptions).toBe(zsubs);
@@ -98,7 +96,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^                        !';
     const expected =  '---a---b---c-----e---f---|';
 
-    expectObservable(e1.mergeAll(1)).toBe(expected);
+    expectObservable(e1.pipe(mergeAll(1))).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -115,7 +113,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^                      !';
     const expected =  '---a--db--ec--f--g---h-|';
 
-    expectObservable(e1.mergeAll(2)).toBe(expected);
+    expectObservable(e1.pipe(mergeAll(2))).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(z.subscriptions).toBe(zsubs);
@@ -132,7 +130,7 @@ describe('Observable.prototype.mergeAll', () => {
     const unsub =     '            !     ';
     const expected =  '--a--db--ec--     ';
 
-    expectObservable(e1.mergeAll(), unsub).toBe(expected);
+    expectObservable(e1.pipe(mergeAll()), unsub).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -148,10 +146,11 @@ describe('Observable.prototype.mergeAll', () => {
     const expected =  '--a--db--ec--     ';
     const unsub =     '            !     ';
 
-    const result = e1
-      .mergeMap((x) => Observable.of(x))
-      .mergeAll()
-      .mergeMap((x) => Observable.of(x));
+    const result = e1.pipe(
+      mergeMap((x) => of(x)),
+      mergeAll(),
+      mergeMap((x) => of(x))
+    );
 
     expectObservable(result, unsub).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
@@ -168,7 +167,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^                   !';
     const expected =  '------(ad)-(be)-(cf)|';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -183,7 +182,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^       !';
     const expected =  '--------|';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -200,7 +199,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^          !';
     const expected =  '-----------|';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(z.subscriptions).toBe(zsubs);
@@ -216,7 +215,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^        ';
     const expected =  '---------';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -231,7 +230,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^        ';
     const expected =  '---------';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -246,7 +245,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^    !   ';
     const expected =  '-----#   ';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -261,7 +260,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^    !   ';
     const expected =  '-----#   ';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -276,7 +275,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^          !';
     const expected =  '-----------#';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -291,7 +290,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =    '^          !';
     const expected =  '-----------#';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -302,7 +301,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =   '(^!)';
     const expected = '|';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
@@ -311,7 +310,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =   '^';
     const expected = '-';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
@@ -320,7 +319,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =   '(^!)';
     const expected = '#';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
@@ -335,7 +334,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =   '^                              !';
     const expected = '--a-b-------c-d-e-f--g-h-i-j-k-|';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(z.subscriptions).toBe(zsubs);
@@ -353,7 +352,7 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =   '^                   !           ';
     const expected = '--a-b-------c-d-e-f-#           ';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(x.subscriptions).toBe(xsubs);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(z.subscriptions).toBe(zsubs);
@@ -369,14 +368,14 @@ describe('Observable.prototype.mergeAll', () => {
     const e1subs =   '^               !              ';
     const expected = '--a-b-------c-d-#              ';
 
-    expectObservable(e1.mergeAll()).toBe(expected);
+    expectObservable(e1.pipe(mergeAll())).toBe(expected);
     expectSubscriptions(y.subscriptions).toBe(ysubs);
     expectSubscriptions(z.subscriptions).toBe(zsubs);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
   });
 
   it('should merge all promises in an observable', (done) => {
-    const e1 = Rx.Observable.from([
+    const e1 = from([
       new Promise<string>((res) => { res('a'); }),
       new Promise<string>((res) => { res('b'); }),
       new Promise<string>((res) => { res('c'); }),
@@ -385,7 +384,7 @@ describe('Observable.prototype.mergeAll', () => {
     const expected = ['a', 'b', 'c', 'd'];
 
     const res: string[] = [];
-    e1.mergeAll().subscribe(
+    e1.pipe(mergeAll()).subscribe(
       (x) => { res.push(x); },
       (err) => { done(new Error('should not be called')); },
       () => {
@@ -396,7 +395,7 @@ describe('Observable.prototype.mergeAll', () => {
 
   it('should raise error when promise rejects', (done) => {
     const error = 'error';
-    const e1 = Rx.Observable.from([
+    const e1 = from([
       new Promise<string>((res) => { res('a'); }),
       new Promise<string>((res: any, rej) => { rej(error); }),
       new Promise<string>((res) => { res('c'); }),
@@ -404,7 +403,7 @@ describe('Observable.prototype.mergeAll', () => {
     ]);
 
     const res: string[] = [];
-    e1.mergeAll().subscribe(
+    e1.pipe(mergeAll()).subscribe(
       (x) => { res.push(x); },
       (err) => {
         expect(res.length).to.equal(1);
@@ -416,51 +415,47 @@ describe('Observable.prototype.mergeAll', () => {
 
   type(() => {
     /* tslint:disable:no-unused-variable */
-    const source1 = Rx.Observable.of(1, 2, 3);
+    const source1 = of(1, 2, 3);
     const source2 = [1, 2, 3];
     const source3 = new Promise<number>(d => d(1));
 
-    let result: Rx.Observable<number> = Rx.Observable
-      .of(source1, source2, source3)
-      .pipe(Rx.operators.mergeAll());
+    let result: Observable<number> = of(source1, source2, source3)
+      .pipe(mergeAll());
     /* tslint:enable:no-unused-variable */
   });
 
   type(() => {
     /* tslint:disable:no-unused-variable */
-    const source1 = Rx.Observable.of(1, 2, 3);
+    const source1 = of(1, 2, 3);
     const source2 = [1, 2, 3];
     const source3 = new Promise<number>(d => d(1));
 
-    let result: Rx.Observable<number> = Rx.Observable
-      .of(source1, source2, source3)
-      .mergeAll();
-    /* tslint:enable:no-unused-variable */
-  });
-
-  type(() => {
-    // coerce type to a specific type
-    /* tslint:disable:no-unused-variable */
-    const source1 = Rx.Observable.of(1, 2, 3);
-    const source2 = [1, 2, 3];
-    const source3 = new Promise<number>(d => d(1));
-
-    let result: Rx.Observable<string> = Rx.Observable
-      .of(<any>source1, <any>source2, <any>source3)
-      .pipe(Rx.operators.mergeAll<string>());
+    let result: Observable<number> = of(source1, source2, source3)
+      .pipe(mergeAll());
     /* tslint:enable:no-unused-variable */
   });
 
   type(() => {
     // coerce type to a specific type
     /* tslint:disable:no-unused-variable */
-    const source1 = Rx.Observable.of(1, 2, 3);
+    const source1 = of(1, 2, 3);
     const source2 = [1, 2, 3];
     const source3 = new Promise<number>(d => d(1));
 
-    let result: Rx.Observable<string> = Rx.Observable
-      .of(<any>source1, <any>source2, <any>source3)
-      .mergeAll<string>();
+    let result: Observable<string> = of(<any>source1, <any>source2, <any>source3)
+      .pipe(mergeAll<string>());
+    /* tslint:enable:no-unused-variable */
+  });
+
+  type(() => {
+    // coerce type to a specific type
+    /* tslint:disable:no-unused-variable */
+    const source1 = of(1, 2, 3);
+    const source2 = [1, 2, 3];
+    const source3 = new Promise<number>(d => d(1));
+
+    let result: Observable<string> = of(<any>source1, <any>source2, <any>source3)
+      .pipe(mergeAll<string>());
     /* tslint:enable:no-unused-variable */
   });
 });
