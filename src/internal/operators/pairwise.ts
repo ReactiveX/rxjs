85e3b4852364c2971c44cd093a92b3e97a4a4b1b
moniuch <moniuch@gmail.com>
2018-06-01 21:21:07 +0200

    docs(jsdoc): pairwise - update example to use pipe()

--- src/internal/operators/pairwise.ts
+++ src/internal/operators/pairwise.ts
@@ -21,15 +21,17 @@ import { OperatorFunction } from '../types';
  * ## Example
  * On every click (starting from the second), emit the relative distance to the previous click
  * ```javascript
- * var clicks = Rx.Observable.fromEvent(document, 'click');
- * var pairs = clicks.pairwise();
- * var distance = pairs.map(pair => {
- *   var x0 = pair[0].clientX;
- *   var y0 = pair[0].clientY;
- *   var x1 = pair[1].clientX;
- *   var y1 = pair[1].clientY;
- *   return Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2));
- * });
+ * const clicks = fromEvent(document, 'click');
+ * const pairs = clicks.pipe(pairwise());
+ * const distance = pairs.pipe(
+ *   map(pair => {
+ *     const x0 = pair[0].clientX;
+ *     const y0 = pair[0].clientY;
+ *     const x1 = pair[1].clientX;
+ *     const y1 = pair[1].clientY;
+ *     return Math.sqrt(Math.pow(x0 - x1, 2) + Math.pow(y0 - y1, 2));
+ *   }),
+ * );
  * distance.subscribe(x => console.log(x));
  * ```
  *
