36183a5dea019bc24a90dff68a26e62dd55df1f5
moniuch <moniuch@gmail.com>
2018-06-01 20:58:49 +0200

    docs(jsdoc): groupBy - update example to use pipe()

--- src/internal/operators/groupBy.ts
+++ src/internal/operators/groupBy.ts
@@ -22,18 +22,20 @@ export function groupBy<T, K, R>(keySelector: (value: T) => K, elementSelector?:
  * ##Examples
  * Group objects by id and return as array
  * ```javascript
- * Observable.of<Obj>({id: 1, name: 'aze1'},
- *                    {id: 2, name: 'sf2'},
- *                    {id: 2, name: 'dg2'},
- *                    {id: 1, name: 'erg1'},
- *                    {id: 1, name: 'df1'},
- *                    {id: 2, name: 'sfqfb2'},
- *                    {id: 3, name: 'qfs3'},
- *                    {id: 2, name: 'qsgqsfg2'}
- *     )
- *     .groupBy(p => p.id)
- *     .flatMap( (group$) => group$.reduce((acc, cur) => [...acc, cur], []))
- *     .subscribe(p => console.log(p));
+ * of<Obj>(
+ *   {id: 1, name: 'aze1'},
+ *   {id: 2, name: 'sf2'},
+ *   {id: 2, name: 'dg2'},
+ *   {id: 1, name: 'erg1'},
+ *   {id: 1, name: 'df1'},
+ *   {id: 2, name: 'sfqfb2'},
+ *   {id: 3, name: 'qfs3'},
+ *   {id: 2, name: 'qsgqsfg2'},
+ * ).pipe(
+ *   groupBy(p => p.id),
+ *   flatMap((group$) => group$.pipe(reduce((acc, cur) => [...acc, cur], []))),
+ * )
+ * .subscribe(p => console.log(p));
  *
  * // displays:
  * // [ { id: 1, name: 'aze1' },
@@ -50,19 +52,21 @@ export function groupBy<T, K, R>(keySelector: (value: T) => K, elementSelector?:
  *
  * Pivot data on the id field
  * ```javascript
- * Observable.of<Obj>({id: 1, name: 'aze1'},
- *                    {id: 2, name: 'sf2'},
- *                    {id: 2, name: 'dg2'},
- *                    {id: 1, name: 'erg1'},
- *                    {id: 1, name: 'df1'},
- *                    {id: 2, name: 'sfqfb2'},
- *                    {id: 3, name: 'qfs1'},
- *                    {id: 2, name: 'qsgqsfg2'}
- *                   )
- *     .groupBy(p => p.id, p => p.name)
- *     .flatMap( (group$) => group$.reduce((acc, cur) => [...acc, cur], ["" + group$.key]))
- *     .map(arr => ({'id': parseInt(arr[0]), 'values': arr.slice(1)}))
- *     .subscribe(p => console.log(p));
+ * of<Obj>(
+ *   {id: 1, name: 'aze1'},
+ *   {id: 2, name: 'sf2'},
+ *   {id: 2, name: 'dg2'},
+ *   {id: 1, name: 'erg1'},
+ *   {id: 1, name: 'df1'},
+ *   {id: 2, name: 'sfqfb2'},
+ *   {id: 3, name: 'qfs1'},
+ *   {id: 2, name: 'qsgqsfg2'},
+ * ).pipe(
+ *   groupBy(p => p.id, p => p.name),
+ *   flatMap( (group$) => group$.pipe(reduce((acc, cur) => [...acc, cur], ["" + group$.key]))),
+ *   map(arr => ({'id': parseInt(arr[0]), 'values': arr.slice(1)})),
+ * )
+ * .subscribe(p => console.log(p));
  *
  * // displays:
  * // { id: 1, values: [ 'aze1', 'erg1', 'df1' ] }
