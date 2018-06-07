44d2b5191def6557d62874bcea26fafe3ac8998f
moniuch <moniuch@gmail.com>
2018-06-07 17:07:52 +0200

    docs(jsdoc): add examples for ignoreElements (#3799)
    * docs(jsdoc): add examples for ignoreElements
    
    * docs(jsdoc): pr update - remove unclear error example of ignoreElements

--- src/internal/operators/ignoreElements.ts
+++ src/internal/operators/ignoreElements.ts
@@ -8,6 +8,20 @@ import { OperatorFunction } from '../types';
  *
  * <img src="./img/ignoreElements.png" width="100%">
  *
+ * ## Examples
+ * ### Ignores emitted values, reacts to observable's completion.
+ * ```javascript
+ * of('you', 'talking', 'to', 'me').pipe(
+ *   ignoreElements(),
+ * )
+ * .subscribe(
+ *   word => console.log(word),
+ *   err => console.log('error:', err),
+ *   () => console.log('the end'),
+ * );
+ * // result:
+ * // 'the end'
+ * ```
  * @return {Observable} An empty Observable that only calls `complete`
  * or `error`, based on which one is called by the source Observable.
  * @method ignoreElements
