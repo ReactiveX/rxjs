4e5941aa74be5b8095adfe11f7206c5955b104e1
moniuch <moniuch@gmail.com>
2018-06-07 20:01:59 +0200

    docs(jsdoc): minor

--- src/internal/operators/endWith.ts
+++ src/internal/operators/endWith.ts
@@ -21,7 +21,7 @@ export function endWith<T>(...array: Array<T | SchedulerLike>): MonoTypeOperator
  * Returns an Observable that emits the items you specify as arguments after it finishes emitting
  * items emitted by the source Observable.
  *
- * ## Examples
+ * ## Example
  * ### After the source observable completes, appends an emission and then completes too.
  *
  * ```javascript
