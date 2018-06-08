01397f0f140b9bfbc767fcf76d0df285de106966
natmegs <nataliemegans@gmail.com>
2018-06-07 19:17:36 -0700

    refactor(retry-spec): update retry operator tests to use pipeable operators

--- spec/operators/retry-spec.ts
+++ spec/operators/retry-spec.ts
@@ -1,13 +1,12 @@
 import { expect } from 'chai';
-import * as Rx from 'rxjs/Rx';
 import { hot, cold, expectObservable, expectSubscriptions } from '../helpers/marble-testing';
+import { retry, map, take, mergeMap, concat, multicast, refCount } from 'rxjs/operators';
+import { Observable, Observer, of, throwError, Subject } from 'rxjs';
 
 declare function asDiagram(arg: string): Function;
 
-const Observable = Rx.Observable;
-
 /** @test {retry} */
-describe('Observable.prototype.retry', () => {
+describe('retry operator', () => {
   asDiagram('retry(2)')('should handle a basic source that emits next then errors, count=3', () => {
     const source = cold('--1-2-3-#');
     const subs =       ['^       !                ',
@@ -15,7 +14,7 @@ describe('Observable.prototype.retry', () => {
                       '                ^       !'];
     const expected =    '--1-2-3---1-2-3---1-2-3-#';
 
-    const result = source.retry(2);
+    const result = source.pipe(retry(2));
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(source.subscriptions).toBe(subs);
@@ -24,19 +23,19 @@ describe('Observable.prototype.retry', () => {
   it('should retry a number of times, without error, then complete', (done: MochaDone) => {
     let errors = 0;
     const retries = 2;
-    Observable.create((observer: Rx.Observer<number>) => {
+    Observable.create((observer: Observer<number>) => {
       observer.next(42);
       observer.complete();
-    })
-      .map((x: any) => {
+    }).pipe(
+      map((x: any) => {
         if (++errors < retries) {
           throw 'bad';
         }
         errors = 0;
         return x;
-      })
-      .retry(retries)
-      .subscribe(
+      }),
+      retry(retries)
+    ).subscribe(
         (x: number) => {
           expect(x).to.equal(42);
         },
@@ -48,16 +47,16 @@ describe('Observable.prototype.retry', () => {
   it('should retry a number of times, then call error handler', (done: MochaDone) => {
     let errors = 0;
     const retries = 2;
-    Observable.create((observer: Rx.Observer<number>) => {
+    Observable.create((observer: Observer<number>) => {
       observer.next(42);
       observer.complete();
-    })
-      .map((x: any) => {
+    }).pipe(
+      map((x: any) => {
         errors += 1;
         throw 'bad';
-      })
-      .retry(retries - 1)
-      .subscribe(
+      }),
+      retry(retries - 1)
+    ).subscribe(
         (x: number) => {
           expect(x).to.equal(42);
         },
@@ -72,20 +71,20 @@ describe('Observable.prototype.retry', () => {
   it('should retry until successful completion', (done: MochaDone) => {
     let errors = 0;
     const retries = 10;
-    Observable.create((observer: Rx.Observer<number>) => {
+    Observable.create((observer: Observer<number>) => {
       observer.next(42);
       observer.complete();
-    })
-      .map((x: any) => {
+    }).pipe(
+      map((x: any) => {
         if (++errors < retries) {
           throw 'bad';
         }
         errors = 0;
         return x;
-      })
-      .retry()
-      .take(retries)
-      .subscribe(
+      }),
+      retry(),
+      take(retries)
+    ).subscribe(
         (x: number) => {
           expect(x).to.equal(42);
         },
@@ -99,7 +98,7 @@ describe('Observable.prototype.retry', () => {
     const subs =        '(^!)';
     const expected =    '|';
 
-    const result = source.retry();
+    const result = source.pipe(retry());
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(source.subscriptions).toBe(subs);
@@ -110,7 +109,7 @@ describe('Observable.prototype.retry', () => {
     const subs =        '^';
     const expected =    '-';
 
-    const result = source.retry();
+    const result = source.pipe(retry());
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(source.subscriptions).toBe(subs);
@@ -121,7 +120,7 @@ describe('Observable.prototype.retry', () => {
     const unsub =       '                                     !';
     const expected =    '--------------------------------------';
 
-    const result = source.retry();
+    const result = source.pipe(retry());
 
     expectObservable(result, unsub).toBe(expected);
   });
@@ -131,7 +130,7 @@ describe('Observable.prototype.retry', () => {
     const subs =               '^            !';
     const expected =           '---3--4--5---|';
 
-    const result = source.retry();
+    const result = source.pipe(retry());
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(source.subscriptions).toBe(subs);
@@ -142,7 +141,7 @@ describe('Observable.prototype.retry', () => {
     const subs =               '^            ';
     const expected =           '---3--4--5---';
 
-    const result = source.retry();
+    const result = source.pipe(retry());
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(source.subscriptions).toBe(subs);
@@ -158,7 +157,7 @@ describe('Observable.prototype.retry', () => {
                       '                                ^    !'];
     const expected =    '--1-2-3---1-2-3---1-2-3---1-2-3---1-2-';
 
-    const result = source.retry();
+    const result = source.pipe(retry());
 
     expectObservable(result, unsub).toBe(expected);
     expectSubscriptions(source.subscriptions).toBe(subs);
@@ -172,7 +171,7 @@ describe('Observable.prototype.retry', () => {
                       '        ^    !           '];
     const expected =    '--1-2-3---1-2-';
 
-    const result = source.retry(3);
+    const result = source.pipe(retry(3));
 
     expectObservable(result, unsub).toBe(expected);
     expectSubscriptions(source.subscriptions).toBe(subs);
@@ -185,10 +184,11 @@ describe('Observable.prototype.retry', () => {
     const expected =    '--1-2-3---1-2-';
     const unsub =       '             !           ';
 
-    const result = source
-      .mergeMap((x: string) => Observable.of(x))
-      .retry(100)
-      .mergeMap((x: string) => Observable.of(x));
+    const result = source.pipe(
+      mergeMap((x: string) => of(x)),
+      retry(100),
+      mergeMap((x: string) => of(x))
+    );
 
     expectObservable(result, unsub).toBe(expected);
     expectSubscriptions(source.subscriptions).toBe(subs);
@@ -197,11 +197,12 @@ describe('Observable.prototype.retry', () => {
   it('should retry a synchronous source (multicasted and refCounted) multiple times', (done: MochaDone) => {
     const expected = [1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3];
 
-    Observable.of(1, 2, 3).concat(Observable.throw('bad!'))
-      .multicast(() => new Rx.Subject())
-      .refCount()
-      .retry(4)
-      .subscribe(
+    of(1, 2, 3).pipe(
+      concat(throwError('bad!')),
+      multicast(() => new Subject()),
+      refCount(),
+      retry(4)
+    ).subscribe(
         (x: number) => { expect(x).to.equal(expected.shift()); },
         (err: any) => {
           expect(err).to.equal('bad!');
