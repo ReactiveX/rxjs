a968db1d6cdab50fabdd890e963e38ad537c84d4
natmegs <nataliemegans@gmail.com>
2018-06-08 09:04:28 -0700

    refactor(withLatestFrom-spec): update withLatestFrom operator tests to use pipeable operators

--- spec/operators/withLatestFrom-spec.ts
+++ spec/operators/withLatestFrom-spec.ts
@@ -1,20 +1,19 @@
 import { expect } from 'chai';
-import * as Rx from 'rxjs/Rx';
 import { lowerCaseO } from '../helpers/test-helper';
 import { hot, cold, expectObservable, expectSubscriptions } from '../helpers/marble-testing';
+import { withLatestFrom, mergeMap, delay } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 declare function asDiagram(arg: string): Function;
 
-const Observable = Rx.Observable;
-
 /** @test {withLatestFrom} */
-describe('Observable.prototype.withLatestFrom', () => {
+describe('withLatestFrom operator', () => {
   asDiagram('withLatestFrom')('should combine events from cold observables', () => {
     const e1 =  cold('-a--b-----c-d-e-|');
     const e2 =  cold('--1--2-3-4---|   ');
     const expected = '----B-----C-D-E-|';
 
-    const result = e1.withLatestFrom(e2, (a: string, b: string) => String(a) + String(b));
+    const result = e1.pipe(withLatestFrom(e2, (a: string, b: string) => String(a) + String(b)));
 
     expectObservable(result).toBe(expected, { B: 'b1', C: 'c4', D: 'd4', E: 'e4' });
   });
@@ -33,7 +32,7 @@ describe('Observable.prototype.withLatestFrom', () => {
       z: ['d', 'h', 'l']
     };
 
-    const result = e1.withLatestFrom(e2, e3);
+    const result = e1.pipe(withLatestFrom(e2, e3));
 
     expectObservable(result).toBe(expected, values);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -57,7 +56,7 @@ describe('Observable.prototype.withLatestFrom', () => {
     };
     const project = function (a: string, b: string, c: string) { return a + b + c; };
 
-    const result = e1.withLatestFrom(e2, e3, project);
+    const result = e1.pipe(withLatestFrom(e2, e3, project));
 
     expectObservable(result).toBe(expected, values);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -81,7 +80,7 @@ describe('Observable.prototype.withLatestFrom', () => {
     };
     const project = function (a: string, b: string, c: string) { return a + b + c; };
 
-    const result = e1.withLatestFrom(e2, e3, project);
+    const result = e1.pipe(withLatestFrom(e2, e3, project));
 
     expectObservable(result, unsub).toBe(expected, values);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -105,10 +104,11 @@ describe('Observable.prototype.withLatestFrom', () => {
     };
     const project = function (a: string, b: string, c: string) { return a + b + c; };
 
-    const result = e1
-      .mergeMap((x: string) => Observable.of(x))
-      .withLatestFrom(e2, e3, project)
-      .mergeMap((x: string) => Observable.of(x));
+    const result = e1.pipe(
+      mergeMap((x: string) => of(x)),
+      withLatestFrom(e2, e3, project),
+      mergeMap((x: string) => of(x))
+    );
 
     expectObservable(result, unsub).toBe(expected, values);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -125,7 +125,7 @@ describe('Observable.prototype.withLatestFrom', () => {
     const e3subs =        '(^!)';
     const expected =      '|'; // empty
 
-    const result = e1.withLatestFrom(e2, e3);
+    const result = e1.pipe(withLatestFrom(e2, e3));
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -142,7 +142,7 @@ describe('Observable.prototype.withLatestFrom', () => {
     const e3subs =        '^              !';
     const expected =      '--------------------'; // never
 
-    const result = e1.withLatestFrom(e2, e3);
+    const result = e1.pipe(withLatestFrom(e2, e3));
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -159,7 +159,7 @@ describe('Observable.prototype.withLatestFrom', () => {
     const e3subs =        '(^!)';
     const expected =      '#'; // throw
 
-    const result = e1.withLatestFrom(e2, e3);
+    const result = e1.pipe(withLatestFrom(e2, e3));
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -179,7 +179,7 @@ describe('Observable.prototype.withLatestFrom', () => {
       x: ['b', 'f', 'j']
     };
 
-    const result = e1.withLatestFrom(e2, e3);
+    const result = e1.pipe(withLatestFrom(e2, e3));
 
     expectObservable(result).toBe(expected, values, new Error('boo-hoo'));
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -200,7 +200,7 @@ describe('Observable.prototype.withLatestFrom', () => {
     };
     const project = function (a: string, b: string, c: string) { return a + b + c; };
 
-    const result = e1.withLatestFrom(e2, e3, project);
+    const result = e1.pipe(withLatestFrom(e2, e3, project));
 
     expectObservable(result).toBe(expected, values, new Error('boo-hoo'));
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -217,7 +217,7 @@ describe('Observable.prototype.withLatestFrom', () => {
     const e3subs =        '^             !   ';
     const expected =      '--------------|   ';
 
-    const result = e1.withLatestFrom(e2, e3);
+    const result = e1.pipe(withLatestFrom(e2, e3));
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -234,7 +234,7 @@ describe('Observable.prototype.withLatestFrom', () => {
     const e3subs =        '^             !   ';
     const expected =      '--------------|   ';
 
-    const result = e1.withLatestFrom(e2, e3);
+    const result = e1.pipe(withLatestFrom(e2, e3));
 
     expectObservable(result).toBe(expected);
     expectSubscriptions(e1.subscriptions).toBe(e1subs);
@@ -243,21 +243,21 @@ describe('Observable.prototype.withLatestFrom', () => {
   });
 
   it('should handle promises', (done: MochaDone) => {
-    Observable.of(1).delay(1).withLatestFrom(Promise.resolve(2), Promise.resolve(3))
+    of(1).pipe(delay(1), withLatestFrom(Promise.resolve(2), Promise.resolve(3)))
       .subscribe((x: any) => {
         expect(x).to.deep.equal([1, 2, 3]);
       }, null, done);
   });
 
   it('should handle arrays', () => {
-    Observable.of(1).delay(1).withLatestFrom([2, 3, 4], [4, 5, 6])
+    of(1).pipe(delay(1), withLatestFrom([2, 3, 4], [4, 5, 6]))
       .subscribe((x: any) => {
         expect(x).to.deep.equal([1, 4, 6]);
       });
   });
 
   it('should handle lowercase-o observables', () => {
-    Observable.of(1).delay(1).withLatestFrom(lowerCaseO(2, 3, 4), lowerCaseO(4, 5, 6))
+    of(1).pipe(delay(1), withLatestFrom(lowerCaseO(2, 3, 4), lowerCaseO(4, 5, 6)))
       .subscribe((x: any) => {
         expect(x).to.deep.equal([1, 4, 6]);
       });
