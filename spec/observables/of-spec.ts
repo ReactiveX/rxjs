0d6c383cfe2942c4319581e23d3427c93583bf5a
natmegs <nataliemegans@gmail.com>
2018-06-26 19:27:50 -0700

    refactor(of-spec): update of tests to use pipeable operators and updated imports

--- spec/observables/of-spec.ts
+++ spec/observables/of-spec.ts
@@ -1,17 +1,19 @@
 import { expect } from 'chai';
-import * as Rx from 'rxjs/Rx';
-import { empty } from 'rxjs';
+import { empty, of, Observable } from 'rxjs';
 import { expectObservable } from '../helpers/marble-testing';
+import { TestScheduler } from 'rxjs/testing';
+import { concatMap, delay, concatAll } from 'rxjs/operators';
+
 declare const asDiagram: any;
-declare const rxTestScheduler: Rx.TestScheduler;
-const Observable = Rx.Observable;
+declare const rxTestScheduler: TestScheduler;
 
 /** @test {of} */
-describe('Observable.of', () => {
+describe('of', () => {
   asDiagram('of(1, 2, 3)')('should create a cold observable that emits 1, 2, 3', () => {
-    const e1 = Observable.of(1, 2, 3)
+    const e1 = of(1, 2, 3).pipe(
       // for the purpose of making a nice diagram, spread out the synchronous emissions
-      .concatMap((x, i) => Observable.of(x).delay(i === 0 ? 0 : 20, rxTestScheduler));
+      concatMap((x, i) => of(x).pipe(delay(i === 0 ? 0 : 20, rxTestScheduler)))
+    );
     const expected = 'x-y-(z|)';
     expectObservable(e1).toBe(expected, {x: 1, y: 2, z: 3});
   });
@@ -21,7 +23,7 @@ describe('Observable.of', () => {
     const expected = [1, 'a', x];
     let i = 0;
 
-    Observable.of<any>(1, 'a', x)
+    of<any>(1, 'a', x)
       .subscribe((y: any) => {
         expect(y).to.equal(expected[i++]);
       }, (x) => {
@@ -32,14 +34,14 @@ describe('Observable.of', () => {
   });
 
   it('should return an empty observable if passed no values', () => {
-    const obs = Observable.of();
+    const obs = of();
     expect(obs).to.equal(empty());
   });
 
   it('should emit one value', (done: MochaDone) => {
     let calls = 0;
 
-    Observable.of(42).subscribe((x: number) => {
+    of(42).subscribe((x: number) => {
       expect(++calls).to.equal(1);
       expect(x).to.equal(42);
     }, (err: any) => {
@@ -50,22 +52,22 @@ describe('Observable.of', () => {
   });
 
   it('should handle an Observable as the only value', () => {
-    const source = Observable.of<Rx.Observable<string>>(
-      Observable.of<string>('a', 'b', 'c', rxTestScheduler),
+    const source = of<Observable<string>>(
+      of<string>('a', 'b', 'c', rxTestScheduler),
       rxTestScheduler
     );
-    const result = source.concatAll();
+    const result = source.pipe(concatAll());
     expectObservable(result).toBe('(abc|)');
   });
 
   it('should handle many Observable as the given values', () => {
-    const source = Observable.of<Rx.Observable<string>>(
-      Observable.of<string>('a', 'b', 'c', rxTestScheduler),
-      Observable.of<string>('d', 'e', 'f', rxTestScheduler),
+    const source = of<Observable<string>>(
+      of<string>('a', 'b', 'c', rxTestScheduler),
+      of<string>('d', 'e', 'f', rxTestScheduler),
       rxTestScheduler
     );
 
-    const result = source.concatAll();
+    const result = source.pipe(concatAll());
     expectObservable(result).toBe('(abcdef|)');
   });
 });
