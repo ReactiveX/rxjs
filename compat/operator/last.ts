a00a390baa6a61069d9bd01bb395b4848734d937
Nicholas Jamieson <nicholas@cartant.com>
2018-07-26 08:18:44 +1000

    chore(compat): fix first/last types

--- compat/operator/last.ts
+++ compat/operator/last.ts
@@ -2,9 +2,9 @@ import { Observable } from 'rxjs';
 import { last as higherOrder } from 'rxjs/operators';
 
 /* tslint:disable:max-line-length */
-export function last<T>(this: Observable<T>, predicate?: null, defaultValue?: T): Observable<T>;
-export function last<T, S extends T>(this: Observable<T>, predicate: (value: T, index: number, source: Observable<T>) => value is S, defaultValue?: T): Observable<S>;
-export function last<T>(this: Observable<T>, predicate: (value: T, index: number, source: Observable<T>) => boolean, defaultValue?: T): Observable<T>;
+export function last<T, D = T>(this: Observable<T>, predicate?: null, defaultValue?: D): Observable<T | D>;
+export function last<T, S extends T>(this: Observable<T>, predicate: (value: T, index: number, source: Observable<T>) => value is S, defaultValue?: S): Observable<S>;
+export function last<T, D = T>(this: Observable<T>, predicate: (value: T, index: number, source: Observable<T>) => boolean, defaultValue?: D): Observable<T | D>;
 /* tslint:enable:max-line-length */
 /**
  * Returns an Observable that emits only the last item emitted by the source Observable.
