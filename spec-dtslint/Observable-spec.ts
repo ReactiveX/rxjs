65d12f7833cc8850e7b505e02cc63d94d72066f2
Nicholas Jamieson <nicholas@cartant.com>
2018-07-26 07:06:44 +1000

    chore(dtslint): add pipe rest params test

--- spec-dtslint/Observable-spec.ts
+++ spec-dtslint/Observable-spec.ts
@@ -52,6 +52,10 @@ describe('pipe', () => {
     const o = of('foo').pipe(a('1'), a('2'), a('3'), a('4'), a('5'), a('6'), a('7'), a('8'), a('9'), a('10')); // $ExpectType Observable<{}>
   });
 
+  it('should require a type assertion for more than 9 arguments', () => {
+    const o: Observable<'10'> = of('foo').pipe(a('1'), a('2'), a('3'), a('4'), a('5'), a('6'), a('7'), a('8'), a('9'), a('10')); // $ExpectError
+  });
+
   it('should enforce types for the 1st argument', () => {
     const o = of('foo').pipe(a('#', '1')); // $ExpectError
   });
