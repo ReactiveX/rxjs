5212b5e46f20c6d5cfbeae90f4958187ffcc8a40
Nicholas Jamieson <nicholas@cartant.com>
2018-07-26 14:05:00 +1000

    chore(dtslint): add first/last default tests

--- spec-dtslint/operators/first-spec.ts
+++ spec-dtslint/operators/first-spec.ts
@@ -7,35 +7,43 @@ it('should support an undefined predicate with no default', () => {
   const o = of('foo').pipe(first(undefined)); // $ExpectType Observable<string>
 });
 
-it('should support an undefined predicate with T default', () => {
+it('should support an undefined predicate with a T default', () => {
   const o = of('foo').pipe(first(undefined, 'bar')); // $ExpectType Observable<string>
 });
 
-it('should support an undefined predicate with non-T default', () => {
+it('should support an undefined predicate with a non-T default', () => {
   const o = of('foo').pipe(first(undefined, false)); // $ExpectType Observable<string | boolean>
+});
+
+it('should default D to T with an undfined predicate', () => {
+  const o = of('foo').pipe(first<string>(undefined)); // $Observable<string>
 });
 
 it('should support a null predicate with no default', () => {
   const o = of('foo').pipe(first(null)); // $ExpectType Observable<string>
 });
 
-it('should support a null predicate with T default', () => {
+it('should support a null predicate with a T default', () => {
   const o = of('foo').pipe(first(null, 'bar')); // $ExpectType Observable<string>
 });
 
-it('should support a null predicate with non-T default', () => {
+it('should support a null predicate with a non-T default', () => {
   const o = of('foo').pipe(first(null, false)); // $ExpectType Observable<string | boolean>
+});
+
+it('should default D to T with a null predicate', () => {
+  const o = of('foo').pipe(first<string>(null)); // $Observable<string>
 });
 
 it('should support a user-defined type guard with no default', () => {
   const o = of('foo').pipe(first(isFooBar)); // $ExpectType Observable<"foo" | "bar">
 });
 
-it('should support a user-defined type guard with a default', () => {
+it('should support a user-defined type guard with an S default', () => {
   const o = of('foo').pipe(first(isFooBar, 'bar')); // $ExpectType Observable<"foo" | "bar">
 });
 
-it('should widen a user-defined type guard with a non-T default', () => {
+it('should widen a user-defined type guard with a non-S default', () => {
   const o = of('foo').pipe(first(isFooBar, false)); // $ExpectType Observable<string | boolean>
 });
 
@@ -43,10 +51,14 @@ it('should support a predicate with no default', () => {
   const o = of('foo').pipe(first(x => !!x)); // $ExpectType Observable<string>
 });
 
-it('should support a predicate with T default', () => {
+it('should support a predicate with a T default', () => {
   const o = of('foo').pipe(first(x => !!x, 'bar')); // $ExpectType Observable<string>
 });
 
-it('should support a predicate with non-T default', () => {
+it('should support a predicate with a non-T default', () => {
   const o = of('foo').pipe(first(x => !!x, false)); // $ExpectType Observable<string | boolean>
+});
+
+it('should default D to T with a predicate', () => {
+  const o = of('foo').pipe(first<string>(x => !!x)); // $Observable<string>
 });
