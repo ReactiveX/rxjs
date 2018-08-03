f8826479f71975b6f2c1785cbe199adb85f7cdab
Ben Lesh <ben@benlesh.com>
2018-08-03 10:39:12 -0700

    Revert "Cancellable forEach"

--- src/internal/util/isSubscription.ts
+++
@@ -1,9 +0,0 @@
-import { Subscription } from '../Subscription';
-
-/**
- * Tests to see if a value is an RxJS {@link Subscription}.
- * @param x the value to test
- */
-export function isSubscription(x: any): x is Subscription {
-  return x && typeof x.unsubscribe === 'function' && typeof x.add === 'function';
-}
