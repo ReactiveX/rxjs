89e081fd8bf3fa5afc84397feb9794073ef29f70
Ben Lesh <ben@benlesh.com>
2018-08-10 13:38:23 -0700

    fix(TS@next): remove unnecessary return statement

--- src/internal/scheduler/AsyncAction.ts
+++ src/internal/scheduler/AsyncAction.ts
@@ -79,7 +79,7 @@ export class AsyncAction<T> extends Action<T> {
     }
     // Otherwise, if the action's delay time is different from the current delay,
     // or the action has been rescheduled before it's executed, clear the interval id
-    return clearInterval(id) && undefined || undefined;
+    clearInterval(id);
   }
 
   /**
