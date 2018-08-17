5fc893c46a8a0eb23c6b8b8f3fb7d7c82d7b3858
Jan-Niklas W <6104311+JWO719@users.noreply.github.com>
2018-08-17 10:01:29 +0200

    docs(timer): migrate old docs repo for timer function (#4021)
    * docs(content): remove aio content
    
    * docs(timer): migrate timer documentation from old docs repo
    
    * docs(timer): improve paramater description
    
    * docs(timer): redo indention
    
    * docs(timer): revert indention and brackets

--- src/internal/observable/timer.ts
+++ src/internal/observable/timer.ts
@@ -6,7 +6,7 @@ import { isScheduler } from '../util/isScheduler';
 import { Subscriber } from '../Subscriber';
 
 /**
- * Creates an Observable that starts emitting after an `initialDelay` and
+ * Creates an Observable that starts emitting after an `dueTime` and
  * emits ever increasing numbers after each `period` of time thereafter.
  *
  * <span class="informal">Its like {@link index/interval}, but you can specify when
@@ -17,7 +17,7 @@ import { Subscriber } from '../Subscriber';
  * `timer` returns an Observable that emits an infinite sequence of ascending
  * integers, with a constant interval of time, `period` of your choosing
  * between those emissions. The first emission happens after the specified
- * `initialDelay`. The initial delay may be a `Date`. By default, this
+ * `dueTime`. The initial delay may be a `Date`. By default, this
  * operator uses the {@link asyncScheduler} {@link SchedulerLike} to provide a notion of time, but you
  * may pass any {@link SchedulerLike} to it. If `period` is not specified, the output
  * Observable emits only one value, `0`. Otherwise, it emits an infinite
@@ -38,14 +38,14 @@ import { Subscriber } from '../Subscriber';
  * @see {@link index/interval}
  * @see {@link delay}
  *
- * @param {number|Date} [dueTime] The initial delay time to wait before
- * emitting the first value of `0`.
+ * @param {number|Date} [dueTime] The initial delay time specified as a Date object or as an integer denoting
+ * milliseconds to wait before emitting the first value of 0`.
  * @param {number|SchedulerLike} [periodOrScheduler] The period of time between emissions of the
  * subsequent numbers.
  * @param {SchedulerLike} [scheduler=async] The {@link SchedulerLike} to use for scheduling
  * the emission of values, and providing a notion of "time".
  * @return {Observable} An Observable that emits a `0` after the
- * `initialDelay` and ever increasing numbers after each `period` of time
+ * `dueTime` and ever increasing numbers after each `period` of time
  * thereafter.
  * @static true
  * @name timer
