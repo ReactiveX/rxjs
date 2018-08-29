ea5e115dac80989d53167bbefeec3e5c401863b8
Rob Simpson <rob.simpson@vbrick.com>
2018-08-29 08:32:31 -0400

    feat(test): Add npm script to test with ts-node not caching
    There is a npm script that can run - `npm run test_no_cache`, that will
    run the test with `TS_NODE_CACHE` set to false.
    
    Closes #3948

--- spec/migration/update-6_0_0/index-spec.ts
+++ spec/migration/update-6_0_0/index-spec.ts
@@ -13,11 +13,12 @@ describe('Migration Schematic', () => {
     tree.create('/package.json', `{}`);
   });
 
-  it('adds missing dependencies', () => {
+  it('adds missing dependencies', (done) => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
     tree = runner.runSchematic('rxjs-migration-01', {}, tree);
 
     const pkg = JSON.parse(tree.readContent('/package.json'));
     expect(pkg.dependencies['rxjs-compat']).to.equal('^6.0.0-rc.0');
+    done();
   });
 });
