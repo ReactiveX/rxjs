4791086b9a7e47aecbff042e3d6f7f92e3a2dc9f
Nicholas Jamieson <nicholas@cartant.com>
2018-08-29 10:18:36 +1000

    test(dtslint): add shareReplay

--- spec-dtslint/operators/shareReplay-spec.ts
+++ spec-dtslint/operators/shareReplay-spec.ts
@@ -1,30 +1,30 @@
-import { of, asyncScheduler } from 'rxjs';
+import { of, asyncScheduler  } from 'rxjs';
 import { shareReplay } from 'rxjs/operators';
 
-it('should infer correctly', () => {
-  const o = of('foo', 'bar', 'baz').pipe(shareReplay()); // $ExpectType Observable<string>
+it('should accept an individual bufferSize parameter', () => {
+  const o = of(1, 2, 3).pipe(shareReplay(1)); // $ExpectType Observable<number>
 });
 
-it('should support a bufferSize', () => {
-  const o = of('foo', 'bar', 'baz').pipe(shareReplay(6)); // $ExpectType Observable<string>
+it('should accept individual bufferSize and windowTime parameters', () => {
+  const o = of(1, 2, 3).pipe(shareReplay(1, 2)); // $ExpectType Observable<number>
 });
 
-it('should support a windowTime', () => {
-  const o = of('foo', 'bar', 'baz').pipe(shareReplay(6, 4)); // $ExpectType Observable<string>
+it('should accept individual bufferSize, windowTime and scheduler parameters', () => {
+  const o3 = of(1, 2, 3).pipe(shareReplay(1, 2, asyncScheduler)); // $ExpectType Observable<number>
 });
 
-it('should support a scheduler', () => {
-  const o = of('foo', 'bar', 'baz').pipe(shareReplay(6, 4, asyncScheduler)); // $ExpectType Observable<string>
+it('should accept a bufferSize config parameter', () => {
+  const o = of(1, 2, 3).pipe(shareReplay({ bufferSize: 1, refCount: true })); // $ExpectType Observable<number>
 });
 
-it('should enforce type of bufferSize', () => {
-  const o = of('foo', 'bar', 'baz').pipe(shareReplay('abc')); // $ExpectError
+it('should accept bufferSize and windowTime config parameters', () => {
+  const o = of(1, 2, 3).pipe(shareReplay({ bufferSize: 1, windowTime: 2, refCount: true })); // $ExpectType Observable<number>
 });
 
-it('should enforce type of windowTime', () => {
-  const o = of('foo', 'bar', 'baz').pipe(shareReplay(5, 'abc')); // $ExpectError
+it('should accept bufferSize, windowTime and scheduler config parameters', () => {
+  const o = of(1, 2, 3).pipe(shareReplay({ bufferSize: 1, windowTime: 2, scheduler: asyncScheduler, refCount: true })); // $ExpectType Observable<number>
 });
 
-it('should enforce type of scheduler', () => {
-  const o = of('foo', 'bar', 'baz').pipe(shareReplay(5, 3, 'abc')); // $ExpectError
+it('should require a refCount config parameter', () => {
+  const o = of(1, 2, 3).pipe(shareReplay({ bufferSize: 1 })); // $ExpectError
 });
