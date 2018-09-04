ff3f06e413ab177b0a6d0048b52130da0468b915
Nicholas Jamieson <nicholas@cartant.com>
2018-09-04 22:00:49 +1000

    fix(subscribe): don't swallow internal errors
    Closes #3803

--- src/internal/Observable.ts
+++ src/internal/Observable.ts
@@ -2,6 +2,7 @@ import { Operator } from './Operator';
 import { Subscriber } from './Subscriber';
 import { Subscription } from './Subscription';
 import { TeardownLogic, OperatorFunction, PartialObserver, Subscribable } from './types';
+import { reportError } from './util/reportError';
 import { toSubscriber } from './util/toSubscriber';
 import { iif } from './observable/iif';
 import { throwError } from './observable/throwError';
@@ -226,7 +227,7 @@ export class Observable<T> implements Subscribable<T> {
         sink.syncErrorThrown = true;
         sink.syncErrorValue = err;
       }
-      sink.error(err);
+      reportError(err, sink);
     }
   }
 
