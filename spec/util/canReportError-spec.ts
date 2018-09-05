ed2a61762c837537843bd92217d3810c1a98bfd1
Nicholas Jamieson <nicholas@cartant.com>
2018-09-05 10:43:29 +1000

    test(canReport): use noop error handlers

--- spec/util/canReportError-spec.ts
+++ spec/util/canReportError-spec.ts
@@ -5,12 +5,12 @@ import { canReportError } from 'rxjs/internal/util/canReportError';
 
 describe('canReportError', () => {
   it('should report errors to an observer if possible', () => {
-    const subscriber = new Subscriber(noop);
+    const subscriber = new Subscriber(noop, noop);
     expect(canReportError(subscriber)).to.be.true;
   });
 
   it('should not report errors to a stopped observer', () => {
-    const subscriber = new Subscriber(noop);
+    const subscriber = new Subscriber(noop, noop);
     subscriber.error(new Error('kaboom'));
     expect(canReportError(subscriber)).to.be.false;
   });
@@ -20,7 +20,7 @@ describe('canReportError', () => {
   });
 
   it('should not report errors an observer with a stopped destination', () => {
-    const destination = new Subscriber(noop);
+    const destination = new Subscriber(noop, noop);
     const subscriber = new Subscriber(destination);
     destination.error(new Error('kaboom'));
     expect(canReportError(subscriber)).to.be.false;
