30440d2e4514350d24d80aa37ec669c669fb5f0b
Nicholas Jamieson <nicholas@cartant.com>
2018-09-08 07:01:54 +1000

    chore(canReport): restrict observer type

--- src/internal/util/canReportError.ts
+++ src/internal/util/canReportError.ts
@@ -1,5 +1,5 @@
-import { isTrustedSubscriber } from '../Subscriber';
-import { ErrorObserver } from '../types';
+import { isTrustedSubscriber, Subscriber } from '../Subscriber';
+import { Subject } from '../Subject';
 
 /**
  * Determines whether the ErrorObserver is closed or stopped or has a
@@ -7,7 +7,7 @@ import { ErrorObserver } from '../types';
  * need to be reported via a different mechanism.
  * @param observer the observer
  */
-export function canReportError(observer: ErrorObserver<any>): boolean {
+export function canReportError(observer: Subscriber<any> | Subject<any>): boolean {
   while (observer) {
     const { closed, destination, isStopped } = observer as any;
     if (closed || isStopped) {
