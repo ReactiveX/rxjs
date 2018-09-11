25b0d5658074ab9ef47b10712c6ba2f2c0b849b2
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2018-09-11 22:46:32 +0200

    test(dtslint): add mergeMapTo (#4097)
    * test(dtslint): add mergeMapTo
    
    * test(dtslint): add extra checks to mergeMapTo

---
+++ spec-dtslint/operators/mergeMapTo-spec.ts
@@ -0,0 +1,59 @@
+import { of } from 'rxjs';
+import { mergeMapTo } from 'rxjs/operators';
+
+it('should infer correctly', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo'))); // $ExpectType Observable<string>
+});
+
+it('should infer correctly multiple types', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo', 4))); // $ExpectType Observable<string | number>
+});
+
+it('should infer correctly with an array', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo([4, 5, 6])); // $ExpectType Observable<number>
+});
+
+it('should infer correctly with a Promise', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(new Promise<string>(() => {}))); // $ExpectType Observable<string>
+});
+
+it('should support a concurrent parameter', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo'), 4)); // $ExpectType Observable<string>
+});
+
+it('should infer correctly by using the resultSelector first parameter', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo'), a => a)); // $ExpectType Observable<number>
+});
+
+it('should infer correctly by using the resultSelector second parameter', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo'), (a, b) => b)); // $ExpectType Observable<string>
+});
+
+it('should support a resultSelector that takes an inner index', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo'), (a, b, innnerIndex) => a)); // $ExpectType Observable<number>
+});
+
+it('should support a resultSelector that takes an inner and outer index', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo'), (a, b, innnerIndex, outerIndex) => a)); // $ExpectType Observable<number>
+});
+
+it('should support a resultSelector and concurrent parameter', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo'), (a, b) => b, 4)); // $ExpectType Observable<string>
+});
+
+it('should enforce types', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo()); // $ExpectError
+});
+
+it('should enforce the return type', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(p => p)); // $ExpectError
+  const p = of(1, 2, 3).pipe(mergeMapTo(4)); // $ExpectError
+});
+
+it('should enforce types of the concurrent parameter', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo'), '4')); // $ExpectError
+});
+
+it('should enforce types of the concurrent parameter with a resultSelector', () => {
+  const o = of(1, 2, 3).pipe(mergeMapTo(of('foo'), (a => a), '4')); // $ExpectError
+});
