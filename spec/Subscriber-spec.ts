1d02c73fbf802e6aba8740f29bac2f9cca0d4a1b
Nicholas Jamieson <nicholas@cartant.com>
2018-09-15 07:53:07 +1000

    test(Subscriber): fake add too

--- spec/Subscriber-spec.ts
+++ spec/Subscriber-spec.ts
@@ -23,7 +23,8 @@ describe('Subscriber', () => {
   it('should accept subscribers as a destination if they meet the proper criteria', () => {
     const fakeSubscriber = {
       [rxSubscriber](this: any) { return this; },
-      _addParentTeardownLogic() { /* noop */ }
+      _addParentTeardownLogic() { /* noop */ },
+      add() { /* noop */ }
     };
 
     const subscriber = new Subscriber(fakeSubscriber as any);
