5c099084108a7c1a8beea45e169b652dd3e9af5f
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2018-09-18 08:21:02 +0200

    test(dtslint): add infer tests to distinctUntilKeyChanged (#4144)

--- spec-dtslint/operators/distinctUntilKeyChanged-spec.ts
+++ spec-dtslint/operators/distinctUntilKeyChanged-spec.ts
@@ -3,6 +3,14 @@ import { distinctUntilKeyChanged } from 'rxjs/operators';
 
 const sample = {name: 'foobar', num: 42};
 
+it('should infer correctly', () => {
+  const o = of(sample).pipe(distinctUntilKeyChanged('name')); // $ExpectType Observable<{ name: string; num: number; }>
+});
+
+it('should infer correctly with compare', () => {
+  const o = of(sample).pipe(distinctUntilKeyChanged('name', () => true)); // $ExpectType Observable<{ name: string; num: number; }>
+});
+
 it('should enforce key set', () => {
   const o = of(sample).pipe(distinctUntilKeyChanged('something')); // $ExpectError
 });
