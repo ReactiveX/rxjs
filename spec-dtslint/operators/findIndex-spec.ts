15bc7893edd2cc3c477ce3d597c2ff821eed1954
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2018-09-18 11:35:11 +0200

    test(dtslint): findIndex use Observable<string> sources as test case

--- spec-dtslint/operators/findIndex-spec.ts
+++ spec-dtslint/operators/findIndex-spec.ts
@@ -2,31 +2,31 @@ import { of, Observable } from 'rxjs';
 import { findIndex } from 'rxjs/operators';
 
 it('should infer correctly', () => {
-  const o = of(1, 2, 3).pipe(findIndex(n => n === 3)); // $ExpectType Observable<number>
+  const o = of('foo', 'bar', 'baz').pipe(findIndex(p => p === 'foo')); // $ExpectType Observable<number>
 });
 
 it('should support a predicate that takes an index ', () => {
-  const o = of(1, 2, 3).pipe(findIndex((n, index) => index === 3)); // $ExpectType Observable<number>
+  const o = of('foo', 'bar', 'baz').pipe(findIndex((p, index) => index === 3)); // $ExpectType Observable<number>
 });
 
 it('should support a predicate that takes a source ', () => {
-  const o = of(1, 2, 3).pipe(findIndex((n, index, source) => n === 3)); // $ExpectType Observable<number>
+  const o = of('foo', 'bar', 'baz').pipe(findIndex((p, index, source) => p === 'foo')); // $ExpectType Observable<number>
 });
 
 it('should support an argument ', () => {
-  const o = of(1, 2, 3).pipe(findIndex((n) => n === 3, 'foo')); // $ExpectType Observable<number>
+  const o = of('foo', 'bar', 'baz').pipe(findIndex(p => p === 'foo', 123)); // $ExpectType Observable<number>
 });
 
 it('should enforce types', () => {
-  const o = of(1, 2, 3).pipe(findIndex()); // $ExpectError
+  const o = of('foo', 'bar', 'baz').pipe(findIndex()); // $ExpectError
 });
 
 it('should enforce predicate types', () => {
-  const o = of(1, 2, 3).pipe(findIndex((n: string) => n === 3)); // $ExpectError
-  const p = of(1, 2, 3).pipe(findIndex((n, index: string) => n === 3)); // $ExpectError
-  const q = of(1, 2, 3).pipe(findIndex((n, index, source: Observable<string>) => n === 3)); // $ExpectError
+  const o = of('foo', 'bar', 'baz').pipe(findIndex((p: number) => p === 3)); // $ExpectError
+  const p = of('foo', 'bar', 'baz').pipe(findIndex((p, index: string) => p === 3)); // $ExpectError
+  const q = of('foo', 'bar', 'baz').pipe(findIndex((p, index, source: Observable<number>) => p === 3)); // $ExpectError
 });
 
 it('should enforce predicate return type', () => {
-  const o = of(1, 2, 3).pipe(findIndex(n => n)); // $ExpectError
+  const o = of('foo', 'bar', 'baz').pipe(findIndex(p => p)); // $ExpectError
 });
