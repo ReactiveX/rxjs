01d104617953e47118d9f3986439d14178d66eef
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2018-09-18 19:57:01 +0200

    docs(accessibility): Anchor element found with a valid href attribute, but no link content Add screen reader text to websites

--- docs_app/src/app/custom-elements/contributor/contributor.component.ts
+++ docs_app/src/app/custom-elements/contributor/contributor.component.ts
@@ -18,15 +18,18 @@ import { CONTENT_URL_PREFIX } from 'app/documents/document.service';
                     </a>
                     <a *ngIf="person.twitter" mat-button class="icon"
                         href="{{person.twitter}}" target="_blank" (click)="$event.stopPropagation()">
-                        <span class="fa fa-twitter fa-2x"></span>
+                        <span class="fa fa-twitter fa-2x" aria-hidden="true"></span>
+                        <span class="sr-only">Twitter {{person.name}}</span>
                     </a>
                     <a *ngIf="person.github" mat-button class="icon"
                         href="{{person.github}}" target="_blank" (click)="$event.stopPropagation()">
-                        <span class="fa fa-github fa-2x"></span>
+                        <span class="fa fa-github fa-2x" aria-hidden="true"></span>
+                        <span class="sr-only">Github {{person.name}}</span>
                     </a>
                     <a *ngIf="person.website" mat-button class="icon"
                         href="{{person.website}}" target="_blank" (click)="$event.stopPropagation()">
-                        <span class="fa fa-link fa-2x"></span>
+                        <span class="fa fa-link fa-2x" aria-hidden="true"></span>
+                        <span class="sr-only">Personal website {{person.name}}</span>
                     </a>
                 </div>
             </div>
