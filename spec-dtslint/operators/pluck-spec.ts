c4eb9cf828c5198a4a45f3613fcaa8ada3d6b6cc
Deddy Kosasih <dkosasih@gmail.com>
2018-10-06 12:56:28 +1000

    test(dtslint): fix dtslint test after typing fix from #4131 (#4093)

--- spec-dtslint/operators/pluck-spec.ts
+++ spec-dtslint/operators/pluck-spec.ts
@@ -1,18 +1,112 @@
 import { of } from 'rxjs';
 import { pluck } from 'rxjs/operators';
 
-it('should infer empty interface when not specified', () => {
-  const a = of([{ name: 'abc', id: 123 }, { name: 'def', id: 256 }]).pipe(pluck('name')); // $ExpectType Observable<{}>
+it('should infer correctly', () => {
+  const a = of({ name: 'abc', id: 123 }).pipe(pluck('name')); // $ExpectType Observable<string>
 });
 
-it('should infer generic type', () => {
-  const a = of([{ name: 'abc', id: 123 }, { name: 'def', id: 256 }]).pipe<string>(pluck('name')); // $ExpectType Observable<string>
+it('should support nested object of 2 layer depth', () => {
+  const a = of({ name: 'def', id: 256, address: { postcode: 2 } }).pipe(pluck('address', 'postcode')); // $ExpectType Observable<number>
 });
 
-it('should support nested object', () => {
-  const a = of([{ name: 'abc', id: 123, address: { postcode: 1 } }, { name: 'def', id: 256, address: { postcode: 2 } }]).pipe(pluck('address', 'postcode')); // $ExpectType Observable<{}
+it('should support nested object of 3 layer depth', () => {
+  const a = of({
+    name: 'def',
+    id: 256,
+    address: {
+      postcode: {
+        x: 1
+      }
+    }
+  }).pipe(pluck('address', 'postcode', 'x')); // $ExpectType Observable<number>
+});
+
+it('should support nested object of 4 layer depth', () => {
+  const a = of({
+    name: 'def',
+    id: 256,
+    address: {
+      postcode: {
+        x: {
+          y: 1
+        }
+      }
+    }
+  }).pipe(pluck('address', 'postcode', 'x', 'y')); // $ExpectType Observable<number>
+});
+
+it('should support nested object of 5 layer depth', () => {
+  const a = of({
+    name: 'def',
+    id: 256,
+    address: {
+      postcode: {
+        x: {
+          y: {
+            z: 1
+          }
+        }
+      }
+    }
+  }).pipe(pluck('address', 'postcode', 'x', 'y', 'z')); // $ExpectType Observable<number>
+});
+
+it('should support nested object of 6 layer depth', () => {
+  const a = of({
+    name: 'def',
+    id: 256,
+    address: {
+      postcode: {
+        x: {
+          y: {
+            z: {
+              aa: 1
+            }
+          }
+        }
+      }
+    }
+  }).pipe(pluck('address', 'postcode', 'x', 'y', 'z', 'aa')); // $ExpectType Observable<number>
+});
+
+it('should support nested object of 6 layer depth', () => {
+  const a = of({
+    name: 'def',
+    id: 256,
+    address: {
+      postcode: {
+        x: {
+          y: {
+            z: {
+              aa: 1
+            }
+          }
+        }
+      }
+    }
+  }).pipe(pluck('address', 'postcode', 'x', 'y', 'z', 'aa')); // $ExpectType Observable<number>
+});
+
+it('should infer empty interface for more than 6 layer depth', () => {
+  const a = of({
+    name: 'def',
+    id: 256,
+    address: {
+      postcode: {
+        x: {
+          y: {
+            z: {
+              aa: {
+                ab: 1
+              }
+            }
+          }
+        }
+      }
+    }
+  }).pipe(pluck('address', 'postcode', 'x', 'y', 'z', 'aa', 'ab')); // $ExpectType Observable<{}>
 });
 
 it('should accept string only', () => {
-  const a = of([{ name: 'abc', id: 123}, { name: 'def', id: 256}]).pipe(pluck(1)); // $ExpectError
+  const a = of({ name: 'abc', id: 123}).pipe(pluck(1)); // $ExpectError
 });
