9dea8b0a6aed113ba6c34a1e1924b427ad7691e2
Fermin Blanco <luillyfe89@gmail.com>
2018-10-10 01:08:13 -0500

    docs($browser): Created examples for using switchAll operator
    Added description for switchAll, 2 ecamples of how to use it and related operators
    
    4009

--- src/internal/operators/switchAll.ts
+++ src/internal/operators/switchAll.ts
@@ -1,9 +1,82 @@
-import { OperatorFunction, ObservableInput } from '../types';
+import {OperatorFunction, ObservableInput, MonoTypeOperatorFunction} from '../types';
 import { switchMap } from './switchMap';
 import { identity } from '../util/identity';
 
 export function switchAll<T>(): OperatorFunction<ObservableInput<T>, T>;
 export function switchAll<R>(): OperatorFunction<any, R>;
+
+/**
+ * Converts a higher-order Observable into a first-order Observable
+ * producing values only from the most recent observable sequence
+ *
+ * <span class="informal">Flattens an Observable-of-Observables.</span>
+ *
+ * ![](switchAll.png)
+ *
+ * `switchAll` subscribes to an Observable that emits Observables, also known as
+ * a higher-order Observable. The observable sequence that at any point in time
+ * produces the elements of the most recent inner observable sequence that has
+ * been received. The output Observable only
+ * completes once all inner Observables have completed. Any error delivered by
+ * a inner Observable will be immediately emitted on the output Observable.
+ *
+ * ## Examples
+ * Spawn a new interval Observable for each click event, but for every new
+ * click it just cancel the previous one then it subscribes to the new one
+ * ```javascript
+ * import { fromEvent } from "rxjs";
+ * import { switchAll, map } from "rxjs/operators";
+ *
+ * const clicks = fromEvent(document, 'click');
+ * const higherOrder = clicks.pipe(map((ev) => interval(1000)));
+ * const firstOrder = higherOrder.pipe(switchAll());
+ * firstOrder.subscribe(x => console.log(x));
+ * ```
+ *
+ * Hit on the screen and it will let you track the last 10 mouse position.
+ * For every click you are able to track the mouse position.
+ * ```javascript
+ * import { fromEvent } from "rxjs";
+ * import { switchAll, map, take } from "rxjs/operators";
+ *
+ * const mousePosition$ = fromEvent<MouseEvent>(document, "mousemove")
+ *   .pipe(
+ *     take(10),
+ *     map(evt => ({ x: evt.clientX, y: evt.clientY})),
+ *   );
+ *
+ * const clicks$ = fromEvent<MouseEvent>(document, "click")
+ *   .pipe(
+ *      map(evt => ({ x: evt.clientX, y: evt.clientY})),
+ *   );
+ *
+ * const higherOrder$ = clicks$
+ *   .pipe(
+ *      map(positions => mousePosition$),
+ *    );
+ *
+ * const firstOrder$ = higherOrder$.pipe(switchAll());
+ *
+ * firstOrder$.subscribe(
+ *   console.log,
+ *   null,
+ *   () => console.log("I complete ;)"))
+ * ```
+ *
+ * @see {@link combineAll}
+ * @see {@link concatAll}
+ * @see {@link exhaust}
+ * @see {@link switchMap}
+ * @see {@link switchMapTo}
+ * @see {@link mergeAll}
+ *
+ * @param {number} [concurrent=Number.POSITIVE_INFINITY] Maximum number of inner
+ * Observables being subscribed to concurrently.
+ * @return {Observable} An Observable that emits values coming from all the
+ * inner Observables emitted by the source Observable.
+ * @method switchAll
+ * @owner Observable
+ */
 
 export function switchAll<T>(): OperatorFunction<ObservableInput<T>, T> {
   return switchMap(identity);
