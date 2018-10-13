938a0066a0229e9aced60d5ecf8e96b532ed3bb9
Deddy Kosasih <dkosasih@gmail.com>
2018-10-13 20:32:29 +1100

    test(dtslint): add more test (#4093)

--- spec-dtslint/operators/publish-spec.ts
+++ spec-dtslint/operators/publish-spec.ts
@@ -6,9 +6,13 @@ it('should support empty parameter', () => {
   const a = of(1, 2, 3).pipe(publish()); // $ExpectType Observable<any>
 });
 
+it('should infer when type is specified', () => {
+  const a = of(1, 2, 3).pipe<number>(publish()); // $ExpectType Observable<number>
+});
+
 it('should infer correctly with parameter', () => {
-  const a = of(1, 2, 3).pipe(publish((x: Observable<number>) => x)); // $ExpectType Observable<number>
-  const b = of('a', 'b', 'c').pipe(publish((x: Observable<string>) => x)); // $ExpectType Observable<string>
+  const a = of(1, 2, 3).pipe(publish(x => x)); // $ExpectType Observable<number>
+  const b = of('a', 'b', 'c').pipe(publish(x => x)); // $ExpectType Observable<string>
 });
 
 it('should enforce type on selector', () => {
