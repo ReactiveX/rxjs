d17a32b5dfed1536f585bdba01e142e9c6a6c654
Ben Lesh <ben@benlesh.com>
2018-10-29 09:00:44 -0700

    Update doc text and narrow example down to one

--- src/internal/operators/switchAll.ts
+++ src/internal/operators/switchAll.ts
@@ -13,81 +13,43 @@ export function switchAll<R>(): OperatorFunction<any, R>;
  *
  * ![](switchAll.png)
  *
- * `switchAll` subscribes to an Observable that emits Observables, also known as a higher-order Observable.
- * The observable sequence that at any point in time produces the elements of the most recent inner
- * observable sequence that has been received. The output Observable only completes once the source observable,
- * and all inner Observables have completed. Any error delivered by the source or inner Observables will be immediately
- * emitted on the output Observable.
+ * `switchAll` subscribes to a source that is an observable of observables, also known as a 
+ * "higher-order observable" (or `Observable<Observable<T>>`). It subscribes to the most recently
+ * provided "inner observable" emitted by the source, unsubscribing from any previously subscribed
+ * to inner observable, such that only the most recent inner observable may be subscribed to at
+ * any point in time. The resulting observable returned by `switchAll` will only complete if the
+ * source observable completes, *and* any currently subscribed to inner observable also has completed, 
+ * if there are any.
  *
  * ## Examples
- * Spawn a new interval Observable for each click event, but for every new
- * click, it just cancel the previous one then it subscribes to the new one
+ * Spawn a new interval observable for each click event, but for every new
+ * click, cancel the previous interval and subscribe to the new one.
+ * 
  * ```ts
- * import { fromEvent, interval } from "rxjs";
- * import { switchAll, map } from "rxjs/operators";
+ * import { fromEvent, interval } from 'rxjs';
+ * import { switchAll, map } from 'rxjs/operators';
  *
- * const clicks = fromEvent(document, 'click');
- * const higherOrder = clicks.pipe(map((ev) => interval(1000)));
- * const firstOrder = higherOrder.pipe(switchAll());
- * firstOrder.subscribe(x => console.log(x));
+ * const clicks = fromEvent(document, 'click').pipe(tap(() => console.log('click')));
+ * const source = clicks.pipe(map((ev) => interval(1000)));
  *
- /* Outout
+ * source.pipe(
+ *   switchAll()
+ * ).subscribe(x => console.log(x));
+ *
+ /* Output
+ *  click
  *  1
  *  2
  *  3
  *  4
  *  ...
- *  (click)
+ *  click
  *  1
  *  2
  *  3
  *  ...
- *  (click)
+ *  click
  *  ...
- * ```
- *
- * Hit on the screen and it will let you track the last 10 mouse position.
- * For every click you are able to track the mouse position.
- * ```ts
- * import { fromEvent } from "rxjs";
- * import { switchAll, map, take } from "rxjs/operators";
- *
- * const mousePosition$ = fromEvent<MouseEvent>(document, "mousemove")
- *   .pipe(
- *     take(10),
- *     map(evt => ({ x: evt.clientX, y: evt.clientY})),
- *   );
- *
- * const clicks$ = fromEvent<MouseEvent>(document, "click")
- *   .pipe(
- *      map(evt => ({ x: evt.clientX, y: evt.clientY})),
- *   );
- *
- * const higherOrder$ = clicks$
- *   .pipe(
- *      map(positions => mousePosition$),
- *    );
- *
- * const firstOrder$ = higherOrder$.pipe(switchAll());
- *
- * firstOrder$.subscribe(
- *   console.log,
- *   null,
- *   () => console.log("I complete ;)"))
- *
- /* Output
- * (click)
- * {x: 187, y: 51}
- * {x: 187, y: 53}
- * ...
- * {x: 127, y: 92}
- * (click)
- * {x: 315, y: 62}
- * {x: 315, y: 63}
- * ...
- * {x: 255, y: 118}
- * (click)
- * ...
  * ```
  *
  * @see {@link combineAll}
