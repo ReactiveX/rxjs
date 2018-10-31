05aebf4cf2709d02fcf63c2719658a0e72507fb1
Alan Agius <alan.agius@betssongroup.com>
2018-10-31 11:46:43 +0100

    build(bundle): update rollup
    This is required so that we can use named UMDs with named AMD, since these are required in Bazel (ts_devserver). This feature doesn't work in the current version rollup.
    Rollup's API also changed quite a bit, thus the large refactoring.

--- tools/make-umd-compat-bundle.js
+++ tools/make-umd-compat-bundle.js
@@ -1,41 +1,14 @@
-var _ = require('lodash');
+var rollupBundle = require('./rollup-bundle');
 
-var rollup = require('rollup');
-var rollupAlias = require('rollup-plugin-alias');
-var rollupInject = require('rollup-plugin-inject');
-var rollupNodeResolve = require('rollup-plugin-node-resolve');
-
-var fs = require('fs');
-var tslib = require('tslib');
-
-rollup.rollup({
-  entry: 'dist-compat/esm5_for_rollup/compat/umd.js',
-  plugins: [
-    rollupAlias({
-      'rxjs/testing': 'dist-compat/esm5_for_rollup/src/testing/index.js',
-      'rxjs/operators': 'dist-compat/esm5_for_rollup/src/operators/index.js',
-      'rxjs/webSocket': 'dist-compat/esm5_for_rollup/src/webSocket/index.js',
-      'rxjs/ajax': 'dist-compat/esm5_for_rollup/src/ajax/index.js',
-      'rxjs/internal-compatibility': 'dist-compat/esm5_for_rollup/src/internal-compatibility/index.js',
-      'rxjs': 'dist-compat/esm5_for_rollup/src/index.js',
-    }),
-    rollupNodeResolve({
-      jsnext: true,
-    }),
-    rollupInject({
-      exclude: 'node_modules/**',
-      modules: _.mapValues(tslib, function (value, key) {
-        return ['tslib', key];
-      }),
-    }),
-  ],
-}).then(function (bundle) {
-  var result = bundle.generate({
-    format: 'umd',
-    moduleName: 'rxjs',
-    sourceMap: true
-  });
-
-  fs.writeFileSync('dist-compat/global/rxjs-compat.umd.js', result.code);
-  fs.writeFileSync('dist-compat/global/rxjs-compat.umd.js.map', result.map);
+rollupBundle({
+  input: 'dist-compat/esm5_for_rollup/compat/umd.js',
+  dest: 'dist-compat/global/rxjs-compat.umd.js',
+  aliases: {
+    'rxjs/testing': 'dist-compat/esm5_for_rollup/src/testing/index.js',
+    'rxjs/operators': 'dist-compat/esm5_for_rollup/src/operators/index.js',
+    'rxjs/webSocket': 'dist-compat/esm5_for_rollup/src/webSocket/index.js',
+    'rxjs/ajax': 'dist-compat/esm5_for_rollup/src/ajax/index.js',
+    'rxjs/internal-compatibility': 'dist-compat/esm5_for_rollup/src/internal-compatibility/index.js',
+    'rxjs': 'dist-compat/esm5_for_rollup/src/index.js',
+  },
 });
