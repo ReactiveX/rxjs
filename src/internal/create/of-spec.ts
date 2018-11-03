e5f30f67c82498aacd2c26822f327346ce858e21
Ben Lesh <ben@benlesh.com>
2018-11-02 19:18:06 -0700

    add of tests

--- src/internal/create/of-spec.ts
+++ src/internal/create/of-spec.ts
@@ -1,41 +1,77 @@
-import { of } from 'rxjs/internal/create/of';
 import { expect } from 'chai';
-import { asapScheduler } from 'rxjs/internal/scheduler/asapScheduler';
+import { EMPTY, of } from 'rxjs';
+import { TestScheduler } from 'rxjs/testing';
+import { concatMap, delay, concatAll } from 'rxjs/operators';
+import { assertDeepEquals } from '../test_helpers/assertDeepEquals';
 
+/** @test {of} */
 describe('of', () => {
-  it('should handle no arguments passed', () => {
-    const source = of();
-    let nexted = false;
-    let completed = false;
-    source.subscribe({
-      next() { nexted = true; },
-      complete() { completed = true; },
-    });
-    expect(completed).to.be.true;
-    expect(nexted).to.be.false;
+  let testScheduler: TestScheduler;
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler(assertDeepEquals);
   });
 
-  it('should emit values and complete synchronously if not scheduled', () => {
-    const source = of(1, 2, 3);
-    const results: any[] = [];
-    results.push('start');
-    source.subscribe({
-      next(value) { results.push(value) },
-      complete() { results.push('done') },
+  //asDiagram('of(1, 2, 3)')
+  it('should create a cold observable that emits 1, 2, 3', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const e1 = of(1, 2, 3).pipe(
+        // for the purpose of making a nice diagram, spread out the synchronous emissions
+        concatMap((x, i) => of(x).pipe(delay(i === 0 ? 0 : 2, testScheduler)))
+      );
+      const expected = 'x-y-(z|)';
+      expectObservable(e1).toBe(expected, {x: 1, y: 2, z: 3});
     });
-    results.push('stop');
-    expect(results).to.deep.equal(['start', 1, 2, 3, 'done', 'stop']);
   });
 
-  it('should not complete after early unsubscribe', () => {
-    const source = of(1, 2, 3);
-    source.subscribe({
-      next(value, subscription) {
-        if (value === 2) subscription.unsubscribe();
-      },
-      complete() {
-        throw new Error('should not be called');
-      }
-    })
+  it('should create an observable from the provided values', done => {
+    const x = { foo: 'bar' };
+    const expected = [1, 'a', x];
+    let i = 0;
+
+    of(1, 'a', x)
+      .subscribe(y => {
+        expect(y).to.equal(expected[i++]);
+      }, (x) => {
+        done(new Error('should not be called'));
+      }, () => {
+        done();
+      });
+  });
+
+  it('should emit one value', done => {
+    let calls = 0;
+
+    of(42).subscribe((x: number) => {
+      expect(++calls).to.equal(1);
+      expect(x).to.equal(42);
+    }, (err: any) => {
+      done(new Error('should not be called'));
+    }, () => {
+      done();
+    });
+  });
+
+  it('should handle an Observable as the only value', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const source = of(
+        of('a', 'b', 'c')
+      );
+      const result = source.pipe(concatAll());
+      expectObservable(result).toBe('(abc|)');
+    });
+  });
+
+  it('should handle many Observable as the given values', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const source = of(
+        of('a', 'b', 'c'),
+        of('d', 'e', 'f'),
+
+      );
+
+      const result = source.pipe(concatAll());
+      expectObservable(result).toBe('(abcdef|)');
+    });
   });
 });
