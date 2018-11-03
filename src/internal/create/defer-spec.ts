b32615977c272fe64a11bad8b8cf7c96e6215a75
Ben Lesh <ben@benlesh.com>
2018-11-02 17:42:18 -0700

    port defer tests

--- src/internal/create/defer-spec.ts
+++ src/internal/create/defer-spec.ts
@@ -1,25 +1,134 @@
-import { of } from "rxjs/internal/create/of";
-import { expect } from "chai";
-import { defer } from 'rxjs/internal/create/defer';
+import { expect } from 'chai';
+import { defer, of } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { TestScheduler } from '../testing/TestScheduler';
+import { assertDeepEquals } from '../test_helpers/assertDeepEquals';
 
+/** @test {defer} */
 describe('defer', () => {
-  it('should create an observable from a deferred call to a function', () => {
-    const results: any[] = [];
-    let called = false;
+  let testScheduler: TestScheduler;
 
-    const source = defer(() => {
-      called = true;
-      return of(1, 2, 3);
+  beforeEach(() => {
+    testScheduler = new TestScheduler(assertDeepEquals);
+  });
+
+  //asDiagram('defer(() => Observable.of(a, b, c))')
+  it('should defer the creation of a simple Observable', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const expected =    '-a--b--c--|';
+      const e1 = defer(() => cold('-a--b--c--|'));
+      expectObservable(e1).toBe(expected);
+    });
+  });
+
+  it('should create an observable from the provided observable factory', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const source = hot('--a--b--c--|');
+      const sourceSubs = '^          !';
+      const expected =   '--a--b--c--|';
+
+      const e1 = defer(() => source);
+
+      expectObservable(e1).toBe(expected);
+      expectSubscriptionsTo(source).toBe(sourceSubs);
+    });
+  });
+
+  it('should create an observable from completed', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const source = hot('|');
+      const sourceSubs = '(^!)';
+      const expected =   '|';
+
+      const e1 = defer(() => source);
+
+      expectObservable(e1).toBe(expected);
+      expectSubscriptionsTo(source).toBe(sourceSubs);
+    });
+  });
+
+  it('should accept factory returns promise resolves', done => {
+    const expected = 42;
+    const e1 = defer(() => {
+      return new Promise<number>((resolve: any) => { resolve(expected); });
     });
 
-    expect(called).to.be.false;
+    e1.subscribe((x: number) => {
+      expect(x).to.equal(expected);
+      done();
+    }, (x: any) => {
+      done(new Error('should not be called'));
+    });
+  });
 
-    source.subscribe({
-      next(value) { results.push(value); },
-      complete() { results.push('done'); },
+  it('should accept factory returns promise rejects', done => {
+    const expected = 42;
+    const e1 = defer(() => {
+      return new Promise<number>((resolve: any, reject: any) => { reject(expected); });
     });
 
-    expect(results).to.deep.equal([1, 2, 3, 'done']);
-    expect(called).to.be.true;
+    e1.subscribe((x: number) => {
+      done(new Error('should not be called'));
+    }, (x: any) => {
+      expect(x).to.equal(expected);
+      done();
+    }, () => {
+      done(new Error('should not be called'));
+    });
+  });
+
+  it('should create an observable from error', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const source = hot('#');
+      const sourceSubs = '(^!)';
+      const expected =   '#';
+
+      const e1 = defer(() => source);
+
+      expectObservable(e1).toBe(expected);
+      expectSubscriptionsTo(source).toBe(sourceSubs);
+    });
+  });
+
+  it('should create an observable when factory throws', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const e1 = defer(() => {
+        throw 'error';
+      });
+      const expected = '#';
+
+      expectObservable(e1).toBe(expected);
+    });
+  });
+
+  it('should allow unsubscribing early and explicitly', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const source = hot('--a--b--c--|');
+      const sourceSubs = '^     !     ';
+      const expected =   '--a--b-     ';
+      const unsub =      '      !     ';
+
+      const e1 = defer(() => source);
+
+      expectObservable(e1, unsub).toBe(expected);
+      expectSubscriptionsTo(source).toBe(sourceSubs);
+    });
+  });
+
+  it('should not break unsubscription chains when result is unsubscribed explicitly', () => {
+    testScheduler.run(({ hot, cold, expectObservable, expectSubscriptionsTo }) => {
+      const source = hot('--a--b--c--|');
+      const sourceSubs = '^     !     ';
+      const expected =   '--a--b-     ';
+      const unsub =      '      !     ';
+
+      const e1 = defer(() => source.pipe(
+        mergeMap(x => of(x)),
+        mergeMap(x => of(x))
+      ));
+
+      expectObservable(e1, unsub).toBe(expected);
+      expectSubscriptionsTo(source).toBe(sourceSubs);
+    });
   });
 });
