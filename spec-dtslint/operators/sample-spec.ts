9fb849ae8fb8dc876c01f54ae34bbcdfd3e0e041
Deddy Kosasih <dkosasih@gmail.com>
2018-11-08 08:41:24 +1100

    test(dtslint): simplify sample operator dtslint test (#4093)

--- spec-dtslint/operators/sample-spec.ts
+++ spec-dtslint/operators/sample-spec.ts
@@ -1,10 +1,11 @@
-import { of, interval } from 'rxjs';
-import { sample, delay } from 'rxjs/operators';
+import { of } from 'rxjs';
+import { sample } from 'rxjs/operators';
 
 it('should enforce parameter', () => {
   const a = of(1, 2, 3).pipe(sample()); // $ExpectError
 });
 
 it('should accept observable as notifier parameter', () => {
-  const a = interval(1000).pipe(sample(of(1).pipe(delay(2000)))); // $ExpectType Observable<number>
+  const a = of(1, 2, 3).pipe(sample(of(4))); // $ExpectType Observable<number>
+  const b = of(1, 2, 3).pipe(sample(of('a'))); // $ExpectType Observable<number>
 });
