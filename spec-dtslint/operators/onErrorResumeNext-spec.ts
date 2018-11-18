46f99abdabf9c97572de14e60947cffc26e0afef
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2018-11-18 18:46:46 +0100

    chore(typings): refine onErrorResumeNext types

--- spec-dtslint/operators/onErrorResumeNext-spec.ts
+++ spec-dtslint/operators/onErrorResumeNext-spec.ts
@@ -2,16 +2,16 @@ import { of } from 'rxjs';
 import { onErrorResumeNext } from 'rxjs/operators';
 
 it('should infer correctly', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext()); // $ExpectType Observable<{}>
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext()); // $ExpectType Observable<string>
 });
 
 it('should accept one input', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(of(1))); // $ExpectType Observable<number>
-  const p = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<string, string>(of(5))); // $ExpectType Observable<string>
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(of(1))); // $ExpectType Observable<string | number>
+  const p = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(of('5'))); // $ExpectType Observable<string>
 });
 
 it('should accept promises', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(Promise.resolve(5))); // $ExpectType Observable<number>
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(Promise.resolve(5))); // $ExpectType Observable<string | number>
 });
 
 it('should accept iterables', () => {
@@ -19,28 +19,32 @@ it('should accept iterables', () => {
 });
 
 it('should accept arrays', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext([5])); // $ExpectType Observable<number>
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext([5])); // $ExpectType Observable<string | number>
 });
 
 it('should accept two inputs', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<string, number, number, number>(of(1), of(2))); // $ExpectType Observable<number>
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(of(1), of(2))); // $ExpectType Observable<string | number>
 });
 
 it('should accept three inputs', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<string, number, number, string, number>(of(1), of(2), of('3'))); // $ExpectType Observable<number>
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(of(1), of(2), of('3'))); // $ExpectType Observable<string | number>
 });
 
 it('should accept four inputs', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<string, number, number, string, string, string>(of(1), of(2), of('3'), of('4'))); // $ExpectType Observable<string>
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(of(1), of(2), of('3'), of('4'))); // $ExpectType Observable<string | number>
 });
 
 it('should accept five inputs', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<string, number, number, string, string, number, string>(of(1), of(2), of('3'), of('4'), of(5))); // $ExpectType Observable<string>
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(of(1), of(2), of('3'), of('4'), of(5))); // $ExpectType Observable<string | number>
 });
 
-it('should accept six or more inputs', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<string, number>(of(1), of(2), of('3'), of('4'), of(5), of('6'))); // $ExpectType Observable<number>
-  const p = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<string, number>(of(1), of(2), of('3'), of('4'), of(5), of('6'), of(7))); // $ExpectType Observable<number>
+it('should accept six inputs', () => {
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(of(1), of(2), of('3'), of('4'), of(5), of('6'))); // $ExpectType Observable<string | number>
+});
+
+it('should accept seven and more inputs', () => {
+  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext(of(1), of(2), of('3'), of('4'), of(5), of('6'), of(7))); // $ExpectType Observable<{}>
+  const p = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<string, string | number>(of(1), of(2), of('3'), of('4'), of(5), of('6'), of(7))); // $ExpectType Observable<string | number>
 });
 
 it('should enforce types', () => {
@@ -48,9 +52,5 @@ it('should enforce types', () => {
 });
 
 it('should enforce source types', () => {
-  const p = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<number, string>(of(5))); // $ExpectError
-});
-
-it('should enforce parameter types', () => {
-  const o = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<string, string, string, number>(of(1), of(2))); // $ExpectError
+  const p = of('apple', 'banana', 'peach').pipe(onErrorResumeNext<number, number>(of(5))); // $ExpectError
 });
--- src/internal/operators/onErrorResumeNext.ts
+++ src/internal/operators/onErrorResumeNext.ts
@@ -10,11 +10,13 @@ import { subscribeToResult } from '../util/subscribeToResult';
 import { ObservableInput, OperatorFunction } from '../types';
 
 /* tslint:disable:max-line-length */
-export function onErrorResumeNext<T, R>(v: ObservableInput<R>): OperatorFunction<T, R>;
-export function onErrorResumeNext<T, T2, T3, R>(v2: ObservableInput<T2>, v3: ObservableInput<T3>): OperatorFunction<T, R>;
-export function onErrorResumeNext<T, T2, T3, T4, R>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, v4: ObservableInput<T4>): OperatorFunction<T, R>;
-export function onErrorResumeNext<T, T2, T3, T4, T5, R>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, v4: ObservableInput<T4>, v5: ObservableInput<T5>): OperatorFunction<T, R>;
-export function onErrorResumeNext<T, T2, T3, T4, T5, T6, R>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, v4: ObservableInput<T4>, v5: ObservableInput<T5>, v6: ObservableInput<T6>): OperatorFunction<T, R> ;
+export function onErrorResumeNext<T>(): OperatorFunction<T, T>;
+export function onErrorResumeNext<T, T2>(v: ObservableInput<T2>): OperatorFunction<T, T | T2>;
+export function onErrorResumeNext<T, T2, T3>(v: ObservableInput<T2>, v2: ObservableInput<T3>): OperatorFunction<T, T | T2 | T3>;
+export function onErrorResumeNext<T, T2, T3, T4>(v: ObservableInput<T2>, v2: ObservableInput<T3>, v3: ObservableInput<T4>): OperatorFunction<T, T | T2 | T3 | T4>;
+export function onErrorResumeNext<T, T2, T3, T4, T5>(v: ObservableInput<T2>, v2: ObservableInput<T3>, v3: ObservableInput<T4>, v4: ObservableInput<T5>): OperatorFunction<T, T | T2 | T3 | T4 | T5>;
+export function onErrorResumeNext<T, T2, T3, T4, T5, T6>(v: ObservableInput<T2>, v2: ObservableInput<T3>, v3: ObservableInput<T4>, v4: ObservableInput<T5>, v5: ObservableInput<T6>): OperatorFunction<T, T | T2 | T3 | T4 | T5 | T6>;
+export function onErrorResumeNext<T, T2, T3, T4, T5, T6, T7>(v: ObservableInput<T2>, v2: ObservableInput<T3>, v3: ObservableInput<T4>, v4: ObservableInput<T5>, v5: ObservableInput<T6>, v6: ObservableInput<T7>): OperatorFunction<T, T | T2 | T3 | T4 | T5 | T6 | T7>;
 export function onErrorResumeNext<T, R>(...observables: Array<ObservableInput<any> | ((...values: Array<any>) => R)>): OperatorFunction<T, R>;
 export function onErrorResumeNext<T, R>(array: ObservableInput<any>[]): OperatorFunction<T, R>;
 /* tslint:enable:max-line-length */
