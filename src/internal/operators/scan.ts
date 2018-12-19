1c10d280ecfbf070a43f4a09e60082eecb3e6143
Ben Lesh <ben@benlesh.com>
2018-12-19 08:11:28 -0800

    scan docs

--- src/internal/operators/scan.ts
+++ src/internal/operators/scan.ts
@@ -5,6 +5,45 @@ import { tryUserFunction, resultIsError } from 'rxjs/internal/util/userFunction'
 import { Subscriber } from '../Subscriber';
 import { OperatorSubscriber } from '../OperatorSubscriber';
 
+/**
+ * Applies an accumulator function over the source Observable, and returns each
+ * intermediate result, with an optional seed value.
+ *
+ * <span class="informal">It's like {@link reduce}, but emits the current
+ * accumulation whenever the source emits a value.</span>
+ *
+ * ![](scan.png)
+ *
+ * Combines together all values emitted on the source, using an accumulator
+ * function that knows how to join a new source value into the accumulation from
+ * the past. Is similar to {@link reduce}, but emits the intermediate
+ * accumulations.
+ *
+ * Returns an Observable that applies a specified `accumulator` function to each
+ * item emitted by the source Observable. If a `seed` value is specified, then
+ * that value will be used as the initial value for the accumulator. If no seed
+ * value is specified, the first item of the source is used as the seed.
+ *
+ * ## Example
+ * Count the number of click events
+ * ```javascript
+ * const clicks = fromEvent(document, 'click');
+ * const ones = clicks.pipe(mapTo(1));
+ * const seed = 0;
+ * const count = ones.pipe(scan((acc, one) => acc + one, seed));
+ * count.subscribe(x => console.log(x));
+ * ```
+ *
+ * @see {@link expand}
+ * @see {@link mergeScan}
+ * @see {@link reduce}
+ *
+ * @param {function(acc: R, value: T, index: number): R} accumulator
+ * The accumulator function called on each source value.
+ * @param {T|R} [seed] The initial accumulation value.
+ * @return {Observable<R>} An observable of the accumulated values.
+ * @method scan
+ */
 export function scan<T>(reducer: (state: T, value: T, index: number) => T): OperatorFunction<T, T>;
 export function scan<T, R>(reducer: (state: T|R, value: T, index: number) => R): OperatorFunction<T, R>;
 export function scan<T, R>(reducer: (state: R, valeu: T, index: number) => R, initialState: R): OperatorFunction<T, R>;
