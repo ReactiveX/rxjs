e2afd7216ac6669d4ea10b5908407b9120e67298
Ben Lesh <ben@benlesh.com>
2018-12-19 08:08:39 -0800

    scan, defaultIfEmpty, and reduce

--- src/internal/operators/defaultIfEmpty.ts
+++ src/internal/operators/defaultIfEmpty.ts
@@ -1,18 +1,36 @@
-import { lift } from 'rxjs/internal/util/lift';
 import { Observable } from 'rxjs/internal/Observable';
-import { FOType, Sink, OperatorFunction, SinkArg } from 'rxjs/internal/types';
 import { Subscription } from 'rxjs/internal/Subscription';
+import { OperatorSubscriber } from '../OperatorSubscriber';
+import { Subscriber } from 'rxjs/internal/Subscriber';
+import { OperatorFunction } from '../types';
 
-export function defaultIfEmpty<T>(defaultValue?: T): OperatorFunction<T, T>;
 export function defaultIfEmpty<T, R>(defaultValue: R = null): OperatorFunction<T, T|R> {
-  return lift((source: Observable<T>, dest: Sink<T|R>, subs: Subscription) => {
-    let hasValue = false;
-    source(FOType.SUBSCRIBE, (t: FOType, v: SinkArg<T>, subs: Subscription) => {
-      hasValue = hasValue || t === FOType.NEXT;
-      if (t === FOType.COMPLETE && !hasValue) {
-        dest(FOType.NEXT, defaultValue, subs);
-      }
-      dest(t, v, subs);
-    }, subs);
-  });
+  return (source: Observable<T>) => source.lift(defaultIfEmptyOperator(defaultValue));
+}
+
+function defaultIfEmptyOperator<T, R>(defaultValue: R) {
+  return function defaultIfEmptyLift(this: Subscriber<T|R>, source: Observable<T>, subscription: Subscription) {
+    return source.subscribe(new DefaultIfEmptySubscriber(subscription, this, defaultValue));
+  };
+}
+
+class DefaultIfEmptySubscriber<T, R> extends OperatorSubscriber<T> {
+  private _hasValue = false;
+
+  constructor(subscription: Subscription, destination: Subscriber<T|R>, private _defaultValue: T|R) {
+    super(subscription, destination);
+  }
+
+  next(value: T) {
+    this._hasValue = true;
+    this._destination.next(value);
+  }
+
+  complete() {
+    const { _destination } = this;
+    if (!this._hasValue) {
+      _destination.next(this._defaultValue);
+    }
+    _destination.complete();
+  }
 }
--- src/internal/operators/scan.ts
+++ src/internal/operators/scan.ts
@@ -1,8 +1,9 @@
-import { OperatorFunction, Sink, FOType, SinkArg } from 'rxjs/internal/types';
+import { OperatorFunction } from 'rxjs/internal/types';
 import { Observable } from 'rxjs/internal/Observable';
 import { Subscription } from 'rxjs/internal/Subscription';
 import { tryUserFunction, resultIsError } from 'rxjs/internal/util/userFunction';
-import { lift } from 'rxjs/internal/util/lift';
+import { Subscriber } from '../Subscriber';
+import { OperatorSubscriber } from '../OperatorSubscriber';
 
 export function scan<T>(reducer: (state: T, value: T, index: number) => T): OperatorFunction<T, T>;
 export function scan<T, R>(reducer: (state: T|R, value: T, index: number) => R): OperatorFunction<T, R>;
@@ -10,24 +11,44 @@ export function scan<T, R>(reducer: (state: R, valeu: T, index: number) => R, in
 export function scan<T, R, I>(reducer: (state: I|R, value: T, index: number) => R, initialState: I): OperatorFunction<T, R|I>;
 export function scan<T, R, I>(reducer: (state: T|R|I, value: T, index: number) => R, initialState?: R|I): OperatorFunction<T, T|R|I> {
   let hasState = arguments.length >= 2;
-  return lift((source: Observable<T>, dest: Sink<R>, subs: Subscription) => {
-    let i = 0;
-    let state = initialState;
-    source(FOType.SUBSCRIBE, (t: FOType, v: SinkArg<T>, subs: Subscription) => {
-      if (t === FOType.NEXT) {
-        const index = i++;
-        if (hasState) {
-          v = tryUserFunction(reducer, state, v, index);
-          if (resultIsError(v)) {
-            dest(FOType.ERROR, v.error, subs);
-            subs.unsubscribe();
-            return;
-          }
-        }
-        state = v;
-        hasState = true;
+  return (source: Observable<T>) => source.lift(scanOperator<T, R, I>(reducer, initialState, hasState));
+}
+
+function scanOperator<T, R, I>(reducer: (state: T|R|I, value: T, index: number) => R, initialState: R|I, hasState: boolean) {
+  return function scanLift(this: Subscriber<T|R|I>, source: Observable<T>, subscription: Subscription) {
+    return source.subscribe(new ScanSubscriber<T, R, I>(subscription, this, reducer, initialState, hasState));
+  };
+}
+
+class ScanSubscriber<T, R, I> extends OperatorSubscriber<T> {
+  private _index = 0;
+  private _state: T|R|I;
+
+  constructor(
+    subscription: Subscription,
+    destination: Subscriber<T|R|I>,
+    private _reducer: (state: T|R|I, value: T, index: number) => R,
+    _initialState: R|I,
+    private _hasState: boolean
+  ) {
+    super(subscription, destination);
+    if (_hasState) {
+      this._state = _initialState;
+    }
+  }
+
+  next(value: T) {
+    const { _destination } = this;
+    const index = this._index++;
+    if (this._hasState) {
+      value = tryUserFunction(this._reducer, [this._state, value, index]) as any;
+      if (resultIsError(value)) {
+        _destination.error(value.error);
+        return;
       }
-      dest(t, v, subs);
-    }, subs);
-  });
+    }
+    this._state = value;
+    this._hasState = true;
+    _destination.next(value);
+  }
 }
