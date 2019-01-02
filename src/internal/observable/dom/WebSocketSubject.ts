85e7f3a873935e1d37f75b14c882e367d763cee9
rsikdar <rsikdar@berkeley.edu>
2019-01-01 16:29:48 -0800

    fix(WebSocketSubject): fix subject failing to close socket
    Fix WebSocketSubject failing to close the underlying socket connection when it is unsubscribed
    before the socket is in the open state. Currently, when unsubscribe is called on the subject or
    when a subscription is unsubscribed there are checks on whether the socket is open (readyState = 1)
    before it closes the connection. If unsubscribe is called before the connection is open, it will not
    close the socket but will reset the state of the subject (_socket set to null), and it becomes
    impossible to close the socket.

--- src/internal/observable/dom/WebSocketSubject.ts
+++ src/internal/observable/dom/WebSocketSubject.ts
@@ -187,6 +187,12 @@ export class WebSocketSubject<T> extends AnonymousSubject<T> {
     });
 
     socket.onopen = (e: Event) => {
+      const { _socket } = this;
+      if (!_socket) {
+        socket.close();
+        this._resetState();
+        return;
+      }
       const { openObserver } = this._config;
       if (openObserver) {
         openObserver.next(e);
@@ -285,14 +291,11 @@ export class WebSocketSubject<T> extends AnonymousSubject<T> {
   }
 
   unsubscribe() {
-    const { source, _socket } = this;
+    const { _socket } = this;
     if (_socket && _socket.readyState === 1) {
       _socket.close();
-      this._resetState();
     }
+    this._resetState();
     super.unsubscribe();
-    if (!source) {
-      this.destination = new ReplaySubject();
-    }
   }
 }
