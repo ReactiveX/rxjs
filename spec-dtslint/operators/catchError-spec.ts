8350622c52c9d2ae3df170bfc50cf1557ba1ccf3
Ben Lesh <ben@benlesh.com>
2019-01-08 23:51:56 -0800

    fix(types): support union type returns from catchError

--- spec-dtslint/operators/catchError-spec.ts
+++ spec-dtslint/operators/catchError-spec.ts
@@ -1,8 +1,12 @@
-import { of, Observable } from 'rxjs';
+import { of, Observable, EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 it('should infer correctly', () => {
   const o = of(1, 2, 3).pipe(catchError((() => of(4, 5, 6)))); // $ExpectType Observable<number>
+});
+
+it('should handle empty (never) appropriately', () => {
+  const o = of(1, 2, 3).pipe(catchError(() => EMPTY));
 });
 
 it('should infer correctly when not returning', () => {
@@ -23,4 +27,8 @@ it('should enforce that selector returns an Observable', () => {
 
 it('should enforce type of caught', () => {
   const o = of(1, 2, 3).pipe(catchError((err, caught: Observable<string>) => of('a', 'b', 'c'))); // $ExpectError
+});
+
+it('should handle union types', () => {
+  const o = of(1, 2, 3).pipe(catchError(err => err.message === 'wee' ? of('fun') : of(123))); // $ExpectType Observable<string | number>
 });
