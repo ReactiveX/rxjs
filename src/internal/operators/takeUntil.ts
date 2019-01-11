83150255917555c4c4ad139b0c43967c1d37749f
Ben Lesh <ben@benlesh.com>
2019-01-11 10:18:25 -0800

    docs(takeUntil): Changed `i` to `source`.
    `i` is usually for indices and integers.

--- src/internal/operators/takeUntil.ts
+++ src/internal/operators/takeUntil.ts
@@ -26,9 +26,9 @@ import { MonoTypeOperatorFunction, TeardownLogic } from '../types';
  * ## Example
  * Tick every second until the first click happens
  * ```javascript
- * const i = interval(1000);
+ * const source = interval(1000);
  * const clicks = fromEvent(document, 'click');
- * const result = i.pipe(takeUntil(clicks));
+ * const result = source.pipe(takeUntil(clicks));
  * result.subscribe(x => console.log(x));
  * ```
  *
