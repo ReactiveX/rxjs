5f189c5b8d6edcadbf1df88f583bf15421b4a01d
萧文 <dolby.xiaowen@gmail.com>
2019-01-29 19:11:00 +0800

    docs(timeInterval) add docs

--- src/internal/operators/timeInterval.ts
+++ src/internal/operators/timeInterval.ts
@@ -6,6 +6,42 @@ import { scan } from './scan';
 import { defer } from '../observable/defer';
 import { map } from './map';
 
+/**
+ *
+ * emit time interval between current value with last value
+ *
+ *
+ * ![](timeinterval.png)
+ *
+ * `timeinterval` operator accepts as an argument as the Scheduler, default value is Scheduler.async
+ *
+ * ## Examples
+ * emit inteval between current value with the last value
+ *
+ * ```javascript
+ * const seconds = interval(1000);
+ *
+ * seconds.pipe(timeinterval())
+ * .subscribe(
+ *     value => console.log(value),
+ *     err => console.log(err),
+ * );
+ *
+ * seconds.pipe(timeout(900))
+ * .subscribe(
+ *     value => console.log(value),
+ *     err => console.log(err),
+ * );
+ * // {value: 0, interval: 1001}
+ * // {value: 1, interval: 1003}
+ * // {value: 2, interval: 997}
+ * ```
+ *
+ * @param {SchedulerLike} [scheduler] Scheduler controlling when timeout checks occur.
+ * @return {Observable<T>} Observable that emit infomation about value and interval
+ * @method timeInterval
+ * @owner Observable
+ */
 export function timeInterval<T>(scheduler: SchedulerLike = async): OperatorFunction<T, TimeInterval<T>> {
   return (source: Observable<T>) => defer(() => {
     return source.pipe(
