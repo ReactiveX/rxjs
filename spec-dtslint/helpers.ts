cb674aa6349206aa72feb40339ae15bed7ff2ea8
Deddy Kosasih <dkosasih@gmail.com>
2019-01-29 08:02:18 +0700

    test(dtslint): change of observable static function dtslint test to use helper (#4093)

---
+++ spec-dtslint/helpers.ts
@@ -0,0 +1,23 @@
+import { of } from 'rxjs';
+
+export class A { a = 0; }
+export class B { b = 0; }
+export class C { c = 0; }
+export class D { d = 0; }
+export class E { e = 0; }
+export class F { f = 0; }
+export class G { g = 0; }
+export class H { h = 0; }
+export class I { i = 0; }
+export class J { j = 0; }
+
+export const a = of(new A());
+export const b = of(new B());
+export const c = of(new C());
+export const d = of(new D());
+export const e = of(new E());
+export const f = of(new F());
+export const g = of(new G());
+export const h = of(new H());
+export const i = of(new I());
+export const j = of(new J());
