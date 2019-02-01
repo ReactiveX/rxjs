41d5486f0401525fe39d894f2b5021741eb3e497
Gobius Dolhain <Goobles@users.noreply.github.com>
2019-02-01 17:10:39 +0100

    chore(typings): add mapped tuple return type
    This makes it possible to have type safety for infinite amount of input arguments without having to define every single case.
    Makes use of typescript 3.1 new "Mapped types on tuples and arrays" feature.
    https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-1.html

--- src/internal/observable/combineLatest.ts
+++ src/internal/observable/combineLatest.ts
@@ -11,6 +11,8 @@ import { fromArray } from './fromArray';
 
 const NONE = {};
 
+type ObservableOutputs<T> = { [K in keyof T]: ObservedValueOf<T[K]> };
+
 /* tslint:disable:max-line-length */
 
 // If called with a single array, it "auto-spreads" the array, with result selector
@@ -59,6 +61,7 @@ export function combineLatest<O1 extends ObservableInput<any>, O2 extends Observ
 export function combineLatest<O1 extends ObservableInput<any>, O2 extends ObservableInput<any>, O3 extends ObservableInput<any>, O4 extends ObservableInput<any>>(v1: O1, v2: O2, v3: O3, v4: O4, scheduler?: SchedulerLike): Observable<[ObservedValueOf<O1>, ObservedValueOf<O2>, ObservedValueOf<O3>, ObservedValueOf<O4>]>;
 export function combineLatest<O1 extends ObservableInput<any>, O2 extends ObservableInput<any>, O3 extends ObservableInput<any>, O4 extends ObservableInput<any>, O5 extends ObservableInput<any>>(v1: O1, v2: O2, v3: O3, v4: O4, v5: O5, scheduler?: SchedulerLike): Observable<[ObservedValueOf<O1>, ObservedValueOf<O2>, ObservedValueOf<O3>, ObservedValueOf<O4>, ObservedValueOf<O5>]>;
 export function combineLatest<O1 extends ObservableInput<any>, O2 extends ObservableInput<any>, O3 extends ObservableInput<any>, O4 extends ObservableInput<any>, O5 extends ObservableInput<any>, O6 extends ObservableInput<any>>(v1: O1, v2: O2, v3: O3, v4: O4, v5: O5, v6: O6, scheduler?: SchedulerLike): Observable<[ObservedValueOf<O1>, ObservedValueOf<O2>, ObservedValueOf<O3>, ObservedValueOf<O4>, ObservedValueOf<O5>, ObservedValueOf<O6>]>;
+export function combineLatest<O extends Array<ObservableInput<any>>>(...sources: O): Observable<ObservableOutputs<O>>;
 
 /** @deprecated resultSelector no longer supported, pipe to map instead */
 export function combineLatest<O extends ObservableInput<any>, R>(array: O[], resultSelector: (...values: ObservedValueOf<O>[]) => R, scheduler?: SchedulerLike): Observable<R>;
