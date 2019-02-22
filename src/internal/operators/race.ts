a326efdfb844b35820a404b28499fbb646a5ba2e
Alok Kumar  Singh <aloksingh3112@gmail.com>
2019-02-22 12:31:47 +0530

    updated grammer

--- src/internal/operators/race.ts
+++ src/internal/operators/race.ts
@@ -15,7 +15,7 @@ export function race<T, R>(...observables: Array<Observable<any> | Array<Observa
 /* tslint:enable:max-line-length */
 
 /**
- * Returns an Observable that mirrors the first source Observable to emit an emits a next,
+ * Returns an Observable that mirrors the first source Observable to emit a next,
  * error or complete notification from the combination of this Observable and supplied Observables.
  * @param {...Observables} ...observables Sources used to race for which Observable emits first.
  * @return {Observable} An Observable that mirrors the output of the first Observable to emit an item.
