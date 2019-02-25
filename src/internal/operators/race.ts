8f7d7fbf0ecb13b9b9f7736674796752349cb7d2
Alok Kumar  Singh <aloksingh3112@gmail.com>
2019-02-26 03:28:28 +0530

    docs(race): Update the race operator  docs (#4584)
    * docs(race): Update the race operator  docs
    
    Updating the documentation of  race operator to replace "emits an item" with "emits a next, error or complete notification"  #4561
    
    * updated grammer

--- src/internal/operators/race.ts
+++ src/internal/operators/race.ts
@@ -15,8 +15,8 @@ export function race<T, R>(...observables: Array<Observable<any> | Array<Observa
 /* tslint:enable:max-line-length */
 
 /**
- * Returns an Observable that mirrors the first source Observable to emit an item
- * from the combination of this Observable and supplied Observables.
+ * Returns an Observable that mirrors the first source Observable to emit a next,
+ * error or complete notification from the combination of this Observable and supplied Observables.
  * @param {...Observables} ...observables Sources used to race for which Observable emits first.
  * @return {Observable} An Observable that mirrors the output of the first Observable to emit an item.
  * @method race
