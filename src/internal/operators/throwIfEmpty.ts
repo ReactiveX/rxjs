a9670151dbb11b4cdd0c8d5014c6407399467f2f
Arpad <avas@mifinity.com>
2019-02-27 16:40:22 +0100

    refactor(throwIfEmpty): re-implement throwIfEmpty

--- src/internal/operators/throwIfEmpty.ts
+++ src/internal/operators/throwIfEmpty.ts
@@ -1,6 +1,8 @@
-import { tap } from './tap';
 import { EmptyError } from '../util/EmptyError';
-import { MonoTypeOperatorFunction } from '../types';
+import { Observable } from '../Observable';
+import { Operator } from '../Operator';
+import { Subscriber } from '../Subscriber';
+import { TeardownLogic } from '../types';
 
 /**
  * If the source observable completes without emitting a value, it will emit
@@ -32,16 +34,41 @@ import { MonoTypeOperatorFunction } from '../types';
  * error to be thrown when the source observable completes without emitting a
  * value.
  */
-export const throwIfEmpty =
-  <T>(errorFactory: (() => any) = defaultErrorFactory) => tap<T>({
-    hasValue: false,
-    next() { this.hasValue = true; },
-    complete() {
-      if (!this.hasValue) {
-        throw errorFactory();
-      }
+export function throwIfEmpty <T>(errorFactory: (() => any) = defaultErrorFactory) {
+  return (source: Observable<T>) => {
+    return source.lift(new ThrowIfEmptyOperator(errorFactory));
+  };
+}
+
+class ThrowIfEmptyOperator<T> implements Operator<T, T> {
+  constructor(private errorFactory: () => any) {
+  }
+
+  call(subscriber: Subscriber<T>, source: any): TeardownLogic {
+    return source.subscribe(new ThrowIfEmptySubscriber(subscriber, this.errorFactory));
+  }
+}
+
+class ThrowIfEmptySubscriber<T> extends Subscriber<T> {
+  private hasValue: boolean = false;
+
+  constructor(destination: Subscriber<T>, private errorFactory: () => any) {
+    super(destination);
+  }
+
+  protected _next(value: T): void {
+    this.hasValue = true;
+    this.destination.next(value);
+  }
+
+  protected _complete() {
+    if (!this.hasValue) {
+        this.destination.error(this.errorFactory());
+    } else {
+        return this.destination.complete();
     }
-  } as any);
+  }
+}
 
 function defaultErrorFactory() {
   return new EmptyError();
