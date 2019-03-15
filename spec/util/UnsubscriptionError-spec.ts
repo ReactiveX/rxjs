da382648b8cc57fc293edb156bcd320266c2004b
Ben Lesh <ben@benlesh.com>
2019-03-14 18:51:46 -0700

    test(UnsubscriptionError): fix imports

--- spec/util/UnsubscriptionError-spec.ts
+++ spec/util/UnsubscriptionError-spec.ts
@@ -1,16 +1,14 @@
 import { expect } from 'chai';
-import * as Rx from 'rxjs/Rx';
-
-const { Observable, UnsubscriptionError } = Rx;
+import { UnsubscriptionError, Observable, timer } from 'rxjs';
 
 /** @test {UnsubscriptionError} */
 describe('UnsubscriptionError', () => {
   it('should create a message that is a clear indication of its internal errors', () => {
     const err1 = new Error('Swiss cheese tastes amazing but smells like socks');
     const err2 = new Error('User too big to fit in tiny European elevator');
-    const source1 = Observable.create(() => () => { throw err1; });
-    const source2 = Observable.timer(1000);
-    const source3 = Observable.create(() => () => { throw err2; });
+    const source1 = new Observable(() => () => { throw err1; });
+    const source2 = timer(1000);
+    const source3 = new Observable(() => () => { throw err2; });
     const source = source1.merge(source2, source3);
 
     const subscription = source.subscribe();
