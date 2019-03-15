a3630e1bdceeac771cbc8a9b3eb819dd315239cb
Ben Lesh <ben@benlesh.com>
2019-03-14 23:37:08 -0700

    fix(race): better typings
    - Actually accept other `ObservableInput` types (Promises, etc)
    - Fixes issue where TypeScript would complain when you tried to subscribe to a race between to Observable types.
    
    Fixes #4390
    Fixes #4642

--- spec-dtslint/observables/race-spec.ts
+++ spec-dtslint/observables/race-spec.ts
@@ -5,65 +5,116 @@ it('should infer correctly with 1 parameter', () => {
   const o = race(a); // $ExpectType Observable<number>
 });
 
-it('should infer correctly with multiple parameters of the same type', () => {
-  const a = of(1);
-  const b = of(2);
-  const o = race(a, b); // $ExpectType Observable<number>
+describe('race(a, b, c)', () => {
+  it('should infer correctly with multiple parameters of the same type', () => {
+    const a = of(1);
+    const b = of(2);
+    const o = race(a, b); // $ExpectType Observable<number>
+  });
+
+  it('should support 2 parameters with different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const o = race(a, b); // $ExpectType Observable<string | number>
+  });
+
+  it('should support 3 parameters with different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const c = of(true);
+    const o = race(a, b, c); // $ExpectType Observable<string | number | boolean>
+  });
+
+  it('should support 4 parameters with different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const c = of(true);
+    const d = of([1, 2, 3]);
+    const o = race(a, b, c, d); // $ExpectType Observable<string | number | boolean | number[]>
+  });
+
+  it('should support 5 parameters with different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const c = of(true);
+    const d = of([1, 2, 3]);
+    const e = of(['blah']);
+    const o = race(a, b, c, d, e); // $ExpectType Observable<string | number | boolean | number[] | string[]>
+  });
+
+  it('should support 6 or more parameters of the same type', () => {
+    const a = of(1);
+    const o = race(a, a, a, a, a, a, a, a, a, a, a, a, a, a); // $ExpectType Observable<number>
+  });
+
+  it('should return {} for 6 or more arguments of different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const c = of(true);
+    const d = of([1, 2, 3]);
+    const e = of(['blah']);
+    const f = of({ foo: 'bar' });
+    const o = race(a, b, c, d, e, f); // $ExpectType Observable<{}>
+  });
 });
 
-it('should support 2 parameters with different types', () => {
-  const a = of(1);
-  const b = of('a');
-  const o = race(a, b); // $ExpectType Observable<string> | Observable<number>
+describe('race([a, b, c])', () => {
+  it('should infer correctly with multiple parameters of the same type', () => {
+    const a = of(1);
+    const b = of(2);
+    const o = race([a, b]); // $ExpectType Observable<number>
+  });
+
+  it('should support 2 parameters with different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const o = race([a, b]); // $ExpectType Observable<string | number>
+  });
+
+  it('should support 3 parameters with different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const c = of(true);
+    const o = race([a, b, c]); // $ExpectType Observable<string | number | boolean>
+  });
+
+  it('should support 4 parameters with different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const c = of(true);
+    const d = of([1, 2, 3]);
+    const o = race([a, b, c, d]); // $ExpectType Observable<string | number | boolean | number[]>
+  });
+
+  it('should support 5 parameters with different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const c = of(true);
+    const d = of([1, 2, 3]);
+    const e = of(['blah']);
+    const o = race([a, b, c, d, e]); // $ExpectType Observable<string | number | boolean | number[] | string[]>
+  });
+
+  it('should support 6 or more parameters of the same type', () => {
+    const a = of(1);
+    const o = race([a, a, a, a, a, a, a, a, a, a, a, a, a, a]); // $ExpectType Observable<number>
+  });
+
+  it('should return {} for 6 or more arguments of different types', () => {
+    const a = of(1);
+    const b = of('a');
+    const c = of(true);
+    const d = of([1, 2, 3]);
+    const e = of(['blah']);
+    const f = of({ foo: 'bar' });
+    const o = race([a, b, c, d, e, f]); // $ExpectType Observable<{}>
+  });
 });
 
-it('should support 3 parameters with different types', () => {
-  const a = of(1);
-  const b = of('a');
-  const c = of(true);
-  const o = race(a, b, c); // $ExpectType Observable<string> | Observable<number> | Observable<boolean>
+it('should race observable inputs', () => {
+  const o = race(of(1), Promise.resolve('foo'), [true, false]); // $ExpectType Observable<string | number | boolean>
 });
 
-it('should support 4 parameters with different types', () => {
-  const a = of(1);
-  const b = of('a');
-  const c = of(true);
-  const d = of([1, 2, 3]);
-  const o = race(a, b, c, d); // $ExpectType Observable<string> | Observable<number> | Observable<boolean> | Observable<number[]>
-});
-
-it('should support 5 parameters with different types', () => {
-  const a = of(1);
-  const b = of('a');
-  const c = of(true);
-  const d = of([1, 2, 3]);
-  const e = of(['blah']);
-  const o = race(a, b, c, d, e); // $ExpectType Observable<string> | Observable<number> | Observable<boolean> | Observable<number[]> | Observable<string[]>
-});
-
-it('should support 6 or more parameters of the same type', () => {
-  const a = of(1);
-  const o = race(a, a, a, a, a, a, a, a, a, a, a, a, a, a); // $ExpectType Observable<number>
-});
-
-it('should return {} for 6 or more arguments of different types', () => {
-  const a = of(1);
-  const b = of('a');
-  const c = of(true);
-  const d = of([1, 2, 3]);
-  const e = of(['blah']);
-  const f = of({ foo: 'bar' });
-  const o = race(a, b, c, d, e, f); // $ExpectType Observable<{}>
-});
-
-it('should handle an array of observables', () => {
-  const a = of(1);
-  const b = of(2);
-  const o = race([a, b]); // $ExpectType Observable<number>
-});
-
-it('should return {} for array of observables of different types', () => {
-  const a = of(1);
-  const b = of('test');
-  const o = race([a, b]); // $ExpectType Observable<{}>
+it('should race an array observable inputs', () => {
+  const o = race([of(1), Promise.resolve('foo'), [true, false]]); // $ExpectType Observable<string | number | boolean>
 });
