e4c5aa98db5c621a9253019a48b04e3f334c7be3
xuemind <dxm@live.com>
2019-03-17 17:52:11 +0800

    docs(operators): updated the sample code in docs

--- src/internal/operators/switchMap.ts
+++ src/internal/operators/switchMap.ts
@@ -41,7 +41,7 @@ export function switchMap<T, R, O extends ObservableInput<any>>(project: (value:
  * import { of } from 'rxjs';
  * import { switchMap } from 'rxjs/operators';
  *
- * const switched = of(1, 2, 3).pipe(switchMap((x: number) => of( x, x ** 2, x ** 3)));
+ * const switched = of(1, 2, 3).pipe(switchMap((x: number) => of(x, x ** 2, x ** 3)));
  * switched.subscribe(x => console.log(x));
  * // outputs
  * // 1
