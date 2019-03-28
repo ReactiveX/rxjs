10650084a8256fa69bb567e735f1cc1efd226e64
Jan-Niklas W <6104311+JWO719@users.noreply.github.com>
2019-03-28 23:09:39 +0100

    docs(analytics): change analytics id (#4670)

--- docs_app/src/environments/environment.stable.ts
+++ docs_app/src/environments/environment.stable.ts
@@ -1,6 +1,6 @@
 // This is for the production site, which is hosted at https://angular.io
 export const environment = {
-  gaId: 'UA-8594346-28', // Production id
+  gaId: 'UA-36380079-2', // Production id
   production: true,
   mode: 'stable'
 };
