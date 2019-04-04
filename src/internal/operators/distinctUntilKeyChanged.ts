a80434653060447dce33f33ef9748e4b9a958d31
Lennart Zimmer <lennart.zimmer@opitz-consulting.com>
2019-04-04 15:36:56 +0200

    Revert "docs(operator): add fitting description and diagramm to distinctUntilKeyChanged"
    This reverts commit 98c23b598cf22fd8d1ffb47885617ddbbb15a5e1.

--- src/internal/operators/distinctUntilKeyChanged.ts
+++ src/internal/operators/distinctUntilKeyChanged.ts
@@ -10,14 +10,9 @@ export function distinctUntilKeyChanged<T, K extends keyof T>(key: K, compare: (
  * Returns an Observable that emits all items emitted by the source Observable that are distinct by comparison from the previous item,
  * using a property accessed by using the key provided to check if the two items are distinct.
  *
- * <span class="informal">It's like {@link distinctUntilChanged}, but the distinct comparison uses a key to access a property.</span>
+ * If a comparator function is provided, then it will be called for each item to test for whether or not that value should be emitted.
  *
- * ![](distinctUntilKeyChanged.png)
- *
- * `distinctUntilKeyChanged` emits all items of the source Observable, wich are distinct by comparison.
- * The comparison checks if the previous item is distinct from the current item, using a `key` to access a property.
- * If a comparator function is provided, then it will be called for each item with the property key
- * to test for whether or not that value should be emitted.
+ * If a comparator function is not provided, an equality check is used by default.
  *
  * ## Examples
  * An example comparing the name of persons
