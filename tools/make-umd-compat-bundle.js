de0858651194f28b00b697e47f24e6a51c7b9339
Ben Lesh <ben@benlesh.com>
2019-04-09 18:58:41 -0700

    feat(fetch): Making fetch happen
    - Adds `fromFetch` implementation that uses native `fetch`
    - Adds tests and basic documentation
    - DOES NOT polyfill fetch

--- tools/make-umd-compat-bundle.js
+++ tools/make-umd-compat-bundle.js
@@ -8,6 +8,7 @@ rollupBundle({
     'rxjs/operators': 'dist-compat/esm5_for_rollup/src/operators/index.js',
     'rxjs/webSocket': 'dist-compat/esm5_for_rollup/src/webSocket/index.js',
     'rxjs/ajax': 'dist-compat/esm5_for_rollup/src/ajax/index.js',
+    'rxjs/fetch': 'dist-compat/esm5_for_rollup/src/fetch/index.js',
     'rxjs/internal-compatibility': 'dist-compat/esm5_for_rollup/src/internal-compatibility/index.js',
     'rxjs': 'dist-compat/esm5_for_rollup/src/index.js',
   },
