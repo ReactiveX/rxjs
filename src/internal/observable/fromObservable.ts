f57e1fc287590b6e20ffe2d6b1b9685068b21cdf
Ben Lesh <ben@benlesh.com>
2019-04-22 21:29:38 -0500

    feat(scheduled): Add scheduled creation method (#4595)
    - Simplifies code around common observable creation and subscription
    - Removes `scalar` internal impl
    - Deprecates a number of APIs that accept schedulers where we would rather people use `scheduled`.

--- src/internal/observable/fromObservable.ts
+++ src/internal/observable/fromObservable.ts
@@ -1,24 +1,12 @@
 import { Observable } from '../Observable';
-import { Subscription } from '../Subscription';
-import { observable as Symbol_observable } from '../symbol/observable';
 import { subscribeToObservable } from '../util/subscribeToObservable';
-import { InteropObservable, SchedulerLike, Subscribable } from '../types';
+import { InteropObservable, SchedulerLike } from '../types';
+import { scheduleObservable } from '../scheduled/scheduleObservable';
 
-export function fromObservable<T>(input: InteropObservable<T>, scheduler: SchedulerLike) {
+export function fromObservable<T>(input: InteropObservable<T>, scheduler?: SchedulerLike) {
   if (!scheduler) {
     return new Observable<T>(subscribeToObservable(input));
   } else {
-    return new Observable<T>(subscriber => {
-      const sub = new Subscription();
-      sub.add(scheduler.schedule(() => {
-        const observable: Subscribable<T> = input[Symbol_observable]();
-        sub.add(observable.subscribe({
-          next(value) { sub.add(scheduler.schedule(() => subscriber.next(value))); },
-          error(err) { sub.add(scheduler.schedule(() => subscriber.error(err))); },
-          complete() { sub.add(scheduler.schedule(() => subscriber.complete())); },
-        }));
-      }));
-      return sub;
-    });
+    return scheduleObservable(input, scheduler);
   }
 }
