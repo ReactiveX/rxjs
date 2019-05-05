091fd0fcc63569653d7bf8f74740ff654e57f7f4
SangKa.Z <sangka23@163.com>
2019-05-05 12:40:41 +0900

    fix(mergeMap): use correctly parent subscription

--- src/internal/operators/mergeMap.ts
+++ src/internal/operators/mergeMap.ts
@@ -164,7 +164,7 @@ export class MergeMapSubscriber<T, R> extends OuterSubscriber<T, R> {
 
   notifyComplete(innerSub: Subscription): void {
     const buffer = this.buffer;
-    this.remove(innerSub);
+    this.destination.remove(innerSub);
     this.active--;
     if (buffer.length > 0) {
       this._next(buffer.shift());
