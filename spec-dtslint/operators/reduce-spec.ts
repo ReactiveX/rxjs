126d2b69bb93000968069691d108b6848632f80d
David Driscoll <david.driscoll@gmail.com>
2019-05-09 20:03:22 -0400

    fix(scan): fixed declarations to properly support different return types (#4598)
    this solves an issue where if you would reduce over `T` you can get `T` out reliably, but you could
    not get out `R` in anyway without manually typing the reduce method call

--- spec-dtslint/operators/reduce-spec.ts
+++ spec-dtslint/operators/reduce-spec.ts
@@ -15,5 +15,17 @@ it('should infer correctly for accumulator of type array', () => {
 
 it('should accept seed parameter of the same type', () => {
   const a = of(1, 2, 3).pipe(reduce((x, y, z) => x + 1, 5)); // $ExpectType Observable<number>
-  const b = of(1, 2, 3).pipe(reduce((x, y, z) => x + 1, '5')); // $ExpectError
+  const b = of(1, 2, 3).pipe(reduce((x, y, z) => x + 1, [])); // $ExpectError
+});
+
+it('should accept seed parameter of the seed array type', () => {
+  const a = of(1, 2, 3).pipe(reduce((x, y, z) => { x.push(y); return x; }, [4])); // $ExpectType Observable<number[]>
+  // Array must be typed...
+  const b = of(1, 2, 3).pipe(reduce((x, y, z) => { x.push(y); return x; }, [])); // $ExpectError
+});
+
+it('should accept seed parameter of a different type', () => {
+  const a = of(1, 2, 3).pipe(reduce((x, y, z) => x + '1', '5')); // $ExpectType Observable<string>
+  const bv: { [key: string]: string } = {};
+  const b = of(1, 2, 3).pipe(reduce((x, y, z) => ({ ...x, [y]: y.toString() }), bv)); // $ExpectType Observable<{ [key: string]: string; }>
 });
