4e9b405501869b5ffcd975dc45d4fba7987a4cd4
Charles Lyding <19598772+clydin@users.noreply.github.com>
2019-05-13 14:54:30 -0400

    fix(migrations): use latest version of package when adding compat

--- migrations/update-6_0_0/index.ts
+++ migrations/update-6_0_0/index.ts
@@ -1,31 +1,8 @@
-import { Rule, SchematicContext, Tree, SchematicsException, chain,  } from '@angular-devkit/schematics';
+import { Rule } from '@angular-devkit/schematics';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
-const rxjsCompatVersion = '^6.0.0-rc.0';
-
-export function rxjsV6MigrationSchematic(_options: any): Rule {
-  return (tree: Tree, context: SchematicContext) => {
-      const pkgPath = '/package.json';
-      const buffer = tree.read(pkgPath);
-      if (buffer == null) {
-        throw new SchematicsException('Could not read package.json');
-      }
-      const content = buffer.toString();
-      const pkg = JSON.parse(content);
-
-      if (pkg === null || typeof pkg !== 'object' || Array.isArray(pkg)) {
-        throw new SchematicsException('Error reading package.json');
-      }
-
-      if (!pkg.dependencies) {
-        pkg.dependencies = {};
-      }
-
-      pkg.dependencies['rxjs-compat'] = rxjsCompatVersion;
-
-      tree.overwrite(pkgPath, JSON.stringify(pkg, null, 2));
-      context.addTask(new NodePackageInstallTask());
-
-      return tree; // <3
+export function rxjsV6MigrationSchematic(): Rule {
+  return (_, context) => {
+      context.addTask(new NodePackageInstallTask({ packageName: 'rxjs-compat@6.5.2' }));
   };
 }
--- spec/migration/update-6_0_0/index-spec.ts
+++ spec/migration/update-6_0_0/index-spec.ts
@@ -1,4 +1,6 @@
-import { Tree } from '@angular-devkit/schematics';
+import { getSystemPath, normalize, virtualFs } from '@angular-devkit/core';
+import { TempScopedNodeJsSyncHost } from '@angular-devkit/core/node/testing';
+import { HostTree } from '@angular-devkit/schematics';
 import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 import { expect } from 'chai';
@@ -7,17 +9,27 @@ const collectionPath = path.join(__dirname, '../../../migrations/collection.json
 
 describe('Migration Schematic', () => {
   let tree: UnitTestTree;
+  let host: TempScopedNodeJsSyncHost;
 
-  beforeEach(() => {
-    tree = Tree.empty() as UnitTestTree;
-    tree.create('/package.json', `{}`);
+  beforeEach((done) => {
+    host = new TempScopedNodeJsSyncHost();
+    tree = new UnitTestTree(new HostTree(host));
+    process.chdir(getSystemPath(host.root));
+    host.write(normalize('/package.json'), virtualFs.stringToFileBuffer('{}'))
+      .toPromise()
+      .then(() => done(), err => done(err));
   });
 
-  it('adds missing dependencies', () => {
+  it('adds missing dependencies', (done) => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
     tree = runner.runSchematic('rxjs-migration-01', {}, tree);
 
-    const pkg = JSON.parse(tree.readContent('/package.json'));
-    expect(pkg.dependencies['rxjs-compat']).to.equal('^6.0.0-rc.0');
+    runner.engine.executePostTasks().toPromise()
+      .then(() => host.read(normalize('/package.json')).toPromise())
+      .then(data => {
+        const pkg = JSON.parse(Buffer.from(data).toString());
+        expect(pkg.dependencies['rxjs-compat']).to.equal('^6.5.2');
+        done();
+      }).catch(err => done(err));
   });
 });
