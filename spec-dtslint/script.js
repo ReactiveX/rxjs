36f0649b6320abdd6706975600daed53d9949eef
Nicholas Jamieson <nicholas@cartant.com>
2019-05-18 08:35:47 +1000

    chore: simplify dtslint tooling

--- spec-dtslint/script.js
+++
@@ -1,4 +0,0 @@
-const { execSync } = require('child_process');
-const path = require('path');
-const local = path.resolve('./node_modules/typescript/lib');
-execSync(`dtslint --localTs ${local} --expectOnly ./spec-dtslint`);
