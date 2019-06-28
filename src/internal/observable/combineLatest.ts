40bd738f0504983915f86d8f31d89dbffac113bf
Eddie <1300100eddiecooro@gmail.com>
2019-06-29 01:41:40 +0430

    Use project function instead of map
    "Use project function to dynamically calculate the Body-Mass Index" example in the combineLatest documentations was using "map" operator instead of project function

--- src/internal/observable/combineLatest.ts
+++ src/internal/observable/combineLatest.ts
@@ -196,9 +196,7 @@ export function combineLatest<R>(...observables: Array<ObservableInput<any> | ((
  *
  * const weight = of(70, 72, 76, 79, 75);
  * const height = of(1.76, 1.77, 1.78);
- * const bmi = combineLatest(weight, height).pipe(
- *   map(([w, h]) => w / (h * h)),
- * );
+ * const bmi = combineLatest(weight, height, (w, h) => w / (h * h))
  * bmi.subscribe(x => console.log('BMI is ' + x));
  *
  * // With output to console:
