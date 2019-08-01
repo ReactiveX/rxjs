596b0e89d9ae546e442125987361cbfb4ca9027a
Dkosasih <dkosasih@gmail.com>
2019-08-02 05:32:41 +1000

    test(dtslint): add dtslint test for merge operator (#4093) (#4392)
    * test(dtslint): add dtslint test for merge operator (#4093)
    
    * test(dtslint): change dtslint test for merge operator to use helpers (#4093)

---
+++ spec-dtslint/helpers.ts
@@ -0,0 +1,17 @@
+import { of } from 'rxjs';
+
+export class A { a = 0; }
+export class B { b = 0; }
+export class C { c = 0; }
+export class D { d = 0; }
+export class E { e = 0; }
+export class F { f = 0; }
+export class G { g = 0; }
+
+export const a = of(new A());
+export const b = of(new B());
+export const c = of(new C());
+export const d = of(new D());
+export const e = of(new E());
+export const f = of(new F());
+export const g = of(new G());
