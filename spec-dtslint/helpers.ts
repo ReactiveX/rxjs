5a537f881a52b598af08ba9449a186b29659e11f
Dkosasih <dkosasih@gmail.com>
2019-08-02 06:14:32 +1000

    test(dtslint): add dtslint test for of observable static function (#4445)
    * test(dtslint): add dtslint test for of observable static function (#4093)
    
    * test(dtslint): change of observable static function dtslint test to use helper (#4093)

--- spec-dtslint/helpers.ts
+++ spec-dtslint/helpers.ts
@@ -7,6 +7,9 @@ export class D { d = 0; }
 export class E { e = 0; }
 export class F { f = 0; }
 export class G { g = 0; }
+export class H { h = 0; }
+export class I { i = 0; }
+export class J { j = 0; }
 
 export const a = of(new A());
 export const b = of(new B());
@@ -15,3 +18,6 @@ export const d = of(new D());
 export const e = of(new E());
 export const f = of(new F());
 export const g = of(new G());
+export const h = of(new H());
+export const i = of(new I());
+export const j = of(new J());
