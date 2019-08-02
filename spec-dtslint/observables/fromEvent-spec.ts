2307864f0363054c67c204ddbb943938121060a5
Deddy Kosasih <deddy.kosasih@vline.com.au>
2019-08-02 13:59:52 +1000

    test(dtslint):  add DOM fromEvent dtslint test (#4093)

--- spec-dtslint/observables/fromEvent-spec.ts
+++ spec-dtslint/observables/fromEvent-spec.ts
@@ -11,10 +11,16 @@ const nodeStyleEventEmitter: NodeStyleEventEmitter = {
   removeListener: (eventName, handler) => nodeStyleEventEmitter
 };
 
+const div: HTMLDivElement = new HTMLDivElement();
+
 it('should infer correctly with HasEventTargetAddRemove event type', () => { 
   const a = fromEvent(hasEventTargetAddRemove, 'foo'); // $ExpectType Observable<string>
 });
 
 it('should infer correctly with NodeStyleEventEmitter event type', () => {
   const a = fromEvent(document, 'foo'); // $ExpectType Observable<Event>
+});
+
+it('should infer correctly DOM event', () => {
+  const a = fromEvent(div, 'click'); // $ExpectType Observable<Event>
 });
