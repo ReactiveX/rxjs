3c3d807dd291391f663b7614163421ce2ec04e4b
Ben Lesh <ben@benlesh.com>
2019-08-20 09:47:53 -0500

    chore: Update side-effect expectations for webSocket

--- integration/side-effects/snapshots/esm2015/websocket.js
+++ integration/side-effects/snapshots/esm2015/websocket.js
@@ -1,7 +1,1 @@
-var NotificationKind;
 
-(function(NotificationKind) {
-    NotificationKind["NEXT"] = "N";
-    NotificationKind["ERROR"] = "E";
-    NotificationKind["COMPLETE"] = "C";
-})(NotificationKind || (NotificationKind = {}));
