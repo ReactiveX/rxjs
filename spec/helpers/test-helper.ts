6a4cd689c02bbbc8c47b90e01dbfabbe9e5983ce
Ben Lesh <ben@benlesh.com>
2019-09-18 08:42:06 -0500

    feat(animationFrames): Adds an observable of animationFrames (#5021)
    * feat(animationFrames): Adds an observable of animationFrames
    
    - Also adds tests and test harness for requestAnimationFrame stubbing with sinon
    - Updates TypeScript lib to use ES2018 (so we can use `findIndex` on `Array`).
    
    * refactor: switch to a loop-per-subscription approach
    
    Now each subscription will cause a new animation frame loop to be kicked off, instead of trying to share a single animation loop.

--- spec/helpers/test-helper.ts
+++ spec/helpers/test-helper.ts
@@ -4,6 +4,7 @@ import { of, asyncScheduler, Observable, scheduled, ObservableInput } from 'rxjs
 import { root } from 'rxjs/internal/util/root';
 import { observable } from 'rxjs/internal/symbol/observable';
 import { iterator } from 'rxjs/internal/symbol/iterator';
+import * as sinon from 'sinon';
 
 export function lowerCaseO<T>(...args: Array<any>): Observable<T> {
   const o = {
@@ -47,3 +48,104 @@ export const createObservableInputs = <T>(value: T) => of(
 ) as Observable<ObservableInput<T>>;
 
 global.__root__ = root;
+
+let _raf: any;
+let _caf: any;
+let _id = 0;
+
+/**
+ * A type used to test `requestAnimationFrame`
+ */
+export interface RAFTestTools {
+  /**
+   * Synchronously fire the next scheduled animation frame
+   */
+  tick(): void;
+
+  /**
+   * Synchronously fire all scheduled animation frames
+   */
+  flush(): void;
+
+  /**
+   * Un-monkey-patch `requestAnimationFrame` and `cancelAnimationFrame`
+   */
+  restore(): void;
+}
+
+/**
+ * Monkey patches `requestAnimationFrame` and `cancelAnimationFrame`, returning a
+ * toolset to allow animation frames to be synchronously controlled.
+ *
+ * ### Usage
+ * ```ts
+ * let raf: RAFTestTools;
+ *
+ * beforeEach(() => {
+ *   // patch requestAnimationFrame
+ *   raf = stubRAF();
+ * });
+ *
+ * afterEach(() => {
+ *   // unpatch
+ *   raf.restore();
+ * });
+ *
+ * it('should fire handlers', () => {
+ *   let test = false;
+ *   // use requestAnimationFrame as normal
+ *   requestAnimationFrame(() => test = true);
+ *   // no frame has fired yet (this would be generally true anyhow)
+ *   expect(test).to.equal(false);
+ *   // manually fire the next animation frame
+ *   raf.tick();
+ *   // frame as fired
+ *   expect(test).to.equal(true);
+ *   // raf is now a SinonStub that can be asserted against
+ *   expect(requestAnimationFrame).to.have.been.calledOnce;
+ * });
+ * ```
+ */
+export function stubRAF(): RAFTestTools {
+  _raf = requestAnimationFrame;
+  _caf = cancelAnimationFrame;
+
+  const handlers: any[] = [];
+
+  (requestAnimationFrame as any) = sinon.stub().callsFake((handler: Function) => {
+    const id = _id++;
+    handlers.push({ id, handler });
+    return id;
+  });
+
+  (cancelAnimationFrame as any) = sinon.stub().callsFake((id: number) => {
+    const index = handlers.findIndex(x => x.id === id);
+    if (index >= 0) {
+      handlers.splice(index, 1);
+    }
+  });
+
+  function tick() {
+    if (handlers.length > 0) {
+      handlers.shift().handler();
+    }
+  }
+
+  function flush() {
+    while (handlers.length > 0) {
+      handlers.shift().handler();
+    }
+  }
+
+  return {
+    tick,
+    flush,
+    restore() {
+      (requestAnimationFrame as any) = _raf;
+      (cancelAnimationFrame as any) = _caf;
+      _raf = _caf = undefined;
+      handlers.length = 0;
+      _id = 0;
+    }
+  };
+}
