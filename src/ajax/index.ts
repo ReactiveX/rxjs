6d4473fa2f4f400ffdd39e89f1a8b2f06abb99c8
OJ Kwon <kwon.ohjoong@gmail.com>
2019-10-15 00:00:38 -0700

    feat(ajax): export new ajax as unstable

--- src/ajax/index.ts
+++ src/ajax/index.ts
@@ -1,2 +1,8 @@
 export { ajax } from '../internal/observable/dom/ajax';
 export { AjaxRequest, AjaxResponse, AjaxError, AjaxTimeoutError } from '../internal/observable/dom/AjaxObservable';
+
+export { Ajax as __unstableAjax, create as __unstableCreate } from '../internal/observable/unstableAjax/core/ajax';
+export * from '../internal/observable/unstableAjax/defaultOptions';
+export * from '../internal/observable/unstableAjax/adapters/xhr';
+export * from '../internal/observable/unstableAjax/Request';
+export * from '../internal/observable/unstableAjax/Response';
