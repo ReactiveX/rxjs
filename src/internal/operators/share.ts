1b2021ff9d8228d52778dd0f6bdef20d5254819c
Harry <xapu.ivanov@gmail.com>
2019-10-19 16:56:17 +0300

    docs: create example for share (#4932)
    create example usage of share operator
    
    closes #4797

--- src/internal/operators/share.ts
+++ src/internal/operators/share.ts
@@ -17,6 +17,43 @@ function shareSubjectFactory() {
  *
  * ![](share.png)
  *
+ * ## Example
+ * Generate new multicast Observable from the source Observable value
+ * ```typescript
+ * import { interval } from 'rxjs';
+ * import { share, map } from 'rxjs/operators';
+ *
+ * const source = interval(1000)
+ *   .pipe(
+ *         map((x: number) => {
+ *             console.log('Processing: ', x);
+ *             return x*x;
+ *         }),
+ *         share()
+ * );
+ *
+ * source.subscribe(x => console.log('subscription 1: ', x));
+ * source.subscribe(x => console.log('subscription 1: ', x));
+ *
+ * // Logs:
+ * // Processing:  0
+ * // subscription 1:  0
+ * // subscription 1:  0
+ * // Processing:  1
+ * // subscription 1:  1
+ * // subscription 1:  1
+ * // Processing:  2
+ * // subscription 1:  4
+ * // subscription 1:  4
+ * // Processing:  3
+ * // subscription 1:  9
+ * // subscription 1:  9
+ * // ... and so on
+ * ```
+ *
+ * @see {@link api/index/function/interval}
+ * @see {@link map}
+ *
  * @return {Observable<T>} An Observable that upon connection causes the source Observable to emit items to its Observers.
  * @method share
  * @owner Observable
