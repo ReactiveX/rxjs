d2f68db3a49079adffff52c5fbb950aa6fcb19ff
Dzhavat Ushev <dzhavatushev@gmail.com>
2019-11-17 23:35:28 +0100

    docs(empty): make examples clearer (#5148)
    * docs(empty): make examples clearer
    
    * docs(empty): remove whitespace

--- src/internal/observable/empty.ts
+++ src/internal/observable/empty.ts
@@ -11,8 +11,7 @@ export const EMPTY = new Observable<never>(subscriber => subscriber.complete());
  * Creates an Observable that emits no items to the Observer and immediately
  * emits a complete notification.
  *
- * <span class="informal">Just emits 'complete', and nothing else.
- * </span>
+ * <span class="informal">Just emits 'complete', and nothing else.</span>
  *
  * ![](empty.png)
  *
@@ -21,16 +20,22 @@ export const EMPTY = new Observable<never>(subscriber => subscriber.complete());
  * Observables, such as in a {@link mergeMap}.
  *
  * ## Examples
+ *
  * ### Emit the number 7, then complete
+ *
  * ```ts
  * import { empty } from 'rxjs';
  * import { startWith } from 'rxjs/operators';
  *
  * const result = empty().pipe(startWith(7));
  * result.subscribe(x => console.log(x));
+ *
+ * // Outputs
+ * // 7
  * ```
  *
  * ### Map and flatten only odd numbers to the sequence 'a', 'b', 'c'
+ *
  * ```ts
  * import { empty, interval, of } from 'rxjs';
  * import { mergeMap } from 'rxjs/operators';
@@ -42,10 +47,10 @@ export const EMPTY = new Observable<never>(subscriber => subscriber.complete());
  * result.subscribe(x => console.log(x));
  *
  * // Results in the following to the console:
- * // x is equal to the count on the interval eg(0,1,2,3,...)
+ * // x is equal to the count on the interval, e.g. (0, 1, 2, 3, ...)
  * // x will occur every 1000ms
- * // if x % 2 is equal to 1 print abc
- * // if x % 2 is not equal to 1 nothing will be output
+ * // if x % 2 is equal to 1, print a, b, c (each on its own)
+ * // if x % 2 is not equal to 1, nothing will be output
  * ```
  *
  * @see {@link Observable}
