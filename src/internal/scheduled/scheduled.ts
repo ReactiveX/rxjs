653a77f6e7c750648ac203f22ccf09c5a8bfd02d
Dzhavat Ushev <dzhavatushev@gmail.com>
2019-11-17 22:12:45 +0100

    docs(scheduled): add links to see also section

--- src/internal/scheduled/scheduled.ts
+++ src/internal/scheduled/scheduled.ts
@@ -13,8 +13,8 @@ import { Observable } from '../Observable';
  * Converts from a common {@link ObservableInput} type to an observable where subscription and emissions
  * are scheduled on the provided scheduler.
  *
- * @see from
- * @see of
+ * @see {@link from}
+ * @see {@link of}
  *
  * @param input The observable, array, promise, iterable, etc you would like to schedule
  * @param scheduler The scheduler to use to schedule the subscription and emissions from
