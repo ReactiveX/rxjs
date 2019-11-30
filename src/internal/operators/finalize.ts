abbc40000ff3a1ee6ac655c7898981fa3995b277
Dávid Paksy <PDavid@users.noreply.github.com>
2019-11-30 23:03:02 +0100

    docs(finalize): examples, improvement #4578 (#5067)
    * docs(angular): migrate app from angular 6 to 8(#4687) (#4688)
    
    * docs(operator): add missing marble diagramms
    
    * Revert "docs(operator): add marbel diagramm to distinctUntilChanged"
    
    This reverts commit 6bf3aae3c5d7a801f876550f93f84c55262653d3.
    
    * Revert "docs(operator): add fitting description and diagramm to distinctUntilKeyChanged"
    
    This reverts commit 98c23b598cf22fd8d1ffb47885617ddbbb15a5e1.
    
    * Revert "docs(operator): add missing marble diagramms"
    
    This reverts commit b61acad8514d9eb36188b1c517416e4b8546cbaf.
    
    * docs(angular): update angular/ codelyzer/ typescript version
    
    * docs(angular): fix minor errors after migration
    
    * chore(eslint): update version
    
    * docs(app): upgrade to angular 8
    
    * docs(migration): fix wrong polyfill imports
    
    * docs(finalize): improved #4578
    
    - Added the fact that finalize will also call its callback if the subscriber explicitly unsubscribes.
    - Added example for finalize basic usage and when  subscriber explicitly unsubscribes.
    
    * docs(finalize): implemented requested changes #4578
    
    - Removed unneeded comment (with the numbers).
    - Changed example console output to be on separate lines.
    
    * docs(finalize): implemented requested changes #4578
    
    Do not pass ``noop`` to error callback because it will swallow the error and this could be a bad example for newcomers.
    
    * docs(finalize): Revert accidentally pushed commit
    
    "docs(angular): migrate app from angular 6 to 8(#4687) (#4688)"
    
    This reverts commit c440b91f

--- src/internal/operators/finalize.ts
+++ src/internal/operators/finalize.ts
@@ -7,6 +7,54 @@ import { MonoTypeOperatorFunction, TeardownLogic } from '../types';
 /**
  * Returns an Observable that mirrors the source Observable, but will call a specified function when
  * the source terminates on complete or error.
+ * The specified function will also be called when the subscriber explicitly unsubscribes.
+ *
+ * ## Examples
+ * Execute callback function when the observable completes
+ *
+ * ```ts
+ * import { interval } from 'rxjs';
+ * import { take, finalize } from 'rxjs/operators';
+ *
+ * // emit value in sequence every 1 second
+ * const source = interval(1000);
+ * const example = source.pipe(
+ *   take(5), //take only the first 5 values
+ *   finalize(() => console.log('Sequence complete')) // Execute when the observable completes
+ * )
+ * const subscribe = example.subscribe(val => console.log(val));
+ *
+ * // results:
+ * //   0
+ * //   1
+ * //   2
+ * //   3
+ * //   4
+ * //   'Sequence complete'
+ * ```
+ *
+ * Execute callback function when the subscriber explicitly unsubscribes
+ *
+ * ```ts
+ * import { interval, timer, noop } from 'rxjs';
+ * import { finalize, tap } from 'rxjs/operators';
+ *
+ * const source = interval(100).pipe(
+ *   finalize(() => console.log('[finalize] Called')),
+ *    tap(() => console.log('[next] Called'),
+ *      () => console.log('[error] Not called'),
+ *      () => console.log('[tap] Not called')),
+ * );
+ *
+ * const sub = source.subscribe(x => console.log(x), noop, () => console.log('[complete] Not called'));
+ *
+ * timer(150).subscribe(() => sub.unsubscribe());
+ *
+ * // results:
+ * //   0
+ * //   '[finalize] Called'
+ * ```
+ *
  * @param {function} callback Function to be called when source terminates.
  * @return {Observable} An Observable that mirrors the source, but will call the specified function on termination.
  * @method finally
