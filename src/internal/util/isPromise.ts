01cce4958e3ff711236321b782e6ea713f370ec8
danranVm <danranvm@gmail.com>
2019-12-01 17:43:09 +0800

    feat(isPromise): fix breaking change

--- src/internal/util/isPromise.ts
+++ src/internal/util/isPromise.ts
@@ -3,6 +3,6 @@
  * @see {@link https://www.ecma-international.org/ecma-262/6.0/#sec-promise-objects}
  * @param value the object to test
  */
-export function isPromise<T>(value: any): value is PromiseLike<T> {
+export function isPromise<T = any>(value: any): value is PromiseLike<T> {
   return !!value && typeof value.subscribe !== 'function' && typeof value.then === 'function';
 }
