daa49a10e75d45f589b5c06700634196d249e3e9
danranVm <danranvm@gmail.com>
2019-12-01 19:45:08 +0800

    feat(isPromise): remove generic

--- src/internal/util/isPromise.ts
+++ src/internal/util/isPromise.ts
@@ -3,6 +3,6 @@
  * @see {@link https://www.ecma-international.org/ecma-262/6.0/#sec-promise-objects}
  * @param value the object to test
  */
-export function isPromise<T = any>(value: any): value is PromiseLike<T> {
+export function isPromise(value: any): value is PromiseLike<any> {
   return !!value && typeof value.subscribe !== 'function' && typeof value.then === 'function';
 }
