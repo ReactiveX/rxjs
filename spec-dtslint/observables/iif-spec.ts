dcd0993f74e37cfba6854281778c894d717c0094
Nicholas Jamieson <nicholas@cartant.com>
2019-12-06 07:07:10 +1000

    chore: fix dtslint expectations
    Closes #4494

--- spec-dtslint/observables/iif-spec.ts
+++ spec-dtslint/observables/iif-spec.ts
@@ -1,15 +1,11 @@
 import { iif, of } from 'rxjs';
 
 it('should accept function as first parameter', () => {
-  // Typescript limitation: cannot infer default value in params
-  // e.g.: const a = iif(() => false, EMPTY, EMPTY) // $ExpectType Observable<never>
-  // However, test below does not infer Observable<never> if no params is passed
-  // although the default value for second and third param is EMPTY observable
-  const a = iif(() => false); // $ExpectType Observable<{}>
+  const a = iif(() => false); // $ExpectType Observable<never>
 });
 
 it('should infer correctly with 2 parameters', () => {
-  const a = iif(() => false, of(1)); // $ExpectType Observable<number | {}>
+  const a = iif(() => false, of(1)); // $ExpectType Observable<number>
 });
 
 it('should infer correctly with 3 parameters', () => {
