743c00bc520d7890e2782616f35503d57ae12441
Ben Lesh <ben@benlesh.com>
2019-12-15 12:16:25 -0600

    fix(asapScheduler): resolved memory leak
    Registered handlers would sometimes leak in memory, this resolves that issue and adds a test.
    
    Related #5016

--- src/internal/util/Immediate.ts
+++ src/internal/util/Immediate.ts
@@ -1,23 +1,41 @@
 let nextHandle = 1;
+const RESOLVED = (() => Promise.resolve())();
+const activeHandles: { [key: number]: any } = {};
 
-const tasksByHandle: { [handle: string]: () => void } = {};
-
-function runIfPresent(handle: number) {
-  const cb = tasksByHandle[handle];
-  if (cb) {
-    cb();
+/**
+ * Finds the handle in the list of active handles, and removes it.
+ * Returns `true` if found, `false` otherwise. Used both to clear
+ * Immediate scheduled tasks, and to identify if a task should be scheduled.
+ */
+function findAndClearHandle(handle: number): boolean {
+  if (handle in activeHandles) {
+    delete activeHandles[handle];
+    return true;
   }
+  return false;
 }
 
+/**
+ * Helper functions to schedule and unschedule microtasks.
+ */
 export const Immediate = {
   setImmediate(cb: () => void): number {
     const handle = nextHandle++;
-    tasksByHandle[handle] = cb;
-    Promise.resolve().then(() => runIfPresent(handle));
+    activeHandles[handle] = true;
+    RESOLVED.then(() => findAndClearHandle(handle) && cb());
     return handle;
   },
 
   clearImmediate(handle: number): void {
-    delete tasksByHandle[handle];
+    findAndClearHandle(handle);
   },
+};
+
+/**
+ * Used for internal testing purposes only. Do not export from library.
+ */
+export const TestTools = {
+  pending() {
+    return Object.keys(activeHandles).length;
+  }
 };
