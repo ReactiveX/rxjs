e03e83308680d1f30960c13651ccd45ad87eef21
Ivan Zimin <iwonz@yandex.ru>
2020-01-22 03:08:21 +0300

    Removing an unused variable (#5235)

--- src/internal/observable/fromEvent.ts
+++ src/internal/observable/fromEvent.ts
@@ -4,8 +4,6 @@ import { isFunction } from '../util/isFunction';
 import { Subscriber } from '../Subscriber';
 import { map } from '../operators/map';
 
-const toString: Function = (() => Object.prototype.toString)();
-
 export interface NodeStyleEventEmitter {
   addListener: (eventName: string | symbol, handler: NodeEventHandler) => this;
   removeListener: (eventName: string | symbol, handler: NodeEventHandler) => this;
