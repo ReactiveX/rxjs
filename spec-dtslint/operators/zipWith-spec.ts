8482ea6fa7bd890f60aa76f8751a98829bc3dafd
Nicholas Jamieson <nicholas@cartant.com>
2020-01-23 14:39:35 +1000

    fix: use non-union n-args zipWith sig

---
+++ spec-dtslint/operators/zipWith-spec.ts
@@ -0,0 +1,57 @@
+import { of } from 'rxjs';
+import { zipWith } from 'rxjs/operators';
+
+describe('zipWith', () => {
+  describe('without project parameter', () => {
+    it('should infer correctly with 1 param', () => {
+      const a = of(1, 2, 3);
+      const b = of('a', 'b', 'c');
+      const res = a.pipe(zipWith(b)); // $ExpectType Observable<[number, string]>
+    });
+
+    it('should infer correctly with 2 params', () => {
+      const a = of(1, 2, 3);
+      const b = of('a', 'b', 'c');
+      const c = of('d', 'e', 'f');
+      const res = a.pipe(zipWith(b, c)); // $ExpectType Observable<[number, string, string]>
+    });
+
+    it('should infer correctly with 3 params', () => {
+      const a = of(1, 2, 3);
+      const b = of('a', 'b', 'c');
+      const c = of('d', 'e', 'f');
+      const d = of('g', 'h', 'i');
+      const res = a.pipe(zipWith(b, c, d)); // $ExpectType Observable<[number, string, string, string]>
+    });
+
+    it('should infer correctly with 4 params', () => {
+      const a = of(1, 2, 3);
+      const b = of('a', 'b', 'c');
+      const c = of('d', 'e', 'f');
+      const d = of('g', 'h', 'i');
+      const e = of('j', 'k', 'l');
+      const res = a.pipe(zipWith(b, c, d, e)); // $ExpectType Observable<[number, string, string, string, string]>
+    });
+
+    it('should infer correctly with 5 params', () => {
+      const a = of(1, 2, 3);
+      const b = of('a', 'b', 'c');
+      const c = of('d', 'e', 'f');
+      const d = of('g', 'h', 'i');
+      const e = of('j', 'k', 'l');
+      const f = of('m', 'n', 'o');
+      const res = a.pipe(zipWith(b, c, d, e, f)); // $ExpectType Observable<[number, string, string, string, string, string]>
+    });
+
+    it('should accept N params', () => {
+      const a = of(1, 2, 3);
+      const b = of('a', 'b', 'c');
+      const c = of('d', 'e', 'f');
+      const d = of('g', 'h', 'i');
+      const e = of('j', 'k', 'l');
+      const f = of('m', 'n', 'o');
+      const g = of('p', 'q', 'r');
+      const res = a.pipe(zipWith(b, c, d, e, f, g)); // $ExpectType Observable<[number, string, string, string, string, string, string]>
+    });
+  });
+});
