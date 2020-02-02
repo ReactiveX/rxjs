d1cb3ed7bfbb1406e42dd2af1eb37fd09bbe3707
Michael Hladky <michael@hladky.at>
2020-02-02 18:47:54 +0100

    Expose the `isPromise` utility function to the public API
    This could help people that deal with converting asynchronous primitives to observables.
    In such cases, the `isObservable` function is used a lot and the check for a `Promise has to get implemented manually.
    
    Exposing `isPromise`  to the public API can help a lot and come in very handy.

--- src/index.ts
+++ src/index.ts
@@ -32,6 +32,7 @@ export { pipe } from './internal/util/pipe';
 export { noop } from './internal/util/noop';
 export { identity } from './internal/util/identity';
 export { isObservable } from './internal/util/isObservable';
+export { isPromise } from './internal/util/isPromise';
 
 /* Error types */
 export { ArgumentOutOfRangeError } from './internal/util/ArgumentOutOfRangeError';
