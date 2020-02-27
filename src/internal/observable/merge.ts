b6ac3fce7a1629776fa23b17a86948d209754d8e
Brian Donovan <1938+eventualbuddha@users.noreply.github.com>
2020-02-27 13:38:39 -0800

    doc(typo): use the correct form of "its" (#5328)
    "it's" means "it is", and "its" is the proper form for possession.

--- src/internal/observable/merge.ts
+++ src/internal/observable/merge.ts
@@ -97,9 +97,9 @@ export function merge<T, R>(...observables: (ObservableInput<any> | SchedulerLik
  * // - First timer1 and timer2 will run concurrently
  * // - timer1 will emit a value every 1000ms for 10 iterations
  * // - timer2 will emit a value every 2000ms for 6 iterations
- * // - after timer1 hits it's max iteration, timer2 will
+ * // - after timer1 hits its max iteration, timer2 will
  * //   continue, and timer3 will start to run concurrently with timer2
- * // - when timer2 hits it's max iteration it terminates, and
+ * // - when timer2 hits its max iteration it terminates, and
  * //   timer3 will continue to emit a value every 500ms until it is complete
  * ```
  *
