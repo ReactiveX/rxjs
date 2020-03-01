d239e66f641fd81a688a0736b20cafbf29f9a5de
Nicholas Jamieson <nicholas@cartant.com>
2020-03-01 16:10:24 +1000

    test: add subscribeWith tests

---
+++ spec/util/subscribeWith-spec.ts
@@ -0,0 +1,20 @@
+import { expect } from 'chai';
+import { of, Subscriber, observable } from 'rxjs';
+import { subscribeWith } from 'rxjs/internal/util/subscribeWith';
+import { asInteropObservable, asInteropSubscriber } from '../helpers/interop-helper';
+
+describe('subscribeWith', () => {
+  it('should return the subscriber for interop observables', () => {
+    const observable = asInteropObservable(of(42));
+    const subscriber = new Subscriber<number>();
+    const subscription = subscribeWith(observable, subscriber);
+    expect(subscription).to.equal(subscriber);
+  });
+
+  it('should return the subscriber for interop subscribers', () => {
+    const observable = of(42);
+    const subscriber = asInteropSubscriber(new Subscriber<number>());
+    const subscription = subscribeWith(observable, subscriber);
+    expect(subscription).to.equal(subscriber);
+  });
+});
