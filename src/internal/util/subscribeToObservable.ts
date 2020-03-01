95b1fc18d2336492dbb1adb27489eb633409cdd5
Nicholas Jamieson <nicholas@cartant.com>
2020-03-01 16:01:16 +1000

    refactor: add subscribeWith

--- src/internal/util/subscribeToObservable.ts
+++ src/internal/util/subscribeToObservable.ts
@@ -1,5 +1,6 @@
 import { Subscriber } from '../Subscriber';
 import { observable as Symbol_observable } from '../symbol/observable';
+import { subscribeWith } from './subscribeWith';
 
 /**
  * Subscribes to an object that implements Symbol.observable with the given
@@ -12,16 +13,6 @@ export const subscribeToObservable = <T>(obj: any) => (subscriber: Subscriber<T>
     // Should be caught by observable subscribe function error handling.
     throw new TypeError('Provided object does not correctly implement Symbol.observable');
   } else {
-    const subscription = obs.subscribe(subscriber);
-    // With observables internal to this package, the returned subscription
-    // will be the subscriber that was passed to the subscribe call. Ensure
-    // that the interop behaviour is the same - that the passed-in subscriber
-    // is returned from this function - and if the received subscription is not
-    // the subscriber that was passed in, add it to ensure that unsubscription
-    // is correctly chained.
-    if (subscription !== subscriber) {
-      subscriber.add(subscription);
-    }
-    return subscriber;
+    return subscribeWith(obs, subscriber);
   }
 };
