0f8c1c600e030b3c144b6e824642fdcecef205c5
Nicholas Jamieson <nicholas@cartant.com>
2020-03-01 12:24:37 +1000

    refactor(subscribeToObservable): #5051 fix

--- src/internal/operators/exhaustMap.ts
+++ src/internal/operators/exhaustMap.ts
@@ -124,13 +124,7 @@ class ExhaustMapSubscriber<T, R> extends OuterSubscriber<T, R> {
     const innerSubscriber = new InnerSubscriber(this, value, index);
     const destination = this.destination as Subscription;
     destination.add(innerSubscriber);
-    const innerSubscription = subscribeToResult<T, R>(this, result, undefined, undefined, innerSubscriber);
-    // The returned subscription will usually be the subscriber that was
-    // passed. However, interop subscribers will be wrapped and for
-    // unsubscriptions to chain correctly, the wrapper needs to be added, too.
-    if (innerSubscription !== innerSubscriber) {
-      destination.add(innerSubscription);
-    }
+    subscribeToResult<T, R>(this, result, undefined, undefined, innerSubscriber);
   }
 
   protected _complete(): void {
--- src/internal/util/subscribeToObservable.ts
+++ src/internal/util/subscribeToObservable.ts
@@ -12,6 +12,16 @@ export const subscribeToObservable = <T>(obj: any) => (subscriber: Subscriber<T>
     // Should be caught by observable subscribe function error handling.
     throw new TypeError('Provided object does not correctly implement Symbol.observable');
   } else {
-    return obs.subscribe(subscriber);
+    const subscription = obs.subscribe(subscriber);
+    // With observables internal to this package, the returned subscription
+    // will be the subscriber that was passed to the subscribe call. Ensure
+    // that the interop behaviour is the same - that the passed-in subscriber
+    // is returned from this function - and if the received subscription is not
+    // the subscriber that was passed in, add it to ensure that unsubscription
+    // is correctly chained.
+    if (subscription !== subscriber) {
+      subscriber.add(subscription);
+    }
+    return subscriber;
   }
 };
