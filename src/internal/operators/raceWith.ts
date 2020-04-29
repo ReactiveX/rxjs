ca7f370d8379f22526cfb17d40deff53e1358742
Nicholas Jamieson <nicholas@cartant.com>
2020-04-30 01:45:55 +1000

    feat: add raceWith (#5303)

--- src/internal/operators/raceWith.ts
+++ src/internal/operators/raceWith.ts
@@ -1,16 +1,16 @@
 import { Observable } from '../Observable';
 import { isArray } from '../util/isArray';
-import { MonoTypeOperatorFunction, OperatorFunction } from '../types';
+import { MonoTypeOperatorFunction, OperatorFunction, ObservableInput, ObservedValueUnionFromArray } from '../types';
 import { race as raceStatic } from '../observable/race';
 
 /* tslint:disable:max-line-length */
-/** @deprecated Deprecated in favor of static race. */
+/** @deprecated Deprecated use {@link raceWith} */
 export function race<T>(observables: Array<Observable<T>>): MonoTypeOperatorFunction<T>;
-/** @deprecated Deprecated in favor of static race. */
+/** @deprecated Deprecated use {@link raceWith} */
 export function race<T, R>(observables: Array<Observable<T>>): OperatorFunction<T, R>;
-/** @deprecated Deprecated in favor of static race. */
+/** @deprecated Deprecated use {@link raceWith} */
 export function race<T>(...observables: Array<Observable<T> | Array<Observable<T>>>): MonoTypeOperatorFunction<T>;
-/** @deprecated Deprecated in favor of static race. */
+/** @deprecated Deprecated use {@link raceWith} */
 export function race<T, R>(...observables: Array<Observable<any> | Array<Observable<any>>>): OperatorFunction<T, R>;
 /* tslint:enable:max-line-length */
 
@@ -19,8 +19,7 @@ export function race<T, R>(...observables: Array<Observable<any> | Array<Observa
  * error or complete notification from the combination of this Observable and supplied Observables.
  * @param {...Observables} ...observables Sources used to race for which Observable emits first.
  * @return {Observable} An Observable that mirrors the output of the first Observable to emit an item.
- * @name race
- * @deprecated Deprecated in favor of static {@link race}.
+ * @deprecated Deprecated use {@link raceWith}
  */
 export function race<T>(...observables: (Observable<T> | Observable<T>[])[]): MonoTypeOperatorFunction<T> {
   return function raceOperatorFunction(source: Observable<T>) {
@@ -34,5 +33,44 @@ export function race<T>(...observables: (Observable<T> | Observable<T>[])[]): Mo
       raceStatic(source, ...(observables as Observable<T>[])),
       undefined
     ) as Observable<T>;
+  };
+}
+
+/**
+ * Creates an Observable that mirrors the first source Observable to emit a next,
+ * error or complete notification from the combination of the Observable to which
+ * the operator is applied and supplied Observables.
+ *
+ * ## Example
+ *
+ * ```ts
+ * import { interval } from 'rxjs';
+ * import { mapTo, raceWith } from 'rxjs/operators';
+ *
+ * const obs1 = interval(1000).pipe(mapTo('fast one'));
+ * const obs2 = interval(3000).pipe(mapTo('medium one'));
+ * const obs3 = interval(5000).pipe(mapTo('slow one'));
+ *
+ * obs2.pipe(
+ *   raceWith(obs3, obs1)
+ * ).subscribe(
+ *   winner => console.log(winner)
+ * );
+ *
+ * // Outputs
+ * // a series of 'fast one'
+ * ```
+ *
+ * @param otherSources Sources used to race for which Observable emits first.
+ */
+
+export function raceWith<T, A extends ObservableInput<any>[]>(
+  ...otherSources: A
+): OperatorFunction<T, T | ObservedValueUnionFromArray<A>> {
+  return function raceWithOperatorFunction(source: Observable<T>) {
+    return source.lift.call(
+      raceStatic(source, ...otherSources),
+      undefined
+    ) as Observable<T | ObservedValueUnionFromArray<A>>;
   };
 }
