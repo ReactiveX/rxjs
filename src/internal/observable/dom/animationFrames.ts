6451d252974af9fdd24c4064256cb9eef84cc2b3
Thomas Mair <thomas.mair86@gmail.com>
2020-05-16 15:31:24 +0200

    feat(animationFrames): provide timestamp and elapsed time

--- src/internal/observable/dom/animationFrames.ts
+++ src/internal/observable/dom/animationFrames.ts
@@ -7,8 +7,8 @@ import { requestAnimationFrameProvider } from '../../scheduler/requestAnimationF
 /**
  * An observable of animation frames
  *
- * Emits the the amount of time elapsed since subscription on each animation frame. Defaults to elapsed
- * milliseconds. Does not end on its own.
+ * Emits the the amount of time elapsed since subscription and the timestamp on each animation frame.
+ * Defaults to milliseconds provided to the requestAnimationFrame's callback. Does not end on its own.
  *
  * Every subscription will start a separate animation loop. Since animation frames are always scheduled
  * by the browser to occur directly before a repaint, scheduling more than one animation frame synchronously
@@ -31,7 +31,7 @@ import { requestAnimationFrameProvider } from '../../scheduler/requestAnimationF
  *   const diff = end - start;
  *   return animationFrames().pipe(
  *     // Figure out what percentage of time has passed
- *     map(elapsed => elapsed / duration),
+ *     map(({elapsed}) => elapsed / duration),
  *     // Take the vector while less than 100%
  *     takeWhile(v => v < 1),
  *     // Finish with 100%
@@ -71,7 +71,7 @@ import { requestAnimationFrameProvider } from '../../scheduler/requestAnimationF
  * const source$ = animationFrames(customTSProvider);
  *
  * // Log increasing numbers 0...1...2... on every animation frame.
- * source$.subscribe(x => console.log(x));
+ * source$.subscribe(({elapsed}) => console.log(elapsed));
  * ```
  *
  * @param timestampProvider An object with a `now` method that provides a numeric timestamp
@@ -84,13 +84,17 @@ export function animationFrames(timestampProvider?: TimestampProvider) {
  * Does the work of creating the observable for `animationFrames`.
  * @param timestampProvider The timestamp provider to use to create the observable
  */
-function animationFramesFactory(timestampProvider: TimestampProvider) {
+function animationFramesFactory(timestampProvider?: TimestampProvider) {
   const { schedule } = requestAnimationFrameProvider;
-  return new Observable<number>(subscriber => {
-    const start = timestampProvider.now();
+  return new Observable<{timestamp: number, elapsed: number}>(subscriber => {
     let subscription: Subscription;
-    const run = () => {
-      subscriber.next(timestampProvider.now() - start);
+    let start: DOMHighResTimeStamp | number | null = null;
+    const run = (timestamp: DOMHighResTimeStamp | number) => {
+      const currentTimestamp = timestampProvider ? timestampProvider.now() : timestamp;
+      if (start === null) {
+        start = currentTimestamp;
+      }
+      subscriber.next({timestamp: currentTimestamp, elapsed: currentTimestamp - start});
       if (!subscriber.closed) {
         subscription = schedule(run);
       }
