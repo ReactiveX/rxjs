27ed3359983eb01a2b941571a6e9ffecd56bb327
Nicholas Jamieson <nicholas@cartant.com>
2020-05-18 11:54:26 +1000

    chore: remove T from MapTo signatures

--- src/internal/operators/concatMapTo.ts
+++ src/internal/operators/concatMapTo.ts
@@ -2,7 +2,7 @@ import { concatMap } from './concatMap';
 import { ObservableInput, OperatorFunction, ObservedValueOf } from '../types';
 
 /* tslint:disable:max-line-length */
-export function concatMapTo<T, O extends ObservableInput<any>>(observable: O): OperatorFunction<T, ObservedValueOf<O>>;
+export function concatMapTo<O extends ObservableInput<any>>(observable: O): OperatorFunction<any, ObservedValueOf<O>>;
 /** @deprecated */
 export function concatMapTo<O extends ObservableInput<any>>(observable: O, resultSelector: undefined): OperatorFunction<any, ObservedValueOf<O>>;
 /** @deprecated */
