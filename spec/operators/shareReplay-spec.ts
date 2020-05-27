e1d8da9af13ee26e4a0e34cbde3c246d97fef560
Josep M Sobrepere <josepot@gmail.com>
2020-05-27 09:02:43 +0200

    test(shareReplay): don't restart when refCount if the source has completed (#5457)
    * test(shareReplay): don't restart when refCount if the source has completed
    
    * test(shareReplay): improve tests by checking on source subscriptions

--- spec/operators/shareReplay-spec.ts
+++ spec/operators/shareReplay-spec.ts
@@ -175,6 +175,7 @@ describe('shareReplay operator', () => {
 
   it('should not restart due to unsubscriptions if refCount is false', () => {
     const source = cold('a-b-c-d-e-f-g-h-i-j');
+    const sourceSubs =  '^------------------';
     const sub1 =        '^------!';
     const expected1 =   'a-b-c-d-';
     const sub2 =        '-----------^-------';
@@ -184,10 +185,14 @@ describe('shareReplay operator', () => {
 
     expectObservable(shared, sub1).toBe(expected1);
     expectObservable(shared, sub2).toBe(expected2);
+    expectSubscriptions(source.subscriptions).toBe(sourceSubs);
   });
 
   it('should restart due to unsubscriptions if refCount is true', () => {
+    const sourceSubs = [];
     const source = cold('a-b-c-d-e-f-g-h-i-j');
+    sourceSubs.push(    '^------!----------------------');
+    sourceSubs.push(    '-----------^------------------');
     const sub1 =        '^------!';
     const expected1 =   'a-b-c-d-';
     const sub2 =        '-----------^------------------';
@@ -197,10 +202,27 @@ describe('shareReplay operator', () => {
 
     expectObservable(shared, sub1).toBe(expected1);
     expectObservable(shared, sub2).toBe(expected2);
+    expectSubscriptions(source.subscriptions).toBe(sourceSubs);
+  });
+
+  it('should not restart due to unsubscriptions if refCount is true when the source has completed', () => {
+    const source = cold('a-(b|)         ');
+    const sourceSubs =  '^-!            ';
+    const sub1 =        '^------!       ';
+    const expected1 =   'a-(b|)         ';
+    const sub2 =        '-----------^!  ';
+    const expected2 =   '-----------(b|)';
+
+    const shared = source.pipe(shareReplay({ bufferSize: 1, refCount: true }));
+
+    expectObservable(shared, sub1).toBe(expected1);
+    expectObservable(shared, sub2).toBe(expected2);
+    expectSubscriptions(source.subscriptions).toBe(sourceSubs);
   });
 
   it('should default to refCount being false', () => {
     const source = cold('a-b-c-d-e-f-g-h-i-j');
+    const sourceSubs =  '^------------------';
     const sub1 =        '^------!';
     const expected1 =   'a-b-c-d-';
     const sub2 =        '-----------^-------';
@@ -210,6 +232,7 @@ describe('shareReplay operator', () => {
 
     expectObservable(shared, sub1).toBe(expected1);
     expectObservable(shared, sub2).toBe(expected2);
+    expectSubscriptions(source.subscriptions).toBe(sourceSubs);
   });
 
   it('should not break lift() composability', (done: MochaDone) => {
