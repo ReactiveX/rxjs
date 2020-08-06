40b32d8fadeb1c0ef752fa4281aff20fa3a2c18b
Nicholas Jamieson <nicholas@cartant.com>
2020-08-07 00:32:13 +1000

    test(mergeAll): add test with multiple types (#5631)

--- spec-dtslint/operators/mergeAll-spec.ts
+++ spec-dtslint/operators/mergeAll-spec.ts
@@ -1,10 +1,15 @@
 import { of } from 'rxjs';
 import { mergeAll } from 'rxjs/operators';
+import { a$, b$ } from '../helpers';
 
-it('should infer correctly', () => {
-  const o = of(of(1, 2, 3)).pipe(mergeAll()); // $ExpectType Observable<number>
+it('should infer correctly with sources of same type', () => {
+  const o = of(a$, a$).pipe(mergeAll()); // $ExpectType Observable<A>
+});
+
+it('should infer correctly with sources of different types', () => {
+  const o = of(a$, b$).pipe(mergeAll()); // $ExpectType Observable<A | B>
 });
 
 it('should enforce types', () => {
-  const o = of(1, 2, 3).pipe(mergeAll()); // $ExpectError
+  const o = a$.pipe(mergeAll()); // $ExpectError
 });
