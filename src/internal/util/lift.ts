c056a409a073659d945c3fea9dda6e4bec4480e0
Ben Lesh <ben@benlesh.com>
2020-09-23 10:11:09 -0500

    refactor: use optional chaining

--- src/internal/util/lift.ts
+++ src/internal/util/lift.ts
@@ -7,7 +7,7 @@ import { OperatorFunction, TeardownLogic } from '../types';
  * Used to determine if an object is an Observable with a lift function.
  */
 export function hasLift(source: any): source is { lift: InstanceType<typeof Observable>['lift'] } {
-  return source && typeof source.lift === 'function';
+  return typeof source?.lift === 'function';
 }
 
 /**
