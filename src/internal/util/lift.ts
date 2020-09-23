19c5fbbc3136740c82884b820015952fed4e4b7a
Ben Lesh <ben@benlesh.com>
2020-09-23 10:17:40 -0500

    refactor(operate): Tweak init return type so Subscribers and Subscriptions can no longer be returned
    This will help force us to make sure we are using the subscriber and subscription chaining in the most efficient way possible. Although it could result in anti-patterns where users return a function that calls unsubscribe on a subscription if we release this to the public.

--- src/internal/util/lift.ts
+++ src/internal/util/lift.ts
@@ -1,7 +1,7 @@
 /** @prettier */
 import { Observable } from '../Observable';
 import { Subscriber } from '../Subscriber';
-import { OperatorFunction, TeardownLogic } from '../types';
+import { OperatorFunction } from '../types';
 
 /**
  * Used to determine if an object is an Observable with a lift function.
@@ -14,7 +14,9 @@ export function hasLift(source: any): source is { lift: InstanceType<typeof Obse
  * Creates an `OperatorFunction`. Used to define operators throughout the library in a concise way.
  * @param init The logic to connect the liftedSource to the subscriber at the moment of subscription.
  */
-export function operate<T, R>(init: (liftedSource: Observable<T>, subscriber: Subscriber<R>) => TeardownLogic): OperatorFunction<T, R> {
+export function operate<T, R>(
+  init: (liftedSource: Observable<T>, subscriber: Subscriber<R>) => (() => void) | void
+): OperatorFunction<T, R> {
   return (source: Observable<T>) => {
     if (hasLift(source)) {
       return source.lift(function (this: Subscriber<R>, liftedSource: Observable<T>) {
