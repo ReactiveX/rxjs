425a4c07d7a5d515170eadf7e2b52a3582243ff7
Ben Lesh <ben@benlesh.com>
2020-09-24 14:13:41 -0500

    fix(from): objects that are thennable that happen to have a subscribe method will no longer error.

--- src/internal/util/isPromise.ts
+++ src/internal/util/isPromise.ts
@@ -1,8 +1,9 @@
+import { isFunction } from "./isFunction";
+
 /**
- * Tests to see if the object is an ES2015 (ES6) Promise
- * @see {@link https://www.ecma-international.org/ecma-262/6.0/#sec-promise-objects}
+ * Tests to see if the object is "thennable".
  * @param value the object to test
  */
 export function isPromise(value: any): value is PromiseLike<any> {
-  return !!value && typeof value.subscribe !== 'function' && typeof value.then === 'function';
+  return isFunction(value?.then);
 }
