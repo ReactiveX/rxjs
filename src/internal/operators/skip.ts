e4e59dbe4766cc9c1131478eeae0314dcea29c90
Hardik Pithva <hardikpthv@gmail.com>
2020-09-30 16:41:41 +0200

    docs(skip): add description and example for the operator (#5773)
    * docs(skip): add description and example for the operator
    
    * docs(skip): improve description

--- src/internal/operators/skip.ts
+++ src/internal/operators/skip.ts
@@ -7,6 +7,28 @@ import { OperatorSubscriber } from './OperatorSubscriber';
  * Returns an Observable that skips the first `count` items emitted by the source Observable.
  *
  * ![](skip.png)
+ * 
+ * Skips the values until the sent notifications are equal or less than provided skip count. It raises 
+ * an error if skip count is equal or more than the actual number of emits and source raises an error.
+ * 
+ * ## Example
+ * Skip the values before the emission
+ * ```ts
+ * import { interval } from 'rxjs';
+ * import { skip } from 'rxjs/operators';
+ * 
+ * //emit every half second
+ * const source = interval(500);
+ * //skip the first 10 emitted values
+ * const example = source.pipe(skip(10));
+ * //output: 10...11...12...13........
+ * const subscribe = example.subscribe(val => console.log(val));
+ * ```
+ * 
+ * @see {@link last}
+ * @see {@link skipWhile}
+ * @see {@link skipUntil}
+ * @see {@link skipLast}
  *
  * @param {Number} count - The number of times, items emitted by source Observable should be skipped.
  * @return {Observable} An Observable that skips values emitted by the source Observable.
