66dc56d6520ec2e3948cdd6f5e7c5512aa0e96de
Ben Lesh <ben@benlesh.com>
2020-10-13 18:12:16 -0500

    refactor(mergeAll): fix typings

--- src/internal/operators/mergeAll.ts
+++ src/internal/operators/mergeAll.ts
@@ -1,6 +1,6 @@
 import { mergeMap } from './mergeMap';
 import { identity } from '../util/identity';
-import { OperatorFunction, ObservableInput } from '../types';
+import { OperatorFunction, ObservableInput, ObservedValueOf } from '../types';
 
 /**
  * Converts a higher-order Observable into a first-order Observable which
@@ -58,6 +58,6 @@ import { OperatorFunction, ObservableInput } from '../types';
  * @return {Observable} An Observable that emits values coming from all the
  * inner Observables emitted by the source Observable.
  */
-export function mergeAll<T>(concurrent: number = Infinity): OperatorFunction<ObservableInput<T>, T> {
+export function mergeAll<O extends ObservableInput<any>>(concurrent: number = Infinity): OperatorFunction<O, ObservedValueOf<O>> {
   return mergeMap(identity, concurrent);
 }
