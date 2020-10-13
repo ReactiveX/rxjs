57c95ebc40186c4a46a5e335d6119dea9770b478
Ben Lesh <ben@benlesh.com>
2020-10-13 15:50:42 -0500

    Update spec/operators/defaultIfEmpty-spec.ts

--- spec/operators/defaultIfEmpty-spec.ts
+++ spec/operators/defaultIfEmpty-spec.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { observableMatcher } from '../helpers/observableMatcher';
 import { of, Observable } from 'rxjs';
 import { defaultIfEmpty, mergeMap, take } from 'rxjs/operators';
-import { TestScheduler } from 'rxjs/internal/testing/TestScheduler';
+import { TestScheduler } from 'rxjs/testing';
 
 /** @test {defaultIfEmpty} */
 describe('defaultIfEmpty', () => {
