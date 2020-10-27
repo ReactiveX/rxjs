a19727cb0539a82ff341d9a12112820a3bcc18e4
Nicholas Jamieson <nicholas@cartant.com>
2020-10-28 08:13:22 +1000

    chore: update api_guardian

--- api_guard/dist/types/index.d.ts
+++ api_guard/dist/types/index.d.ts
@@ -107,8 +107,8 @@ export interface CompletionObserver<T> {
     next?: (value: T) => void;
 }
 
-export declare function concat<T extends readonly unknown[]>(...inputsAndScheduler: [...ObservableInputTuple<T>, SchedulerLike]): Observable<T[number]>;
 export declare function concat<T extends readonly unknown[]>(...inputs: [...ObservableInputTuple<T>]): Observable<T[number]>;
+export declare function concat<T extends readonly unknown[]>(...inputsAndScheduler: [...ObservableInputTuple<T>, SchedulerLike]): Observable<T[number]>;
 
 export declare const config: {
     onUnhandledError: ((err: any) => void) | null;
