248a1a78aafe909a07040fa951f4519f44a390f1
Nicholas Jamieson <nicholas@cartant.com>
2020-11-09 10:24:31 +1000

    chore: fix type error exposed by TS 4.1

--- src/internal/util/deferred.ts
+++ src/internal/util/deferred.ts
@@ -1,5 +1,5 @@
 export class Deferred<T> {
-  resolve: (value?: T | PromiseLike<T> | undefined) => void = null!;
+  resolve: (value: T | PromiseLike<T>) => void = null!;
   reject: (reason?: any) => void = null!;
   promise = new Promise<T>((a, b) => {
     this.resolve = a;
