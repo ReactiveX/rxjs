facdc5278d7d1aa54edd4ad51e9c25a7559577a5
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2020-12-09 10:02:30 +0100

    chore: update findIndex tests to run mode (#5921)

--- spec/operators/findIndex-spec.ts
+++ spec/operators/findIndex-spec.ts
@@ -1,176 +1,207 @@
+/** @prettier */
 import { expect } from 'chai';
-import { findIndex, mergeMap, delay, take } from 'rxjs/operators';
+import { findIndex, mergeMap, delay } from 'rxjs/operators';
 import { TestScheduler } from 'rxjs/testing';
-import { hot, cold, expectObservable, expectSubscriptions } from '../helpers/marble-testing';
+import { observableMatcher } from '../helpers/observableMatcher';
 import { of, Observable } from 'rxjs';
 
-declare const rxTestScheduler: TestScheduler;
-
 /** @test {findIndex} */
-describe('findIndex operator', () => {
+describe('findIndex', () => {
+  let testScheduler: TestScheduler;
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler(observableMatcher);
+  });
+
   function truePredicate(x: any) {
     return true;
   }
 
   it('should return matching element from source emits single element', () => {
-    const values = {a: 3, b: 9, c: 15, d: 20};
-    const source = hot('---a--b--c--d---|', values);
-    const subs =       '^        !       ';
-    const expected =   '---------(x|)    ';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const values = { a: 3, b: 9, c: 15, d: 20 };
+      const e1 = hot('  ---a--b--c--d---|', values);
+      const e1subs = '  ^--------!       ';
+      const expected = '---------(x|)    ';
 
-    const predicate = function (x: number) { return x % 5 === 0; };
+      const predicate = function (x: number) {
+        return x % 5 === 0;
+      };
 
-    expectObservable(source.pipe(findIndex(predicate))).toBe(expected, { x: 2 });
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(e1.pipe(findIndex(predicate))).toBe(expected, { x: 2 });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should not emit if source does not emit', () => {
-    const source = hot('-');
-    const subs =       '^';
-    const expected =   '-';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  -');
+      const e1subs = '  ^';
+      const expected = '-';
 
-    expectObservable(source.pipe(findIndex(truePredicate))).toBe(expected);
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(e1.pipe(findIndex(truePredicate))).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should return negative index if source is empty to match predicate', () => {
-    const source = cold('|');
-    const subs =        '(^!)';
-    const expected =    '(x|)';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' |   ');
+      const e1subs = '  (^!)';
+      const expected = '(x|)';
 
-    const result = source.pipe(findIndex(truePredicate));
+      const result = e1.pipe(findIndex(truePredicate));
 
-    expectObservable(result).toBe(expected, {x: -1});
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(result).toBe(expected, { x: -1 });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should return index of element from source emits single element', () => {
-    const sourceValue = 1;
-    const source = hot('--a--|', { a: sourceValue });
-    const subs =       '^ !   ';
-    const expected =   '--(x|)';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --a--|', { a: 1 });
+      const e1subs = '  ^-!   ';
+      const expected = '--(x|)';
 
-    const predicate = function (value: number) {
-      return value === sourceValue;
-    };
+      const predicate = function (value: number) {
+        return value === 1;
+      };
 
-    expectObservable(source.pipe(findIndex(predicate))).toBe(expected, { x: 0 });
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(e1.pipe(findIndex(predicate))).toBe(expected, { x: 0 });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should return index of matching element from source emits multiple elements', () => {
-    const source = hot('--a--b---c-|', { b: 7 });
-    const subs =       '^    !';
-    const expected =   '-----(x|)';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --a--b---c-|', { b: 7 });
+      const e1subs = '  ^----!      ';
+      const expected = '-----(x|)   ';
 
-    const predicate = function (value: number) {
-      return value === 7;
-    };
+      const predicate = function (value: number) {
+        return value === 7;
+      };
 
-    expectObservable(source.pipe(findIndex(predicate))).toBe(expected, { x: 1 });
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(e1.pipe(findIndex(predicate))).toBe(expected, { x: 1 });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should work with a custom thisArg', () => {
-    const sourceValues = { b: 7 };
-    const source = hot('--a--b---c-|', sourceValues);
-    const subs =       '^    !';
-    const expected =   '-----(x|)';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const sourceValues = { b: 7 };
+      const e1 = hot('  --a--b---c-|', sourceValues);
+      const e1subs = '  ^----!      ';
+      const expected = '-----(x|)   ';
 
-    const predicate = function (this: typeof sourceValues, value: number) {
-      return value === this.b;
-    };
-    const result = source.pipe(findIndex(predicate, sourceValues));
+      const predicate = function (this: typeof sourceValues, value: number) {
+        return value === this.b;
+      };
+      const result = e1.pipe(findIndex(predicate, sourceValues));
 
-    expectObservable(result).toBe(expected, { x: 1 });
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(result).toBe(expected, { x: 1 });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should return negative index if element does not match with predicate', () => {
-    const source = hot('--a--b--c--|');
-    const subs =       '^          !';
-    const expected =   '-----------(x|)';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --a--b--c--|   ');
+      const e1subs = '  ^----------!   ';
+      const expected = '-----------(x|)';
 
-    const predicate = function (value: string) {
-      return value === 'z';
-    };
+      const predicate = function (value: string) {
+        return value === 'z';
+      };
 
-    expectObservable(source.pipe(findIndex(predicate))).toBe(expected, { x: -1 });
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(e1.pipe(findIndex(predicate))).toBe(expected, { x: -1 });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should allow unsubscribing early and explicitly', () => {
-    const source = hot('--a--b--c--|');
-    const subs =       '^     !     ';
-    const expected =   '-------     ';
-    const unsub =      '      !     ';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --a--b--c--|');
+      const e1subs = '  ^-----!     ';
+      const expected = '-------     ';
+      const unsub = '   ------!     ';
 
-    const result = source.pipe(findIndex((value: string) => value === 'z'));
+      const result = e1.pipe(findIndex((value: string) => value === 'z'));
 
-    expectObservable(result, unsub).toBe(expected);
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(result, unsub).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should not break unsubscription chains when result is unsubscribed explicitly', () => {
-    const source = hot('--a--b--c--|');
-    const subs =       '^     !     ';
-    const expected =   '-------     ';
-    const unsub =      '      !     ';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --a--b--c--|');
+      const e1subs = '  ^-----!     ';
+      const expected = '-------     ';
+      const unsub = '   ------!     ';
 
-    const result = source.pipe(
-      mergeMap((x: string) => of(x)),
-      findIndex((value: string) => value === 'z'),
-      mergeMap((x: number) => of(x))
-    );
+      const result = e1.pipe(
+        mergeMap((x: string) => of(x)),
+        findIndex((value: string) => value === 'z'),
+        mergeMap((x: number) => of(x))
+      );
 
-    expectObservable(result, unsub).toBe(expected);
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(result, unsub).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should unsubscribe when the predicate is matched', () => {
-    const source = hot('--a--b---c-|');
-    const subs =       '^    !';
-    const expected =   '-------(x|)';
-    const t =               '--|';
+    testScheduler.run(({ hot, time, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --a--b---c-|');
+      const e1subs = '  ^----!      ';
+      const t = time('    --|       ');
+      //                     --|
+      const expected = '-------(x|) ';
 
-    const duration = rxTestScheduler.createTime(t);
+      const result = e1.pipe(
+        findIndex((value: string) => value === 'b'),
+        delay(t)
+      );
 
-    expectObservable(source.pipe(
-      findIndex((value: string) => value === 'b'),
-      delay(duration, rxTestScheduler)
-    )).toBe(expected, { x: 1 });
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(result).toBe(expected, { x: 1 });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should raise if source raise error while element does not match with predicate', () => {
-    const source = hot('--a--b--#');
-    const subs =       '^       !';
-    const expected =   '--------#';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --a--b--#');
+      const e1subs = '  ^-------!';
+      const expected = '--------#';
 
-    const predicate = function (value: string) {
-      return value === 'z';
-    };
+      const predicate = function (value: string) {
+        return value === 'z';
+      };
 
-    expectObservable(source.pipe(findIndex(predicate))).toBe(expected);
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(e1.pipe(findIndex(predicate))).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should raise error if predicate throws error', () => {
-    const source = hot('--a--b--c--|');
-    const subs =       '^ !';
-    const expected =   '--#';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --a--b--c--|');
+      const e1subs = '  ^-!         ';
+      const expected = '--#         ';
 
-    const predicate = function (value: string) {
-      throw 'error';
-    };
+      const predicate = function (value: string) {
+        throw 'error';
+      };
 
-    expectObservable(source.pipe(findIndex(predicate))).toBe(expected);
-    expectSubscriptions(source.subscriptions).toBe(subs);
+      expectObservable(e1.pipe(findIndex(predicate))).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should stop listening to a synchronous observable when unsubscribed', () => {
     const sideEffects: number[] = [];
-    const synchronousObservable = new Observable<number>(subscriber => {
+    const synchronousObservable = new Observable<number>((subscriber) => {
       // This will check to see if the subscriber was closed on each loop
       // when the unsubscribe hits, it should be closed
       for (let i = 0; !subscriber.closed && i < 10; i++) {
@@ -179,9 +210,9 @@ describe('findIndex operator', () => {
       }
     });
 
-    synchronousObservable.pipe(
-      findIndex(value => value === 2),
-    ).subscribe(() => { /* noop */ });
+    synchronousObservable.pipe(findIndex((value) => value === 2)).subscribe(() => {
+      /* noop */
+    });
 
     expect(sideEffects).to.deep.equal([0, 1, 2]);
   });
