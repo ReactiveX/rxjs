0d78d21643d9d9ce8b1778696b23cb17a2bc2879
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2021-01-16 23:37:18 +0100

    chore: update map tests to run mode (#5956)

--- spec/operators/map-spec.ts
+++ spec/operators/map-spec.ts
@@ -1,252 +1,323 @@
+/** @prettier */
 import { expect } from 'chai';
 import { map, tap, mergeMap, take } from 'rxjs/operators';
-import { hot, cold, expectObservable, expectSubscriptions } from '../helpers/marble-testing';
+import { TestScheduler } from 'rxjs/testing';
 import { of, Observable, identity } from 'rxjs';
+import { observableMatcher } from '../helpers/observableMatcher';
 
 // function shortcuts
-const addDrama = function (x: number | string) { return x + '!'; };
+const addDrama = (x: number | string) => x + '!';
 
 /** @test {map} */
-describe('map operator', () => {
+describe('map', () => {
+  let testScheduler: TestScheduler;
+
+  beforeEach(() => {
+    testScheduler = new TestScheduler(observableMatcher);
+  });
+
   it('should map multiple values', () => {
-    const a =   cold('--1--2--3--|');
-    const asubs =    '^          !';
-    const expected = '--x--y--z--|';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' --1--2--3--|');
+      const e1subs = '  ^----------!';
+      const expected = '--x--y--z--|';
 
-    const r = a.pipe(map(function (x) { return 10 * (+x); }));
+      const result = e1.pipe(map((x) => 10 * +x));
 
-    expectObservable(r).toBe(expected, {x: 10, y: 20, z: 30});
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, { x: 10, y: 20, z: 30 });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should map one value', () => {
-    const a =   cold('--x--|', {x: 42});
-    const asubs =    '^    !';
-    const expected = '--y--|';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const values = { x: 42 };
+      const e1 = cold(' --x--|', values);
+      const e1subs = '  ^----!';
+      const expected = '--y--|';
 
-    const r = a.pipe(map(addDrama));
+      const result = e1.pipe(map(addDrama));
 
-    expectObservable(r).toBe(expected, {y: '42!'});
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, { y: '42!' });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should map multiple values', () => {
-    const a =   cold('--1--2--3--|');
-    const asubs =    '^          !';
-    const expected = '--x--y--z--|';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' --1--2--3--|');
+      const e1subs = '  ^----------!';
+      const expected = '--x--y--z--|';
 
-    const r = a.pipe(map(addDrama));
+      const result = e1.pipe(map(addDrama));
 
-    expectObservable(r).toBe(expected, {x: '1!', y: '2!', z: '3!'});
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, { x: '1!', y: '2!', z: '3!' });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should propagate errors from map function', () => {
-    const a =   cold('--x--|', {x: 42});
-    const asubs =    '^ !   ';
-    const expected = '--#   ';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const values = { x: 42 };
+      const e1 = cold(' --x--|', values);
+      const e1subs = '  ^-!   ';
+      const expected = '--#   ';
 
-    const r = a.pipe(map((x: any) => {
-      throw 'too bad';
-    }));
+      const result = e1.pipe(
+        map((x: any) => {
+          throw 'too bad';
+        })
+      );
 
-    expectObservable(r).toBe(expected, null, 'too bad');
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, null, 'too bad');
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should propagate errors from observable that emits only errors', () => {
-    const a =   cold('#');
-    const asubs =    '(^!)';
-    const expected = '#';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' #   ');
+      const e1subs = '  (^!)';
+      const expected = '#   ';
 
-    const r = a.pipe(map(identity));
-    expectObservable(r).toBe(expected);
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      const result = e1.pipe(map(identity));
+
+      expectObservable(result).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should propagate errors from observable that emit values', () => {
-    const a =   cold('--a--b--#', {a: 1, b: 2}, 'too bad');
-    const asubs =    '^       !';
-    const expected = '--x--y--#';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const values = { a: 1, b: 2 };
+      const e1 = cold(' --a--b--#', values, 'too bad');
+      const e1subs = '  ^-------!';
+      const expected = '--x--y--#';
 
-    const r = a.pipe(map(addDrama));
-    expectObservable(r).toBe(expected, {x: '1!', y: '2!'}, 'too bad');
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      const result = e1.pipe(map(addDrama));
+
+      expectObservable(result).toBe(expected, { x: '1!', y: '2!' }, 'too bad');
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should not map an empty observable', () => {
-    const a =   cold('|');
-    const asubs =    '(^!)';
-    const expected = '|';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' |   ');
+      const e1subs = '  (^!)';
+      const expected = '|   ';
 
-    let invoked = 0;
-    const r = a.pipe(
-      map((x: any) => { invoked++; return x; }),
-      tap(null, null, () => {
-        expect(invoked).to.equal(0);
-      })
-    );
+      let invoked = 0;
+      const result = e1.pipe(
+        map((x: any) => {
+          invoked++;
+          return x;
+        }),
+        tap({
+          complete() {
+            expect(invoked).to.equal(0);
+          },
+        })
+      );
 
-    expectObservable(r).toBe(expected);
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should allow unsubscribing explicitly and early', () => {
-    const a =   cold('--1--2--3--|');
-    const unsub =    '      !     ';
-    const asubs =    '^     !     ';
-    const expected = '--x--y-     ';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' --1--2--3--|');
+      const e1subs = '  ^-----!     ';
+      const expected = '--x--y-     ';
+      const unsub = '   ------!     ';
 
-    const r = a.pipe(map(addDrama));
+      const result = e1.pipe(map(addDrama));
 
-    expectObservable(r, unsub).toBe(expected, {x: '1!', y: '2!'});
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result, unsub).toBe(expected, { x: '1!', y: '2!' });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should map with index', () => {
-    const a = hot('-5-^-4--3---2----1--|');
-    const asubs =    '^                !';
-    const expected = '--a--b---c----d--|';
-    const values = {a: 5, b: 14, c: 23, d: 32};
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('-5-^-4--3---2----1--|');
+      const e1subs = '   ^----------------!';
+      const expected = ' --a--b---c----d--|';
+      const values = { a: 5, b: 14, c: 23, d: 32 };
 
-    let invoked = 0;
-    const r = a.pipe(
-      map((x: string, index: number) => {
-        invoked++;
-        return (parseInt(x) + 1) + (index * 10);
-      }),
-      tap(null, null, () => {
-        expect(invoked).to.equal(4);
-      })
-    );
+      let invoked = 0;
+      const result = e1.pipe(
+        map((x: string, index: number) => {
+          invoked++;
+          return parseInt(x) + 1 + index * 10;
+        }),
+        tap({
+          complete() {
+            expect(invoked).to.equal(4);
+          },
+        })
+      );
 
-    expectObservable(r).toBe(expected, values);
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, values);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should map with index until completed', () => {
-    const a = hot('-5-^-4--3---2----1--|');
-    const asubs =    '^                !';
-    const expected = '--a--b---c----d--|';
-    const values = {a: 5, b: 14, c: 23, d: 32};
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('-5-^-4--3---2----1--|');
+      const e1subs = '   ^----------------!';
+      const expected = ' --a--b---c----d--|';
+      const values = { a: 5, b: 14, c: 23, d: 32 };
 
-    let invoked = 0;
-    const r = a.pipe(
-      map((x: string, index: number) => {
-        invoked++;
-        return (parseInt(x) + 1) + (index * 10);
-      }),
-      tap(null, null, () => {
-        expect(invoked).to.equal(4);
-      })
-    );
+      let invoked = 0;
+      const result = e1.pipe(
+        map((x: string, index: number) => {
+          invoked++;
+          return parseInt(x) + 1 + index * 10;
+        }),
+        tap({
+          complete() {
+            expect(invoked).to.equal(4);
+          },
+        })
+      );
 
-    expectObservable(r).toBe(expected, values);
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, values);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should map with index until an error occurs', () => {
-    const a = hot('-5-^-4--3---2----1--#', undefined, 'too bad');
-    const asubs =    '^                !';
-    const expected = '--a--b---c----d--#';
-    const values = {a: 5, b: 14, c: 23, d: 32};
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('-5-^-4--3---2----1--#', undefined, 'too bad');
+      const e1subs = '   ^----------------!';
+      const expected = ' --a--b---c----d--#';
+      const values = { a: 5, b: 14, c: 23, d: 32 };
 
-    let invoked = 0;
-    const r = a.pipe(
-      map((x: string, index: number) => {
-        invoked++;
-        return (parseInt(x) + 1) + (index * 10);
-      }),
-      tap(null, null, () => {
-        expect(invoked).to.equal(4);
-      })
-    );
+      let invoked = 0;
+      const result = e1.pipe(
+        map((x: string, index: number) => {
+          invoked++;
+          return parseInt(x) + 1 + index * 10;
+        }),
+        tap({
+          error() {
+            expect(invoked).to.equal(4);
+          },
+        })
+      );
 
-    expectObservable(r).toBe(expected, values, 'too bad');
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, values, 'too bad');
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should map using a custom thisArg', () => {
-    const a = hot('-5-^-4--3---2----1--|');
-    const asubs =    '^                !';
-    const expected = '--a--b---c----d--|';
-    const values = {a: 5, b: 14, c: 23, d: 32};
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('-5-^-4--3---2----1--|');
+      const e1subs = '   ^----------------!';
+      const expected = ' --a--b---c----d--|';
+      const values = { a: 46, b: 55, c: 64, d: 73 };
 
-    const foo = {
-      value: 42
-    };
-    const r = a
-      .pipe(map(function (this: typeof foo, x: string, index: number) {
-        expect(this).to.equal(foo);
-        return (parseInt(x) + 1) + (index * 10);
-      }, foo));
+      const foo = {
+        value: 42,
+      };
+      const result = e1.pipe(
+        map(function (this: typeof foo, x: string, index: number) {
+          expect(this).to.equal(foo);
+          return parseInt(x) + foo.value + index * 10;
+        }, foo)
+      );
 
-    expectObservable(r).toBe(expected, values);
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, values);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should map twice', () => {
-    const a = hot('-0----1-^-2---3--4-5--6--7-8-|');
-    const asubs =         '^                    !';
-    const expected =      '--a---b--c-d--e--f-g-|';
-    const values = {a: 2, b: 3, c: 4, d: 5, e: 6, f: 7, g: 8};
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('-0----1-^-2---3--4-5--6--7-8-|');
+      const e1subs = '        ^--------------------!';
+      const expected = '      --a---b--c-d--e--f-g-|';
+      const values = { a: 2, b: 3, c: 4, d: 5, e: 6, f: 7, g: 8 };
 
-    let invoked1 = 0;
-    let invoked2 = 0;
-    const r = a.pipe(
-      map((x: string) => { invoked1++; return parseInt(x) * 2; }),
-      map((x: number) => { invoked2++; return x / 2; }),
-      tap(null, null, () => {
-        expect(invoked1).to.equal(7);
-        expect(invoked2).to.equal(7);
-      })
-    );
+      let invoked1 = 0;
+      let invoked2 = 0;
+      const result = e1.pipe(
+        map((x: string) => {
+          invoked1++;
+          return parseInt(x) * 2;
+        }),
+        map((x: number) => {
+          invoked2++;
+          return x / 2;
+        }),
+        tap({
+          complete() {
+            expect(invoked1).to.equal(7);
+            expect(invoked2).to.equal(7);
+          },
+        })
+      );
 
-    expectObservable(r).toBe(expected, values);
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, values);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should do multiple maps using a custom thisArg', () => {
-    const a =    hot('--1--2--3--4--|');
-    const asubs =    '^             !';
-    const expected = '--a--b--c--d--|';
-    const values = {a: 11, b: 14, c: 17, d: 20};
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --1--2--3--4--|');
+      const e1subs = '  ^-------------!';
+      const expected = '--a--b--c--d--|';
+      const values = { a: 11, b: 14, c: 17, d: 20 };
 
-    class Filterer {
-      selector1 = (x: string) => parseInt(x) + 2;
-      selector2 = (x: string) => parseInt(x) * 3;
-    }
-    const filterer = new Filterer();
+      class Filterer {
+        selector1 = (x: string) => parseInt(x) + 2;
+        selector2 = (x: string) => parseInt(x) * 3;
+      }
+      const filterer = new Filterer();
 
-    const r = a.pipe(
-      map(function (this: any, x) { return this.selector1(x); }, filterer),
-      map(function (this: any, x) { return this.selector2(x); }, filterer),
-      map(function (this: any, x) { return this.selector1(x); }, filterer)
-    );
+      const result = e1.pipe(
+        map(function (this: any, x) {
+          return this.selector1(x);
+        }, filterer),
+        map(function (this: any, x) {
+          return this.selector2(x);
+        }, filterer),
+        map(function (this: any, x) {
+          return this.selector1(x);
+        }, filterer)
+      );
 
-    expectObservable(r).toBe(expected, values);
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result).toBe(expected, values);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should not break unsubscription chain when unsubscribed explicitly', () => {
-    const a =   cold('--1--2--3--|');
-    const unsub =    '      !     ';
-    const asubs =    '^     !     ';
-    const expected = '--x--y-     ';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' --1--2--3--|');
+      const e1subs = '  ^-----!     ';
+      const expected = '--x--y-     ';
+      const unsub = '   ------!     ';
 
-    const r = a.pipe(
-      mergeMap((x: string) => of(x)),
-      map(addDrama),
-      mergeMap((x: string) => of(x))
-    );
+      const result = e1.pipe(
+        mergeMap((x: string) => of(x)),
+        map(addDrama),
+        mergeMap((x: string) => of(x))
+      );
 
-    expectObservable(r, unsub).toBe(expected, {x: '1!', y: '2!'});
-    expectSubscriptions(a.subscriptions).toBe(asubs);
+      expectObservable(result, unsub).toBe(expected, { x: '1!', y: '2!' });
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should stop listening to a synchronous observable when unsubscribed', () => {
     const sideEffects: number[] = [];
-    const synchronousObservable = new Observable<number>(subscriber => {
+    const synchronousObservable = new Observable<number>((subscriber) => {
       // This will check to see if the subscriber was closed on each loop
       // when the unsubscribe hits (from the `take`), it should be closed
       for (let i = 0; !subscriber.closed && i < 10; i++) {
@@ -255,10 +326,9 @@ describe('map operator', () => {
       }
     });
 
-    synchronousObservable.pipe(
-      map(value => value),
-      take(3),
-    ).subscribe(() => { /* noop */ });
+    synchronousObservable.pipe(map(identity), take(3)).subscribe(() => {
+      /* noop */
+    });
 
     expect(sideEffects).to.deep.equal([0, 1, 2]);
   });
