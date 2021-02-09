d7e508f17653d92bf17d4a5e67b838ad21b4bea7
Ben Lesh <ben@benlesh.com>
2021-02-09 00:25:06 -0600

    chore: remove cruft

--- src/internal/util/deferred.ts
+++
@@ -1,8 +0,0 @@
-export class Deferred<T> {
-  resolve: (value: T | PromiseLike<T>) => void = null!;
-  reject: (reason?: any) => void = null!;
-  promise = new Promise<T>((a, b) => {
-    this.resolve = a;
-    this.reject = b;
-  });
-}
