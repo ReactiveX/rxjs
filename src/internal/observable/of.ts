0f2ffe801c2f453ed569015c0e816efe8bc3aa53
Nicholas Jamieson <nicholas@cartant.com>
2021-03-05 22:25:56 +1000

    refactor: use n-args for of scheduler signatures

--- src/internal/observable/of.ts
+++ src/internal/observable/of.ts
@@ -4,64 +4,18 @@ import { Observable } from '../Observable';
 import { scheduleArray } from '../scheduled/scheduleArray';
 import { popScheduler } from '../util/args';
 
+// Devs are more likely to pass null or undefined than they are a scheduler
+// without accompanying values. To make things easier for (naughty) devs who
+// use the `strictNullChecks: false` TypeScript compiler option, these
+// overloads with explicit null and undefined values are included.
+
+export function of(value: null): Observable<null>;
+export function of(value: undefined): Observable<undefined>;
+
 /** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
 export function of(scheduler: SchedulerLike): Observable<never>;
 /** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
-export function of<T>(a: T, scheduler: SchedulerLike): Observable<T>;
-/** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
-export function of<T, T2>(a: T, b: T2, scheduler: SchedulerLike): Observable<T | T2>;
-/** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
-export function of<T, T2, T3>(a: T, b: T2, c: T3, scheduler: SchedulerLike): Observable<T | T2 | T3>;
-/** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
-export function of<T, T2, T3, T4>(a: T, b: T2, c: T3, d: T4, scheduler: SchedulerLike): Observable<T | T2 | T3 | T4>;
-/** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
-export function of<T, T2, T3, T4, T5>(a: T, b: T2, c: T3, d: T4, e: T5, scheduler: SchedulerLike): Observable<T | T2 | T3 | T4 | T5>;
-/** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
-export function of<T, T2, T3, T4, T5, T6>(
-  a: T,
-  b: T2,
-  c: T3,
-  d: T4,
-  e: T5,
-  f: T6,
-  scheduler: SchedulerLike
-): Observable<T | T2 | T3 | T4 | T5 | T6>;
-/** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
-export function of<T, T2, T3, T4, T5, T6, T7>(
-  a: T,
-  b: T2,
-  c: T3,
-  d: T4,
-  e: T5,
-  f: T6,
-  g: T7,
-  scheduler: SchedulerLike
-): Observable<T | T2 | T3 | T4 | T5 | T6 | T7>;
-/** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
-export function of<T, T2, T3, T4, T5, T6, T7, T8>(
-  a: T,
-  b: T2,
-  c: T3,
-  d: T4,
-  e: T5,
-  f: T6,
-  g: T7,
-  h: T8,
-  scheduler: SchedulerLike
-): Observable<T | T2 | T3 | T4 | T5 | T6 | T7 | T8>;
-/** @deprecated The scheduler argument is deprecated, use scheduled. Details: https://rxjs.dev/deprecations/scheduler-argument */
-export function of<T, T2, T3, T4, T5, T6, T7, T8, T9>(
-  a: T,
-  b: T2,
-  c: T3,
-  d: T4,
-  e: T5,
-  f: T6,
-  g: T7,
-  h: T8,
-  i: T9,
-  scheduler: SchedulerLike
-): Observable<T | T2 | T3 | T4 | T5 | T6 | T7 | T8 | T9>;
+export function of<A extends readonly unknown[]>(...args: [...A, SchedulerLike]): Observable<ValueFromArray<A>>;
 
 export function of(): Observable<never>;
 /** @deprecated remove in v8. Do not use generic arguments directly, allow inference or cast with `as` */
