817ab03669dcc26af1b2149a5b4768d55f2c62e7
Nicholas Jamieson <nicholas@cartant.com>
2021-03-10 18:41:50 +1000

    refactor: use n-args for zip op

--- src/internal/operators/zip.ts
+++ src/internal/operators/zip.ts
@@ -1,77 +1,20 @@
 import { zip as zipStatic } from '../observable/zip';
-import { ObservableInput, OperatorFunction } from '../types';
+import { ObservableInput, ObservableInputTuple, OperatorFunction, Cons } from '../types';
 import { operate } from '../util/lift';
 
-/* tslint:disable:max-line-length */
 /** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, R>(project: (v1: T) => R): OperatorFunction<T, R>;
+export function zip<T, A extends readonly unknown[]>(otherInputs: [...ObservableInputTuple<A>]): OperatorFunction<T, Cons<T, A>>;
 /** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2, R>(v2: ObservableInput<T2>, project: (v1: T, v2: T2) => R): OperatorFunction<T, R>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2, T3, R>(
-  v2: ObservableInput<T2>,
-  v3: ObservableInput<T3>,
-  project: (v1: T, v2: T2, v3: T3) => R
+export function zip<T, A extends readonly unknown[], R>(
+  otherInputsAndProject: [...ObservableInputTuple<A>],
+  project: (...values: Cons<T, A>) => R
 ): OperatorFunction<T, R>;
 /** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2, T3, T4, R>(
-  v2: ObservableInput<T2>,
-  v3: ObservableInput<T3>,
-  v4: ObservableInput<T4>,
-  project: (v1: T, v2: T2, v3: T3, v4: T4) => R
+export function zip<T, A extends readonly unknown[]>(...otherInputs: [...ObservableInputTuple<A>]): OperatorFunction<T, Cons<T, A>>;
+/** @deprecated Deprecated use {@link zipWith} */
+export function zip<T, A extends readonly unknown[], R>(
+  ...otherInputsAndProject: [...ObservableInputTuple<A>, (...values: Cons<T, A>) => R]
 ): OperatorFunction<T, R>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2, T3, T4, T5, R>(
-  v2: ObservableInput<T2>,
-  v3: ObservableInput<T3>,
-  v4: ObservableInput<T4>,
-  v5: ObservableInput<T5>,
-  project: (v1: T, v2: T2, v3: T3, v4: T4, v5: T5) => R
-): OperatorFunction<T, R>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2, T3, T4, T5, T6, R>(
-  v2: ObservableInput<T2>,
-  v3: ObservableInput<T3>,
-  v4: ObservableInput<T4>,
-  v5: ObservableInput<T5>,
-  v6: ObservableInput<T6>,
-  project: (v1: T, v2: T2, v3: T3, v4: T4, v5: T5, v6: T6) => R
-): OperatorFunction<T, R>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2>(v2: ObservableInput<T2>): OperatorFunction<T, [T, T2]>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2, T3>(v2: ObservableInput<T2>, v3: ObservableInput<T3>): OperatorFunction<T, [T, T2, T3]>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2, T3, T4>(
-  v2: ObservableInput<T2>,
-  v3: ObservableInput<T3>,
-  v4: ObservableInput<T4>
-): OperatorFunction<T, [T, T2, T3, T4]>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2, T3, T4, T5>(
-  v2: ObservableInput<T2>,
-  v3: ObservableInput<T3>,
-  v4: ObservableInput<T4>,
-  v5: ObservableInput<T5>
-): OperatorFunction<T, [T, T2, T3, T4, T5]>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, T2, T3, T4, T5, T6>(
-  v2: ObservableInput<T2>,
-  v3: ObservableInput<T3>,
-  v4: ObservableInput<T4>,
-  v5: ObservableInput<T5>,
-  v6: ObservableInput<T6>
-): OperatorFunction<T, [T, T2, T3, T4, T5, T6]>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, R>(...observables: Array<ObservableInput<T> | ((...values: Array<T>) => R)>): OperatorFunction<T, R>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, R>(array: Array<ObservableInput<T>>): OperatorFunction<T, R>;
-/** @deprecated Deprecated use {@link zipWith} */
-export function zip<T, TOther, R>(
-  array: Array<ObservableInput<TOther>>,
-  project: (v1: T, ...values: Array<TOther>) => R
-): OperatorFunction<T, R>;
-/* tslint:enable:max-line-length */
 
 /**
  * @deprecated Deprecated. Use {@link zipWith}.
