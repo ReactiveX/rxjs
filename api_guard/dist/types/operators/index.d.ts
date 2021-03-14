6502f6cbab3eeb68f69d980665c04d4df13dab4b
Nicholas Jamieson <nicholas@cartant.com>
2021-03-14 13:06:46 +1000

    chore: update api_guardian

--- api_guard/dist/types/operators/index.d.ts
+++ api_guard/dist/types/operators/index.d.ts
@@ -26,7 +26,6 @@ export declare function combineLatest<T, A extends readonly unknown[], R>(...sou
 export declare function combineLatestAll<T>(): OperatorFunction<ObservableInput<T>, T[]>;
 export declare function combineLatestAll<T>(): OperatorFunction<any, T[]>;
 export declare function combineLatestAll<T, R>(project: (...values: T[]) => R): OperatorFunction<ObservableInput<T>, R>;
-export declare function combineLatestAll<R>(project: (...values: Array<any>) => R): OperatorFunction<any, R>;
 
 export declare function combineLatestWith<T, A extends readonly unknown[]>(...otherSources: [...ObservableInputTuple<A>]): OperatorFunction<T, Cons<T, A>>;
 
