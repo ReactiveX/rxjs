9474ed6b1d8fa3bd6675216e8fba1d4f44c5d72f
Nicholas Jamieson <nicholas@cartant.com>
2021-03-14 13:36:54 +1000

    test: add dtslint tests with comments

--- spec-dtslint/operators/combineLatestAll-spec.ts
+++ spec-dtslint/operators/combineLatestAll-spec.ts
@@ -9,8 +9,13 @@ it('should infer correctly with the projector', () => {
   const o = of([1, 2, 3]).pipe(combineLatestAll((values: number) => ['x', 'y', 'z'])); // $ExpectType Observable<string[]>
 });
 
-it('is possible to make the projector have an `any` type', () => {
-  const o = of([1, 2, 3]).pipe(combineLatestAll<string[]>(values => ['x', 'y', 'z'])); // $ExpectType Observable<string[]>
+it('should be accept projectors for observables with different types', () => {
+  // An `any` signature is required for the projector to deal with situations
+  // like this in which the source emits observables of different types. The
+  // types of the values passed to the projector depend on the order in which
+  // the source emits its observables and that can't be expressed in the type
+  // system.
+  const o = of(of(['a', 'b', 'c']), of([1, 2, 3])).pipe(combineLatestAll((a: string, b: number) => a + b)); // $ExpectType Observable<string>
 });
 
 it('should enforce types', () => {
