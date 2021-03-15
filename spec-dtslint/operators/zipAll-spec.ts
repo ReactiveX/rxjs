b414cf094d8accef1a5f7b3b42af2539da475a05
Nicholas Jamieson <nicholas@cartant.com>
2021-03-16 02:35:42 +1000

    test: add dtslint tests with comments (#6130)

--- spec-dtslint/operators/zipAll-spec.ts
+++ spec-dtslint/operators/zipAll-spec.ts
@@ -9,6 +9,15 @@ it('should support projecting values', () => {
   const o = of(of(1, 2, 3)).pipe(zipAll(value => String(value))); // $ExpectType Observable<string>
 });
 
+it('should be accept projectors for observables with different types', () => {
+  // An `any` signature is required for the projector to deal with situations
+  // like this in which the source emits observables of different types. The
+  // types of the values passed to the projector depend on the order in which
+  // the source emits its observables and that can't be expressed in the type
+  // system.
+  const o = of(of(['a', 'b', 'c']), of([1, 2, 3])).pipe(zipAll((a: string, b: number) => a + b)); // $ExpectType Observable<string>
+});
+
 it('should enforce types', () => {
   const o = of(1, 2, 3).pipe(zipAll()); // $ExpectError
 });
