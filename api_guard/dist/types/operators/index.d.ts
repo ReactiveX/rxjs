17d29d6648063ab59201ed1f8548e14dc897b2ac
Nicholas Jamieson <nicholas@cartant.com>
2021-03-16 02:17:53 +1000

    refactor: use n-args for zip operator (#6113)
    * refactor: use n-args for zip op
    
    * chore: update api_guardian

--- api_guard/dist/types/operators/index.d.ts
+++ api_guard/dist/types/operators/index.d.ts
@@ -324,20 +324,10 @@ export declare function windowWhen<T>(closingSelector: () => ObservableInput<any
 export declare function withLatestFrom<T, O extends unknown[]>(...inputs: [...ObservableInputTuple<O>]): OperatorFunction<T, [T, ...O]>;
 export declare function withLatestFrom<T, O extends unknown[], R>(...inputs: [...ObservableInputTuple<O>, (...value: [T, ...O]) => R]): OperatorFunction<T, R>;
 
-export declare function zip<T, R>(project: (v1: T) => R): OperatorFunction<T, R>;
-export declare function zip<T, T2, R>(v2: ObservableInput<T2>, project: (v1: T, v2: T2) => R): OperatorFunction<T, R>;
-export declare function zip<T, T2, T3, R>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, project: (v1: T, v2: T2, v3: T3) => R): OperatorFunction<T, R>;
-export declare function zip<T, T2, T3, T4, R>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, v4: ObservableInput<T4>, project: (v1: T, v2: T2, v3: T3, v4: T4) => R): OperatorFunction<T, R>;
-export declare function zip<T, T2, T3, T4, T5, R>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, v4: ObservableInput<T4>, v5: ObservableInput<T5>, project: (v1: T, v2: T2, v3: T3, v4: T4, v5: T5) => R): OperatorFunction<T, R>;
-export declare function zip<T, T2, T3, T4, T5, T6, R>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, v4: ObservableInput<T4>, v5: ObservableInput<T5>, v6: ObservableInput<T6>, project: (v1: T, v2: T2, v3: T3, v4: T4, v5: T5, v6: T6) => R): OperatorFunction<T, R>;
-export declare function zip<T, T2>(v2: ObservableInput<T2>): OperatorFunction<T, [T, T2]>;
-export declare function zip<T, T2, T3>(v2: ObservableInput<T2>, v3: ObservableInput<T3>): OperatorFunction<T, [T, T2, T3]>;
-export declare function zip<T, T2, T3, T4>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, v4: ObservableInput<T4>): OperatorFunction<T, [T, T2, T3, T4]>;
-export declare function zip<T, T2, T3, T4, T5>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, v4: ObservableInput<T4>, v5: ObservableInput<T5>): OperatorFunction<T, [T, T2, T3, T4, T5]>;
-export declare function zip<T, T2, T3, T4, T5, T6>(v2: ObservableInput<T2>, v3: ObservableInput<T3>, v4: ObservableInput<T4>, v5: ObservableInput<T5>, v6: ObservableInput<T6>): OperatorFunction<T, [T, T2, T3, T4, T5, T6]>;
-export declare function zip<T, R>(...observables: Array<ObservableInput<T> | ((...values: Array<T>) => R)>): OperatorFunction<T, R>;
-export declare function zip<T, R>(array: Array<ObservableInput<T>>): OperatorFunction<T, R>;
-export declare function zip<T, TOther, R>(array: Array<ObservableInput<TOther>>, project: (v1: T, ...values: Array<TOther>) => R): OperatorFunction<T, R>;
+export declare function zip<T, A extends readonly unknown[]>(otherInputs: [...ObservableInputTuple<A>]): OperatorFunction<T, Cons<T, A>>;
+export declare function zip<T, A extends readonly unknown[], R>(otherInputsAndProject: [...ObservableInputTuple<A>], project: (...values: Cons<T, A>) => R): OperatorFunction<T, R>;
+export declare function zip<T, A extends readonly unknown[]>(...otherInputs: [...ObservableInputTuple<A>]): OperatorFunction<T, Cons<T, A>>;
+export declare function zip<T, A extends readonly unknown[], R>(...otherInputsAndProject: [...ObservableInputTuple<A>, (...values: Cons<T, A>) => R]): OperatorFunction<T, R>;
 
 export declare function zipAll<T>(): OperatorFunction<ObservableInput<T>, T[]>;
 export declare function zipAll<T>(): OperatorFunction<any, T[]>;
--- spec-dtslint/operators/zip-spec.ts
+++ spec-dtslint/operators/zip-spec.ts
@@ -1,16 +1,15 @@
 import { Observable, of } from 'rxjs';
 import { zip } from 'rxjs/operators';
 
+it('should support observables', () => {
+  const o = of(1); // $ExpectType Observable<number>
+  const a = o.pipe(zip(of(2))); // $ExpectType Observable<[number, number]>
+});
+
 it('should support rest parameter observables', () => {
   const o = of(1); // $ExpectType Observable<number>
   const z = [of(2)]; // $ExpectType Observable<number>[]
-  const a = o.pipe(zip(...z)); // $ExpectType Observable<unknown>
-});
-
-it('should support rest parameter observables with type parameters', () => {
-  const o = of(1); // $ExpectType Observable<number>
-  const z = [of(2)]; // $ExpectType Observable<number>[]
-  const a = o.pipe(zip<number, number[]>(...z)); // $ExpectType Observable<number[]>
+  const a = o.pipe(zip(...z)); // $ExpectType Observable<[arg: number, ...rest: number[]]>
 });
 
 it('should support projected rest parameter observables', () => {
@@ -19,20 +18,8 @@ it('should support projected rest parameter observables', () => {
   const a = o.pipe(zip(...z, (...r) => r.map(v => v.toString()))); // $ExpectType Observable<string[]>
 });
 
-it('should support projected rest parameter observables with type parameters', () => {
-  const o = of(1); // $ExpectType Observable<number>
-  const z = [of(2)]; // $ExpectType Observable<number>[]
-  const a = o.pipe(zip<number, string[]>(...z, (...r) => r.map(v => v.toString()))); // $ExpectType Observable<string[]>
-});
-
 it('should support projected arrays of observables', () => {
   const o = of(1); // $ExpectType Observable<number>
   const z = [of(2)]; // $ExpectType Observable<number>[]
   const a = o.pipe(zip(z, (...r: any[]) => r.map(v => v.toString()))); // $ExpectType Observable<any[]>
-});
-
-it('should support projected arrays of observables with type parameters', () => {
-  const o = of(1); // $ExpectType Observable<number>
-  const z = [of(2)]; // $ExpectType Observable<number>[]
-  const a = o.pipe(zip<number, number, string[]>(z, (...r: any[]) => r.map(v => v.toString()))); // $ExpectType Observable<string[]>
 });
