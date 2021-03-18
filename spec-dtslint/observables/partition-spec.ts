acdab5ce8a59724c9062468ba29026d5659f29bb
Hutch <tech_hutch@verizon.net>
2021-03-18 09:44:16 -0400

    docs(partition): add an overload for better typing (#5756)
    * docs(partition): add an overload for better typing
    
    * refactor: add thisArg support
    
    * test: add dtslint tests
    
    * chore: update api_guardian
    
    Co-authored-by: Nicholas Jamieson <nicholas@cartant.com>

--- spec-dtslint/observables/partition-spec.ts
+++ spec-dtslint/observables/partition-spec.ts
@@ -5,8 +5,8 @@ it('should infer correctly', () => {
   const p = partition(of('a', 'b', 'c'), () => true); // $ExpectType [Observable<string>, Observable<string>]
 });
 
-it('should accept a thisArg parameter', () => {
-  const o = partition(of('a', 'b', 'c'), () => true, 5); // $ExpectType [Observable<string>, Observable<string>]
+it('should support a user-defined type guard', () => {
+  const o = partition(of(1, 2, 3), (value: number): value is 1 => value === 1); // $ExpectType [Observable<1>, Observable<number>]
 });
 
 it('should enforce predicate', () => {
@@ -19,9 +19,17 @@ it('should enforce predicate types', () => {
   const q = partition(of('a', 'b', 'c'), (value, index: string) => true); // $ExpectError
 });
 
-it('should support this', () => {
+it('should support this with type guard', () => {
   const thisArg = { limit: 2 };
-  const a = partition(of(1, 2, 3), function (val) {
+  const a = partition(of(1, 2, 3), function (val): val is 1 { // $ExpectType [Observable<1>, Observable<number>]
+    const limit = this.limit; // $ExpectType number
+    return val < limit;
+  }, thisArg);
+});
+
+it('should support this with predicate', () => {
+  const thisArg = { limit: 2 };
+  const a = partition(of(1, 2, 3), function (val) { // $ExpectType [Observable<number>, Observable<number>]
     const limit = this.limit; // $ExpectType number
     return val < limit;
   }, thisArg);
