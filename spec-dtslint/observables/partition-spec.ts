70d3afc2af3f3415b2b8c8d94500ce871ba4bc87
Boudewijn van Groos <B.vanGroos@sping.nl>
2021-03-22 20:15:07 +0100

    docs(partition): add exclusion when using type guard

--- spec-dtslint/observables/partition-spec.ts
+++ spec-dtslint/observables/partition-spec.ts
@@ -1,4 +1,4 @@
-import { of, partition } from 'rxjs';
+import { of, from, partition } from 'rxjs';
 
 it('should infer correctly', () => {
   const o = partition(of('a', 'b', 'c'), (value, index) => true); // $ExpectType [Observable<string>, Observable<string>]
@@ -7,6 +7,10 @@ it('should infer correctly', () => {
 
 it('should support a user-defined type guard', () => {
   const o = partition(of(1, 2, 3), (value: number): value is 1 => value === 1); // $ExpectType [Observable<1>, Observable<number>]
+});
+
+it('should support exclusion based on the user-defined type guard', () => {
+  const o = partition(from([1, 2] as const), (value: number): value is 1 => value === 1); // $ExpectType [Observable<1>, Observable<2>]
 });
 
 it('should enforce predicate', () => {
@@ -22,6 +26,14 @@ it('should enforce predicate types', () => {
 it('should support this with type guard', () => {
   const thisArg = { limit: 2 };
   const a = partition(of(1, 2, 3), function (val): val is 1 { // $ExpectType [Observable<1>, Observable<number>]
+    const limit = this.limit; // $ExpectType number
+    return val < limit;
+  }, thisArg);
+});
+
+it('should support this with exclusion based on the user-defined type guard', () => {
+  const thisArg = { limit: 2 };
+  const a = partition(from([1, 2] as const), function (val): val is 1 { // $ExpectType [Observable<1>, Observable<2>]
     const limit = this.limit; // $ExpectType number
     return val < limit;
   }, thisArg);
