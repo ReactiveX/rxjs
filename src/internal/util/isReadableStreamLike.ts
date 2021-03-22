381b0ad6836c09edb69c1378193e9130319be565
Jay Phelps <hello@jayphelps.com>
2021-03-21 20:37:20 -0500

    feat(ReadableStreams): add comments.

--- src/internal/util/isReadableStreamLike.ts
+++ src/internal/util/isReadableStreamLike.ts
@@ -1,10 +1,13 @@
 import { isFunction } from './isFunction';
 
+// We shouldn't rely on this type definition being available globally yet since it's
+// not necessarily available in every TS environment.
 interface ReadableStreamDefaultReaderLike<T> {
   read(): IteratorResult<T>;
   releaseLock(): void;
 }
 
+// This is the only part that matters to us.
 export interface ReadableStreamLike<T> {
   getReader(): ReadableStreamDefaultReaderLike<T>;
 }
@@ -25,5 +28,7 @@ export async function* readableStreamLikeToAsyncGenerator<T>(readableStream: Rea
 }
 
 export function isReadableStreamLike<T>(obj: any): obj is ReadableStreamLike<T> {
+  // We don't want to use instanceof checks because they would return
+  // false for instances from another Realm, like an <iframe>.
   return isFunction(obj?.getReader);
 }
