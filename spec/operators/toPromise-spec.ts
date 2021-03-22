a0613b0812b947d2377e030937f6b8fb5128af8d
tmair <thomas.mair86@gmail.com>
2021-03-22 09:49:27 +0100

    chore(toPromise): replace deprecated types and use prettier (#6166)
    * chore(toPromise): update deprecated mocha types in toPromise tests
    
    * chore(toPromise): format toPromise tests with prettier

--- spec/operators/toPromise-spec.ts
+++ spec/operators/toPromise-spec.ts
@@ -1,42 +1,52 @@
+/** @prettier */
 import { expect } from 'chai';
 import { of, EMPTY, throwError, config } from 'rxjs';
 
 /** @test {toPromise} */
 describe('Observable.toPromise', () => {
-  it('should convert an Observable to a promise of its last value', (done: MochaDone) => {
-    of(1, 2, 3).toPromise(Promise).then(x => {
-      expect(x).to.equal(3);
-      done();
-    });
+  it('should convert an Observable to a promise of its last value', (done: Mocha.Done) => {
+    of(1, 2, 3)
+      .toPromise(Promise)
+      .then((x) => {
+        expect(x).to.equal(3);
+        done();
+      });
   });
 
-  it('should convert an empty Observable to a promise of undefined', (done: MochaDone) => {
+  it('should convert an empty Observable to a promise of undefined', (done: Mocha.Done) => {
     EMPTY.toPromise(Promise).then((x) => {
       expect(x).to.be.undefined;
       done();
     });
   });
 
-  it('should handle errors properly', (done: MochaDone) => {
-    throwError('bad').toPromise(Promise).then(() => {
-      done(new Error('should not be called'));
-    }, (err: any) => {
-      expect(err).to.equal('bad');
-      done();
-    });
+  it('should handle errors properly', (done: Mocha.Done) => {
+    throwError('bad')
+      .toPromise(Promise)
+      .then(
+        () => {
+          done(new Error('should not be called'));
+        },
+        (err: any) => {
+          expect(err).to.equal('bad');
+          done();
+        }
+      );
   });
 
-  it('should allow for global config via config.Promise', (done: MochaDone) => {
+  it('should allow for global config via config.Promise', (done: Mocha.Done) => {
     let wasCalled = false;
     config.Promise = function MyPromise(callback: Function) {
       wasCalled = true;
       return new Promise(callback as any);
     } as any;
 
-    of(42).toPromise().then(x => {
-      expect(wasCalled).to.be.true;
-      expect(x).to.equal(42);
-      done();
-    });
+    of(42)
+      .toPromise()
+      .then((x) => {
+        expect(wasCalled).to.be.true;
+        expect(x).to.equal(42);
+        done();
+      });
   });
 });
