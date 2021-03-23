2ebe53a4e3daefaf4a25a1e11eae3eba7161d167
Ben Lesh <ben@benlesh.com>
2021-03-23 16:34:01 -0500

    refactor: Export ReadableStreamLike, add dtslint test

--- src/internal/util/isReadableStreamLike.ts
+++ src/internal/util/isReadableStreamLike.ts
@@ -1,17 +1,5 @@
 import { isFunction } from './isFunction';
 
-// We shouldn't rely on this type definition being available globally yet since it's
-// not necessarily available in every TS environment.
-interface ReadableStreamDefaultReaderLike<T> {
-  read(): IteratorResult<T>;
-  releaseLock(): void;
-}
-
-// This is the only part that matters to us.
-export interface ReadableStreamLike<T> {
-  getReader(): ReadableStreamDefaultReaderLike<T>;
-}
-
 export async function* readableStreamLikeToAsyncGenerator<T>(readableStream: ReadableStreamLike<T>): AsyncGenerator<T> {
   const reader = readableStream.getReader();
   try {
