80d8602bfb149cc659f9fc1eccd73b7215213d49
tmair <thomas.mair86@gmail.com>
2021-03-27 23:39:59 +0100

    chore(tap): convert tap tests to run mode  (#6183)
    * chore(tap): convert tap tests to run mode
    
    * chore(tap): replace deprecated mocha types in tests
    
    * chore(tap): update deprecations in tests
    
    * chore(tap): remove unused parameters in tests

--- spec/operators/tap-spec.ts
+++ spec/operators/tap-spec.ts
@@ -1,64 +1,88 @@
+/** @prettier */
 import { expect } from 'chai';
 import { tap, mergeMap, take } from 'rxjs/operators';
 import { Subject, of, throwError, Observer, EMPTY, Observable } from 'rxjs';
-import { hot, cold, expectObservable, expectSubscriptions } from '../helpers/marble-testing';
+import { TestScheduler } from 'rxjs/testing';
+import { observableMatcher } from '../helpers/observableMatcher';
 
 /** @test {tap} */
-describe('tap operator', () => {
-  it('should mirror multiple values and complete', () => {
-    const e1 =  cold('--1--2--3--|');
-    const e1subs =   '^          !';
-    const expected = '--1--2--3--|';
+describe('tap', () => {
+  let testScheduler: TestScheduler;
 
-    const result = e1.pipe(tap(() => {
-      //noop
-    }));
-    expectObservable(result).toBe(expected);
-    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+  beforeEach(() => {
+    testScheduler = new TestScheduler(observableMatcher);
+  });
+
+  it('should mirror multiple values and complete', () => {
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' --1--2--3--|');
+      const e1subs = '  ^----------!';
+      const expected = '--1--2--3--|';
+
+      const result = e1.pipe(
+        tap(() => {
+          //noop
+        })
+      );
+
+      expectObservable(result).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should next with a callback', () => {
     let value = null;
-    of(42).pipe(tap(function (x) {
-      value = x;
-    }))
-    .subscribe();
+    of(42)
+      .pipe(
+        tap(function (x) {
+          value = x;
+        })
+      )
+      .subscribe();
 
     expect(value).to.equal(42);
   });
 
   it('should error with a callback', () => {
     let err = null;
-    throwError('bad').pipe(tap(null, function (x) {
-      err = x;
-    }))
-    .subscribe(null, function (ex) {
-      expect(ex).to.equal('bad');
-    });
+    throwError(() => 'bad')
+      .pipe(
+        tap(null, function (x) {
+          err = x;
+        })
+      )
+      .subscribe({
+        error(ex) {
+          expect(ex).to.equal('bad');
+        },
+      });
 
     expect(err).to.equal('bad');
   });
 
-  it('should handle everything with an observer', (done: MochaDone) => {
+  it('should handle everything with an observer', (done: Mocha.Done) => {
     const expected = [1, 2, 3];
     const results: number[] = [];
 
-    of(1, 2, 3).pipe(
-      tap(<Observer<number>>{
-        next: (x: number) => {
-          results.push(x);
-        },
-        error: (err: any) => {
-          done(new Error('should not be called'));
-        },
-        complete: () => {
-          expect(results).to.deep.equal(expected);
-          done();
-        }
-      })).subscribe();
+    of(1, 2, 3)
+      .pipe(
+        tap(<Observer<number>>{
+          next: (x: number) => {
+            results.push(x);
+          },
+          error: () => {
+            done(new Error('should not be called'));
+          },
+          complete: () => {
+            expect(results).to.deep.equal(expected);
+            done();
+          },
+        })
+      )
+      .subscribe();
   });
 
-  it('should handle everything with a Subject', (done: MochaDone) => {
+  it('should handle everything with a Subject', (done: Mocha.Done) => {
     const expected = [1, 2, 3];
     const results: number[] = [];
     const subject = new Subject<number>();
@@ -67,42 +91,52 @@ describe('tap operator', () => {
       next: (x: any) => {
         results.push(x);
       },
-      error: (err: any) => {
+      error: () => {
         done(new Error('should not be called'));
       },
       complete: () => {
         expect(results).to.deep.equal(expected);
         done();
-      }
+      },
     });
 
-    of(1, 2, 3).pipe(
-      tap(subject)
-    ).subscribe();
+    of(1, 2, 3).pipe(tap(subject)).subscribe();
   });
 
   it('should handle an error with a callback', () => {
     let errored = false;
-    throwError('bad').pipe(tap(null, (err: any) => {
-      expect(err).to.equal('bad');
-    }))
-    .subscribe(null, (err: any) => {
-      errored = true;
-      expect(err).to.equal('bad');
-    });
+    throwError(() => 'bad')
+      .pipe(
+        tap(null, (err: any) => {
+          expect(err).to.equal('bad');
+        })
+      )
+      .subscribe({
+        error(err: any) {
+          errored = true;
+          expect(err).to.equal('bad');
+        },
+      });
 
     expect(errored).to.be.true;
   });
 
   it('should handle an error with observer', () => {
     let errored = false;
-    throwError('bad').pipe(tap(<any>{ error: function (err: string) {
-      expect(err).to.equal('bad');
-    } }))
-    .subscribe(null, function (err) {
-      errored = true;
-      expect(err).to.equal('bad');
-    });
+    throwError(() => 'bad')
+      .pipe(
+        tap(<any>{
+          error: function (err: string) {
+            expect(err).to.equal('bad');
+          },
+        })
+      )
+      .subscribe({
+        error(err) {
+          errored = true;
+          expect(err).to.equal('bad');
+        },
+      });
 
     expect(errored).to.be.true;
   });
@@ -110,11 +144,13 @@ describe('tap operator', () => {
   it('should handle complete with observer', () => {
     let completed = false;
 
-    EMPTY.pipe(tap(<any>{
-      complete: () => {
-        completed = true;
-      }
-    })).subscribe();
+    EMPTY.pipe(
+      tap(<any>{
+        complete: () => {
+          completed = true;
+        },
+      })
+    ).subscribe();
 
     expect(completed).to.be.true;
   });
@@ -122,103 +158,137 @@ describe('tap operator', () => {
   it('should handle next with observer', () => {
     let value = null;
 
-    of('hi').pipe(tap(<any>{
-      next: (x: string) => {
-        value = x;
-      }
-    })).subscribe();
+    of('hi')
+      .pipe(
+        tap(<any>{
+          next: (x: string) => {
+            value = x;
+          },
+        })
+      )
+      .subscribe();
 
     expect(value).to.equal('hi');
   });
 
   it('should raise error if next handler raises error', () => {
-    of('hi').pipe(tap(<any>{
-      next: (x: string) => {
-        throw new Error('bad');
-      }
-    })).subscribe(null, (err: any) => {
-      expect(err.message).to.equal('bad');
-    });
+    of('hi')
+      .pipe(
+        tap(<any>{
+          next: () => {
+            throw new Error('bad');
+          },
+        })
+      )
+      .subscribe({
+        error(err: any) {
+          expect(err.message).to.equal('bad');
+        },
+      });
   });
 
   it('should raise error if error handler raises error', () => {
-    throwError('ops').pipe(tap(<any>{
-      error: (x: any) => {
-        throw new Error('bad');
-      }
-    })).subscribe(null, (err: any) => {
-      expect(err.message).to.equal('bad');
-    });
+    throwError(() => 'ops')
+      .pipe(
+        tap(<any>{
+          error: () => {
+            throw new Error('bad');
+          },
+        })
+      )
+      .subscribe({
+        error(err: any) {
+          expect(err.message).to.equal('bad');
+        },
+      });
   });
 
   it('should raise error if complete handler raises error', () => {
-    EMPTY.pipe(tap(<any>{
-      complete: () => {
-        throw new Error('bad');
-      }
-    })).subscribe(null, (err: any) => {
-      expect(err.message).to.equal('bad');
+    EMPTY.pipe(
+      tap(<any>{
+        complete: () => {
+          throw new Error('bad');
+        },
+      })
+    ).subscribe({
+      error(err: any) {
+        expect(err.message).to.equal('bad');
+      },
     });
   });
 
   it('should allow unsubscribing explicitly and early', () => {
-    const e1 =   hot('--1--2--3--#');
-    const unsub =    '       !    ';
-    const e1subs =   '^      !    ';
-    const expected = '--1--2--    ';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --1--2--3--#');
+      const unsub = '   -------!    ';
+      const e1subs = '  ^------!    ';
+      const expected = '--1--2--    ';
 
-    const result = e1.pipe(tap(() => {
-      //noop
-    }));
-    expectObservable(result, unsub).toBe(expected);
-    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+      const result = e1.pipe(
+        tap(() => {
+          //noop
+        })
+      );
+      expectObservable(result, unsub).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should not break unsubscription chains when result is unsubscribed explicitly', () => {
-    const e1 =   hot('--1--2--3--#');
-    const e1subs =   '^      !    ';
-    const expected = '--1--2--    ';
-    const unsub =    '       !    ';
+    testScheduler.run(({ hot, expectObservable, expectSubscriptions }) => {
+      const e1 = hot('  --1--2--3--#');
+      const e1subs = '  ^------!    ';
+      const expected = '--1--2--    ';
+      const unsub = '   -------!    ';
 
-    const result = e1.pipe(
-      mergeMap((x: any) => of(x)),
-      tap(() => {
-        //noop
-      }),
-      mergeMap((x: any) => of(x))
-    );
+      const result = e1.pipe(
+        mergeMap((x: any) => of(x)),
+        tap(() => {
+          //noop
+        }),
+        mergeMap((x: any) => of(x))
+      );
 
-    expectObservable(result, unsub).toBe(expected);
-    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+      expectObservable(result, unsub).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should mirror multiple values and complete', () => {
-    const e1 =  cold('--1--2--3--|');
-    const e1subs =   '^          !';
-    const expected = '--1--2--3--|';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' --1--2--3--|');
+      const e1subs = '  ^----------!';
+      const expected = '--1--2--3--|';
 
-    const result = e1.pipe(tap(() => {
-      //noop
-    }));
-    expectObservable(result).toBe(expected);
-    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+      const result = e1.pipe(
+        tap(() => {
+          //noop
+        })
+      );
+      expectObservable(result).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should mirror multiple values and terminate with error', () => {
-    const e1 =  cold('--1--2--3--#');
-    const e1subs =   '^          !';
-    const expected = '--1--2--3--#';
+    testScheduler.run(({ cold, expectObservable, expectSubscriptions }) => {
+      const e1 = cold(' --1--2--3--#');
+      const e1subs = '  ^----------!';
+      const expected = '--1--2--3--#';
 
-    const result = e1.pipe(tap(() => {
-      //noop
-    }));
-    expectObservable(result).toBe(expected);
-    expectSubscriptions(e1.subscriptions).toBe(e1subs);
+      const result = e1.pipe(
+        tap(() => {
+          //noop
+        })
+      );
+      expectObservable(result).toBe(expected);
+      expectSubscriptions(e1.subscriptions).toBe(e1subs);
+    });
   });
 
   it('should stop listening to a synchronous observable when unsubscribed', () => {
     const sideEffects: number[] = [];
-    const synchronousObservable = new Observable<number>(subscriber => {
+    const synchronousObservable = new Observable<number>((subscriber) => {
       // This will check to see if the subscriber was closed on each loop
       // when the unsubscribe hits (from the `take`), it should be closed
       for (let i = 0; !subscriber.closed && i < 10; i++) {
@@ -227,10 +297,16 @@ describe('tap operator', () => {
       }
     });
 
-    synchronousObservable.pipe(
-      tap(() => { /* noop */ }),
-      take(3),
-    ).subscribe(() => { /* noop */ });
+    synchronousObservable
+      .pipe(
+        tap(() => {
+          /* noop */
+        }),
+        take(3)
+      )
+      .subscribe(() => {
+        /* noop */
+      });
 
     expect(sideEffects).to.deep.equal([0, 1, 2]);
   });
