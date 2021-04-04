ea57c6a50b91bec9d872cd3d21a99e03b1bdb885
Nicholas Jamieson <nicholas@cartant.com>
2021-04-04 22:24:12 +1000

    test(fromEvent): make dtslint tests fail

--- spec-dtslint/observables/fromEvent-spec.ts
+++ spec-dtslint/observables/fromEvent-spec.ts
@@ -1,44 +1,55 @@
 import { fromEvent } from 'rxjs';
-import { JQueryStyleEventEmitter } from '../../src/internal/observable/fromEvent';
-import { A, B } from '../helpers';
+import {
+  HasEventTargetAddRemove,
+  NodeStyleEventEmitter,
+  NodeCompatibleEventEmitter,
+  JQueryStyleEventEmitter
+} from '../../src/internal/observable/fromEvent';
+import { B } from '../helpers';
 
 declare const eventTargetSource: EventTarget;
 
 it('should support an event target source', () => {
+  const source: HasEventTargetAddRemove<Event> = eventTargetSource;
   const a = fromEvent(eventTargetSource, "click"); // $ExpectType Observable<Event>
 });
 
 declare const documentSource: HTMLDocument;
 
 it('should support a document source', () => {
+  const source: HasEventTargetAddRemove<Event> = documentSource;
   const a = fromEvent(documentSource, "click"); // $ExpectType Observable<Event>
 });
 
-interface NodeStyleSource {
-  addListener: (eventName: string | symbol, handler: (...args: any[]) => void) => this;
-  removeListener: (eventName: string | symbol, handler: (...args: any[]) => void) => this;
-};
-
-declare const nodeStyleSource : NodeStyleSource;
+// Pick the parts that will match NodeStyleEventEmitter. If this isn't done, it
+// will match JQueryStyleEventEmitter - because of the `on` and `off` methods -
+// despite the latter being declared last in the EventTargetLike union.
+declare const nodeStyleSource: Pick<typeof process, 'addListener' | 'removeListener'>;
 
 it('should support a node-style source', () => {
+  const source: NodeStyleEventEmitter = nodeStyleSource;
   const a = fromEvent(nodeStyleSource, "something"); // $ExpectType Observable<unknown>
   const b = fromEvent<B>(nodeStyleSource, "something"); // $ExpectType Observable<B>
 });
 
-declare const nodeCompatibleSource: {
-  addListener: (eventName: string, handler: (...args: any[]) => void) => void;
-  removeListener: (eventName: string, handler: (...args: any[]) => void) => void;
+const nodeCompatibleSource = {
+  addListener(eventName: "something", handler: () => void) {},
+  removeListener(eventName: "something", handler: () => void) {}
 };
 
 it('should support a node-compatible source', () => {
+  const source: NodeCompatibleEventEmitter = nodeCompatibleSource;
   const a = fromEvent(nodeCompatibleSource, "something"); // $ExpectType Observable<unknown>
   const b = fromEvent<B>(nodeCompatibleSource, "something"); // $ExpectType Observable<B>
 });
 
-declare const jQueryStyleSource: JQueryStyleEventEmitter<A, B>;
+const jQueryStyleSource = {
+  on(eventName: "something", handler: (this: any, b: B) => any) {},
+  off(eventName: "something", handler: (this: any, b: B) => any) {}
+};
 
 it('should support a jQuery-style source', () => {
+  const source: JQueryStyleEventEmitter<any, any> = jQueryStyleSource;
   const a = fromEvent(jQueryStyleSource, "something"); // $ExpectType Observable<B>
   const b = fromEvent<B>(jQueryStyleSource, "something"); // $ExpectType Observable<B>
 });
