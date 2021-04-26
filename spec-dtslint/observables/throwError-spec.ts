5297016feb0df929e5f2854aff46f76a0596ab1a
Nitin Malave <nitin.malave@nvent.com>
2021-04-26 15:07:44 +0530

    test: add unit test for throwError factory support

--- spec-dtslint/observables/throwError-spec.ts
+++ spec-dtslint/observables/throwError-spec.ts
@@ -1,12 +1,23 @@
 import { throwError, animationFrameScheduler } from 'rxjs';
 
 it('should accept any type and return never observable', () => {
-  const a = throwError(() => (1)); // $ExpectType Observable<never>
-  const b = throwError(() => ('a')); // $ExpectType Observable<never>
-  const c = throwError(() => ({a: 1})); // $ExpectType Observable<never>
+  const a = throwError(1); // $ExpectType Observable<never>
+  const b = throwError('a'); // $ExpectType Observable<never>
+  const c = throwError({ a: 1 }); // $ExpectType Observable<never>
   const d = throwError(() => ({ a: 2 })); // $ExpectType Observable<never>
 });
 
-it('should support scheduler', () => {
-  const a = throwError(() => (1, animationFrameScheduler)); // $ExpectType Observable<never>
+it('should support an error value and a scheduler', () => {
+  const a = throwError(1, animationFrameScheduler); // $ExpectType Observable<never>
+});
+
+it('should accept any type and return never observable with support of factory', () => {
+  const a = throwError(() => (1)); // $ExpectType Observable<never>
+  const b = throwError(() => ('a')); // $ExpectType Observable<never>
+  const c = throwError(() => ({ a: 1 })); // $ExpectType Observable<never>
+  const d = throwError(() => ({ a: 2 })); // $ExpectType Observable<never>
+});
+
+it('should support a factory and a scheduler', () => {
+  const a = throwError(() => 1, animationFrameScheduler); // $ExpectType Observable<never>
 });
