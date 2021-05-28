7834c31587552c8a7d52d07b6c232afa9f5aee38
Oliver Hoff <oliver@hofff.com>
2021-05-28 18:47:34 +0200

    feat(share): accept ObservableInput for notifiers

--- src/internal/operators/share.ts
+++ src/internal/operators/share.ts
@@ -1,10 +1,9 @@
-import { Observable } from '../Observable';
-import { from } from '../observable/from';
+import { innerFrom } from '../observable/from';
 import { take } from '../operators/take';
 import { Subject } from '../Subject';
 import { SafeSubscriber } from '../Subscriber';
 import { Subscription } from '../Subscription';
-import { MonoTypeOperatorFunction, SubjectLike } from '../types';
+import { MonoTypeOperatorFunction, ObservableInput, SubjectLike } from '../types';
 import { operate } from '../util/lift';
 
 export interface ShareConfig<T> {
@@ -27,7 +26,7 @@ export interface ShareConfig<T> {
    *
    * @defaultValue `true`
    */
-  resetOnError?: boolean | ((error: any) => Observable<any>);
+  resetOnError?: boolean | ((error: any) => ObservableInput<any>);
   /**
    * If true, the resulting observable will reset internal state on completion from source and return to a "cold" state. This
    * allows the resulting observable to be "repeated" after it is done.
@@ -39,7 +38,7 @@ export interface ShareConfig<T> {
    *
    * @defaultValue `true`
    */
-  resetOnComplete?: boolean | (() => Observable<any>);
+  resetOnComplete?: boolean | (() => ObservableInput<any>);
   /**
    * If true, when the number of subscribers to the resulting observable reaches zero due to those subscribers unsubscribing, the
    * internal state will be reset and the resulting observable will return to a "cold" state. This means that the next
@@ -52,7 +51,7 @@ export interface ShareConfig<T> {
    *
    * @defaultValue `true`
    */
-  resetOnRefCountZero?: boolean | (() => Observable<any>);
+  resetOnRefCountZero?: boolean | (() => ObservableInput<any>);
 }
 
 export function share<T>(): MonoTypeOperatorFunction<T>;
@@ -232,7 +231,7 @@ export function share<T>(options: ShareConfig<T> = {}): MonoTypeOperatorFunction
             dest.complete();
           },
         });
-        from(source).subscribe(connection);
+        source.subscribe(connection);
       }
     })(wrapperSource);
   };
@@ -240,7 +239,7 @@ export function share<T>(options: ShareConfig<T> = {}): MonoTypeOperatorFunction
 
 function handleReset<T extends unknown[] = never[]>(
   reset: () => void,
-  on: boolean | ((...args: T) => Observable<any>),
+  on: boolean | ((...args: T) => ObservableInput<any>),
   ...args: T
 ): Subscription | null {
   if (on === true) {
@@ -253,7 +252,7 @@ function handleReset<T extends unknown[] = never[]>(
     return null;
   }
 
-  return on(...args)
+  return innerFrom(on(...args))
     .pipe(take(1))
-    .subscribe(() => reset());
+    .subscribe(reset);
 }
