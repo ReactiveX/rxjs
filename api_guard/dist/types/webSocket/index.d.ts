7f9a2d81206162e6c4e0714eea21b7731a50e6a2
Raymond Davis IV <raymond.davis.4th@gmail.com>
2021-06-17 11:06:01 -0700

    chore: api_guardian update

--- api_guard/dist/types/webSocket/index.d.ts
+++ api_guard/dist/types/webSocket/index.d.ts
@@ -1,23 +1,27 @@
-export declare function webSocket<T>(urlConfigOrSource: string | WebSocketSubjectConfig<T>): WebSocketSubject<T>;
+export declare function webSocket<In, Out = In>(urlConfigOrSource: string | WebSocketSubjectConfig<In, Out>): WebSocketSubject<In, Out>;
 
-export declare class WebSocketSubject<T> extends AnonymousSubject<T> {
-    constructor(urlConfigOrSource: string | WebSocketSubjectConfig<T> | Observable<T>, destination?: Observer<T>);
-    lift<R>(operator: Operator<T, R>): WebSocketSubject<R>;
-    multiplex(subMsg: () => any, unsubMsg: () => any, messageFilter: (value: T) => boolean): Observable<T>;
+export declare class WebSocketSubject<In, Out = In> extends Observable<Out> implements Observer<In> {
+    destination?: Observer<In>;
+    constructor(urlConfigOrSource: string | WebSocketSubjectConfig<In, Out> | Observable<In>, destination?: Observer<In>);
+    complete(): void;
+    error(err: any): void;
+    lift<R>(operator: Operator<In, R>): WebSocketSubject<R>;
+    multiplex(subMsg: () => any, unsubMsg: () => any, messageFilter: (value: Out) => boolean): Observable<Out>;
+    next(value: In): void;
     unsubscribe(): void;
 }
 
-export interface WebSocketSubjectConfig<T> {
+export interface WebSocketSubjectConfig<In, Out = In> {
     WebSocketCtor?: {
         new (url: string, protocols?: string | string[]): WebSocket;
     };
     binaryType?: 'blob' | 'arraybuffer';
     closeObserver?: NextObserver<CloseEvent>;
     closingObserver?: NextObserver<void>;
-    deserializer?: (e: MessageEvent) => T;
+    deserializer?: (e: MessageEvent) => Out;
     openObserver?: NextObserver<Event>;
     protocol?: string | Array<string>;
-    resultSelector?: (e: MessageEvent) => T;
-    serializer?: (value: T) => WebSocketMessage;
+    resultSelector?: (e: MessageEvent) => Out;
+    serializer?: (value: In) => WebSocketMessage;
     url: string;
 }
