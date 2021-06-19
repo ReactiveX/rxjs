d6f03012ac101a58693f9c81d3e22c07654bda5d
Marc Redemske <marc.redemske@gmail.com>
2021-06-19 03:05:10 +0200

    fix(isArrayLike): function will now return false when a string is given
    and returns a proper boolean value

--- src/internal/util/isArrayLike.ts
+++ src/internal/util/isArrayLike.ts
@@ -1,1 +1,2 @@
-export const isArrayLike = (<T>(x: any): x is ArrayLike<T> => x && typeof x.length === 'number' && typeof x !== 'function');
+export const isArrayLike = <T>(x: any): x is ArrayLike<T> =>
+  Boolean(x && typeof x.length === 'number' && typeof x !== 'function' && typeof x !== 'string');
