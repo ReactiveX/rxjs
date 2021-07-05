512adc25f350660113275d8277d16b7f3eec1d49
Ben Lesh <ben@benlesh.com>
2021-07-05 12:15:26 -0500

    feat: operators are all exported at the top level, from "rxjs". (#6488)
    * feat: operators are all exported at the top level, from "rxjs".
    
    Related #6242
    
    * chore: update golden files
    
    * chore: fix symbol type on symbol observable

--- src/internal/symbol/observable.ts
+++ src/internal/symbol/observable.ts
@@ -1,2 +1,2 @@
 /** Symbol.observable or a string "@@observable". Used for interop */
-export const observable = (() => (typeof Symbol === 'function' && Symbol.observable) || '@@observable')();
+export const observable: string | symbol = (() => (typeof Symbol === 'function' && Symbol.observable) || '@@observable')();
