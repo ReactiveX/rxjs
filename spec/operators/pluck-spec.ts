2739c419bf4e12de49c3534986caba193859f713
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2021-07-16 12:24:48 +0200

    test(pluck): add firehose test

--- spec/operators/pluck-spec.ts
+++ spec/operators/pluck-spec.ts
@@ -1,8 +1,8 @@
 /** @prettier */
 import { expect } from 'chai';
 import { TestScheduler } from 'rxjs/internal/testing/TestScheduler';
-import { pluck, map, mergeMap } from 'rxjs/operators';
-import { of } from 'rxjs';
+import { pluck, map, mergeMap, take } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
 import { observableMatcher } from '../helpers/observableMatcher';
 
 /** @test {pluck} */
@@ -251,5 +251,23 @@ describe('pluck', () => {
       expectObservable(result).toBe(expected, { y: undefined });
       expectSubscriptions(e1.subscriptions).toBe(e1subs);
     });
+  });
+
+  it('should stop listening to a synchronous observable when unsubscribed', () => {
+    const sideEffects: number[] = [];
+    const synchronousObservable = new Observable<number>((subscriber) => {
+      // This will check to see if the subscriber was closed on each loop
+      // when the unsubscribe hits (from the `take`), it should be closed
+      for (let i = 0; !subscriber.closed && i < 10; i++) {
+        sideEffects.push(i);
+        subscriber.next(i);
+      }
+    });
+
+    synchronousObservable.pipe(pluck('whatever'), take(3)).subscribe(() => {
+      /* noop */
+    });
+
+    expect(sideEffects).to.deep.equal([0, 1, 2]);
   });
 });
