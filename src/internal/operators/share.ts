df209affa10acdd7c48d5366f7566cfdc2688b78
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2021-12-07 19:54:00 +0100

    docs(share): use tap for logging in share example

--- src/internal/operators/share.ts
+++ src/internal/operators/share.ts
@@ -72,34 +72,37 @@ export function share<T>(options: ShareConfig<T>): MonoTypeOperatorFunction<T>;
  * Generate new multicast Observable from the `source` Observable value
  *
  * ```ts
- * import { interval, map, share } from 'rxjs';
+ * import { interval, tap, map, take, share } from 'rxjs';
  *
- * const source = interval(1000)
- *   .pipe(
- *         map((x: number) => {
- *             console.log('Processing: ', x);
- *             return x*x;
- *         }),
- *         share()
+ * const source = interval(1000).pipe(
+ *   tap(x => console.log('Processing: ', x)),
+ *   map(x => x * x),
+ *   take(6),
+ *   share()
  * );
  *
  * source.subscribe(x => console.log('subscription 1: ', x));
  * source.subscribe(x => console.log('subscription 2: ', x));
  *
  * // Logs:
- * // Processing:  0
- * // subscription 1:  0
- * // subscription 2:  0
- * // Processing:  1
- * // subscription 1:  1
- * // subscription 2:  1
- * // Processing:  2
- * // subscription 1:  4
- * // subscription 2:  4
- * // Processing:  3
- * // subscription 1:  9
- * // subscription 2:  9
- * // ... and so on
+ * // Processing: 0
+ * // subscription 1: 0
+ * // subscription 2: 0
+ * // Processing: 1
+ * // subscription 1: 1
+ * // subscription 2: 1
+ * // Processing: 2
+ * // subscription 1: 4
+ * // subscription 2: 4
+ * // Processing: 3
+ * // subscription 1: 9
+ * // subscription 2: 9
+ * // Processing: 4
+ * // subscription 1: 16
+ * // subscription 2: 16
+ * // Processing: 5
+ * // subscription 1: 25
+ * // subscription 2: 25
  * ```
  *
  * ## Example with notifier factory: Delayed reset
