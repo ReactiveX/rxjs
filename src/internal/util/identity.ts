f0fdd4ca8cea59117cdeff8b0d93f4bbd75d6dc4
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2022-01-11 22:02:31 +0100

    docs(identity): add selectively apply an operator example

--- src/internal/util/identity.ts
+++ src/internal/util/identity.ts
@@ -2,7 +2,7 @@
  * This function takes one parameter and just returns it. Simply put,
  * this is like `<T>(x: T): T => x`.
  *
- * ## Example
+ * ## Examples
  *
  * This is useful in some cases when using things like `mergeMap`
  *
@@ -15,6 +15,22 @@
  *   map(i => range(i)),
  *   mergeMap(identity) // same as mergeMap(x => x)
  * );
+ *
+ * result$.subscribe({
+ *   next: console.log
+ * });
+ * ```
+ *
+ * Or when you want to selectively apply an operator
+ *
+ * ```ts
+ * import { interval, take, identity } from 'rxjs';
+ *
+ * const shouldLimit = () => Math.random() < 0.5;
+ *
+ * const source$ = interval(1000);
+ *
+ * const result$ = source$.pipe(shouldLimit() ? take(5) : identity);
  *
  * result$.subscribe({
  *   next: console.log
