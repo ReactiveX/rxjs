6d7196b061a6cccb8b8a19b4d9a0e6e7bcc66247
Ziv <ziv.perry@zoominfo.com>
2022-02-03 17:27:41 +0200

    build: set typescript isolatedModules and fix the code to pass build and tests

--- src/ajax/index.ts
+++ src/ajax/index.ts
@@ -1,4 +1,5 @@
 export { ajax } from '../internal/ajax/ajax';
-export { AjaxError, AjaxTimeoutError } from '../internal/ajax/errors';
+export { AjaxTimeoutError } from '../internal/ajax/errors';
 export { AjaxResponse } from '../internal/ajax/AjaxResponse';
-export { AjaxRequest, AjaxConfig, AjaxDirection } from '../internal/ajax/types';
+export { AjaxError } from '../internal/ajax/errors';
+export type { AjaxRequest, AjaxConfig, AjaxDirection } from '../internal/ajax/types';
