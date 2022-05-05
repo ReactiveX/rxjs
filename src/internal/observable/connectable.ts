105e3db96e8de58d17b37332f256caefafd79cf6
Josh Soref <jsoref@users.noreply.github.com>
2022-05-05 17:38:06 -0400

    spelling: disconnection
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/internal/observable/connectable.ts
+++ src/internal/observable/connectable.ts
@@ -11,7 +11,7 @@ export interface ConnectableConfig<T> {
    */
   connector: () => SubjectLike<T>;
   /**
-   * If true, the resulting observable will reset internal state upon disconnetion
+   * If true, the resulting observable will reset internal state upon disconnection
    * and return to a "cold" state. This allows the resulting observable to be
    * reconnected.
    * If false, upon disconnection, the connecting subject will remain the
