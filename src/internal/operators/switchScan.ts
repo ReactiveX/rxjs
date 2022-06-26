deaf9dc85c542698678ffde64c472c41d280febe
demensky <dmitry@demensky.com>
2022-06-27 02:43:22 +0300

    refactor(switchScan): avoid using deprecated switchMap signature

--- src/internal/operators/switchScan.ts
+++ src/internal/operators/switchScan.ts
@@ -1,5 +1,6 @@
 import { ObservableInput, ObservedValueOf, OperatorFunction } from '../types';
 import { switchMap } from './switchMap';
+import { tap } from './tap';
 import { operate } from '../util/lift';
 
 // TODO: Generate a marble diagram for these docs.
@@ -30,17 +31,20 @@ export function switchScan<T, R, O extends ObservableInput<any>>(
     // accumulator function at each new value from the source.
     let state = seed;
 
-    // Use `switchMap` on our `source` to do the work of creating
-    // this operator. Note the backwards order here of `switchMap()(source)`
-    // to avoid needing to use `pipe` unnecessarily
-    switchMap(
-      // On each value from the source, call the accumulator with
-      // our previous state, the value and the index.
-      (value: T, index) => accumulator(state, value, index),
-      // Using the deprecated result selector here as a dirty trick
-      // to update our state with the flattened value.
-      (_, innerValue) => ((state = innerValue), innerValue)
-    )(source).subscribe(subscriber);
+    source
+      .pipe(
+        // Use `switchMap` on our `source` to do the work of creating this operator.
+        switchMap(
+          // On each value from the source, call the accumulator with
+          // our previous state, the value and the index.
+          (value, index) => accumulator(state, value, index)
+        ),
+        tap((innerValue) => {
+          // Update our state with the flattened value.
+          state = innerValue;
+        })
+      )
+      .subscribe(subscriber);
 
     return () => {
       // Release state on finalization
