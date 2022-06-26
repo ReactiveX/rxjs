3753a259188176ecb1d0a10d2db558507c05b1f2
demensky <dmitry@demensky.com>
2022-06-27 02:08:00 +0300

    feat(exhaustMap): removed deprecated exhaustMap(project, resultSelector) call pattern
    BREAKING CHANGE: `exhaustMap(project, resultSelector)` call pattern is no longer available. [Read more](https://rxjs.dev/deprecations/resultSelector).

--- spec-dtslint/operators/exhaustMap-spec.ts
+++ spec-dtslint/operators/exhaustMap-spec.ts
@@ -9,26 +9,6 @@ it('should support a projector that takes an index', () => {
   const o = of(1, 2, 3).pipe(exhaustMap((p, index) => of(Boolean(p)))); // $ExpectType Observable<boolean>
 });
 
-it('should infer correctly by using the resultSelector first parameter', () => {
-  const o = of(1, 2, 3).pipe(exhaustMap(p => of(Boolean(p)), a => a)); // $ExpectType Observable<number>
-});
-
-it('should infer correctly by using the resultSelector second parameter', () => {
-  const o = of(1, 2, 3).pipe(exhaustMap(p => of(Boolean(p)), (a, b) => b)); // $ExpectType Observable<boolean>
-});
-
-it('should support a resultSelector that takes an inner index', () => {
-  const o = of(1, 2, 3).pipe(exhaustMap(p => of(Boolean(p)), (a, b, innerIndex) => a)); // $ExpectType Observable<number>
-});
-
-it('should support a resultSelector that takes an inner and outer index', () => {
-  const o = of(1, 2, 3).pipe(exhaustMap(p => of(Boolean(p)), (a, b, innerIndex, outerIndex) => a)); // $ExpectType Observable<number>
-});
-
-it('should support an undefined resultSelector', () => {
-  const o = of(1, 2, 3).pipe(exhaustMap(p => of(Boolean(p)), undefined)); // $ExpectType Observable<boolean>
-});
-
 it('should report projections to union types', () => {
   const o = of(Math.random()).pipe(exhaustMap(n => n > 0.5 ? of('life') : of(42))); // $ExpectType Observable<string | number>
 });
