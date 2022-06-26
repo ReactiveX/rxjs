c6b75626932be944f91ffeaf92fec2167d8293fa
demensky <dmitry@demensky.com>
2022-06-26 18:09:39 +0300

    chore(map): update dtslint

--- spec-dtslint/operators/map-spec.ts
+++ spec-dtslint/operators/map-spec.ts
@@ -13,10 +13,6 @@ it('should support an index parameter', () => {
   const o = of('a', 'b', 'c').pipe(map((value, index) => index)); // $ExpectType Observable<number>
 });
 
-it('should support an extra parameter', () => {
-  const o = of(1, 2, 3).pipe(map(value => value, 'something')); // $ExpectType Observable<number>
-});
-
 it('should enforce types', () => {
   const o = of(1, 2, 3).pipe(map()); // $ExpectError
 });
@@ -24,17 +20,4 @@ it('should enforce types', () => {
 it('should enforce the projector types', () => {
   const o = of(1, 2, 3).pipe(map((value: string) => value)); // $ExpectError
   const p = of(1, 2, 3).pipe(map((value, index: string) => value)); // $ExpectError
-});
-
-it('should support this', () => {
-  const thisArg = { limit: 2 };
-  const o = of(1, 2, 3).pipe(map(function (val) {
-    const limit = this.limit; // $ExpectType number
-    return val < limit ? val : limit;
-  }, thisArg));
-});
-
-it('should deprecate thisArg usage', () => {
-  const a = of(1, 2, 3).pipe(map((value) => value)); // $ExpectNoDeprecation
-  const b = of(1, 2, 3).pipe(map((value) => value, {})); // $ExpectDeprecation
 });
