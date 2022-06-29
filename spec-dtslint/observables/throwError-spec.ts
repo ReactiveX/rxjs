678b6eda134c448a746a5485b28bc4ff7d2b748c
demensky <dmitry@demensky.com>
2022-06-30 01:28:11 +0300

    feat(throwError): removed deprecated throwError(error) call pattern
    BREAKING CHANGE: `throwError(error)` call pattern is no longer available. Use `throwError(() => error)`

--- spec-dtslint/observables/throwError-spec.ts
+++ spec-dtslint/observables/throwError-spec.ts
@@ -1,14 +1,9 @@
 import { throwError, animationFrameScheduler } from 'rxjs';
 
-it('should accept any type and return never observable', () => {
-  const a = throwError(1); // $ExpectType Observable<never>
-  const b = throwError('a'); // $ExpectType Observable<never>
-  const c = throwError({ a: 1 }); // $ExpectType Observable<never>
-  const d = throwError(() => ({ a: 2 })); // $ExpectType Observable<never>
-});
-
-it('should support an error value and a scheduler', () => {
-  const a = throwError(1, animationFrameScheduler); // $ExpectType Observable<never>
+it('should error for incorrect errorFactory', () => {
+  const a = throwError(1); // $ExpectError
+  const b = throwError('a'); // $ExpectError
+  const c = throwError({ a: 1 }); // $ExpectError
 });
 
 it('should accept any type and return never observable with support of factory', () => {
