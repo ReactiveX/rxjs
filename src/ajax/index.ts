59a4e83915de7b9db2c215c16bc5c615e2892af2
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2022-07-11 14:28:29 +0200

    chore: revert "export type" to "export" (#6943)

--- src/ajax/index.ts
+++ src/ajax/index.ts
@@ -1,4 +1,4 @@
 export { ajax } from '../internal/ajax/ajax';
 export { AjaxError, AjaxTimeoutError } from '../internal/ajax/errors';
 export { AjaxResponse } from '../internal/ajax/AjaxResponse';
-export type { AjaxRequest, AjaxConfig, AjaxDirection } from '../internal/ajax/types';
+export { AjaxRequest, AjaxConfig, AjaxDirection } from '../internal/ajax/types';
