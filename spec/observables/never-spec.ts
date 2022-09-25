45b734bcd22f04519fbec4e9ee491999697444d4
tmair <thomas.mair86@gmail.com>
2022-09-25 20:25:06 +0200

    chore(never): convert never specs to run mode (#6962)

--- spec/observables/never-spec.ts
+++ spec/observables/never-spec.ts
@@ -1,13 +1,23 @@
+/** @prettier */
 import { NEVER } from 'rxjs';
 import { expect } from 'chai';
-import { expectObservable } from '../helpers/marble-testing';
+import { TestScheduler } from 'rxjs/testing';
+import { observableMatcher } from '../helpers/observableMatcher';
 
 /** @test {NEVER} */
 describe('NEVER', () => {
+  let rxTestScheduler: TestScheduler;
+
+  beforeEach(() => {
+    rxTestScheduler = new TestScheduler(observableMatcher);
+  });
+
   it('should create a cold observable that never emits', () => {
-    const expected = '-';
-    const e1 = NEVER;
-    expectObservable(e1).toBe(expected);
+    rxTestScheduler.run(({ expectObservable }) => {
+      const expected = '-';
+      const e1 = NEVER;
+      expectObservable(e1).toBe(expected);
+    });
   });
 
   it('should return the same instance every time', () => {
