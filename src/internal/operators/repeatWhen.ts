8bc23d49c56ad2590137f3771e32ab0d99f55158
Jeremy Wells <jmw.jeremymwells@gmail.com>
2022-10-31 18:14:41 -0400

    chore(repeatWhen): further updated docs about deprecation

--- src/internal/operators/repeatWhen.ts
+++ src/internal/operators/repeatWhen.ts
@@ -38,7 +38,8 @@ import { createOperatorSubscriber } from './OperatorSubscriber';
  * which a user can `complete` or `error`, aborting the repetition.
  * @return A function that returns an `ObservableInput` that mirrors the source
  * Observable with the exception of a `complete`.
- * @deprecated Will be removed in v9 or v10. Use {@link repeat}'s `delay` option instead.
+ * @deprecated Will be removed in v9 or v10. Use {@link repeat}'s {@link RepeatConfig#delay delay} option instead.
+ * Instead of `repeatWhen(() => notify$)`, use: `repeat({ delay: () => notify$ })`.
  */
 export function repeatWhen<T>(notifier: (notifications: Observable<void>) => ObservableInput<any>): MonoTypeOperatorFunction<T> {
   return operate((source, subscriber) => {
