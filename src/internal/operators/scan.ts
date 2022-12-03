11342ad0b9c8d023e324e5b6d5eb400059dfe848
naticaceres <nataliacaceres91@gmail.com>
2022-12-03 13:28:40 -0500

    docs(operators): fixing a typo (#7086)
    There seems to be a typo in this doc,
    ... received so ~var~ -> ... received so _far_

--- src/internal/operators/scan.ts
+++ src/internal/operators/scan.ts
@@ -44,7 +44,7 @@ export function scan<V, A, S>(accumulator: (acc: A | S, value: V, index: number)
  *     // Get the sum of the numbers coming in.
  *     scan((total, n) => total + n),
  *     // Get the average by dividing the sum by the total number
- *     // received so var (which is 1 more than the zero-based index).
+ *     // received so far (which is 1 more than the zero-based index).
  *     map((sum, index) => sum / (index + 1))
  *   )
  *   .subscribe(console.log);
