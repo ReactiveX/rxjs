5df07eb45c11ffd3df28595000f627f7a3be6577
Ben Lesh <ben@benlesh.com>
2022-12-03 14:34:33 -0600

    chore: remove ts-api-guardian, as it is outmoded (#7128)

--- api_guard/dist/types/fetch/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function fromFetch<T>(input: string | Request, init: RequestInit & {
-    selector: (response: Response) => ObservableInput<T>;
-}): Observable<T>;
-export declare function fromFetch(input: string | Request, init?: RequestInit): Observable<Response>;
