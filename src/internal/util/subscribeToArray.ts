f002b8aca6c334e0bba552d0d67feaf052cebd68
demensky <dmitry@demensky.com>
2023-01-23 01:53:05 +0200

    perf(operator): simplify the implementation of the `endWith` operator

--- src/internal/util/subscribeToArray.ts
+++ src/internal/util/subscribeToArray.ts
@@ -3,10 +3,20 @@ import { Subscriber } from '../Subscriber';
 /**
  * Subscribes to an ArrayLike with a subscriber
  * @param array The array or array-like to subscribe to
+ * @param subscriber
  */
-export const subscribeToArray = <T>(array: ArrayLike<T>) => (subscriber: Subscriber<T>) => {
-  for (let i = 0, len = array.length; i < len && !subscriber.closed; i++) {
+export function subscribeToArray<T>(array: ArrayLike<T>, subscriber: Subscriber<T>) {
+  // Loop over the array and emit each value. Note two things here:
+  // 1. We're making sure that the subscriber is not closed on each loop.
+  //    This is so we don't continue looping over a very large array after
+  //    something like a `take`, `takeWhile`, or other synchronous unsubscription
+  //    has already unsubscribed.
+  // 2. In this form, reentrant code can alter that array we're looping over.
+  //    This is a known issue, but considered an edge case. The alternative would
+  //    be to copy the array before executing the loop, but this has
+  //    performance implications.
+  for (let i = 0, { length } = array; i < length && !subscriber.closed; i++) {
     subscriber.next(array[i]);
   }
   subscriber.complete();
-};
+}
