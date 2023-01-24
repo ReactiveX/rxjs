6d18c493aa13cb52f04240d066e350c64425c94b
Ben Lesh <ben@benlesh.com>
2023-01-24 08:56:40 -0600

    chore: Remove old comments.

--- src/internal/operators/startWith.ts
+++ src/internal/operators/startWith.ts
@@ -2,11 +2,6 @@ import { OperatorFunction, ValueFromArray } from '../types';
 import { operate } from '../util/lift';
 import { createOperatorSubscriber } from './OperatorSubscriber';
 
-// Devs are more likely to pass null or undefined than they are a scheduler
-// without accompanying values. To make things easier for (naughty) devs who
-// use the `strictNullChecks: false` TypeScript compiler option, these
-// overloads with explicit null and undefined values are included.
-
 export function startWith<T>(value: null): OperatorFunction<T, T | null>;
 export function startWith<T>(value: undefined): OperatorFunction<T, T | undefined>;
 export function startWith<T, A extends readonly unknown[] = T[]>(...values: A): OperatorFunction<T, T | ValueFromArray<A>>;
@@ -52,8 +47,6 @@ export function startWith<T, A extends readonly unknown[] = T[]>(...values: A): 
  */
 export function startWith<T, D>(...values: D[]): OperatorFunction<T, T | D> {
   return operate((source, subscriber) => {
-    // Because this will run synchronously, we don't need to do any fancy chaining here.
-    // Just run it and check to see if we're closed before we move on.
     subscribeToArray(
       values,
       createOperatorSubscriber(subscriber, undefined, () => source.subscribe(subscriber))
