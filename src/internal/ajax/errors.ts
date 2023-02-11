d9ad6f84fd87ad40fcc39b05f1db52b1be998a15
demensky <dmitry@demensky.com>
2023-02-12 00:14:48 +0200

    refactor: use native classes for `AjaxError` & `AjaxTimeoutError`
    BREAKING CHANGE: `AjaxError` cannot be used if `target` is below ES2015
    BREAKING CHANGE: `AjaxTimeoutError` cannot be used if `target` is below ES2015

--- src/internal/ajax/errors.ts
+++ src/internal/ajax/errors.ts
@@ -1,15 +1,14 @@
 import { AjaxRequest } from './types';
 import { getXHRResponse } from './getXHRResponse';
-import { createErrorClass } from '../util/createErrorClass';
 
 /**
- * A normalized AJAX error.
+ * Thrown when an error occurs during an AJAX request.
  *
  * @see {@link ajax}
- *
- * @class AjaxError
  */
-export interface AjaxError extends Error {
+export class AjaxError extends Error {
+  name = 'AjaxError';
+
   /**
    * The XHR instance associated with the error.
    */
@@ -35,72 +34,34 @@ export interface AjaxError extends Error {
    * The response data.
    */
   response: any;
-}
 
-export interface AjaxErrorCtor {
-  /**
-   * @deprecated Internal implementation detail. Do not construct error instances.
-   * Cannot be tagged as internal: https://github.com/ReactiveX/rxjs/issues/6269
-   */
-  new (message: string, xhr: XMLHttpRequest, request: AjaxRequest): AjaxError;
-}
+  constructor(message: string, xhr: XMLHttpRequest, request: AjaxRequest) {
+    super(message);
 
-/**
- * Thrown when an error occurs during an AJAX request.
- * This is only exported because it is useful for checking to see if an error
- * is an `instanceof AjaxError`. DO NOT create new instances of `AjaxError` with
- * the constructor.
- *
- * @class AjaxError
- * @see {@link ajax}
- */
-export const AjaxError: AjaxErrorCtor = createErrorClass(
-  (_super) =>
-    function AjaxErrorImpl(this: any, message: string, xhr: XMLHttpRequest, request: AjaxRequest) {
-      this.message = message;
-      this.name = 'AjaxError';
-      this.xhr = xhr;
-      this.request = request;
-      this.status = xhr.status;
-      this.responseType = xhr.responseType;
-      let response: any;
-      try {
-        // This can throw in IE, because we have to do a JSON.parse of
-        // the response in some cases to get the expected response property.
-        response = getXHRResponse(xhr);
-      } catch (err) {
-        response = xhr.responseText;
-      }
-      this.response = response;
+    this.xhr = xhr;
+    this.request = request;
+    this.status = xhr.status;
+    this.responseType = xhr.responseType;
+
+    try {
+      // This can throw in IE, because we have to do a JSON.parse of
+      // the response in some cases to get the expected response property.
+      this.response = getXHRResponse(xhr);
+    } catch (err) {
+      this.response = xhr.responseText;
     }
-);
-
-export interface AjaxTimeoutError extends AjaxError {}
-
-export interface AjaxTimeoutErrorCtor {
-  /**
-   * @deprecated Internal implementation detail. Do not construct error instances.
-   * Cannot be tagged as internal: https://github.com/ReactiveX/rxjs/issues/6269
-   */
-  new (xhr: XMLHttpRequest, request: AjaxRequest): AjaxTimeoutError;
+  }
 }
 
 /**
  * Thrown when an AJAX request times out. Not to be confused with {@link TimeoutError}.
  *
- * This is exported only because it is useful for checking to see if errors are an
- * `instanceof AjaxTimeoutError`. DO NOT use the constructor to create an instance of
- * this type.
- *
- * @class AjaxTimeoutError
  * @see {@link ajax}
  */
-export const AjaxTimeoutError: AjaxTimeoutErrorCtor = (() => {
-  function AjaxTimeoutErrorImpl(this: any, xhr: XMLHttpRequest, request: AjaxRequest) {
-    AjaxError.call(this, 'ajax timeout', xhr, request);
-    this.name = 'AjaxTimeoutError';
-    return this;
+export class AjaxTimeoutError extends AjaxError {
+  name = 'AjaxTimeoutError';
+
+  constructor(xhr: XMLHttpRequest, request: AjaxRequest) {
+    super('ajax timeout', xhr, request);
   }
-  AjaxTimeoutErrorImpl.prototype = Object.create(AjaxError.prototype);
-  return AjaxTimeoutErrorImpl;
-})() as any;
+}
