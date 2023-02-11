ad650f8ce9a84bf62948c19d9569416ef5ecd7b4
demensky <dmitry@demensky.com>
2023-02-11 23:35:44 +0200

    refactor: use native classes for `EmptyError`
    BREAKING CHANGE: `EmptyError` cannot be used if `target` is below ES2015

--- src/internal/util/EmptyError.ts
+++ src/internal/util/EmptyError.ts
@@ -1,15 +1,3 @@
-import { createErrorClass } from './createErrorClass';
-
-export interface EmptyError extends Error {}
-
-export interface EmptyErrorCtor {
-  /**
-   * @deprecated Internal implementation detail. Do not construct error instances.
-   * Cannot be tagged as internal: https://github.com/ReactiveX/rxjs/issues/6269
-   */
-  new (): EmptyError;
-}
-
 /**
  * An error thrown when an Observable or a sequence was queried but has no
  * elements.
@@ -19,11 +7,11 @@ export interface EmptyErrorCtor {
  * @see {@link single}
  * @see {@link firstValueFrom}
  * @see {@link lastValueFrom}
- *
- * @class EmptyError
  */
-export const EmptyError: EmptyErrorCtor = createErrorClass((_super) => function EmptyErrorImpl(this: any) {
-  _super(this);
-  this.name = 'EmptyError';
-  this.message = 'no elements in sequence';
-});
+export class EmptyError extends Error {
+  name = 'EmptyError';
+
+  constructor() {
+    super('no elements in sequence');
+  }
+}
