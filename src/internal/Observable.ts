2bc507d2753b781d434ef03673b0642394cee05e
Mladen Jakovljević <jakovljevic.mladen@gmail.com>
2023-03-15 20:01:53 +0100

    docs: remove @constructor tag definition

--- src/internal/Observable.ts
+++ src/internal/Observable.ts
@@ -10,7 +10,6 @@ import { pipeFromArray } from './util/pipe';
  */
 export class Observable<T> implements Subscribable<T> {
   /**
-   * @constructor
    * @param {Function} subscribe the function that is called when the Observable is
    * initially subscribed to. This function is given a Subscriber, to which new values
    * can be `next`ed, or an `error` method can be called to raise an error, or
