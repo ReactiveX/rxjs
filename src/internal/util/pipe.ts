4d4604a89dabd72606f213c6417f43c122a88b44
Mamaev Yuriy <mamaev@embria.com>
2023-03-17 20:58:26 +0300

    fix(pipe): Infer type for any number of functions

--- src/internal/util/pipe.ts
+++ src/internal/util/pipe.ts
@@ -1,79 +1,34 @@
 import { identity } from './identity';
 import { UnaryFunction } from '../types';
 
+type PipeParameters<T extends any[], U extends any[] = []> = T extends [infer F, infer S, ...infer R]
+  ? F extends UnaryFunction<any, infer FR>
+    ? S extends UnaryFunction<infer SA, infer SR>
+      ? [FR] extends [SA]
+        ? PipeParameters<[S, ...R], [...U, F]>
+        : PipeParameters<[UnaryFunction<FR, SR>, ...R], [...U, F]>
+      : [...U, ...T]
+    : [...U, ...T]
+  : T extends [any]
+  ? [...U, ...T]
+  : [];
+
+type PipeReturnType<T extends Array<UnaryFunction<any, any>>> = T extends [infer F, ...any[]] | [...any[], infer L]
+  ? F extends UnaryFunction<infer FA, any>
+    ? L extends UnaryFunction<any, infer LR>
+      ? UnaryFunction<FA, LR>
+      : never
+    : never
+  : never;
+
 export function pipe(): typeof identity;
-export function pipe<T, A>(fn1: UnaryFunction<T, A>): UnaryFunction<T, A>;
-export function pipe<T, A, B>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>): UnaryFunction<T, B>;
-export function pipe<T, A, B, C>(fn1: UnaryFunction<T, A>, fn2: UnaryFunction<A, B>, fn3: UnaryFunction<B, C>): UnaryFunction<T, C>;
-export function pipe<T, A, B, C, D>(
-  fn1: UnaryFunction<T, A>,
-  fn2: UnaryFunction<A, B>,
-  fn3: UnaryFunction<B, C>,
-  fn4: UnaryFunction<C, D>
-): UnaryFunction<T, D>;
-export function pipe<T, A, B, C, D, E>(
-  fn1: UnaryFunction<T, A>,
-  fn2: UnaryFunction<A, B>,
-  fn3: UnaryFunction<B, C>,
-  fn4: UnaryFunction<C, D>,
-  fn5: UnaryFunction<D, E>
-): UnaryFunction<T, E>;
-export function pipe<T, A, B, C, D, E, F>(
-  fn1: UnaryFunction<T, A>,
-  fn2: UnaryFunction<A, B>,
-  fn3: UnaryFunction<B, C>,
-  fn4: UnaryFunction<C, D>,
-  fn5: UnaryFunction<D, E>,
-  fn6: UnaryFunction<E, F>
-): UnaryFunction<T, F>;
-export function pipe<T, A, B, C, D, E, F, G>(
-  fn1: UnaryFunction<T, A>,
-  fn2: UnaryFunction<A, B>,
-  fn3: UnaryFunction<B, C>,
-  fn4: UnaryFunction<C, D>,
-  fn5: UnaryFunction<D, E>,
-  fn6: UnaryFunction<E, F>,
-  fn7: UnaryFunction<F, G>
-): UnaryFunction<T, G>;
-export function pipe<T, A, B, C, D, E, F, G, H>(
-  fn1: UnaryFunction<T, A>,
-  fn2: UnaryFunction<A, B>,
-  fn3: UnaryFunction<B, C>,
-  fn4: UnaryFunction<C, D>,
-  fn5: UnaryFunction<D, E>,
-  fn6: UnaryFunction<E, F>,
-  fn7: UnaryFunction<F, G>,
-  fn8: UnaryFunction<G, H>
-): UnaryFunction<T, H>;
-export function pipe<T, A, B, C, D, E, F, G, H, I>(
-  fn1: UnaryFunction<T, A>,
-  fn2: UnaryFunction<A, B>,
-  fn3: UnaryFunction<B, C>,
-  fn4: UnaryFunction<C, D>,
-  fn5: UnaryFunction<D, E>,
-  fn6: UnaryFunction<E, F>,
-  fn7: UnaryFunction<F, G>,
-  fn8: UnaryFunction<G, H>,
-  fn9: UnaryFunction<H, I>
-): UnaryFunction<T, I>;
-export function pipe<T, A, B, C, D, E, F, G, H, I>(
-  fn1: UnaryFunction<T, A>,
-  fn2: UnaryFunction<A, B>,
-  fn3: UnaryFunction<B, C>,
-  fn4: UnaryFunction<C, D>,
-  fn5: UnaryFunction<D, E>,
-  fn6: UnaryFunction<E, F>,
-  fn7: UnaryFunction<F, G>,
-  fn8: UnaryFunction<G, H>,
-  fn9: UnaryFunction<H, I>,
-  ...fns: UnaryFunction<any, any>[]
-): UnaryFunction<T, unknown>;
+export function pipe<T extends Array<UnaryFunction<any, any>>>(...fns: PipeParameters<T>): PipeReturnType<T>;
 
 /**
  * pipe() can be called on one or more functions, each of which can take one argument ("UnaryFunction")
  * and uses it to return a value.
  * It returns a function that takes one argument, passes it to the first UnaryFunction, and then
- * passes the result to the next one, passes that result to the next one, and so on.  
+ * passes the result to the next one, passes that result to the next one, and so on.
  */
 export function pipe(...fns: Array<UnaryFunction<any, any>>): UnaryFunction<any, any> {
   return pipeFromArray(fns);
