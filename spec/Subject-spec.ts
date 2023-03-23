4529ac24ed710c42994b6bc96f1e2e1c23507907
Nicolas DUBIEN <github@dubien.org>
2023-03-23 19:22:11 +0000

    fix(subject): Faster subscriptions management in Subject
    The current implementation of the Subject was backing itself on an array of observers. When one observer unsubscribed itself, it was performing a linear scan on the array of observers and dropping the item from the array of observers. The algorithm complexity of this operation was O(n) with n being the number of observers connected to the Subject.
    
    It caused code like:
    
    ```js
    const allSubscriptions = [];
    const source = new Subject();
    for (let index = 0 ; index !== 1_000_000 ; ++index) {
      allSubscriptions.push(source.subscribe()); // rather quick
    }
    for (const subscription of allSubscriptions) {
      subscription.unsubscribe(); // taking very long
    }
    ```
    
    The proposed approach consists into changing our backing collection (an array) into a Map. But we lose somehow the set capability on subject.observers (might possibly be patched in some way).
    
    Following that change the command `cross-env TS_NODE_PROJECT=tsconfig.mocha.json mocha --config spec/support/.mocharc.js "spec/**/Subje*-spec.ts"` passed to 452ms from 10s.

--- spec/Subject-spec.ts
+++ spec/Subject-spec.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { Subject, ObjectUnsubscribedError, Observable, AsyncSubject, Observer, of, config, throwError, concat } from 'rxjs';
+import { Subject, ObjectUnsubscribedError, Observable, AsyncSubject, Observer, of, config, throwError, concat, Subscription } from 'rxjs';
 import { AnonymousSubject } from 'rxjs/internal/Subject';
 import { catchError, delay, map, mergeMap } from 'rxjs/operators';
 import { TestScheduler } from 'rxjs/testing';
@@ -681,6 +681,30 @@ describe('Subject', () => {
         expect(true).to.be.false;
       }
       expect(true).to.be.true;
+    });
+  });
+
+  describe('many subscribers', () => {
+    it('should be able to subscribe and unsubscribe huge amounts of subscribers', () => {
+      let numResultsReceived = 0;
+      const allSubscriptions: Subscription[] = [];
+      const source = new Subject<number>();
+      const numSubscribers = 100000;
+      for (let index = 0 ; index !== numSubscribers ; ++index) {
+        allSubscriptions.push(source.subscribe(() => {
+          ++numResultsReceived;
+        }));
+      }
+      expect(numResultsReceived).to.eq(0);
+      expect(source.observed).to.be.true;
+      source.next(42);
+      expect(numResultsReceived).to.eq(numSubscribers);
+      expect(source.observed).to.be.true;
+      for (const subscription of allSubscriptions) {
+        subscription.unsubscribe();
+      }
+      expect(numResultsReceived).to.eq(numSubscribers);
+      expect(source.observed).to.be.false;
     });
   });
 });
