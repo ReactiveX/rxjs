fce318a6455a179a197d9d126d9fc09b535788ef
Ben Lesh <ben@benlesh.com>
2023-05-11 16:21:28 -0500

    feat: No longer publishing UMD or ES5 builds

--- tools/generate-alias.js
+++ tools/generate-alias.js
@@ -6,27 +6,25 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const aliasRoot = [
-  'ajax', 'fetch', 'operators', 'testing', 'webSocket'
-]
+const aliasRoot = ['ajax', 'fetch', 'operators', 'testing', 'webSocket'];
 
-aliasRoot.map((alias) => path.resolve(__dirname, `../${alias}`)).forEach((alias) => {
-  if (fs.existsSync(alias)) {
-    fs.removeSync(alias);
-  }
-  fs.ensureDirSync(alias);
-});
+aliasRoot
+  .map((alias) => path.resolve(__dirname, `../${alias}`))
+  .forEach((alias) => {
+    if (fs.existsSync(alias)) {
+      fs.removeSync(alias);
+    }
+    fs.ensureDirSync(alias);
+  });
 
 aliasRoot.forEach((alias) => {
   const pkgManifest = {
-    "name": `rxjs/${alias}`,
-    "types": `../dist/types/${alias}/index.d.ts`,
-    "main": `../dist/cjs/${alias}/index.js`,
-    "module": `../dist/esm5/${alias}/index.js`,
-    "es2015": `../dist/esm/${alias}/index.js`,
-    "sideEffects": false
+    name: `rxjs/${alias}`,
+    types: `../dist/types/${alias}/index.d.ts`,
+    main: `../dist/cjs/${alias}/index.js`,
+    module: `../dist/esm/${alias}/index.js`,
+    sideEffects: false,
   };
 
   fs.writeJSON(path.resolve(__dirname, `../${alias}/package.json`), pkgManifest, { spaces: 2 });
 });
-
