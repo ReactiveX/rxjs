60aeaffbd9c10b527e31eb67163c0aab6cdea01a
Ben Lesh <ben@benlesh.com>
2023-05-11 16:32:41 -0500

    chore: cleanup rollup-related files and deps

--- tools/make-umd-bundle.js
+++
@@ -1,9 +0,0 @@
-const rollupBundle = require('./rollup-bundle');
-const fs = require('fs-extra');
-
-fs.ensureDirSync('dist/global');
-
-rollupBundle({
-  input: 'dist/esm5_for_rollup/internal/umd.js',
-  dest: 'dist/global/rxjs.umd.js',
-});
