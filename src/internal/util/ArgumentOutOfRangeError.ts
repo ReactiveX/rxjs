687e412cbf6327dc269660818cf00e53cf4ef968
Ben Lesh <ben@benlesh.com>
2023-05-11 17:49:52 -0500

    refactor(ArgumentOutOfRangeError): remove downleveling workaround

--- src/internal/util/ArgumentOutOfRangeError.ts
+++ src/internal/util/ArgumentOutOfRangeError.ts
@@ -1,15 +1,3 @@
-import { createErrorClass } from './createErrorClass';
-
-export interface ArgumentOutOfRangeError extends Error {}
-
-export interface ArgumentOutOfRangeErrorCtor {
-  /**
-   * @deprecated Internal implementation detail. Do not construct error instances.
-   * Cannot be tagged as internal: https://github.com/ReactiveX/rxjs/issues/6269
-   */
-  new (): ArgumentOutOfRangeError;
-}
-
 /**
  * An error thrown when an element was queried at a certain index of an
  * Observable, but no such index or position exists in that sequence.
@@ -18,11 +6,13 @@ export interface ArgumentOutOfRangeErrorCtor {
  * @see {@link take}
  * @see {@link takeLast}
  */
-export const ArgumentOutOfRangeError: ArgumentOutOfRangeErrorCtor = createErrorClass(
-  (_super) =>
-    function ArgumentOutOfRangeErrorImpl(this: any) {
-      _super(this);
-      this.name = 'ArgumentOutOfRangeError';
-      this.message = 'argument out of range';
-    }
-);
+export class ArgumentOutOfRangeError extends Error {
+  /**
+   * @deprecated Internal implementation detail. Do not construct error instances.
+   * Cannot be tagged as internal: https://github.com/ReactiveX/rxjs/issues/6269
+   */
+  constructor() {
+    super('argument out of range');
+    this.name = 'ArgumentOutOfRangeError';
+  }
+}
