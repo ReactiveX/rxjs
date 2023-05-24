cf7ace942aa80843b65d3f8f61f05f070e363d0b
Tristan LAROUBINE <laroubine.tristan@yahoo.fr>
2023-05-24 21:21:09 +0200

    test(ignoreElements): expand dtslint testing a bit
    ---------
    
    Co-authored-by: Ben Lesh <ben@benlesh.com>

--- spec-dtslint/operators/ignoreElements-spec.ts
+++ spec-dtslint/operators/ignoreElements-spec.ts
@@ -1,5 +1,5 @@
 import { of } from 'rxjs';
-import { ignoreElements } from 'rxjs/operators';
+import { ignoreElements, tap } from 'rxjs/operators';
 
 it('should infer correctly', () => {
   const o = of(1, 2, 3).pipe(ignoreElements()); // $ExpectType Observable<never>
@@ -7,4 +7,28 @@ it('should infer correctly', () => {
 
 it('should enforce types', () => {
   const o = of(1, 2, 3).pipe(ignoreElements('nope')); // $ExpectError
+});
+
+it('should not break the inference of type', () => {
+  const o$ = of(1, 2, 3).pipe(
+    tap((o) => {
+      const t = o; // $ExpectType number
+    }),
+    ignoreElements(),
+  );
+});
+
+it('should not break the inference of types when piped in a larger chain', () => {
+  const o$ = of('a string').pipe(
+    tap((o) => {
+      const t = o; // $ExpectType string
+    }),
+    tap((o) => {
+      const t = o; // $ExpectType string
+    }),
+    ignoreElements(),
+    tap((o) => {
+      const t = o; // $ExpectType never
+    }),
+  );
 });
