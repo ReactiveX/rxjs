85bc3e280411a116f4a323c38157b9836e94b318
Thomas Mair <thomas.mair86@gmail.com>
2023-10-26 15:30:08 +0200

    docs: fix the usage of docs-lint

--- apps/rxjs.dev/tools/transforms/.eslintrc.js
+++ apps/rxjs.dev/tools/transforms/.eslintrc.js
@@ -1,21 +1,20 @@
 module.exports = {
-  'env': {
-    'es6': true,
-    'jasmine': true,
-    'node': true
+  root: true,
+  env: {
+    es6: true,
+    jasmine: true,
+    node: true,
   },
-  'extends': [
-    'eslint:recommended',
-    'plugin:jasmine/recommended'
-  ],
-  'plugins': [
-    'jasmine'
-  ],
-  'rules': {
-    'indent': ['error', 2],
+  extends: ['eslint:recommended', 'plugin:jasmine/recommended'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  plugins: ['jasmine'],
+  rules: {
     'linebreak-style': ['error', 'unix'],
-    'quotes': ['error', 'single'],
-    'semi': ['error', 'always'],
-    'jasmine/new-line-before-expect': 0
-  }
+    'no-prototype-builtins': ['off'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'jasmine/new-line-before-expect': ['off'],
+  },
 };
