a8a8ca33ae2a403520996b8ef949c370e9fe192a
“JamesHenry” <james@henry.sc>
2024-01-18 16:19:17 +0400

    chore: allow integration tests to work with local @rxjs/observable

--- packages/rxjs/integration/import/runner.js
+++ packages/rxjs/integration/import/runner.js
@@ -1,10 +1,13 @@
 const path = require('path');
+const fs = require('fs');
 const { spawn } = require('child_process');
 const projectRoot = process.cwd();
 const rxjsRoot = path.join(__dirname, '../../');
+const rxjsObservableRoot = path.join(__dirname, '../../../observable');
 const fixturesDirectory = path.join(__dirname, 'fixtures');
 const rxjsVersion = require(path.join(rxjsRoot, 'package.json')).version;
 const tgzPath = path.join(rxjsRoot, `rxjs-${rxjsVersion}.tgz`);
+const rxjsObservableTgzPath = path.join(rxjsObservableRoot, `rxjs-observable-${rxjsVersion}.tgz`);
 
 // These are the fixtures to run the import test against
 // they map to directories in the fixtures directory
@@ -57,6 +60,14 @@ function execAsync(cmd, cwd = '.') {
 
 async function main() {
   try {
+    console.log('Building and packaging @rxjs/observable...');
+    try {
+      await execAsync('yarn build && npm pack', rxjsObservableRoot);
+    } catch (err) {
+      console.error('❌ Failed to build and package @rxjs/observable!');
+      console.error(err);
+      throw err;
+    }
     console.log('Building and packaging RxJS...');
     try {
       await execAsync('yarn build && npm pack', rxjsRoot);
@@ -76,6 +87,10 @@ async function main() {
       try {
         console.log('\n');
         console.log(`Running ${fixtureName}...`);
+
+        console.log('Setting custom dependency resolution for local @rxjs/observable...');
+        addYarnResolution(fixturePath, '@rxjs/observable', `file:${path.relative(fixturePath, rxjsObservableTgzPath)}`);
+
         await execAsync(`yarn install && yarn add ${tgzPath}`, fixturePath);
         await execAsync('yarn test', fixturePath);
         console.log(`✅ ${fixtureName} import test passed!`);
@@ -89,6 +104,7 @@ async function main() {
         try {
           await execAsync('yarn remove rxjs', fixturePath);
           await execAsync('rm -rf ./node_modules ./package-lock.json ./yarn.lock', fixturePath);
+          removeYarnResolution(fixturePath, '@rxjs/observable');
         } catch (err) {
           console.warn('fixtured not cleaned up', err);
         }
@@ -101,7 +117,26 @@ async function main() {
     }
   } finally {
     await execAsync(`rm ${tgzPath}`, projectRoot);
+    await execAsync(`rm ${rxjsObservableTgzPath}`, rxjsObservableRoot);
   }
 }
 
 main();
+
+function addYarnResolution(fixturePath, name, version) {
+  const fixturePackageJson = fs.readFileSync(path.join(fixturePath, 'package.json'));
+  const fixturePackageJsonObj = JSON.parse(fixturePackageJson);
+  fixturePackageJsonObj.resolutions = fixturePackageJsonObj.resolutions || {};
+  fixturePackageJsonObj.resolutions[name] = version;
+  fs.writeFileSync(path.join(fixturePath, 'package.json'), JSON.stringify(fixturePackageJsonObj, null, 2));
+}
+
+function removeYarnResolution(fixturePath, name) {
+  const fixturePackageJson = fs.readFileSync(path.join(fixturePath, 'package.json'));
+  const fixturePackageJsonObj = JSON.parse(fixturePackageJson);
+  delete fixturePackageJsonObj.resolutions[name];
+  if (!Object.keys(fixturePackageJsonObj.resolutions).length) {
+    delete fixturePackageJsonObj.resolutions;
+  }
+  fs.writeFileSync(path.join(fixturePath, 'package.json'), JSON.stringify(fixturePackageJsonObj, null, 2) + '\n');
+}
