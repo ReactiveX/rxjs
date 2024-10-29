8f2c44795c34e6af28b03e75bbcddbd1c88df644
JamesHenry <james@henry.sc>
2024-10-29 16:50:32 +0400

    chore: try and troubleshoot mocha failure

--- packages/rxjs/spec/support/.mocharc.js
+++ packages/rxjs/spec/support/.mocharc.js
@@ -1,6 +1,8 @@
 module.exports = {
   require: ['ts-node/register', 'spec/helpers/setup.ts'],
-  reporter: 'dot',
+  reporter: 'spec',
+  'full-trace': true,
+  'stack-trace-limit': 25,
   extensions: ['ts', 'js'],
   timeout: 5000,
   recursive: true,
