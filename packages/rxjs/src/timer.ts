6b75558c004eadef4ac89ee63c939a6d05f91e77
Ben Lesh <ben@benlesh.com>
2026-08-01 14:02:44 -0500

    refactor(rxjs): construct timer directly

--- packages/rxjs/src/timer.ts
+++ packages/rxjs/src/timer.ts
@@ -1,4 +1,4 @@
-import { createDerivedObservable } from './util/observable-helpers.js';
+import { create } from './create.js';
 
 export const timer: unique symbol = Symbol('timer');
 
@@ -12,26 +12,23 @@ declare global {
 }
 
 function timerFactory(this: ObservableCtor, delay: number, interval?: number): Observable<number> {
-  return createDerivedObservable({
-    receiver: this,
-    init: (subscriber) => {
-      let n = 0;
+  return this[create]((subscriber) => {
+    let n = 0;
 
-      let id = globalThis.setInterval(() => {
-        subscriber.next(n++);
+    let id = globalThis.setInterval(() => {
+      subscriber.next(n++);
 
-        if (interval == null || interval < 0) {
-          subscriber.complete();
-        } else if (interval !== delay) {
-          globalThis.clearInterval(id);
-          id = globalThis.setInterval(() => {
-            subscriber.next(n++);
-          });
-        }
-      }, delay);
+      if (interval == null || interval < 0) {
+        subscriber.complete();
+      } else if (interval !== delay) {
+        globalThis.clearInterval(id);
+        id = globalThis.setInterval(() => {
+          subscriber.next(n++);
+        });
+      }
+    }, delay);
 
-      subscriber.addTeardown(() => globalThis.clearInterval(id));
-    },
+    subscriber.addTeardown(() => globalThis.clearInterval(id));
   });
 }
 
