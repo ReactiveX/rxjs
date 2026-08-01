cfceb9643889733a143be43f12ff5bfe1f3dd1fa
Ben Lesh <ben@benlesh.com>
2026-08-01 13:06:53 -0500

    refactor(rxjs): centralize switchMap subscriptions

--- packages/rxjs/src/switch-map.ts
+++ packages/rxjs/src/switch-map.ts
@@ -1,4 +1,4 @@
-import { convertObservableValue, createDerivedObservable, runWithErrorForwarding, subscribeToSource } from './util/observable-helpers.js';
+import { convertObservableValue, createDerivedObservable, subscribeToSource } from './util/observable-helpers.js';
 
 export const switchMap: unique symbol = Symbol('switchMap');
 
@@ -22,9 +22,7 @@ function switchMapOperator<T, R>(
       let index = 0;
       const active: AbortController[] = [];
 
-      subscribeToSource({
-        source: this,
-        subscriber,
+      subscribeToSource(this, subscriber, {
         next: (value) => {
           if (active.length >= concurrent) {
             active.shift()!.abort();
@@ -32,31 +30,24 @@ function switchMapOperator<T, R>(
 
           const innerController = new AbortController();
           active.push(innerController);
-          const signal = AbortSignal.any([subscriber.signal, innerController.signal]);
 
-          const source = runWithErrorForwarding({
+          subscribeToSource(
+            convertObservableValue({ value: mapper(value, index++) }),
             subscriber,
-            run: () => convertObservableValue({ value: mapper(value, index++) }),
-          });
-          if (!source.ok) {
-            return;
-          }
-
-          subscribeToSource({
-            source: source.value,
-            subscriber,
-            signal,
-            next: (innerValue) => subscriber.next(innerValue),
-            complete: () => {
-              const activeIndex = active.indexOf(innerController);
-              if (activeIndex !== -1) {
-                active.splice(activeIndex, 1);
-              }
-              if (outerComplete && active.length === 0) {
-                subscriber.complete();
-              }
+            {
+              next: (innerValue) => subscriber.next(innerValue),
+              complete: () => {
+                const activeIndex = active.indexOf(innerController);
+                if (activeIndex !== -1) {
+                  active.splice(activeIndex, 1);
+                }
+                if (outerComplete && active.length === 0) {
+                  subscriber.complete();
+                }
+              },
             },
-          });
+            innerController.signal
+          );
         },
         complete: () => {
           outerComplete = true;
